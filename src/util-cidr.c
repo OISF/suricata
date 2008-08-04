d2b089f359208feefb5e772110b52ba589b6806a
Victor Julien <victor@inliniac.net>
2008-08-04 22:30:02 +0200

    Fix including the header for htonl breaking the code.

--- src/util-cidr.c
+++ src/util-cidr.c
@@ -8,7 +8,9 @@ static u_int32_t cidrs[33];
 void CIDRInit(void) {
     int i = 0;
 
-    for (i = 0; i < 33; i++) {
+    /* skip 0 as it will result in 0xffffffff */
+    cidrs[0] = 0;
+    for (i = 1; i < 33; i++) {
         cidrs[i] = htonl(0xFFFFFFFF << (32 - i));
         //printf("CIDRInit: cidrs[%02d] = 0x%08X\n", i, cidrs[i]);
     }
