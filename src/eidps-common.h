9a9b506c340fda363c0075a4e17e7ab893371c41
Brian Rectanus <brectanu@gmail.com>
2009-08-15 21:40:39 -0700

    64 bit cleanup

---
+++ src/eidps-common.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2008 Victor Julien <victor@inliniac.net> */
+
+/**
+ * Common includes, etc.
+ */
+
+#ifndef __EIDPS_COMMON_H__
+#define __EIDPS_COMMON_H__
+
+#define TRUE   1
+#define FALSE  0
+
+#include <stdint.h>
+#include <inttypes.h>
+
+#endif /* __EIDPS_COMMON_H__ */
+
