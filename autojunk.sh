2651f807f175c6ad815567420f8387957d47ce0d
Victor Julien <victor@inliniac.net>
2010-02-17 14:48:11 +0100

    Update autojunk.sh + build update.

--- autojunk.sh
+++ autojunk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # the list of commands that need to run before we do a compile
-aclocal -I m4
+aclocal --force -I m4
 libtoolize --force --automake --copy
 autoheader
 automake --add-missing --copy
