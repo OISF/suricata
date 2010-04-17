9e6b897ebcbc09fb85177fede8797bbae58d7958
Victor Julien <victor@inliniac.net>
2010-04-17 23:00:09 +0200

    Improve detect-threshold parser error handling.

--- src/detect-threshold.c
+++ src/detect-threshold.c
@@ -158,16 +158,18 @@ static DetectThresholdData *DetectThresholdParse (char *rawstr)
             second_pos = i+1;
     }
 
-    if (args[count_pos] != NULL &&
-            (ByteExtractStringUint32(&de->count, 10, strlen(args[count_pos]),
-            args[count_pos]) <= 0))
+    if (args[count_pos] == NULL) {
+        goto error;
+    }
+
+    if (ByteExtractStringUint32(&de->count, 10, strlen(args[count_pos]),
+                args[count_pos]) <= 0)
     {
         goto error;
     }
 
-    if (args[second_pos] != NULL &&
-            ByteExtractStringUint32(&de->seconds, 10, strlen(args[second_pos]),
-            args[second_pos]) <= 0)
+    if (ByteExtractStringUint32(&de->seconds, 10, strlen(args[second_pos]),
+                args[second_pos]) <= 0)
     {
         goto error;
     }
