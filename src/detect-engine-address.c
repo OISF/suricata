afa8a2b8ba08940b849c44fb48e0aa6fa45f282f
William Metcalf <william.metcalf@gmail.com>
2010-05-20 13:04:23 -0500

    Null deref fixe for detect-engine-address.c

--- src/detect-engine-address.c
+++ src/detect-engine-address.c
@@ -637,6 +637,9 @@ static int DetectAddressParseString(DetectAddress *dd, char *str)
 
     /* we dup so we can put a nul-termination in it later */
     ip = ipdup;
+    if (ip == NULL) {
+        goto error;
+    }
 
     /* handle the negation case */
     if (ip[0] == '!') {
