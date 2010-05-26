c73e9318b002f91bf5656ed45a6a62dd32ab3cea
Victor Julien <victor@inliniac.net>
2010-05-26 13:35:52 +0200

    Bump version 0.9.1.

--- src/suricata.h
+++ src/suricata.h
@@ -29,7 +29,7 @@
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
-#define PROG_VER "0.9.0"
+#define PROG_VER "0.9.1"
 
 /* number of packets in processing right now
  * This is the diff between recv'd and verdicted
