7d644b2aca488b4ae1c804fe4ddaa755f7115e03
Victor Julien <victor@inliniac.net>
2010-12-29 16:50:48 +0100

    Remove BUG_ON check from uri content detection code.

--- src/detect-engine-uri.c
+++ src/detect-engine-uri.c
@@ -217,7 +217,13 @@ static int DoInspectPacketUri(DetectEngineCtx *de_ctx,
                     goto match;
                 }
 
-                BUG_ON(sm->next == NULL);
+                /* bail out if we have no next match. Technically this is an
+                 * error, as the current cd has the DETECT_CONTENT_RELATIVE_NEXT
+                 * flag set. */
+                if (sm->next == NULL) {
+                    SCReturnInt(0);
+                }
+
                 SCLogDebug("uricontent %"PRIu32, ud->id);
 
                 /* see if the next payload keywords match. If not, we will
