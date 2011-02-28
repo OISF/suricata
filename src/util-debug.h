0d6d0ae37113a42384f87725b6afa74d669cc77b
Victor Julien <victor@inliniac.net>
2011-02-28 15:24:16 +0100

    Increase logline max length.

--- src/util-debug.h
+++ src/util-debug.h
@@ -79,7 +79,7 @@ typedef enum {
 #define SC_LOG_DEF_LOG_FORMAT "[%i] %t - (%f:%l) <%d> (%n) -- "
 
 /* The maximum length of the log message */
-#define SC_LOG_MAX_LOG_MSG_LEN 1024
+#define SC_LOG_MAX_LOG_MSG_LEN 2048
 
 /* The maximum length of the log format */
 #define SC_LOG_MAX_LOG_FORMAT_LEN 128
