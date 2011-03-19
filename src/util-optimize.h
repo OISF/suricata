bc5738d57df83e28de6364f6ec87ea6e842e14d5
Victor Julien <victor@inliniac.net>
2011-03-19 15:48:13 +0100

    Add compiler and hardware barrier macro's.

--- src/util-optimize.h
+++ src/util-optimize.h
@@ -27,5 +27,17 @@
 #define likely(expr) __builtin_expect(!!(expr), 1)
 #define unlikely(expr) __builtin_expect(!!(expr), 0)
 
+/** from http://en.wikipedia.org/wiki/Memory_ordering
+ *
+ *  C Compiler memory barrier
+ */
+#define cc_barrier() __asm__ __volatile__("": : :"memory")
+
+/** from http://gcc.gnu.org/onlinedocs/gcc-4.1.2/gcc/Atomic-Builtins.html
+ *
+ * Hardware memory barrier
+ */
+#define hw_barrier() __sync_synchronize()
+
 #endif /* __UTIL_OPTIMIZE_H__ */
 
