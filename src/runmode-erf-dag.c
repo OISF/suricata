90ebb6f01f1e28ec6d5fb44bec1a9c9291b4dd5c
Victor Julien <victor@inliniac.net>
2011-06-06 21:11:31 +0200

    Fix broken fix.

--- src/runmode-erf-dag.c
+++ src/runmode-erf-dag.c
@@ -207,7 +207,7 @@ int RunModeErfDagAuto(DetectEngineCtx *de_ctx)
                                     "alert-queue1", "simple",
                                     "packetpool", "packetpool",
                                     "varslot");
-    if (tv_outputs = NULL) {
+    if (tv_outputs == NULL) {
         printf("ERROR: TmThreadCreatePacketHandler for Outputs failed\n");
         exit(EXIT_FAILURE);
     }
