e40cd916f977c93f580a977a3403cd5e26ccef8f
Victor Julien <victor@inliniac.net>
2011-07-25 15:16:27 +0200

    Bump to 1.0.5 and update ChangeLog.

--- src/suricata.h
+++ src/suricata.h
@@ -30,7 +30,7 @@
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
-#define PROG_VER "1.0.4"
+#define PROG_VER "1.0.5"
 
 /* runtime engine control flags */
 #define SURICATA_STOP    0x01   /**< gracefully stop the engine: process all
