7beb5cdf585514c30034f03579f90d179d8fc931
Victor Julien <victor@inliniac.net>
2011-10-06 21:12:55 +0200

    Add util-optimize.h to suricata-common.h so all code can use it.

--- src/suricata-common.h
+++ src/suricata-common.h
@@ -189,6 +189,7 @@ typedef enum PacketProfileDetectId_ {
 #include "util-error.h"
 #include "util-mem.h"
 #include "detect-engine-alert.h"
+#include "util-optimize.h"
 
 size_t strlcat(char *, const char *src, size_t siz);
 size_t strlcpy(char *dst, const char *src, size_t siz);
