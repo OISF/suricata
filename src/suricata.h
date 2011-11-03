9f0e3f7c8543ae2217f18d508a0df5601e99fb18
Victor Julien <victor@inliniac.net>
2011-11-03 14:42:10 +0100

    Bump version to 1.1rc1.

--- src/suricata.h
+++ src/suricata.h
@@ -71,7 +71,7 @@
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
-#define PROG_VER "1.1beta3"
+#define PROG_VER "1.1rc1"
 
 /* runtime engine control flags */
 #define SURICATA_STOP    0x01   /**< gracefully stop the engine: process all
