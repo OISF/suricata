a6a0d4eae6f876eefade2a31c3a435a7a66ebb64
Eric Leblond <eric@regit.org>
2011-11-07 14:48:26 +0100

    pfring: use deinit function.
    This fixes #368.

--- src/source-pfring.c
+++ src/source-pfring.c
@@ -138,7 +138,7 @@ void TmModuleReceivePfringRegister (void) {
     tmm_modules[TMM_RECEIVEPFRING].Func = NULL;
     tmm_modules[TMM_RECEIVEPFRING].PktAcqLoop = ReceivePfringLoop;
     tmm_modules[TMM_RECEIVEPFRING].ThreadExitPrintStats = ReceivePfringThreadExitStats;
-    tmm_modules[TMM_RECEIVEPFRING].ThreadDeinit = NULL;
+    tmm_modules[TMM_RECEIVEPFRING].ThreadDeinit = ReceivePfringThreadDeinit;
     tmm_modules[TMM_RECEIVEPFRING].RegisterTests = NULL;
 }
 
