d40fb5b933c16c6fec5bac933c917d3efe61361e
Anoop Saldanha <poonaatsoc@gmail.com>
2011-12-05 21:56:59 +0530

    Remove unnecessary flow NULL check

--- src/detect.c
+++ src/detect.c
@@ -1299,12 +1299,10 @@ int SigMatchSignatures(ThreadVars *th_v, DetectEngineCtx *de_ctx, DetectEngineTh
         SCMutexLock(&p->flow->m);
         {
             /* set the iponly stuff */
-            if (p->flow != NULL) {
-                if (p->flow->flags & FLOW_TOCLIENT_IPONLY_SET)
-                    p->flowflags |= FLOW_PKT_TOCLIENT_IPONLY_SET;
-                if (p->flow->flags & FLOW_TOSERVER_IPONLY_SET)
-                    p->flowflags |= FLOW_PKT_TOSERVER_IPONLY_SET;
-            }
+            if (p->flow->flags & FLOW_TOCLIENT_IPONLY_SET)
+                p->flowflags |= FLOW_PKT_TOCLIENT_IPONLY_SET;
+            if (p->flow->flags & FLOW_TOSERVER_IPONLY_SET)
+                p->flowflags |= FLOW_PKT_TOSERVER_IPONLY_SET;
 
             /* Get the stored sgh from the flow (if any). Make sure we're not using
              * the sgh for icmp error packets part of the same stream. */
