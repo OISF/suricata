18d458870f84649e51176ecb252af882ace416dd
Victor Julien <victor@inliniac.net>
2012-01-21 09:45:58 +0100

    1.3 branch has opened

--- src/suricata.h
+++ src/suricata.h
@@ -71,7 +71,7 @@
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
-#define PROG_VER "1.2dev"
+#define PROG_VER "1.3dev"
 
 /* runtime engine control flags */
 #define SURICATA_STOP    0x01   /**< gracefully stop the engine: process all
