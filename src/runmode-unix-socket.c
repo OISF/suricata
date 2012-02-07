523ddcaacb2d547d9759c74c69e4a30a5fb63e6b
Eric Leblond <eric@regit.org>
2012-02-07 23:48:56 +0100

    unix-manager: add unix command socket and associated script
    This patch introduces a unix command socket. JSON formatted messages
    can be exchanged between suricata and a program connecting to a
    dedicated socket.
    The protocol is the following:
     * Client connects to the socket
     * It sends a version message: { "version": "$VERSION_ID" }
     * Server answers with { "return": "OK|NOK" }
    If server returns OK, the client is now allowed to send command.
    
    The format of command is the following:
     {
       "command": "pcap-file",
       "arguments": { "filename": "smtp-clean.pcap", "output-dir": "/tmp/out" }
     }
    The server will try to execute the "command" specified with the
    (optional) provided "arguments".
    The answer by server is the following:
     {
       "return": "OK|NOK",
       "message": JSON_OBJECT or information string
     }
    
    A simple script is provided and is available under scripts/suricatasc. It
    is not intended to be enterprise-grade tool but it is more a proof of
    concept/example code.  The first command line argument of suricatasc is
    used to specify the socket to connect to.
    
    Configuration of the feature is made in the YAML under the 'unix-command'
    section:
      unix-command:
        enabled: yes
        filename: custom.socket
    The path specified in 'filename' is not absolute and is relative to the
    state directory.
    
    A new running mode called 'unix-socket' is also added.
    When starting in this mode, only a unix socket manager
    is started. When it receives a 'pcap-file' command, the manager
    start a 'pcap-file' running mode which does not really leave at
    the end of file but simply exit. The manager is then able to start
    a new running mode with a new file.
    
    To start this mode, Suricata must be started with the --unix-socket
     option which has an optional argument which fix the file name of the
    socket. The path is not absolute and is relative to the state directory.
    
    THe 'pcap-file' command adds a file to the list of files to treat.
    For each pcap file, a pcap file running mode is started and the output
    directory is changed to what specified in the command. The running
    mode specified in the 'runmode' YAML setting is used to select which
    running mode must be use for the pcap file treatment.
    
    This requires modification in suricata.c file where initialisation code
    is now conditional to the fact 'unix-socket' mode is not used.
    
    Two other commands exists to get info on the remaining tasks:
     * pcap-file-number: return the number of files in the waiting queue
     * pcap-file-list: return the list of waiting files
    'pcap-file-list' returns a structured object as message. The
    structure is the following:
     {
      'count': 2,
      'files': ['file1.pcap', 'file2.pcap']
     }

---
+++ src/runmode-unix-socket.c
@@ -0,0 +1,77 @@
+/* Copyright (C) 2012 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "suricata-common.h"
+#include "tm-threads.h"
+#include "conf.h"
+#include "runmodes.h"
+#include "runmode-pcap-file.h"
+#include "log-httplog.h"
+#include "output.h"
+#include "cuda-packet-batcher.h"
+#include "source-pfring.h"
+#include "detect-engine-mpm.h"
+
+#include "alert-fastlog.h"
+#include "alert-prelude.h"
+#include "alert-unified2-alert.h"
+#include "alert-debuglog.h"
+
+#include "util-debug.h"
+#include "util-time.h"
+#include "util-cpu.h"
+#include "util-affinity.h"
+#include "unix-manager.h"
+
+static const char *default_mode = NULL;
+
+int unix_socket_mode_is_running = 0;
+
+const char *RunModeUnixSocketGetDefaultMode(void)
+{
+    return default_mode;
+}
+
+void RunModeUnixSocketRegister(void)
+{
+#ifdef BUILD_UNIX_SOCKET
+    RunModeRegisterNewRunMode(RUNMODE_UNIX_SOCKET, "single",
+                              "Unix socket mode",
+                              RunModeUnixSocketSingle);
+    default_mode = "single";
+#endif
+    return;
+}
+
+/**
+ * \brief Single thread version of the Pcap file processing.
+ */
+int RunModeUnixSocketSingle(DetectEngineCtx *de_ctx)
+{
+#ifdef BUILD_UNIX_SOCKET
+    UnixManagerThreadSpawn(de_ctx, 1);
+
+    unix_socket_mode_is_running = 1;
+#endif
+
+    return 0;
+}
+
+int RunModeUnixSocketIsActive(void)
+{
+    return unix_socket_mode_is_running;
+}
