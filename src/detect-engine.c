c3eab5cf4e6ce00b3ad16f9409d4aed077e69d87
Anoop Saldanha <poonaatsoc@gmail.com>
2012-06-22 23:49:44 +0530

    Replace the old atomic sets using cas with the new sc_atomic_set macro

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -227,8 +227,7 @@ static void *DetectEngineLiveRuleSwap(void *arg)
             }
 
 
-            BUG_ON(SC_ATOMIC_CAS(&slots->slot_data, SC_ATOMIC_GET(slots->slot_data),
-                                 det_ctx) == 0);;
+            SC_ATOMIC_SET(slots->slot_data, det_ctx);
             SCLogDebug("swapping new det_ctx with older one");
 
             slots = slots->slot_next;
