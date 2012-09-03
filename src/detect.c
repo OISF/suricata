5d27518bbd8343094188bdd81d40c0439f658243
Victor Julien <victor@inliniac.net>
2012-09-03 11:30:19 +0200

    Make sure we never underflow len in DetectLoadSigFile

--- src/detect.c
+++ src/detect.c
@@ -526,7 +526,7 @@ int DetectLoadSigFile(DetectEngineCtx *de_ctx, char *sig_file, int *sigs_tot) {
             continue;
 
         /* Check for multiline rules. */
-        while (isspace(line[--len]));
+        while (len > 0 && isspace(line[--len]));
         if (line[len] == '\\') {
             multiline++;
             offset = len;
