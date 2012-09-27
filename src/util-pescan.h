5ec1a638313f953b91d75c07159079b4a771c979
David Abarbanel <david.abarbanel@baesystems.com>
2012-09-27 13:17:19 -0400

    Portable Executable header anomaly scanner
    Updated revision 3
    
    This patch details the changes made to the Suricata baseline for integrating the PEScan service. The service involves creating a detection engine keyword 'pescan' that matches when a PE file is detected via a byte scanner, or when the file has an anomaly score that meets certain condition criteria.  In addition, when PE files are detected, their header and anomaly flag information is logged to a separate file (pescan.log) using the logging module.

---
+++ src/util-pescan.h
@@ -0,0 +1,66 @@
+/* Copyright (C) 2012 BAE Systems
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author David Abarbanel <david.abarbanel@baesystems.com>
+ *
+ */
+
+#ifndef __UTIL_PESCAN_H_
+#define __UTIL_PESCAN_H_
+
+#include "suricata-common.h"
+#include "threads.h"
+#include "debug.h"
+#include "decode.h"
+
+#include "detect.h"
+#include "detect-parse.h"
+
+#include "detect-engine.h"
+#include "detect-engine-mpm.h"
+#include "detect-engine-state.h"
+
+#include "flow.h"
+#include "flow-var.h"
+#include "flow-util.h"
+
+#include "util-pescan.h"
+#include "util-debug.h"
+#include "util-spm-bm.h"
+
+/**
+ * \brief Structure for containing configuration options
+ *
+ */
+typedef struct PEScanConfig {
+
+    uint32_t max_scan_bytes;  /**< Maximum file bytes to scan */
+    uint32_t pref_scan_bytes;  /**< Preferrable amount of bytes to scan to achieve result */
+    uint32_t wait_scan_bytes;  /**< Number of bytes to wait for before invoking
+                                    pescan detector match function */
+
+} PEScanConfig;
+
+/* Function prototypes */
+uint32_t PEScanGetMaxBytes(File *file);
+int PEScanFile(File *file, uint8_t *scanbuf);
+PEScanConfig * PEScanGetConfig();
+
+#endif
