f5f7f5fd7d40a8ee5cab62322f373359cc087aa7
Victor Julien <victor@inliniac.net>
2012-11-08 09:47:02 +0100

    OpenBSD 5.2 build fixes, Unit test fix.

--- src/source-pcap-file.c
+++ src/source-pcap-file.c
@@ -23,12 +23,6 @@
  * File based pcap packet acquisition support
  */
 
-#if LIBPCAP_VERSION_MAJOR == 1
-#include <pcap/pcap.h>
-#else
-#include <pcap.h>
-#endif /* LIBPCAP_VERSION_MAJOR */
-
 #include "suricata-common.h"
 #include "suricata.h"
 #include "decode.h"
