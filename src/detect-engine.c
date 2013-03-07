1bebe0be59bb8004b6d47e689825a886c5a84e43
Anoop Saldanha <anoopsaldanha@gmail.com>
2013-03-07 16:38:16 +0530

    fix for #769.
    Packet inserted by live swap flagged as pseudo packet.

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -520,6 +520,7 @@ static void *DetectEngineLiveRuleSwap(void *arg)
                 pseudo_pkt_inserted = 1;
                 if (detect_tvs[i]->inq != NULL) {
                     Packet *p = PacketGetFromAlloc();
+                    p->flags |= PKT_PSEUDO_STREAM_END;
                     if (p != NULL) {
                         PacketQueue *q = &trans_q[detect_tvs[i]->inq->id];
                         SCMutexLock(&q->mutex_q);
