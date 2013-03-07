eeb439c1a3a682a5bc76e7ff244ec213b4b67bb2
Victor Julien <victor@inliniac.net>
2013-03-07 13:55:01 +0100

    Open 2.0 dev branch

--- src/suricata.h
+++ src/suricata.h
@@ -71,7 +71,7 @@
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
-#define PROG_VER "1.4dev"
+#define PROG_VER "2.0dev"
 
 /* workaround SPlint error (don't know __gnuc_va_list) */
 #ifdef S_SPLINT_S
