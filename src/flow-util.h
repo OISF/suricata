e0483ebea332b52e4d0f91916345f135bf522ac6
Eric Leblond <eric@regit.org>
2013-03-20 11:28:58 +0100

    flow tag: conversion to flow storage API
    This patch is updating the flow tag system to use the flow
    storage API. The tag_list member of Flow structure is suppressed
    and its cleaning operation are suppressed too as this is handled
    transparently by the flow storage API.

--- src/flow-util.h
+++ src/flow-util.h
@@ -56,7 +56,6 @@
         (f)->de_state = NULL; \
         (f)->sgh_toserver = NULL; \
         (f)->sgh_toclient = NULL; \
-        (f)->tag_list = NULL; \
         (f)->flowvar = NULL; \
         SCMutexInit(&(f)->de_state_m, NULL); \
         (f)->hnext = NULL; \
@@ -92,8 +91,6 @@
         } \
         (f)->sgh_toserver = NULL; \
         (f)->sgh_toclient = NULL; \
-        DetectTagDataListFree((f)->tag_list); \
-        (f)->tag_list = NULL; \
         GenericVarFree((f)->flowvar); \
         (f)->flowvar = NULL; \
         if (SC_ATOMIC_GET((f)->autofp_tmqh_flow_qid) != -1) {   \
@@ -110,11 +107,9 @@
         if ((f)->de_state != NULL) { \
             DetectEngineStateFree((f)->de_state); \
         } \
-        DetectTagDataListFree((f)->tag_list); \
         GenericVarFree((f)->flowvar); \
         SCMutexDestroy(&(f)->de_state_m); \
         SC_ATOMIC_DESTROY((f)->autofp_tmqh_flow_qid);   \
-        (f)->tag_list = NULL; \
     } while(0)
 
 /** \brief check if a memory alloc would fit in the memcap
