e68d44b051e44ce5f2619bd5a89fa1de9da47574
Anoop Saldanha <anoopsaldanha@gmail.com>
2013-08-31 21:43:06 +0530

    fix for #932.
    ipv6 tunnel decoder wrongly treats the tunneled ipv6 packets as an ipv4
    packet.

--- src/decode-ipv6.c
+++ src/decode-ipv6.c
@@ -93,7 +93,7 @@ static void DecodeIP6inIP6(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p, uin
             if (tp != NULL) {
                 PKT_SET_SRC(tp, PKT_SRC_DECODER_IPV6);
                 DecodeTunnel(tv, dtv, tp, GET_PKT_DATA(tp),
-                             GET_PKT_LEN(tp), pq, IPPROTO_IP);
+                             GET_PKT_LEN(tp), pq, IPPROTO_IPV6);
                 PacketEnqueue(pq,tp);
                 SCPerfCounterIncr(dtv->counter_ipv6inipv6, tv->sc_perf_pca);
                 return;
