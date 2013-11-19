a8b971c710233bf59f0b68732a83bb7a5db0054e
Victor Julien <victor@inliniac.net>
2013-11-19 15:26:36 +0100

    http: strip 'proxy' part of http_uri
    Strip the 'proxy' parts from the normalized uri as inspected by http_uri,
    urilen, pcre /U and others.
    
      In a request line like:
        GET http://suricata-ids.org/blah/ HTTP/1.1
      the normalized URI will now be:
        /blah/
    
    This doesn't affect http_raw_uri. So matching the hostname, etc is still
    possible through this keyword.
    
    Additionally, a new per HTTP 'personality' option was added to change
    this behavior: "uri-include-all":
    
      uri-include-all: <true|false>
        Include all parts of the URI. By default the
        'scheme', username/password, hostname and port
        are excluded. Setting this option to true adds
        all of them to the normalized uri as inspected
        by http_uri, urilen, pcre with /U and the other
        keywords that inspect the normalized uri.
        Note that this does not affect http_raw_uri.
    
    So adding uri-include-all:true to all personalities in the yaml will
    restore the old default behavior.
    
    Ticket 1008.

--- src/app-layer-htp-libhtp.h
+++ src/app-layer-htp-libhtp.h
@@ -45,7 +45,7 @@
 #include "suricata.h"
 #include "suricata-common.h"
 
-bstr *SCHTPGenerateNormalizedUri(htp_tx_t *tx, htp_uri_t *uri);
+bstr *SCHTPGenerateNormalizedUri(htp_tx_t *tx, htp_uri_t *uri, int uri_include_all);
 int64_t SC_htp_parse_content_length(bstr *b);
 
 #endif /* __APP_LAYER_HTP_LIBHTP__H__ */
