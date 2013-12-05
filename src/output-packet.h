ddae52d4648f8e9729c667bf64cd192b425e4447
Victor Julien <victor@inliniac.net>
2013-12-05 15:35:15 +0100

    Introduce packet logging output API
    This patch introduces a new API for outputs that log based on the
    packet, such as alert outputs. In converts fast-log to the new API.
    
    The API gets rid of the concept of each logger being a thread module,
    but instead there is one thread module that runs all packet loggers.
    Through the registration function OutputRegisterPacketModule a log
    module can register itself to be considered for each packet.
    
    Each logger registers itself to this new API with 2 functions and the
    OutputCtx object that was already used in the old implementation.
    The function pointers are:
    
    LogFunc:       the log function
    
    ConditionFunc: this function is called before the LogFunc and only
                   if this returns TRUE the LogFunc is called.
    
    For a simple alert logger like fast-log, the condition function will
    simply return TRUE if p->alerts.cnt > 0.

---
+++ src/output-packet.h
@@ -0,0 +1,44 @@
+/* Copyright (C) 2007-2013 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Victor Julien <victor@inliniac.net>
+ *
+ * Packet Logger Output registration functions
+ */
+
+#ifndef __OUTPUT_PACKET_H__
+#define __OUTPUT_PACKET_H__
+
+#include "decode.h"
+
+/** packet logger function pointer type */
+typedef int (*PacketLogger)(ThreadVars *, void *thread_data, const Packet *);
+
+/** packet logger condition function pointer type,
+ *  must return true for packets that should be logged
+ */
+typedef int (*PacketLogCondition)(ThreadVars *, const Packet *);
+
+int OutputRegisterPacketLogger(char *name, PacketLogger LogFunc,
+        PacketLogCondition ConditionFunc, OutputCtx *);
+
+void TmModulePacketLoggerRegister (void);
+
+#endif /* __OUTPUT_PACKET_H__ */
