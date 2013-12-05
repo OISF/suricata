f21a9157548f06b55913408c02b1126906fc4243
Victor Julien <victor@inliniac.net>
2013-12-05 18:35:27 +0100

    TX logging API: convert HTTP log
    Convert the HTTP log to the new TX logging API.

--- src/suricata.c
+++ src/suricata.c
@@ -793,8 +793,6 @@ void RegisterAllModules()
     TmModuleLogDropLogRegister();
     /* http log */
     TmModuleLogHttpLogRegister();
-    TmModuleLogHttpLogIPv4Register();
-    TmModuleLogHttpLogIPv6Register();
     TmModuleLogTlsLogRegister();
     TmModuleLogTlsLogIPv4Register();
     TmModuleLogTlsLogIPv6Register();
