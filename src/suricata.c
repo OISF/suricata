c8e23d129d89de0a18a3496544a9e0b43626296e
Victor Julien <victor@inliniac.net>
2013-12-05 15:35:15 +0100

    Introduce packet logging output API
    This patch introduces a new API for outputs that log based on the
    packet, such as alert outputs. In converts fast-log to the new API.
    
    The API gets rid of the concept of each logger being a thread module,
    but instead there is one thread module that runs all packet loggers.
    
    Each logger registers itself to this new API with 2 functions and the
    OutputCtx object that was already used. The function pointers are:
    
    LogFunc: the log function
    ConditionFunc: this function is called before the LogFunc and only
                   if this returns TRUE the LogFunc is called.
    
    For a simple alert logger like fast-log, the condition function will
    simply return TRUE if p->alerts.cnt > 0.

--- src/suricata.c
+++ src/suricata.c
@@ -149,6 +149,7 @@
 #include "reputation.h"
 
 #include "output.h"
+#include "output-packet.h"
 #include "util-privs.h"
 
 #include "tmqh-packetpool.h"
@@ -775,8 +776,6 @@ void RegisterAllModules()
 
     /* fast log */
     TmModuleAlertFastLogRegister();
-    TmModuleAlertFastLogIPv4Register();
-    TmModuleAlertFastLogIPv6Register();
     /* debug log */
     TmModuleAlertDebugLogRegister();
     /* prelue log */
@@ -805,7 +804,7 @@ void RegisterAllModules()
     TmModuleLogFilestoreRegister();
     /* dns log */
     TmModuleLogDnsLogRegister();
-    /* cuda */
+    TmModulePacketLoggerRegister();
     TmModuleDebugList();
 
 }
