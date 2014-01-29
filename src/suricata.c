0113281aa75ce7bd11d9eb400b6bd225f05db1c9
Victor Julien <victor@inliniac.net>
2014-01-29 18:50:32 +0100

    alert-json: make full module out of json alert
    Make a full module out of the json alert code in output-json-alert.[ch].

--- src/suricata.c
+++ src/suricata.c
@@ -78,6 +78,7 @@
 #include "alert-prelude.h"
 #include "alert-syslog.h"
 #include "alert-pcapinfo.h"
+#include "output-json-alert.h"
 
 #include "log-droplog.h"
 #include "log-httplog.h"
@@ -809,6 +810,10 @@ void RegisterAllModules()
     /* dns log */
     TmModuleLogDnsLogRegister();
     TmModuleJsonDnsLogRegister();
+
+    TmModuleJsonAlertLogRegister();
+
+    /* log api */
     TmModulePacketLoggerRegister();
     TmModuleTxLoggerRegister();
     TmModuleFileLoggerRegister();
