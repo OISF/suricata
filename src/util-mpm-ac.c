e32aedc8e7be0e9172bbe067278738193e9c6ad0
Ken Steele <ken@tilera.com>
2014-02-13 22:38:33 -0500

    When assigning Pattern IDs pids, check Case flags
    This fixes bug 1110. When assigning PIDs, use the NO_CASE flag when comparing
    for duplicates. The state of the flag must be the same, but also use the same
    type of comparisons when checking for duplicates.
    
    Previously, "foo":CS would match with "foo":CI when it should not.
    and "foo":CI would not match "FoO":CI when it should. Both of those
    cases are fixed with this change.
    
    This then allows simplifying the use of pid in MPMs because now if they
    pids match, then so do the flags, so checking the flags is not required.

--- src/util-mpm-ac.c
+++ src/util-mpm-ac.c
@@ -120,33 +120,6 @@ static void SCACGetConfig()
 
 /**
  * \internal
- * \brief Compares 2 patterns.  We use it for the hashing process during the
- *        the initial pattern insertion time, to cull duplicate sigs.
- *
- * \param p      Pointer to the first pattern(SCACPattern).
- * \param pat    Pointer to the second pattern(raw pattern array).
- * \param patlen Pattern length.
- * \param flags  Flags.  We don't need this.
- *
- * \retval hash A 32 bit unsigned hash.
- */
-static inline int SCACCmpPattern(SCACPattern *p, uint8_t *pat, uint16_t patlen,
-                                 char flags)
-{
-    if (p->len != patlen)
-        return 0;
-
-    if (p->flags != flags)
-        return 0;
-
-    if (memcmp(p->cs, pat, patlen) != 0)
-        return 0;
-
-    return 1;
-}
-
-/**
- * \internal
  * \brief Creates a hash of the pattern.  We use it for the hashing process
  *        during the initial pattern insertion time, to cull duplicate sigs.
  *
@@ -182,14 +155,13 @@ static inline SCACPattern *SCACInitHashLookup(SCACCtx *ctx, uint8_t *pat,
 {
     uint32_t hash = SCACInitHashRaw(pat, patlen);
 
-    if (ctx->init_hash == NULL || ctx->init_hash[hash] == NULL) {
+    if (ctx->init_hash == NULL) {
         return NULL;
     }
 
     SCACPattern *t = ctx->init_hash[hash];
     for ( ; t != NULL; t = t->next) {
-        //if (SCACCmpPattern(t, pat, patlen, flags) == 1)
-        if (t->flags == flags && t->id == pid)
+        if (t->id == pid)
             return t;
     }
 
