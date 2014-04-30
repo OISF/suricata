26226d4f298730fc89ab91814f8b7446f6c2516d
Victor Julien <victor@inliniac.net>
2014-04-30 13:37:30 +0200

    flow: introduce FlowRecycler stub
    FlowRecycler thread stub. Start/stop code.

--- src/suricata.c
+++ src/suricata.c
@@ -2285,6 +2285,7 @@ int main(int argc, char **argv)
         }
         /* Spawn the flow manager thread */
         FlowManagerThreadSpawn();
+        FlowRecyclerThreadSpawn();
         StreamTcpInitConfig(STREAM_VERBOSE);
 
         SCPerfSpawnThreads();
@@ -2403,6 +2404,7 @@ int main(int argc, char **argv)
 
     if (suri.run_mode != RUNMODE_UNIX_SOCKET) {
         SCPerfReleaseResources();
+        FlowKillFlowRecyclerThread();
         FlowShutdown();
         StreamTcpFreeConfig(STREAM_VERBOSE);
     }
