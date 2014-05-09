991b8a16680b2b4d74127a4e862acea6b0c7025a
Victor Julien <victor@inliniac.net>
2014-05-09 15:10:54 +0200

    decode: pass ThreadVars to DecodeThreadVarsFree
    Flow output thread data deinit function which will be called from
    DecodeThreadVarsFree will need it.

--- src/decode.c
+++ src/decode.c
@@ -489,7 +489,7 @@ DecodeThreadVars *DecodeThreadVarsAlloc(ThreadVars *tv)
     return dtv;
 }
 
-void DecodeThreadVarsFree(DecodeThreadVars *dtv)
+void DecodeThreadVarsFree(ThreadVars *tv, DecodeThreadVars *dtv)
 {
     if (dtv != NULL) {
         if (dtv->app_tctx != NULL)
