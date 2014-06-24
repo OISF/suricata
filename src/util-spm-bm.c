5ecaebae989b88ac9e60dcc9babaf06994235888
Ken Steele <ken@tilera.com>
2014-06-24 10:05:03 -0400

    Fix comment wording in Boyer Moore pattern matcher.

--- src/util-spm-bm.c
+++ src/util-spm-bm.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2010 Open Information Security Foundation
+/* Copyright (C) 2007-2014 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -22,10 +22,10 @@
  *
  * Boyer Moore simple pattern matcher implementation
  *
- * Boyer Moore algorithm has a really good performance. It need to arrays
+ * Boyer Moore algorithm has a really good performance. It need two arrays
  * of context for each pattern that hold applicable shifts on the text
  * to seach in, based on characters not available in the pattern
- * and combinations of characters that start a sufix on the pattern.
+ * and combinations of characters that start a sufix of the pattern.
  * If possible, we should store the context of patterns that we are going
  * to search for multiple times, so we don't spend time on rebuilding them.
  */
