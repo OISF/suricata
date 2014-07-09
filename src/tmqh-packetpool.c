548daeae81f4b58a76f7bd1149e9e6582ffc2871
Victor Julien <victor@inliniac.net>
2014-07-09 08:51:29 +0200

    packet recycle: do most clean up on packet reuse
    Call PACKET_RELEASE_REFS from PacketPoolGetPacket() so that
    we only access the large packet structure just before actually
    using it. Should give better cache behaviour.

--- src/tmqh-packetpool.c
+++ src/tmqh-packetpool.c
@@ -114,6 +114,8 @@ Packet *PacketPoolGetPacket(void) {
         return NULL;
 
     Packet *p = RingBufferMrMwGetNoWait(ringbuffer);
+    if (p != NULL)
+        PACKET_REINIT(p);
     return p;
 }
 
@@ -122,7 +124,7 @@ Packet *PacketPoolGetPacket(void) {
  */
 void PacketPoolReturnPacket(Packet *p)
 {
-    PACKET_RECYCLE(p);
+    PACKET_RELEASE_REFS(p);
     RingBufferMrMwPut(ringbuffer, (void *)p);
 }
 
