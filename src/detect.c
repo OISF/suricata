b334b8a6e9bebe3286c0dbc8c8aa20e62ba90868
Anoop Saldanha <anoopsaldanha@gmail.com>
2014-07-25 13:20:28 +0530

    CUDA: Update the inspection engine to inform the cuda module that it doesn't need the gpu results and to release the packet for the next run.
    Previously the inspection engine wouldn't inform the cuda module, if it
    didn't need the results.  As a consequence, when the packet is next taken
    for re-use, and if the packet is still being processed by the cuda module,
    the engine would wait till the cuda module frees the packet.
    
    This commits updates this functionality to inform the cuda module to
    release the packet for the afore-mentioned case.

--- src/detect.c
+++ src/detect.c
@@ -1572,6 +1572,10 @@ next:
     PACKET_PROFILING_DETECT_END(p, PROF_DETECT_RULES);
 
 end:
+#ifdef __SC_CUDA_SUPPORT__
+    CudaReleasePacket(p);
+#endif
+
     /* see if we need to increment the inspect_id and reset the de_state */
     if (has_state && AppLayerParserProtocolSupportsTxs(p->proto, alproto)) {
         PACKET_PROFILING_DETECT_START(p, PROF_DETECT_STATEFUL);
