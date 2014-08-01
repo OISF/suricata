5e06548cce3fcb2ddda6c870458dd3628e1b71a1
Victor Julien <victor@inliniac.net>
2014-08-01 16:29:53 +0200

    Rename Lua code to just Lua
    As we support regular Lua as well as LuaJIT, it makes more sense to call
    it all Lua.

--- src/detect-parse.c
+++ src/detect-parse.c
@@ -41,7 +41,7 @@
 #include "detect-flow.h"
 #include "detect-app-layer-protocol.h"
 #include "detect-engine-apt-event.h"
-#include "detect-luajit.h"
+#include "detect-lua.h"
 #include "detect-app-layer-event.h"
 
 #include "pkt-var.h"
@@ -1319,7 +1319,7 @@ int SigValidate(DetectEngineCtx *de_ctx, Signature *s)
     }
 
 #ifdef HAVE_LUA
-    DetectLuajitPostSetup(s);
+    DetectLuaPostSetup(s);
 #endif
 
 #ifdef DEBUG
