94fd8722702e067a4aca9364087c2a822bcc027a
Victor Julien <victor@inliniac.net>
2014-11-06 16:05:57 +0100

    Detect perf counters

--- src/detect.h
+++ src/detect.h
@@ -791,6 +791,9 @@ typedef struct DetectionEngineThreadCtx_ {
 
     /** id for alert counter */
     uint16_t counter_alerts;
+    uint16_t counter_mpm_list;
+    uint16_t counter_nonmpm_list;
+    uint16_t counter_match_list;
 
     /* used to discontinue any more matching */
     uint16_t discontinue_matching;
