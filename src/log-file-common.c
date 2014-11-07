5d8abeade27c64db1b977e51d8d8693abbbcdc1e
root <root@ioids.com>
2014-11-07 23:01:47 +0100

    Fixed issues with double escaping of chars in json output. Fixed issue with missing meta files from filestore.

--- src/log-file-common.c
+++ src/log-file-common.c
@@ -23,18 +23,8 @@ void LogFileMetaGetSmtpMessageID(const Packet *p, const File *ff, MemBuffer *buf
 
         /* Message Id */
         if (tx->msg_tail->msg_id != NULL) {
-#ifdef HAVE_LIBJANSSON
-            if (fflag & META_FORMAT_JSON) {
-                PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
-                                (uint8_t *)tx->msg_tail->msg_id, tx->msg_tail->msg_id_len);
-            } else {
                 PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
                                (uint8_t *)tx->msg_tail->msg_id, tx->msg_tail->msg_id_len);
-            }
-#else
-                PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
-                               (uint8_t *)tx->msg_tail->msg_id, tx->msg_tail->msg_id_len);
-#endif
         }
 
     }
@@ -50,18 +40,8 @@ void LogFileMetaGetSmtpSender(const Packet *p, const File *ff, MemBuffer *buffer
         /* Sender */
         MimeDecField *field = MimeDecFindField(tx->msg_tail, "from");
         if (field != NULL) {
-#ifdef HAVE_LIBJANSSON
-            if (fflag & META_FORMAT_JSON) {
-                PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
-                                (uint8_t *) field->value, field->value_len);
-            } else {
                 PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
                                (uint8_t *) field->value, field->value_len);
-            }
-#else
-                PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
-                               (uint8_t *) field->value, field->value_len);
-#endif
         }
 
     }
@@ -75,26 +55,11 @@ void LogFileMetaGetUri(const Packet *p, const File *ff, MemBuffer *buffer, uint3
         if (tx != NULL) {
             HtpTxUserData *tx_ud = htp_tx_get_user_data(tx);
             if (tx_ud->request_uri_normalized != NULL) {
-#ifdef HAVE_LIBJANSSON
-                if (fflag & META_FORMAT_JSON) {
-                    PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset,
-                                    buffer->size,
-                                    (uint8_t *)bstr_ptr(tx_ud->request_uri_normalized),
-                                    bstr_len(tx_ud->request_uri_normalized));
-                } else {
-                    PrintRawUriBuf((char *)buffer->buffer, &buffer->offset,
-                                   buffer->size,
-                                   (uint8_t *)bstr_ptr(tx_ud->request_uri_normalized),
-                                   bstr_len(tx_ud->request_uri_normalized));
-                }
-                return;
-#else
                 PrintRawUriBuf((char *)buffer->buffer, &buffer->offset,
                                buffer->size,
                                (uint8_t *)bstr_ptr(tx_ud->request_uri_normalized),
                                bstr_len(tx_ud->request_uri_normalized));
                 return;
-#endif
             }
             return;
         }
@@ -107,23 +72,10 @@ void LogFileMetaGetHost(const Packet *p, const File *ff, MemBuffer *buffer, uint
     if (htp_state != NULL) {
         htp_tx_t *tx = AppLayerParserGetTx(IPPROTO_TCP, ALPROTO_HTTP, htp_state, ff->txid);
         if (tx != NULL && tx->request_hostname != NULL) {
-#ifdef HAVE_LIBJANSSON
-            if (fflag & META_FORMAT_JSON) {
-                PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
-                                (uint8_t *)bstr_ptr(tx->request_hostname),
-                                bstr_len(tx->request_hostname));
-            } else {
-                PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
-                               (uint8_t *)bstr_ptr(tx->request_hostname),
-                               bstr_len(tx->request_hostname));
-            }
-            return;
-#else
             PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size,
                            (uint8_t *)bstr_ptr(tx->request_hostname),
                            bstr_len(tx->request_hostname));
             return;
-#endif
         }
     }
     sprintf((char *)buffer->buffer, "unknown");
@@ -137,23 +89,10 @@ void LogFileMetaGetReferer(const Packet *p, const File *ff, MemBuffer *buffer, u
             htp_header_t *h = NULL;
             h = (htp_header_t *)htp_table_get_c(tx->request_headers, "Referer");
             if (h != NULL) {
-#ifdef HAVE_LIBJANSSON
-                if (fflag & META_FORMAT_JSON) {
-                    PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset,
-                                    buffer->size, (uint8_t *)bstr_ptr(h->value),
-                                    bstr_len(h->value));
-                } else {
-                    PrintRawUriBuf((char *)buffer->buffer, &buffer->offset,
-                                   buffer->size, (uint8_t *)bstr_ptr(h->value),
-                                   bstr_len(h->value));
-                }
-                return;
-#else
                 PrintRawUriBuf((char *)buffer->buffer, &buffer->offset,
                                buffer->size, (uint8_t *)bstr_ptr(h->value),
                                bstr_len(h->value));
                 return;
-#endif
             }
         }
     }
@@ -168,23 +107,10 @@ void LogFileMetaGetUserAgent(const Packet *p, const File *ff, MemBuffer *buffer,
             htp_header_t *h = NULL;
             h = (htp_header_t *)htp_table_get_c(tx->request_headers, "User-Agent");
             if (h != NULL) {
-#ifdef HAVE_LIBJANSSON
-                if (fflag & META_FORMAT_JSON) {
-                    PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset,
-                                    buffer->size, (uint8_t *)bstr_ptr(h->value),
-                                    bstr_len(h->value));
-                } else {
-                    PrintRawUriBuf((char *)buffer->buffer, &buffer->offset,
-                                   buffer->size, (uint8_t *)bstr_ptr(h->value),
-                                   bstr_len(h->value));
-                }
-                return;
-#else
                 PrintRawUriBuf((char *)buffer->buffer, &buffer->offset,
                                buffer->size, (uint8_t *)bstr_ptr(h->value),
                                bstr_len(h->value));
                 return;
-#endif
             }
         }
     }
@@ -197,17 +123,13 @@ void LogFileLogPrintJsonObj(FILE *fp, json_t *js) {
     fprintf(fp, "%s", js_data);
 }
 
-json_t *LogFileLogFileJson(const Packet *p, const File *ff) {
-    json_t *container = json_object();
-    json_t *http = json_object();
-    json_t *smtp = json_object();
-    json_t *meta = json_object();
-
-
+void LogFileLogTransactionMeta(const Packet *p, const File *ff, json_t *js) {
     MemBuffer *buffer;
     buffer = MemBufferCreateNew(META_BUFFER_SIZE);
 
     if (p->flow->alproto == ALPROTO_HTTP) {
+        json_t *http = json_object();
+
         LogFileMetaGetUri(p, ff, buffer, META_FORMAT_JSON);
         json_object_set_new(http, "uri", json_string((char *)buffer->buffer));
         MemBufferReset(buffer);
@@ -223,30 +145,42 @@ json_t *LogFileLogFileJson(const Packet *p, const File *ff) {
         LogFileMetaGetUserAgent(p, ff, buffer, META_FORMAT_JSON);
         json_object_set_new(http, "useragent", json_string((char *)buffer->buffer));
         MemBufferReset(buffer);
-        json_object_set_new(container, "http", http);
+        
+        json_object_set_new(js, "http", http);
     } else if (p->flow->alproto == ALPROTO_SMTP) {
-        /* Only applicable to SMTP */
+        json_t *smtp = json_object();
+    
         LogFileMetaGetSmtpMessageID(p, ff, buffer, META_FORMAT_JSON);
         json_object_set_new(smtp, "message-id", json_string((char *)buffer->buffer));
         MemBufferReset(buffer);
 
         LogFileMetaGetSmtpSender(p, ff, buffer, META_FORMAT_JSON);
         json_object_set_new(smtp, "sender", json_string((char *)buffer->buffer));
-        json_object_set_new(container, "smtp", smtp);
+        json_object_set_new(js, "smtp", smtp);
     }
 
-    PrintRawJsonBuf((char *)buffer->buffer, &buffer->offset, buffer->size, ff->name, ff->name_len);
-    json_object_set_new(http, "filename", json_string((char *)buffer->buffer));
+    MemBufferFree(buffer);
+}
+
+
+void LogFileLogFileMeta(const Packet *p, const File *ff, json_t *js) {
+    json_t *container = json_object();
+
+    MemBuffer *buffer;
+    buffer = MemBufferCreateNew(META_BUFFER_SIZE);
+    
+    PrintRawUriBuf((char *)buffer->buffer, &buffer->offset, buffer->size, ff->name, ff->name_len);
+    json_object_set_new(container, "filename", json_string((char *)buffer->buffer));
 
     if (ff->magic == NULL) {
-        json_object_set_new(meta, "magic", json_string("unknown"));
+        json_object_set_new(container, "magic", json_string("unknown"));
     } else {
-        json_object_set_new(meta, "magic", json_string(ff->magic));
+        json_object_set_new(container, "magic", json_string(ff->magic));
     }
 
     switch(ff->state) {
         case FILE_STATE_CLOSED:
-            json_object_set_new(meta, "state", json_string("closed"));
+            json_object_set_new(container, "state", json_string("closed"));
 #ifdef HAVE_NSS
             if (ff->flags & FILE_MD5) {
                 char md5_buffer[512];
@@ -254,31 +188,30 @@ json_t *LogFileLogFileJson(const Packet *p, const File *ff) {
                 for (x = 0; x < sizeof(ff->md5); x++) {
                     sprintf(md5_buffer, "%02x", ff->md5[x]);
                 }
-                json_object_set_new(meta, "md5", json_string(md5_buffer));
+                json_object_set_new(container, "md5", json_string(md5_buffer));
             }
 #endif
             break;
         case FILE_STATE_TRUNCATED:
-            json_object_set_new(meta, "state", json_string("truncated"));
+            json_object_set_new(container, "state", json_string("truncated"));
             break;
         case FILE_STATE_ERROR:
-            json_object_set_new(meta, "state", json_string("error"));
+            json_object_set_new(container, "state", json_string("error"));
             break;
         default:
-            json_object_set_new(meta, "state", json_string("unknown"));
+            json_object_set_new(container, "state", json_string("unknown"));
             break;
     }
 
     MemBufferReset(buffer);
     sprintf((char *)buffer->buffer, "%"PRIu64"", ff->size);
-    json_object_set_new(meta, "size", json_string((char *)buffer->buffer));
+    json_object_set_new(container, "size", json_string((char *)buffer->buffer));
 
     MemBufferReset(buffer);
     sprintf((char *)buffer->buffer, ff->flags & FILE_STORED ? "true" : "false");
-    json_object_set_new(meta, "stored", json_string((char *)buffer->buffer));
+    json_object_set_new(container, "stored", json_string((char *)buffer->buffer));
 
     MemBufferFree(buffer);
-    json_object_set_new(container, "metadata", meta);
-    return container;
+    json_object_set_new(js, "metadata", container);
 }
 #endif
