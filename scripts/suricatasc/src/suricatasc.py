bee43727a2fdbc404c9a29bedd5afc152d03d912
Victor Julien <victor@inliniac.net>
2015-02-06 15:06:32 +0100

    squash: into pcap-file update in suricatasc
    Tenant argument in pcap-file is optional. If ommited it's 0, which
    means it's disabled.

--- scripts/suricatasc/src/suricatasc.py
+++ scripts/suricatasc/src/suricatasc.py
@@ -178,15 +178,19 @@ class SuricataSC:
                     parts = command.split(' ');
                 except:
                     raise SuricataCommandException("Arguments to command '%s' is missing" % (command))
-                cmd, filename, output, tenant = parts[0], parts[1], parts[2], parts[3] or None
+                cmd, filename, output = parts[0], parts[1], parts[2]
+                tenant = None
+                if len(parts) > 3:
+                    tenant = parts[3]
                 if cmd != "pcap-file":
                     raise SuricataCommandException("Invalid command '%s'" % (command))
                 else:
                     arguments = {}
                     arguments["filename"] = filename
                     arguments["output-dir"] = output
-                    print(tenant)
-                    arguments["tenant"] = int(tenant)
+                    if tenant != None:
+                        print(tenant)
+                        arguments["tenant"] = int(tenant)
             elif "iface-stat" in command:
                 try:
                     [cmd, iface] = command.split(' ', 1)
