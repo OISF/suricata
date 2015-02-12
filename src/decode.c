e138a2ac1e0e0be8421b21d1c461cbfb0733731e
Eric Leblond <eric@regit.org>
2015-02-12 21:15:27 +0100

    decode: introduce PacketCallocExtPkt function
    In flow timeout handling we need a function that allocate and blank
    a place that will be used to put constructed packet data. This new
    function has no other goal.

--- src/decode.c
+++ src/decode.c
@@ -171,6 +171,18 @@ Packet *PacketGetFromQueueOrAlloc(void)
     return p;
 }
 
+inline int PacketCallocExtPkt(Packet *p, int datalen)
+{
+    if (! p->ext_pkt) {
+        p->ext_pkt = SCCalloc(1, datalen);
+        if (unlikely(p->ext_pkt == NULL)) {
+            SET_PKT_LEN(p, 0);
+            return -1;
+        }
+    }
+    return 0;
+}
+
 /**
  *  \brief Copy data to Packet payload at given offset
  *
