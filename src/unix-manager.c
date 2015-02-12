501d11edb6e72d18c5aeed47f679c23e11f74730
Victor Julien <victor@inliniac.net>
2015-02-12 15:56:38 +0100

    multi-detect: implement unregister-tenant-handler
    Remove a tenant handler from the list and apply it.

--- src/unix-manager.c
+++ src/unix-manager.c
@@ -884,6 +884,7 @@ void *UnixManagerThread(void *td)
     UnixManagerRegisterCommand("dump-counters", SCPerfOutputCounterSocket, NULL, 0);
     UnixManagerRegisterCommand("reload-rules", UnixManagerReloadRules, NULL, 0);
     UnixManagerRegisterCommand("register-tenant-handler", UnixSocketRegisterTenantHandler, &command, UNIX_CMD_TAKE_ARGS);
+    UnixManagerRegisterCommand("unregister-tenant-handler", UnixSocketUnregisterTenantHandler, &command, UNIX_CMD_TAKE_ARGS);
     UnixManagerRegisterCommand("register-tenant", UnixSocketRegisterTenant, &command, UNIX_CMD_TAKE_ARGS);
     UnixManagerRegisterCommand("unregister-tenant", UnixSocketUnregisterTenant, &command, UNIX_CMD_TAKE_ARGS);
 
