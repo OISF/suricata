51be1b2d131a416e5fc9909d947b70d0efbd74ee
Victor Julien <victor@inliniac.net>
2015-02-20 10:40:32 +0100

    multi-detect: refuse to add duplicate tenant
    Generate error if tentant to be added is already loaded.

--- src/util-error.h
+++ src/util-error.h
@@ -297,6 +297,7 @@ typedef enum {
     SC_ERR_THREAD_DEINIT, /**< thread's deinit function failed */
     SC_ERR_IPPAIR_INIT,
     SC_ERR_MT_NO_SELECTOR,
+    SC_ERR_MT_DUPLICATE_TENANT,
 } SCError;
 
 const char *SCErrorToString(SCError);
