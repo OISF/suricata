bdd62bed37d2f66a9db73eb803f5c616c0b50a04
Victor Julien <victor@inliniac.net>
2015-03-10 17:41:03 +0100

    detect-state: implement tx state reset for reload
    In case of Detect Reload, we need to reset active tx' state.

--- src/detect.c
+++ src/detect.c
@@ -1162,6 +1162,7 @@ int SigMatchSignatures(ThreadVars *th_v, DetectEngineCtx *de_ctx, DetectEngineTh
 
                 DetectEngineStateReset(pflow->de_state,
                         (STREAM_TOSERVER|STREAM_TOCLIENT));
+                DetectEngineStateResetTxs(pflow);
             }
 
             /* set the iponly stuff */
