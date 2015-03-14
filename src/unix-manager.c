299f0c587db0c33c927fbee47ca01b516be1750b
Victor Julien <victor@inliniac.net>
2015-03-14 20:23:13 +0100

    multi-detect: add reload-tenant command
    Allow for a tenant to be reloaded. The command is the same as the
    register-tenant command, so with a yaml and tenant-id as argument.
    However this replaces an existing tenant.

--- src/unix-manager.c
+++ src/unix-manager.c
@@ -902,6 +902,7 @@ static TmEcode UnixManager(ThreadVars *th_v, void *thread_data)
     UnixManagerRegisterCommand("register-tenant-handler", UnixSocketRegisterTenantHandler, &command, UNIX_CMD_TAKE_ARGS);
     UnixManagerRegisterCommand("unregister-tenant-handler", UnixSocketUnregisterTenantHandler, &command, UNIX_CMD_TAKE_ARGS);
     UnixManagerRegisterCommand("register-tenant", UnixSocketRegisterTenant, &command, UNIX_CMD_TAKE_ARGS);
+    UnixManagerRegisterCommand("reload-tenant", UnixSocketReloadTenant, &command, UNIX_CMD_TAKE_ARGS);
     UnixManagerRegisterCommand("unregister-tenant", UnixSocketUnregisterTenant, &command, UNIX_CMD_TAKE_ARGS);
 
 
