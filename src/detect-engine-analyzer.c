81ff0b8be833da157cfc89774e67fd03d80439ea
Jason Ish <ish@unx.ca>
2015-04-10 10:22:25 -0600

    DNP3: dnp3_data, dnp3_func, dnp3_ind, dnp3_obj rule keywords

--- src/detect-engine-analyzer.c
+++ src/detect-engine-analyzer.c
@@ -480,6 +480,8 @@ static void EngineAnalysisRulesPrintFP(const Signature *s)
         fprintf(rule_engine_analysis_FD, "dns query name content");
     else if (list_type == DETECT_SM_LIST_TLSSNI_MATCH)
         fprintf(rule_engine_analysis_FD, "tls sni extension content");
+    else if (list_type == DETECT_SM_LIST_DNP3_DATA_MATCH)
+        fprintf(rule_engine_analysis_FD, "dnp3 data content");
 
     fprintf(rule_engine_analysis_FD, "\" buffer.\n");
 
--- src/detect-engine.c
+++ src/detect-engine.c
@@ -63,6 +63,7 @@
 #include "detect-engine-tls.h"
 #include "detect-engine-modbus.h"
 #include "detect-engine-filedata-smtp.h"
+#include "detect-engine-dnp3.h"
 #include "detect-engine-template.h"
 
 #include "detect-engine.h"
@@ -299,6 +300,20 @@ void DetectEngineRegisterAppInspectionEngines(void)
           DE_STATE_FLAG_FD_SMTP_INSPECT,
           0,
           DetectEngineInspectSMTPFiledata },
+        /* DNP3. */
+        { IPPROTO_TCP,
+          ALPROTO_DNP3,
+          DETECT_SM_LIST_DNP3_DATA_MATCH,
+          DE_STATE_FLAG_DNP3_DATA_INSPECT,
+          0,
+          DetectEngineInspectDNP3Data },
+        /* DNP3 func, obj, ind. */
+        { IPPROTO_TCP,
+          ALPROTO_DNP3,
+          DETECT_SM_LIST_DNP3_MATCH,
+          DE_STATE_FLAG_DNP3_INSPECT,
+          0,
+          DetectEngineInspectDNP3 },
         /* Template. */
         { IPPROTO_TCP,
           ALPROTO_TEMPLATE,
@@ -373,6 +388,20 @@ void DetectEngineRegisterAppInspectionEngines(void)
           DE_STATE_FLAG_DNSRESPONSE_INSPECT,
           1,
           DetectEngineInspectDnsResponse },
+        /* DNP3. */
+        { IPPROTO_TCP,
+          ALPROTO_DNP3,
+          DETECT_SM_LIST_DNP3_DATA_MATCH,
+          DE_STATE_FLAG_DNP3_DATA_INSPECT,
+          1,
+          DetectEngineInspectDNP3Data },
+        /* DNP3 func, ind, obj. */
+        { IPPROTO_TCP,
+          ALPROTO_DNP3,
+          DETECT_SM_LIST_DNP3_MATCH,
+          DE_STATE_FLAG_DNP3_INSPECT,
+          1,
+          DetectEngineInspectDNP3 },
         /* Template. */
         { IPPROTO_TCP,
           ALPROTO_TEMPLATE,
@@ -2725,6 +2754,10 @@ const char *DetectSigmatchListEnumToString(enum DetectSigmatchListEnum type)
 
         case DETECT_SM_LIST_MODBUS_MATCH:
             return "modbus";
+        case DETECT_SM_LIST_DNP3_DATA_MATCH:
+            return "dnp3_data";
+        case DETECT_SM_LIST_DNP3_MATCH:
+            return "dnp3";
 
         case DETECT_SM_LIST_BASE64_DATA:
             return "base64_data";
