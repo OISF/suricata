54e1218ff24740514e4ede63927eb5f1e1e22d1b
maxtors <moe.andreas@gmail.com>
2015-04-15 16:38:58 +0200

    Fixed string copy and cat functions and made shortening safer.
    Changed out strcpy, strncpy to strlcat and strlcpy. Also added
    checks to see if the shortening did work or if it would fail in
    advance. Fixed code in util-device and util-runmodes.

--- src/util-device.h
+++ src/util-device.h
@@ -35,9 +35,9 @@ typedef struct LiveDevice_ {
 int LiveRegisterDevice(const char *dev);
 int LiveGetDeviceCount(void);
 char *LiveGetDeviceName(int number);
-void LiveSafeDeviceName(const char *devname, char *newdevname);
+int LiveSafeDeviceName(const char *devname, char *newdevname, size_t destlen);
 LiveDevice *LiveGetDevice(const char *dev);
-int LiveBuildDeviceList(const char * base);
+int LiveBuildDeviceList(const char *base);
 void LiveDeviceHasNoStats(void);
 int LiveDeviceListClean(void);
 int LiveBuildDeviceListCustom(const char *base, const char *itemname);
