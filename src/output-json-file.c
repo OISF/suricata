2d25fb48edc020da016e558f2a3282976a7067ac
Eric Leblond <eric@regit.org>
2015-04-30 15:24:52 +0200

    file-json: log 'email' information
    Log information coming from email/MIME decoding in the message.

--- src/output-json-file.c
+++ src/output-json-file.c
@@ -51,13 +51,14 @@
 #include "util-buffer.h"
 #include "util-byte.h"
 
+#include "log-file.h"
+#include "util-logopenfile.h"
+
 #include "output.h"
 #include "output-json.h"
 #include "output-json-http.h"
 #include "output-json-smtp.h"
-
-#include "log-file.h"
-#include "util-logopenfile.h"
+#include "output-json-email-common.h"
 
 #include "app-layer-htp.h"
 #include "util-memcmp.h"
@@ -101,6 +102,9 @@ static void FileWriteJsonRecord(JsonFileLogThread *aft, const Packet *p, const F
             hjs = JsonSMTPAddMetadata(p->flow);
             if (hjs)
                 json_object_set_new(js, "smtp", hjs);
+            hjs = JsonEmailAddMetadata(p->flow);
+            if (hjs)
+                json_object_set_new(js, "email", hjs);
             break;
     }
 
@@ -164,6 +168,7 @@ static void FileWriteJsonRecord(JsonFileLogThread *aft, const Packet *p, const F
             break;
         case ALPROTO_SMTP:
             json_object_del(js, "smtp");
+            json_object_del(js, "email");
             break;
     }
 
