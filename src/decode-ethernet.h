586db57632a25d520d8b9f1e90de5951207fa43e
Victor Julien <victor@inliniac.net>
2015-05-20 23:01:22 +0200

    decode: add ERSPANv1 decoder
    Only allow v1 to be parsed as thats what is tested.
    
    Take vlan_id from the ERSPAN layer.

--- src/decode-ethernet.h
+++ src/decode-ethernet.h
@@ -40,6 +40,7 @@
 #define ETHERNET_TYPE_8021Q           0x8100
 #define ETHERNET_TYPE_LOOP            0x9000
 #define ETHERNET_TYPE_8021QINQ        0x9100
+#define ETHERNET_TYPE_ERSPAN          0x88BE
 
 typedef struct EthernetHdr_ {
     uint8_t eth_dst[6];
