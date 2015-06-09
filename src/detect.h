7abaccd10ba7ccf7ffca6af8d07e9a02c9e8fb74
Victor Julien <victor@inliniac.net>
2015-06-09 15:31:32 +0200

    dns: generic request/response detect lists

--- src/detect.h
+++ src/detect.h
@@ -120,7 +120,10 @@ enum DetectSigmatchListEnum {
 
     DETECT_SM_LIST_FILEMATCH,
 
-    DETECT_SM_LIST_DNSQUERYNAME_MATCH,
+    DETECT_SM_LIST_DNSREQUEST_MATCH,    /**< per DNS query tx match list */
+    DETECT_SM_LIST_DNSRESPONSE_MATCH,   /**< per DNS response tx match list */
+    DETECT_SM_LIST_DNSQUERYNAME_MATCH,  /**< per query in a tx list */
+
     DETECT_SM_LIST_MODBUS_MATCH,
 
     /* list for post match actions: flowbit set, flowint increment, etc */
