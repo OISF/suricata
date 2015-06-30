871fab6f7e47dc77d69b649b362fe52d76dec41e
Giuseppe Longo <giuseppelng@gmail.com>
2015-06-30 11:33:58 +0200

    mixed-mode: set packet mode
    Since in the mixed-mode two runmodes are running,
    a packet can come from nflog or nfq.
    This sets the mode, IPS or IDS, to a packet.

--- src/detect.c
+++ src/detect.c
@@ -634,7 +634,7 @@ static StreamMsg *SigMatchSignaturesGetSmsg(Flow *f, Packet *p, uint8_t flags)
         TcpSession *ssn = (TcpSession *)f->protoctx;
 
         /* at stream eof, or in inline mode, inspect all smsg's */
-        if ((flags & STREAM_EOF) || StreamTcpInlineMode()) {
+        if ((flags & STREAM_EOF) || StreamTcpInlineMode(p)) {
             if (p->flowflags & FLOW_PKT_TOSERVER) {
                 smsg = ssn->toserver_smsg_head;
                 /* deref from the ssn */
@@ -9554,6 +9554,7 @@ static int SigTestDropFlow03(void)
     p2->flowflags |= FLOW_PKT_TOSERVER;
     p2->flowflags |= FLOW_PKT_ESTABLISHED;
     p2->flags |= PKT_HAS_FLOW|PKT_STREAM_EST;
+    p2->pkt_mode = PKT_MODE_IPS;
     f.alproto = ALPROTO_HTTP;
 
     StreamTcpInitConfig(TRUE);
@@ -9728,6 +9729,7 @@ static int SigTestDropFlow04(void)
     p2->flowflags |= FLOW_PKT_TOSERVER;
     p2->flowflags |= FLOW_PKT_ESTABLISHED;
     p2->flags |= PKT_HAS_FLOW|PKT_STREAM_EST;
+    p2->pkt_mode = PKT_MODE_IDS;
     f.alproto = ALPROTO_HTTP;
 
     StreamTcpInitConfig(TRUE);
