58a321f857a20c5581045cd00bc8f9e58147f628
Victor Julien <victor@inliniac.net>
2015-09-21 15:01:56 +0200

    stream: improve retransmission detection
    Consider packets starting before last_ack and ending after it also
    to be retransmissions. This way we can see if they are having
    different data.

--- src/stream-tcp.c
+++ src/stream-tcp.c
@@ -760,6 +760,13 @@ static int StreamTcpPacketIsRetransmission(TcpStream *stream, Packet *p) {
     if (p->payload_len == 0)
         SCReturnInt(0);
 
+    /* retransmission of already partially ack'd data */
+    if (SEQ_LT(TCP_GET_SEQ(p), stream->last_ack) && SEQ_GT((TCP_GET_SEQ(p) + p->payload_len), stream->last_ack))
+    {
+        StreamTcpSetEvent(p, STREAM_PKT_RETRANSMISSION);
+        SCReturnInt(1);
+    }
+
     /* retransmission of already ack'd data */
     if (SEQ_LEQ((TCP_GET_SEQ(p) + p->payload_len), stream->last_ack)) {
         StreamTcpSetEvent(p, STREAM_PKT_RETRANSMISSION);
@@ -772,8 +779,8 @@ static int StreamTcpPacketIsRetransmission(TcpStream *stream, Packet *p) {
         SCReturnInt(2);
     }
 
-    SCLogDebug("seq %u payload_len %u => %u, last_ack %u", TCP_GET_SEQ(p),
-            p->payload_len, (TCP_GET_SEQ(p) + p->payload_len), stream->last_ack);
+    SCLogDebug("seq %u payload_len %u => %u, last_ack %u, next_seq %u", TCP_GET_SEQ(p),
+            p->payload_len, (TCP_GET_SEQ(p) + p->payload_len), stream->last_ack, stream->next_seq);
     SCReturnInt(0);
 }
 
