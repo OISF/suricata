35c931950c498aef9a5539ce623f2715af619afc
Victor Julien <victor@inliniac.net>
2015-10-12 18:39:57 +0200

    detect: remove unused flag

--- src/detect.c
+++ src/detect.c
@@ -3291,7 +3291,6 @@ int RulesGroupByProto(DetectEngineCtx *de_ctx)
 
             SigGroupHeadFree(sgh_ts[p]);
             sgh_ts[p] = lookup_sgh;
-            sgh_ts[p]->flags |= SIG_GROUP_HEAD_REFERENCED;
 
             de_ctx->gh_reuse++;
             ref++;
@@ -3328,7 +3327,6 @@ int RulesGroupByProto(DetectEngineCtx *de_ctx)
 
             SigGroupHeadFree(sgh_tc[p]);
             sgh_tc[p] = lookup_sgh;
-            sgh_tc[p]->flags |= SIG_GROUP_HEAD_REFERENCED;
 
             de_ctx->gh_reuse++;
             ref++;
@@ -3512,7 +3510,6 @@ static DetectPort *RulesGroupByPorts(DetectEngineCtx *de_ctx, int ipproto, uint3
             SigGroupHeadFree(iter->sh);
             iter->sh = lookup_sgh;
             iter->flags |= PORT_SIGGROUPHEAD_COPY;
-            iter->sh->flags |= SIG_GROUP_HEAD_REFERENCED;
 
             de_ctx->gh_reuse++;
             ref++;
