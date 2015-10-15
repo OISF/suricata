db50585bfa3d66125070ab9cdf91c3789f32cd96
Victor Julien <victor@inliniac.net>
2015-10-15 14:52:01 +0200

    detect: move app_mpms array to init data

--- src/detect.h
+++ src/detect.h
@@ -962,6 +962,8 @@ typedef struct SigGroupHeadInitData_ {
     uint32_t direction;     /**< set to SIG_FLAG_TOSERVER, SIG_FLAG_TOCLIENT or both */
     int whitelist;          /**< try to make this group a unique one */
 
+    MpmCtx *app_mpms[APP_MPMS_MAX];
+
     /* port ptr */
     struct DetectPort_ *port;
 } SigGroupHeadInitData;
@@ -1019,8 +1021,6 @@ typedef struct SigGroupHead_ {
 
     /** Array with sig ptrs... size is sig_cnt * sizeof(Signature *) */
     Signature **match_array;
-
-    MpmCtx *app_mpms[APP_MPMS_MAX];
 
     /* ptr to our init data we only use at... init :) */
     SigGroupHeadInitData *init;
