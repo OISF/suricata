24a0438859b588ab3ccd2014bf1a3c3cf74b9fad
Victor Julien <victor@inliniac.net>
2015-10-17 00:21:00 +0200

    TODO profiling: initial rulegroup tracking
    Per rule group tracking of checks, use of lists, mpm matches,
    post filter counts.
    
    Logs SGH id so it can be compared with the rule_group.json output.
    
    TODO json output

--- src/suricata.c
+++ src/suricata.c
@@ -2104,6 +2104,7 @@ static int PostConfLoadedSetup(SCInstance *suri)
     if (suri->run_mode != RUNMODE_UNIX_SOCKET) {
         SCProfilingRulesGlobalInit();
         SCProfilingKeywordsGlobalInit();
+        SCProfilingSghsGlobalInit();
         SCProfilingInit();
     }
 #endif /* PROFILING */
