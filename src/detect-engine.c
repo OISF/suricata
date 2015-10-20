482e92c0a4843dafcb04829cc75d6f9f619735f0
Giuseppe Longo <giuseppe@glongo.it>
2015-10-20 09:41:51 +0200

    suricata: run multiple runmodes
    This permits to run multiple runmode

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -1047,7 +1047,7 @@ static uint8_t DetectEngineCtxLoadConf(DetectEngineCtx *de_ctx)
         }
     }
 
-    if (run_mode == RUNMODE_UNITTEST) {
+    if (RunmodeGetPrimary(&runmodeslist) == RUNMODE_UNITTEST) {
         de_ctx->sgh_mpm_context = ENGINE_SGH_MPM_FACTORY_CONTEXT_FULL;
     }
 
