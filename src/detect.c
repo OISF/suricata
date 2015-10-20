ea5696812fd392af1ed680b9c9f16c890491e877
Mats Klepsland <mats.klepsland@gmail.com>
2015-10-20 14:16:13 +0200

    detect: add tls_cert_notbefore and tls_cert_notafter keywords
    Detection plugin for TLS certificate fields notBefore and notAfter.
    
    Supports equal to, less than, greater than, and range operations
    for both keywords. Dates can be represented as either ISO 8601 or
    epoch (Unix time).
    
    Examples:
    alert tls [...] tls_cert_notafter:1445852105; [...]
    alert tls [...] tls_cert_notbefore:<2015-10-22T23:59:59; [...]
    alert tls [...] tls_cert_notbefore:>2015-10-22; [...]
    alert tls [...] tls_cert_notafter:2000-10-22<>2020-05-15; [...]

--- src/detect.c
+++ src/detect.c
@@ -173,6 +173,7 @@
 #include "app-layer-smtp.h"
 #include "app-layer-template.h"
 #include "detect-tls.h"
+#include "detect-tls-cert-validity.h"
 #include "detect-tls-version.h"
 #include "detect-ssh-proto-version.h"
 #include "detect-ssh-software-version.h"
@@ -4418,6 +4419,7 @@ void SigTableSetup(void)
     DetectHttpMethodRegister();
     DetectHttpStatMsgRegister();
     DetectTlsRegister();
+    DetectTlsValidityRegister();
     DetectTlsVersionRegister();
     DetectUrilenRegister();
     DetectDetectionFilterRegister();
