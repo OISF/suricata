9fb38557cc87f18425d7fe4c1556871c94a15bb5
Victor Julien <victor@inliniac.net>
2015-11-20 18:10:50 +0100

    dns: response delta logging
    Log the response time in microseconds as 'response_time' in JSON.

--- src/app-layer-dns-udp.c
+++ src/app-layer-dns-udp.c
@@ -151,6 +151,11 @@ static int DNSUDPRequestParse(Flow *f, void *dstate,
         }
     }
 
+    if (dns_state != NULL && dns_state->curr != NULL) {
+        dns_state->curr->request_ts.tv_sec = f->lastts.tv_sec;
+        dns_state->curr->request_ts.tv_usec = f->lastts.tv_usec;
+    }
+
     SCReturnInt(1);
 bad_data:
 insufficient_data:
@@ -277,25 +282,30 @@ static int DNSUDPResponseParse(Flow *f, void *dstate,
         }
     }
 
-    /* parse rcode, e.g. "noerror" or "nxdomain" */
-    uint8_t rcode = ntohs(dns_header->flags) & 0x0F;
-    if (rcode <= DNS_RCODE_NOTZONE) {
-        SCLogDebug("rcode %u", rcode);
-        if (tx != NULL)
-            tx->rcode = rcode;
-    } else {
-        /* this is not invalid, rcodes can be user defined */
-        SCLogDebug("unexpected DNS rcode %u", rcode);
+    /* if we previously didn't have a tx, it could have been created by the
+     * above code, so lets check again */
+    if (tx == NULL) {
+        tx = DNSTransactionFindByTxId(dns_state, ntohs(dns_header->tx_id));
     }
-
-    if (ntohs(dns_header->flags) & 0x0080) {
-        SCLogDebug("recursion desired");
-        if (tx != NULL)
-            tx->recursion_desired = 1;
-    }
-
     if (tx != NULL) {
+        /* parse rcode, e.g. "noerror" or "nxdomain" */
+        uint8_t rcode = ntohs(dns_header->flags) & 0x0F;
+        if (rcode <= DNS_RCODE_NOTZONE) {
+            SCLogDebug("rcode %u", rcode);
+            tx->rcode = rcode;
+        } else {
+            /* this is not invalid, rcodes can be user defined */
+            SCLogDebug("unexpected DNS rcode %u", rcode);
+        }
+
+        if (ntohs(dns_header->flags) & 0x0080) {
+            SCLogDebug("recursion desired");
+            tx->recursion_desired = 1;
+        }
+
         tx->replied = 1;
+        tx->response_ts.tv_sec = f->lastts.tv_sec;
+        tx->response_ts.tv_usec = f->lastts.tv_usec;
     }
 
     SCReturnInt(1);
