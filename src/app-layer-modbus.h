17b289b3ed635f1e8c0dfe9da79e412a0a0bf786
Eric Leblond <eric@regit.org>
2015-11-25 15:34:21 +0100

    app-layer-modbus: add function code conversion
    Add a function to convert function code to name.

--- src/app-layer-modbus.h
+++ src/app-layer-modbus.h
@@ -125,6 +125,7 @@ typedef struct ModbusState_ {
 
 void RegisterModbusParsers(void);
 void ModbusParserRegisterTests(void);
+const char * ModbusGetFunctionName(uint8_t function);
 
 /* Modbus Application Data Unit (ADU) length range. */
 #define MODBUS_MIN_ADU_LEN  2
