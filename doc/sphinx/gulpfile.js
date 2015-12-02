8422656885b038397251ff66d4aba1dba28eb5ac
Jason Ish <ish@unx.ca>
2015-12-02 15:23:52 -0600

    doc: helper tools

---
+++ doc/sphinx/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var spawn = require("child_process").spawn;
+var server = require("gulp-webserver")
+
+gulp.task("watch", function(cb) {
+    watch("*.rst", function() {
+	console.log("Changed.");
+	spawn("make", ["html"], {
+	    stdio: "inherit",
+	    stderr: "inherit"
+	});
+    });
+});
+
+gulp.task("server", function(cb) {
+    gulp.src(".")
+	.pipe(server({
+	    livereload: {
+		enable: true,
+		filter: function(filename) {
+		    if (filename.match(/^_build/)) {
+			return true;
+		    }
+		    return false;
+		}
+	    },
+	    directoryListing: true
+	}));
+});
+
+gulp.task("serve", ["watch", "server"])
