10106de4b6aae8df3bb01b789703b03787174de9
Jason Ish <ish@unx.ca>
2015-12-10 11:59:48 -0600

    DNP3: Lua detect support.
    Adds support for access the DNP3 transaction in Lua rules.

--- src/output.c
+++ src/output.c
@@ -69,6 +69,7 @@
 #include "output-json.h"
 #include "output-json-template.h"
 #include "output-lua.h"
+#include "output-json-dnp3.h"
 
 typedef struct RootLogger_ {
     ThreadInitFunc ThreadInit;
@@ -1086,6 +1087,9 @@ void OutputRegisterLoggers(void)
     JsonNetFlowLogRegister();
     /* json stats */
     JsonStatsLogRegister();
+
+    /* DNP3. */
+    JsonDNP3LogRegister();
 
     /* Template JSON logger. */
     JsonTemplateLogRegister();
