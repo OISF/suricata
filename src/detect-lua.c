c5d108f9824c7019eda7ec15d25c24e804f9790a
Jason Ish <ish@unx.ca>
2015-12-10 11:59:48 -0600

    DNP3: Lua detect support.
    Adds support for access the DNP3 transaction in Lua rules.

--- src/detect-lua.c
+++ src/detect-lua.c
@@ -168,6 +168,8 @@ void DetectLuaRegister(void)
 
 #define DATATYPE_SSH                        (1<<19)
 
+#define DATATYPE_DNP3                       (1<<20)
+
 #ifdef HAVE_LUAJIT
 static void *LuaStatePoolAlloc(void)
 {
@@ -1027,6 +1029,12 @@ static int DetectLuaSetupPrime(DetectEngineCtx *de_ctx, DetectLuaData *ld)
 
             ld->flags |= DATATYPE_SSH;
 
+        } else if (strncmp(k, "dnp3", 4) == 0 && strcmp(v, "true") == 0) {
+
+            ld->alproto = ALPROTO_DNP3;
+
+            ld->flags |= DATATYPE_DNP3;
+
         } else {
             SCLogError(SC_ERR_LUA_ERROR, "unsupported data type %s", k);
             goto error;
@@ -1126,6 +1134,8 @@ static int DetectLuaSetup (DetectEngineCtx *de_ctx, Signature *s, char *str)
         SigMatchAppendSMToList(s, sm, DETECT_SM_LIST_AMATCH);
     } else if (luajit->alproto == ALPROTO_SSH) {
         SigMatchAppendSMToList(s, sm, DETECT_SM_LIST_AMATCH);
+    } else if (luajit->alproto == ALPROTO_DNP3) {
+        SigMatchAppendSMToList(s, sm, DETECT_SM_LIST_DNP3_LUA_MATCH);
     } else {
         SCLogError(SC_ERR_LUA_ERROR, "luajit can't be used with protocol %s",
                    AppLayerGetProtoName(luajit->alproto));
