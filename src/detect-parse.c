3c0954cb8c3e253cd8d0fab910b3347198167ce3
Jason Ish <ish@unx.ca>
2016-01-26 23:22:27 -0600

    detect: denote the max detection list; fix issue 1674.
    Denotes the max detection list so that rule validation can
    allow post-detection lists to come after base64_data, but
    disallow detection lists to come after it.

--- src/detect-parse.c
+++ src/detect-parse.c
@@ -1322,7 +1322,7 @@ int SigValidate(DetectEngineCtx *de_ctx, Signature *s)
     if (s->sm_lists[DETECT_SM_LIST_BASE64_DATA] != NULL) {
         int list;
         uint16_t idx = s->sm_lists[DETECT_SM_LIST_BASE64_DATA]->idx;
-        for (list = 0; list < DETECT_SM_LIST_MAX; list++) {
+        for (list = 0; list < DETECT_SM_LIST_DETECT_MAX; list++) {
             if (list != DETECT_SM_LIST_BASE64_DATA &&
                 s->sm_lists[list] != NULL) {
                 if (s->sm_lists[list]->idx > idx) {
