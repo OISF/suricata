398993c622fc268eba939e8438fcd77625700d7b
Jason Ish <ish@unx.ca>
2016-02-26 17:30:15 -0600

    doc: minimal man page: suricata.1

--- doc/userguide/conf.py
+++ doc/userguide/conf.py
@@ -259,8 +259,9 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'suricata', u'Suricata Documentation',
-     [author], 1)
+    # (master_doc, 'suricata', u'Suricata Documentation',
+    #  [author], 1)
+    ("manpages/suricata", "suricata", "Suricata", [], 1)
 ]
 
 # If true, show URL addresses after external links.
