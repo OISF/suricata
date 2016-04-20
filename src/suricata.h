9376104d66969ff4eba3287df1a8e449e70e95b8
Giuseppe Longo <glongo@stamus-networks.com>
2016-04-20 18:52:52 +0200

    mixed-mode: start runmodes

--- src/suricata.h
+++ src/suricata.h
@@ -68,6 +68,7 @@
 #include "suricata-common.h"
 #include "packet-queue.h"
 #include "data-queue.h"
+#include "runmodes.h"
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
@@ -91,6 +92,8 @@
 #define SURICATA_KILL    (1 << 1)   /**< shut down asap, discarding outstanding
                                      packets. */
 #define SURICATA_DONE    (1 << 2)   /**< packets capture ended */
+
+#define RUNMODES_MAX     2
 
 /* Engine stage/status*/
 enum {
@@ -128,8 +131,13 @@ PacketQueue trans_q[256];
 
 SCDQDataQueue data_queues[256];
 
+typedef struct RunModesList_ {
+    enum RunModes run_mode[RUNMODES_MAX];
+    int runmodes_cnt;
+} RunModesList;
+
 typedef struct SCInstance_ {
-    enum RunModes run_mode;
+    RunModesList runmodeslist;
 
     char pcap_dev[128];
     char *sig_file;
@@ -184,11 +192,14 @@ void EngineStop(void);
 void EngineKill(void);
 void EngineDone(void);
 
+int RunmodeGetPrimary(const RunModesList *runmodes);
+int RunmodeGetSecondary(const RunModesList *runmodes);
+int RunmodeGetNumber(const RunModesList *runmodes);
 int RunmodeIsUnittests(void);
-int RunmodeGetCurrent(void);
+int RunmodeGetCurrent(const RunModesList *runmodes, int index);
 int IsRuleReloadSet(int quiet);
 
-extern int run_mode;
+extern RunModesList runmodeslist;
 
 #endif /* __SURICATA_H__ */
 
