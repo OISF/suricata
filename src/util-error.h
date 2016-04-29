f2f4788330c613d0423c849262d0606ee0d0ffc5
Duarte Silva <development@serializing.me>
2016-04-29 21:51:12 +0200

    Added support for SHA-1 file hashing

--- src/util-error.h
+++ src/util-error.h
@@ -315,6 +315,7 @@ typedef enum {
     SC_ERR_SMTP_LOG_GENERIC,
     SC_ERR_SSH_LOG_GENERIC,
     SC_ERR_INVALID_HASH,
+    SC_ERR_NO_SHA1_SUPPORT,
 } SCError;
 
 const char *SCErrorToString(SCError);
