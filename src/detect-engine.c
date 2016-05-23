609f10e7839dcd799e03c252e2464c7373589df6
Justin Viiret <justin.viiret@intel.com>
2016-05-23 14:22:30 +1000

    detect-engine: log MPM/SPM matchers being used

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -839,7 +839,9 @@ static DetectEngineCtx *DetectEngineCtxInitReal(int minimal, const char *prefix)
     }
 
     de_ctx->mpm_matcher = PatternMatchDefaultMatcher();
+    SCLogInfo("using MPM matcher: %s", mpm_table[de_ctx->mpm_matcher].name);
     de_ctx->spm_matcher = SinglePatternMatchDefaultMatcher();
+    SCLogInfo("using SPM matcher: %s", spm_table[de_ctx->spm_matcher].name);
 
     de_ctx->spm_global_thread_ctx = SpmInitGlobalThreadCtx(de_ctx->spm_matcher);
     if (de_ctx->spm_global_thread_ctx == NULL) {
