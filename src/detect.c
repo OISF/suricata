305b1b90fd11b8684dbf85c44da5171abf6c3035
Victor Julien <victor@inliniac.net>
2016-07-14 12:11:02 +0200

    detect: minor cleanup

--- src/detect.c
+++ src/detect.c
@@ -695,18 +695,13 @@ end:
 
 static inline void DetectPrefilterMergeSort(DetectEngineCtx *de_ctx,
                                             DetectEngineThreadCtx *det_ctx)
-//                                            SigGroupHead *sgh)
 {
     SigIntId mpm, nonmpm;
     det_ctx->match_array_cnt = 0;
     SigIntId *mpm_ptr = det_ctx->pmq.rule_id_array;
     SigIntId *nonmpm_ptr = det_ctx->non_mpm_id_array;
-    //SigIntId *nonmpm_ptr = sgh->non_mpm_id_array;
     uint32_t m_cnt = det_ctx->pmq.rule_id_array_cnt;
-    //uint32_t n_cnt = sgh->non_mpm_id_cnt;
     uint32_t n_cnt = det_ctx->non_mpm_id_cnt;
-    SCLogDebug("PMQ rule id array count %d", det_ctx->pmq.rule_id_array_cnt);
-//    SCLogDebug("SGH non-MPM id count %d", sgh->non_mpm_id_cnt);
     SigIntId *final_ptr;
     uint32_t final_cnt;
     SigIntId id;
@@ -714,6 +709,8 @@ static inline void DetectPrefilterMergeSort(DetectEngineCtx *de_ctx,
     Signature **sig_array = de_ctx->sig_array;
     Signature **match_array = det_ctx->match_array;
     Signature *s;
+
+    SCLogDebug("PMQ rule id array count %d", det_ctx->pmq.rule_id_array_cnt);
 
     /* Load first values. */
     if (likely(m_cnt)) {
