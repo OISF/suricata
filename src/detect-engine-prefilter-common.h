9762e66e30f634b2e876c558b87a382f99664421
Victor Julien <victor@inliniac.net>
2016-08-22 15:21:19 +0200

    prefilter: intro common engine for u8 matches

--- src/detect-engine-prefilter-common.h
+++ src/detect-engine-prefilter-common.h
@@ -33,9 +33,32 @@ typedef struct PrefilterPacketHeaderCtx_ {
     SigIntId *sigs_array;
 } PrefilterPacketHeaderCtx;
 
+typedef struct SigsArray_ {
+    SigIntId *sigs;
+    uint32_t cnt;
+    uint32_t offset; // used to track assign pos
+} SigsArray;
+
+typedef struct PrefilterPacketU8HashCtx_ {
+    SigsArray *array[256];
+} PrefilterPacketU8HashCtx;
+
+#define PREFILTER_U8HASH_MODE_EQ    0
+#define PREFILTER_U8HASH_MODE_LT    1
+#define PREFILTER_U8HASH_MODE_GT    2
+#define PREFILTER_U8HASH_MODE_RA    3
+
 int PrefilterSetupPacketHeader(SigGroupHead *sgh, int sm_type,
         void (*Set)(PrefilterPacketHeaderValue *v, void *),
         _Bool (*Compare)(PrefilterPacketHeaderValue v, void *),
-        void (*Match)(DetectEngineThreadCtx *det_ctx, Packet *p, const void *pectx));
+        void (*Match)(DetectEngineThreadCtx *det_ctx,
+            Packet *p, const void *pectx));
+
+int PrefilterSetupPacketHeaderU8Hash(SigGroupHead *sgh, int sm_type,
+        void (*Set)(PrefilterPacketHeaderValue *v, void *),
+        _Bool (*Compare)(PrefilterPacketHeaderValue v, void *),
+        void (*Match)(DetectEngineThreadCtx *det_ctx,
+            Packet *p, const void *pectx));
 
 #endif /* __DETECT_ENGINE_PREFILTER_COMMON_H__ */
+
