317985909efff1f3dce3537be5a1b9d8347fa612
Victor Julien <victor@inliniac.net>
2016-08-22 16:29:48 +0200

    detect-icode: implement as u8 hash prefilter

--- src/detect-icode.h
+++ src/detect-icode.h
@@ -26,18 +26,6 @@
 #ifndef __DETECT_ICODE_H__
 #define __DETECT_ICODE_H__
 
-#define DETECT_ICODE_EQ   0   /**< "equal" operator */
-#define DETECT_ICODE_LT   1   /**< "less than" operator */
-#define DETECT_ICODE_GT   2   /**< "greater than" operator */
-#define DETECT_ICODE_RN   3   /**< "range" operator */
-
-typedef struct DetectICodeData_ {
-    uint8_t code1;
-    uint8_t code2;
-
-    uint8_t mode;
-}DetectICodeData;
-
 /* prototypes */
 void DetectICodeRegister(void);
 
