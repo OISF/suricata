d5e5059053dc978c4d111cc55a43d86f52ded047
Victor Julien <victor@inliniac.net>
2016-08-22 15:21:55 +0200

    detect-itype: implement as u8 hash prefilter

--- src/detect-itype.h
+++ src/detect-itype.h
@@ -24,18 +24,6 @@
 #ifndef __DETECT_ITYPE_H__
 #define __DETECT_ITYPE_H__
 
-#define DETECT_ITYPE_EQ   0   /**< "equal" operator */
-#define DETECT_ITYPE_LT   1   /**< "less than" operator */
-#define DETECT_ITYPE_GT   2   /**< "greater than" operator */
-#define DETECT_ITYPE_RN   3   /**< "range" operator */
-
-typedef struct DetectITypeData_ {
-    uint8_t type1;
-    uint8_t type2;
-
-    uint8_t mode;
-} DetectITypeData;
-
 /* prototypes */
 void DetectITypeRegister(void);
 
