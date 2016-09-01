b81ea0d7dbcfd3c997b6d02806e79214fb45244d
Victor Julien <victor@inliniac.net>
2016-09-01 20:34:26 +0200

    eve: reduce flow_id to 51 bits
    Evebox & ELK couldn't handle the large integers. It looks like (partly)
    a javascript limitation that doesn't treat 64bit ints as real ints.

--- src/output-json.c
+++ src/output-json.c
@@ -167,6 +167,9 @@ void CreateJSONFlowId(json_t *js, const Flow *f)
     if (f == NULL)
         return;
     int64_t flow_id = FlowGetId(f);
+    /* reduce to 51 bits as Javascript and even JSON often seem to
+     * max out there. */
+    flow_id &= 0x7ffffffffffffLL;
     json_object_set_new(js, "flow_id", json_integer(flow_id));
 }
 
