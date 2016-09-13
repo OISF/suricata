77655f833bc09589c4f17534569c70cf1626bbf7
Tom DeCanio <decanio.tom@gmail.com>
2016-09-13 03:18:28 -0700

    app-layer-imap: Add IMAP support.
    Add IMAP application layer parser and eve-log JSON logger.

--- src/output.c
+++ src/output.c
@@ -61,6 +61,7 @@
 #include "log-pcap.h"
 #include "log-file.h"
 #include "output-json-file.h"
+#include "output-json-imap.h"
 #include "output-json-smtp.h"
 #include "output-json-stats.h"
 #include "log-filestore.h"
@@ -1056,6 +1057,7 @@ void OutputRegisterLoggers(void)
     /* json log */
     OutputJsonRegister();
     /* email logs */
+    JsonImapLogRegister();
     JsonSmtpLogRegister();
     /* http log */
     LogHttpLogRegister();
