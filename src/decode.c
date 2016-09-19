c54d49dd7ae8c8aa3c0e715b8375677dce7bec03
Eric Leblond <eric@regit.org>
2016-09-19 16:21:55 +0200

    decode: implement bypass function
    Call the packet bypass callback if necessary and update the flow
    state. In case of failure we switch to local bypassed state and set
    capture bypassed state if the callback is successful.

--- src/decode.c
+++ src/decode.c
@@ -384,8 +384,19 @@ void PacketDefragPktSetupParent(Packet *parent)
 
 void PacketBypassCallback(Packet *p)
 {
-    if (p->BypassPacketsFlow) {
-        p->BypassPacketsFlow(p);
+    /* Don't try to bypass if flow is already out or
+     * if we have failed to do it once */
+    int state = SC_ATOMIC_GET(p->flow->flow_state);
+    if ((state == FLOW_STATE_LOCAL_BYPASSED) ||
+           (state == FLOW_STATE_CAPTURE_BYPASSED)) {
+        return;
+    }
+
+    if (p->BypassPacketsFlow && p->BypassPacketsFlow(p)) {
+        /* only set bypassed state if succesful */
+        FlowUpdateState(p->flow, FLOW_STATE_CAPTURE_BYPASSED);
+    } else {
+        FlowUpdateState(p->flow, FLOW_STATE_LOCAL_BYPASSED);
     }
 }
 
