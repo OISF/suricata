5cd84503eb48216d30c1fbfbcf6f21640c692cdb
kwong <kwong@solananetworks.com>
2016-09-28 17:30:37 -0400

    Adding SCADA EtherNet/IP and CIP protocol support
    1.  The enip-feature-1495-v13 branch contains support for the ENIP/CIP
    Industrial protocol
    
    2. This is an app layer implementation which uses the "enip" protocol
    and "cip_service" and "enip_command" keywords
    
    3.  Rebased master to clean out commits done on master and then
    merged to latest code
    
    4.  Addresses failed PR's
    https://github.com/inliniac/suricata/pull/1810 and
    https://github.com/inliniac/suricata/pull/1819 and
    https://github.com/inliniac/suricata/pull/1827 and
    https://github.com/inliniac/suricata/pull/2009 and
    https://github.com/inliniac/suricata/pull/2011 and
    https://github.com/inliniac/suricata/pull/2012 and
    https://github.com/inliniac/suricata/pull/2019 and
    https://github.com/inliniac/suricata/pull/2030 and
    https://github.com/inliniac/suricata/pull/2068 and
    https://github.com/inliniac/suricata/pull/2107
    
    5.  As requested, added AFL entry points

--- src/app-layer-detect-proto.c
+++ src/app-layer-detect-proto.c
@@ -691,6 +691,8 @@ void AppLayerProtoDetectPrintProbingParsers(AppLayerProtoDetectProbingParser *pp
                         printf("            alproto: ALPROTO_DNS\n");
                     else if (pp_pe->alproto == ALPROTO_MODBUS)
                         printf("            alproto: ALPROTO_MODBUS\n");
+                    else if (pp_pe->alproto == ALPROTO_ENIP)
+                        printf("            alproto: ALPROTO_ENIP\n");
                     else if (pp_pe->alproto == ALPROTO_TEMPLATE)
                         printf("            alproto: ALPROTO_TEMPLATE\n");
                     else
@@ -744,6 +746,8 @@ void AppLayerProtoDetectPrintProbingParsers(AppLayerProtoDetectProbingParser *pp
                     printf("            alproto: ALPROTO_DNS\n");
                 else if (pp_pe->alproto == ALPROTO_MODBUS)
                     printf("            alproto: ALPROTO_MODBUS\n");
+                else if (pp_pe->alproto == ALPROTO_ENIP)
+                    printf("            alproto: ALPROTO_ENIP\n");
                 else if (pp_pe->alproto == ALPROTO_TEMPLATE)
                     printf("            alproto: ALPROTO_TEMPLATE\n");
                 else
