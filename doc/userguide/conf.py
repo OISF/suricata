dbde356053f5b994f29577db3c7b1475cd9d3346
Jason Ish <ish@unx.ca>
2016-10-04 08:56:16 -0600

    doc: exclude docs in partials/ from reference errors
    These docs are already included with the include statement,
    but older versions of Sphinx still complain that they
    are not in a table of contents.

--- doc/userguide/conf.py
+++ doc/userguide/conf.py
@@ -87,7 +87,12 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = [
+    '_build',
+
+    # Documents that are included, rather than in a TOC.
+    'partials',
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
