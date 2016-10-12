4008783a44e069e696015f138f2b646a23e21e17
Maxim Uvarov <maxim.uvarov@linaro.org>
2016-10-12 21:03:22 +0300

    ODP initial support
    This patch is initial support of OpenDataPlane API
    (http://opendataplane.org). Supported Long Term Stable
    ODP release (tag v1.11.0.0_monarch).
    
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ src/source-odp.h
@@ -0,0 +1,41 @@
+/* Copyright (C) 2016 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Maxim Uvarov <maxim.uvarov@linaro.org>, Linaro
+ *
+ * OpenDataPlane ingress packet support
+ */
+
+#ifndef __SOURCE_ODP_H__
+#define __SOURCE_ODP_H__
+
+#ifdef HAVE_ODP
+#include <odp_api.h>
+
+typedef struct ODPIfaceConfig_
+{
+    SC_ATOMIC_DECLARE(unsigned int, threads);
+} ODPIfaceConfig;
+
+void TmModuleReceiveODPRegister(void);
+void TmModuleDecodeODPRegister(void);
+
+#endif /* HAVE_ODP */
+#endif /* __SOURCE_ODP_H__ */
--- src/util-error.h
+++ src/util-error.h
@@ -320,6 +320,7 @@ typedef enum {
     SC_ERR_NO_SHA1_SUPPORT,
     SC_ERR_NO_SHA256_SUPPORT,
     SC_ERR_ENIP_CONFIG,
+    SC_ERR_NO_ODP, /**< No OpenDataPlane support. */
 } SCError;
 
 const char *SCErrorToString(SCError);
