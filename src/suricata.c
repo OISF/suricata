59303d1fbb02e33e839145a32cacb6d7cf81c546
Victor Julien <victor@inliniac.net>
2016-10-16 21:12:32 +0200

    threshold: fix and redo tests

--- src/suricata.c
+++ src/suricata.c
@@ -2386,7 +2386,6 @@ static int LoadSignatures(DetectEngineCtx *de_ctx, SCInstance *suri)
             return TM_ECODE_FAILED;
     }
 
-    SCThresholdConfInitContext(de_ctx, NULL);
     return TM_ECODE_OK;
 }
 
