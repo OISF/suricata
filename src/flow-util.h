8c5962590b5054ae9397b15080deaf14b622eeba
Mats Klepsland <mats.klepsland@gmail.com>
2017-01-31 14:34:30 +0100

    app-layer: support changing flow alproto
    Support changing the application level protocol for a flow. This is
    needed by STARTTLS and HTTP CONNECT to switch from the original
    alproto to tls.
    
    This commit allows a flag to be set 'FLOW_CHANGE_PROTO', which
    triggers a new protocol detection on the next packet for a flow.

--- src/flow-util.h
+++ src/flow-util.h
@@ -55,6 +55,7 @@
         (f)->alproto = 0; \
         (f)->alproto_ts = 0; \
         (f)->alproto_tc = 0; \
+        (f)->alproto_orig = 0; \
         (f)->de_ctx_version = 0; \
         (f)->thread_id = 0; \
         (f)->detect_alversion[0] = 0; \
@@ -97,6 +98,7 @@
         (f)->alproto = 0; \
         (f)->alproto_ts = 0; \
         (f)->alproto_tc = 0; \
+        (f)->alproto_orig = 0; \
         (f)->de_ctx_version = 0; \
         (f)->thread_id = 0; \
         (f)->detect_alversion[0] = 0; \
