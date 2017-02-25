7c56c9ada0a9db67c6ca0fc8736a1cc46cbf52d5
Victor Julien <victor@inliniac.net>
2017-02-25 13:33:40 +0100

    stream: allow raw reassembly catch up
    If raw reassembly falls behind, for example because no raw mpm is
    active, then we need to sync up to the app progress if that is
    available, or to the generic tcp tracking otherwise.

--- src/detect.c
+++ src/detect.c
@@ -1432,8 +1432,7 @@ end:
         }
 
         /* update inspected tracker for raw reassembly */
-        if (p->proto == IPPROTO_TCP && pflow->protoctx != NULL &&
-            det_ctx->raw_stream_progress > 0)
+        if (p->proto == IPPROTO_TCP && pflow->protoctx != NULL)
         {
             StreamReassembleRawUpdateProgress(pflow->protoctx, p,
                     det_ctx->raw_stream_progress);
