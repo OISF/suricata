772a83cfd5f175ed937f4a250538d1fe0f7efce2
Phil Young <py@napatech.com>
2017-07-17 10:59:00 -0400

    napatech: Implementation of packet counters
    added util-napatech module which contains implementation threads
    for processing statistics.  And modified source-napatech and
    runmode-napatech to instantiate the threads.
    
    napatech: Implementation of packet counters
    
    napatech: implementation of statistics counters
    
    napatech: Implementation of packet counters.
    
    napatech: added util-napatech module
    
    napatech: added utils-napatech module.
    
    added include declaration and napatech specific structure when HAVE_NAPATECH
    is defined.
    
    Added util-napatech module to project.

--- src/runmode-napatech.h
+++ src/runmode-napatech.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Open Information Security Foundation
+/* Copyright (C) 2012-2017 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -22,16 +22,24 @@
  *  \author Matt Keeler <mk@npulsetech.com>
  */
 
+
 #ifndef __RUNMODE_NAPATECH_H__
 #define __RUNMODE_NAPATECH_H__
 
 #ifdef HAVE_NAPATECH
+#include "util-napatech.h"
 #include <nt.h>
+
+
 #endif
 
 int RunModeNapatechAutoFp(void);
 int RunModeNapatechWorkers(void);
 void RunModeNapatechRegister(void);
 const char *RunModeNapatechGetDefaultMode(void);
+
+uint16_t GetNumConfiguredStreams(void);
+
+
 
 #endif /* __RUNMODE_NAPATECH_H__ */
