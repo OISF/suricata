7170d0fd25b481acb0cdf61942fe6ca01b62876e
Eric Leblond <eric@regit.org>
2017-09-17 10:50:32 +0200

    detect-ftpcommand: match on ftp operation
    This keyword mathes on ftp operation STOR and RETR. It will allow
    rules writer to select if the alert has to be on a put or a fetch
    operation.
    
    It is now possible to write a signature like:
    
      alert ftp-data any any -> any any (msg:"FTP data get firwmare"; ftpcommand:retr; sid:2; rev:1;)
    
    to alert when a file is retrieved from a FTP server.

--- src/detect.c
+++ src/detect.c
@@ -174,6 +174,7 @@
 #include "detect-geoip.h"
 #include "detect-app-layer-protocol.h"
 #include "detect-template.h"
+#include "detect-ftpcommand.h"
 #include "detect-target.h"
 #include "detect-template-buffer.h"
 #include "detect-bypass.h"
@@ -3859,6 +3860,7 @@ void SigTableSetup(void)
     DetectBase64DecodeRegister();
     DetectBase64DataRegister();
     DetectTemplateRegister();
+    DetectFtpcommandRegister();
     DetectTargetRegister();
     DetectTemplateBufferRegister();
     DetectBypassRegister();
