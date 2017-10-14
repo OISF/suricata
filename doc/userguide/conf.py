6860b5aea6f532cc3ee507373b5631e030f03fcd
Ralph Broenink <ralph@ralphbroenink.net>
2017-10-14 11:17:19 +0200

    doc: Add suricata.css to allow for some custom styling

--- doc/userguide/conf.py
+++ doc/userguide/conf.py
@@ -134,6 +134,16 @@ if not on_rtd:
         html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
     except:
         html_theme = 'default'
+    def setup(app):
+        app.add_stylesheet('css/suricata.css')
+else:
+    html_context = {
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/css/suricata.css',
+        ],
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
