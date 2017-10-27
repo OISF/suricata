e5a704d96ea9a65a92b430d72fcb411984f8fc58
Pierre Chifflier <chifflier@wzdftpd.net>
2017-10-27 13:10:04 +0200

    Add a common interface to declare and register all parsers
    Add a common structure definition to contain all required elements
    required for registering a parser.
    This also reduces the risk of incorrectly registering a parser: the
    compiler will type-check functions.
    
    The registration function allows factorization of the code. It can be
    used to register parsers, but is not mandatory.
    
    If extra registration code (for functions not in the structure)
    it is still possible by calling the C functions after the registration.

---
+++ src/app-layer-register.h
@@ -0,0 +1,73 @@
+/* Copyright (C) 2017 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Pierre Chifflier <chifflier@wzdftpd.net>
+ */
+
+#ifndef __APP_LAYER_REGISTER_H__
+#define __APP_LAYER_REGISTER_H__
+
+typedef struct AppLayerParser {
+    const char *Name;
+    const char *DefaultPort;
+    int IpProto;
+    const char *ProtoName;
+
+    ProbingParserFPtr ProbeTS;
+    ProbingParserFPtr ProbeTC;
+
+    uint16_t MinDepth;
+    uint16_t MaxDepth;
+
+    void *(*StateAlloc)(void);
+    void (*StateFree)(void *);
+
+    ParserFPtr ParseTS;
+    ParserFPtr ParseTC;
+
+    uint64_t (*StateGetTxCnt)(void *alstate);
+    void *(*StateGetTx)(void *alstate, uint64_t tx_id);
+    void (*StateTransactionFree)(void *, uint64_t);
+    int (*StateGetProgressCompletionStatus)(uint8_t direction);
+    int (*StateGetProgress)(void *alstate, uint8_t direction);
+    int (*StateGetTxLogged)(void *alstate, void *tx, uint32_t logger);
+    void (*StateSetTxLogged)(void *alstate, void *tx, uint32_t logger);
+
+    DetectEngineState *(*GetTxDetectState)(void *tx);
+    int (*SetTxDetectState)(void *alstate, void *tx, DetectEngineState *);
+    int (*StateHasTxDetectState)(void *alstate);
+
+    int (*StateHasEvents)(void *);
+    AppLayerDecoderEvents *(*StateGetEvents)(void *, uint64_t);
+    int (*StateGetEventInfo)(const char *event_name,
+                             int *event_id, AppLayerEventType *event_type);
+
+    void *(*LocalStorageAlloc)(void);
+    void (*LocalStorageFree)(void *);
+
+    uint64_t (*GetTxMpmIDs)(void *tx);
+    int (*SetTxMpmIDs)(void *tx, uint64_t);
+
+    FileContainer *(*StateGetFiles)(void *, uint8_t);
+} AppLayerParser;
+
+AppProto AppLayerRegisterParser(const struct AppLayerParser *parser);
+
+#endif /* __APP_LAYER_REGISTER_H__ */
