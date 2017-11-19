a6d4a0323054028968ab4d032bcf6a4ac4b55f5d
Eric Leblond <eric@regit.org>
2017-11-19 20:21:08 +0100

    detect-ftpdata: match on ftp-data operation
    This keyword mathes on ftp operation STOR and RETR. It will allow
    rules writer to select if the alert has to be on a put or a fetch
    operation.
    
    It is now possible to write a signature like:
    
      alert ftp-data any any -> any any (msg:"FTP data get firwmare"; ftdata_command:retr; sid:2; rev:1;)
    
    to alert when a file is retrieved from a FTP server.

---
+++ src/detect-ftpdata.h
@@ -0,0 +1,43 @@
+/* Copyright (C) 2017 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Eric Leblond <eric@regit.org>
+ */
+
+#ifndef __DETECT_FTPDATA_H__
+#define __DETECT_FTPDATA_H__
+
+#include "detect-engine.h"
+#include "app-layer-ftp.h"
+
+/** Per keyword data. This is set up by the DetectFtpcommandSetup() function.
+ *  Each signature will have an instance of DetectFtpcommandData per occurence
+ *  of the keyword.
+ *  The structure should be considered static/readonly after initialization.
+ */
+typedef struct DetectFtpdataData_ {
+    FtpRequestCommand command;
+} DetectFtpdataData;
+
+/** \brief registers the keyword into the engine. Called from
+ *         detect.c::SigTableSetup() */
+void DetectFtpdataRegister(void);
+
+#endif /* __DETECT_FTPDATA_H__ */
--- src/detect.c
+++ src/detect.c
@@ -175,6 +175,7 @@
 #include "detect-geoip.h"
 #include "detect-app-layer-protocol.h"
 #include "detect-template.h"
+#include "detect-ftpdata.h"
 #include "detect-target.h"
 #include "detect-template-buffer.h"
 #include "detect-bypass.h"
@@ -3829,6 +3830,7 @@ void SigTableSetup(void)
     DetectBase64DecodeRegister();
     DetectBase64DataRegister();
     DetectTemplateRegister();
+    DetectFtpdataRegister();
     DetectTargetRegister();
     DetectTemplateBufferRegister();
     DetectBypassRegister();
