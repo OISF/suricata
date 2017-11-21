e60bfc78c1e5cdd58cf8bc97acf5b28cd1a98576
Victor Julien <victor@inliniac.net>
2017-11-21 17:31:36 +0100

    Open 4.1 development branch

--- src/suricata.h
+++ src/suricata.h
@@ -70,7 +70,7 @@
 
 /* the name of our binary */
 #define PROG_NAME "Suricata"
-#define PROG_VER "4.0.0-dev"
+#define PROG_VER "4.1.0-dev"
 
 /* workaround SPlint error (don't know __gnuc_va_list) */
 #ifdef S_SPLINT_S
