0c16cd01200a6ae576e5fdcd53506928eda632c5
Mats Klepsland <mats.klepsland@gmail.com>
2017-11-30 11:04:03 +0100

    app-layer-ssl: generate JA3 fingerprints
    Decode additional fields from the client hello packet and generate
    JA3 fingerprints.

---
+++ src/util-ja3.h
@@ -0,0 +1,41 @@
+/* Copyright (C) 2007-2017 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Mats Klepsland <mats.klepsland@gmail.com>
+ */
+
+#ifndef __UTIL_JA3_H__
+#define __UTIL_JA3_H__
+
+#define JA3_BUFFER_INITIAL_SIZE 128
+
+typedef struct JA3Buffer_ {
+    char *data;
+    size_t size;
+    size_t used;
+} JA3Buffer;
+
+JA3Buffer *Ja3BufferInit(void);
+void Ja3BufferFree(JA3Buffer *);
+int Ja3BufferAppendBuffer(JA3Buffer *, JA3Buffer *);
+int Ja3BufferAddValue(JA3Buffer *, uint32_t);
+char *Ja3GenerateHash(JA3Buffer *);
+
+#endif /* __UTIL_JA3_H__ */
