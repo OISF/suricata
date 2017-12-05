277c63adaf0f2332844814bfe6d9b8e2c2595b78
Danny Browning <danny.browning@protectwise.com>
2017-12-05 11:21:20 -0700

    Use gettimeofday

--- src/source-pcap-file-directory-helper.c
+++ src/source-pcap-file-directory-helper.c
@@ -26,10 +26,6 @@
 #include "source-pcap-file-directory-helper.h"
 #include "runmode-unix-socket.h"
 #include "util-mem.h"
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
 
 static void GetTime(struct timespec *tm);
 static void CopyTime(struct timespec *from, struct timespec *to);
@@ -50,17 +46,11 @@ static TmEcode PcapDirectoryDispatchForTimeRange(PcapFileDirectoryVars *pv,
 
 void GetTime(struct timespec *tm)
 {
-#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    tm->tv_sec = mts.tv_sec;
-    tm->tv_nsec = mts.tv_nsec;
-#else
-    clock_gettime(CLOCK_REALTIME, &tm);
-#endif
+    struct timeval now;
+    if(gettimeofday(&now, NULL) == 0) {
+        tm->tv_sec  = now.tv_sec;
+        tm->tv_nsec = now.tv_usec * 1000;
+    }
 }
 
 void CopyTime(struct timespec *from, struct timespec *to)
