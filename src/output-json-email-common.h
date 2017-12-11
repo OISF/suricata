245fd59fcc345bca0bf1e15be31f2d471b1d9775
Jason Ish <ish@unx.ca>
2017-12-11 15:50:07 -0600

    eve: email: respect global metadata config

--- src/output-json-email-common.h
+++ src/output-json-email-common.h
@@ -28,6 +28,7 @@ typedef struct OutputJsonEmailCtx_ {
     LogFileCtx *file_ctx;
     uint32_t flags; /** Store mode */
     uint64_t fields;/** Store fields */
+    bool     include_metadata;
 } OutputJsonEmailCtx;
 
 
