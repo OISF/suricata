7a2c08dcc6a400e53f37b8cdd8af928326580d19
Eric Leblond <eric@regit.org>
2018-01-02 22:08:21 +0100

    af-packet: add support for XDP cpu redirect map
    This patch adds a boolean option "xdp-cpu-redirect" to af-packet
    interface configuration. If set, then the XDP filter will load
    balance the skb creation on specified CPUs instead of doing the
    creation on the CPU handling the packet. In the case of a card
    with asymetric hashing this will allow to avoid saturating the
    single CPU handling the trafic.
    
    The XDP filter must contains a set of map allowing load balancing.
    This is the case of xdp_filter.bpf.
    
    Fixed-by: Jesper Dangaard Brouer <netoptimizer@brouer.com>

--- src/runmode-af-packet.c
+++ src/runmode-af-packet.c
@@ -479,6 +479,22 @@ static void *ParseAFPConfig(const char *iface)
             if (ret != 0) {
                 SCLogWarning(SC_ERR_INVALID_VALUE,
                              "Error when setting up XDP");
+            } else {
+                /* Try to get the xdp-cpu-redirect key */
+                const char *cpuset;
+                if (ConfGetChildValueWithDefault(if_root, if_default,
+                                                 "xdp-cpu-redirect", &cpuset) == 1) {
+                    SCLogConfig("Setting up CPU map XDP");
+                    ConfNode *node = ConfGetChildWithDefault(if_root, if_default, "xdp-cpu-redirect");
+                    if (node == NULL) {
+                        SCLogError(SC_ERR_INVALID_VALUE, "Should not be there");
+                    } else {
+                        EBPFBuildCPUSet(node, aconf->iface);
+                    }
+                } else {
+                        /* It will just set CPU count to 0 */
+                        EBPFBuildCPUSet(NULL, aconf->iface);
+                }
             }
         }
 #else
