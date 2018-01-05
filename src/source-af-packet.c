ea629e0f9e9460ca35401e66c18e557a757c2222
Eric Leblond <eric@regit.org>
2018-01-05 23:32:54 +0100

    af-packet: code cleaning and comments

--- src/source-af-packet.c
+++ src/source-af-packet.c
@@ -2262,6 +2262,7 @@ TmEcode AFPSetBPFFilter(AFPThreadVars *ptv)
  * \param mapfd file descriptor of the protocol bypass table
  * \param key data to use as key in the table
  * \param inittime time of creation of the entry (in monotonic clock)
+ * \return 0 in case of error, 1 if success
  */
 static int AFPInsertHalfFlow(int mapd, void *key, uint64_t inittime)
 {
@@ -2295,6 +2296,9 @@ static int AFPInsertHalfFlow(int mapd, void *key, uint64_t inittime)
 }
 #endif
 
+/**
+ * Bypass function for AF_PACKET capture
+ */
 static int AFPBypassCallback(Packet *p)
 {
 #ifdef HAVE_PACKET_EBPF
