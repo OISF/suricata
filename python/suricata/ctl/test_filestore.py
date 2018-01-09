50b5a3a56d3b4623d2cf193c2e796fb345385dac
Jason Ish <ish@unx.ca>
2018-01-09 07:51:26 -0600

    suricatactl: a new python script for misc. tasks
    Use a new directory, Python to host the Suricata python modules.
    One entry point is suricatactl, a control script for
    miscalleneous tasks. Currently onl filestore pruning
    is implemented.

---
+++ python/suricata/ctl/test_filestore.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+import unittest
+
+import filestore
+
+class PruneTestCase(unittest.TestCase):
+
+    def test_parse_age(self):
+        self.assertEqual(filestore.parse_age("1s"), 1)
+        self.assertEqual(filestore.parse_age("1m"), 60)
+        self.assertEqual(filestore.parse_age("1h"), 3600)
+        self.assertEqual(filestore.parse_age("1d"), 86400)
+
+        with self.assertRaises(filestore.InvalidAgeFormatError) as err:
+            filestore.parse_age("1")
+        with self.assertRaises(filestore.InvalidAgeFormatError) as err:
+            filestore.parse_age("1y")
