2075b334055c437273495f195441d8866e199c1e
Jason Ish <ish@unx.ca>
2018-01-24 16:40:11 -0600

    output-json-vars: rename to metadata
    No functional change, just rename of files and functions
    to reflect the metadata event type now used.

--- src/output.c
+++ src/output.c
@@ -72,7 +72,7 @@
 #include "output-json-template.h"
 #include "output-lua.h"
 #include "output-json-dnp3.h"
-#include "output-json-vars.h"
+#include "output-json-metadata.h"
 #include "output-filestore.h"
 
 typedef struct RootLogger_ {
@@ -1085,7 +1085,7 @@ void OutputRegisterLoggers(void)
 
     /* DNP3. */
     JsonDNP3LogRegister();
-    JsonVarsLogRegister();
+    JsonMetadataLogRegister();
 
     /* NFS JSON logger. */
     JsonNFSLogRegister();
