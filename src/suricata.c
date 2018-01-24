8354f62b19ddef4aa49d980f614f1147ebf13a7e
Maurizio Abba <mabba@lastline.com>
2018-01-24 12:09:11 +0000

    signal: enable SIGUSR2 after Reload when delayed-detect
    Enable SIGUSR2 Handler after the first rule reload when delayed-detect
    is enabled

--- src/suricata.c
+++ src/suricata.c
@@ -2466,6 +2466,8 @@ static void PostRunStartedDetectSetup(const SCInstance *suri)
         /* force 'reload', this will load the rules and swap engines */
         DetectEngineReload(suri);
         SCLogNotice("Signature(s) loaded, Detect thread(s) activated.");
+        UtilSignalHandlerSetup(SIGUSR2, SignalHandlerSigusr2);
+        UtilSignalUnblock(SIGUSR2);
     }
 }
 
