4f06f080d5493b68dbbc31eefefa9cff763ce60e
Giuseppe Longo <glongo@stamus-networks.com>
2018-02-14 11:17:59 +0100

    global-memcap: add new feature
    This adds a new memcap defined as global
    which rule them all.
    
    The value specified as 'global-memcap' in
    suricata.yaml cannot be exceed by
    the sum of all the other memcaps.

--- src/suricata.c
+++ src/suricata.c
@@ -2448,6 +2448,8 @@ int PostConfLoadedSetup(SCInstance *suri)
         ConfSet("runmode", suri->runmode_custom_mode);
     }
 
+    GlobalMemcapInitConfig();
+
     StorageInit();
 #ifdef HAVE_PACKET_EBPF
     EBPFRegisterExtension();
