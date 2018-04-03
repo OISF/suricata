01bfada1f43a7cfaa754193f748d60459cff6386
Victor Julien <victor@inliniac.net>
2018-04-03 12:10:46 +0200

    mpm/hs: fix minor coverity warning
    CID 1428797 (#1 of 1): Unchecked return value (CHECKED_RETURN)
        check_return: Calling HashTableAdd without checking return value
        (as is done elsewhere 5 out of 6 times).

--- src/util-mpm-hs.c
+++ src/util-mpm-hs.c
@@ -579,6 +579,7 @@ int SCHSPreparePatterns(MpmCtx *mpm_ctx)
         return 0;
     }
 
+    int r;
     hs_error_t err;
     hs_compile_error_t *compile_err = NULL;
     SCHSCompileData *cd = NULL;
@@ -716,8 +717,10 @@ int SCHSPreparePatterns(MpmCtx *mpm_ctx)
 
     /* Cache this database globally for later. */
     pd->ref_cnt = 1;
-    HashTableAdd(g_db_table, pd, 1);
+    int r = HashTableAdd(g_db_table, pd, 1);
     SCMutexUnlock(&g_db_table_mutex);
+    if (r < 0)
+        goto error;
 
     SCHSFreeCompileData(cd);
     return 0;
