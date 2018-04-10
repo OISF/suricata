a02afee0abeea0db6b8921b08b73ed23a79d98ee
Jacob Masen-Smith <smith.jacob@gmail.com>
2018-04-10 08:11:22 -0700

    Made POSIX compatibility functions conditional
    Windows Vista and later define these functions for compatibility

--- src/win32-misc.c
+++ src/win32-misc.c
@@ -52,69 +52,72 @@ void unsetenv(const char *name)
     SCFree(str);
 }
 
-// const char* inet_ntop(int af, const void *src, char *dst, uint32_t cnt)
-// {
-//     if (af == AF_INET)
-//     {
-//         struct sockaddr_in in;
-//         memset(&in, 0, sizeof(in));
-//         in.sin_family = AF_INET;
-//         memcpy(&in.sin_addr, src, sizeof(struct in_addr));
-//         if (0 == getnameinfo((struct sockaddr *)&in, sizeof(struct sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST))
-//             return dst;
-//     }
-//     else if (af == AF_INET6)
-//     {
-//         struct sockaddr_in6 in6;
-//         memset(&in6, 0, sizeof(in6));
-//         in6.sin6_family = AF_INET6;
-//         memcpy(&in6.sin6_addr, src, sizeof(struct in_addr6));
-//         if (0 == getnameinfo((struct sockaddr *)&in6, sizeof(struct sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST))
-//             return dst;
-//     }
-//     return NULL;
-// }
+/* these functions have been defined on Vista and later */
+#if NTDDI_VERSION < NTDDI_VISTA || _WIN32_WINNT < _WIN32_WINNT_VISTA
+const char* inet_ntop(int af, const void *src, char *dst, uint32_t cnt)
+{
+    if (af == AF_INET)
+    {
+        struct sockaddr_in in;
+        memset(&in, 0, sizeof(in));
+        in.sin_family = AF_INET;
+        memcpy(&in.sin_addr, src, sizeof(struct in_addr));
+        if (0 == getnameinfo((struct sockaddr *)&in, sizeof(struct sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST))
+            return dst;
+    }
+    else if (af == AF_INET6)
+    {
+        struct sockaddr_in6 in6;
+        memset(&in6, 0, sizeof(in6));
+        in6.sin6_family = AF_INET6;
+        memcpy(&in6.sin6_addr, src, sizeof(struct in_addr6));
+        if (0 == getnameinfo((struct sockaddr *)&in6, sizeof(struct sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST))
+            return dst;
+    }
+    return NULL;
+}
 
-// int inet_pton(int af, const char *src, void *dst)
-// {
-//     struct addrinfo hints;
-//     memset(&hints, 0, sizeof(struct addrinfo));
-//     hints.ai_family = af;
+int inet_pton(int af, const char *src, void *dst)
+{
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = af;
 
-//     /* as getaddrinfo below seems more liberal that inet_pton on Linux,
-//      * add this check here that does a guess at the validity of the
-//      * input address. */
-//     if (af == AF_INET) {
-//         if (!IPv4AddressStringIsValid(src))
-//             return -1;
-//     } else if (af == AF_INET6) {
-//         if (!IPv6AddressStringIsValid(src))
-//             return -1;
-//     }
+    /* as getaddrinfo below seems more liberal that inet_pton on Linux,
+     * add this check here that does a guess at the validity of the
+     * input address. */
+    if (af == AF_INET) {
+        if (!IPv4AddressStringIsValid(src))
+            return -1;
+    } else if (af == AF_INET6) {
+        if (!IPv6AddressStringIsValid(src))
+            return -1;
+    }
 
-//     struct addrinfo* result = NULL;
-//     if (0 != getaddrinfo(src, NULL, &hints, &result))
-//         return -1;
+    struct addrinfo* result = NULL;
+    if (0 != getaddrinfo(src, NULL, &hints, &result))
+        return -1;
 
-//     if (result) {
-//         if (result->ai_family == AF_INET) {
-//             struct sockaddr_in* in = (struct sockaddr_in*)result->ai_addr;
-//             memcpy(dst, &in->sin_addr, 4);
-//         }
-//         else if (result->ai_family == AF_INET6) {
-//             struct sockaddr_in6* in6 = (struct sockaddr_in6*)result->ai_addr;
-//             memcpy(dst, &in6->sin6_addr, 16);
-//         }
-//         else {
-//             freeaddrinfo(result);
-//             return -1;
-//         }
+    if (result) {
+        if (result->ai_family == AF_INET) {
+            struct sockaddr_in* in = (struct sockaddr_in*)result->ai_addr;
+            memcpy(dst, &in->sin_addr, 4);
+        }
+        else if (result->ai_family == AF_INET6) {
+            struct sockaddr_in6* in6 = (struct sockaddr_in6*)result->ai_addr;
+            memcpy(dst, &in6->sin6_addr, 16);
+        }
+        else {
+            freeaddrinfo(result);
+            return -1;
+        }
 
-//         freeaddrinfo(result);
-//         return 1;
-//     }
+        freeaddrinfo(result);
+        return 1;
+    }
 
-//     return -1;
-// }
+    return -1;
+}
+#endif
 
 #endif /* OS_WIN32 */
