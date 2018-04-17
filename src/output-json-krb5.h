8ed6ce4656a3332c5d4963c5d1e1de9d6419a67a
Pierre Chifflier <chifflier@wzdftpd.net>
2018-04-17 07:50:43 +0200

    Add logger for Kerberos 5 metadata

--- src/output-json-krb5.h
+++ src/output-json-krb5.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017-2018 Open Information Security Foundation
+/* Copyright (C) 2015 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -15,7 +15,15 @@
  * 02110-1301, USA.
  */
 
-// written by Pierre Chifflier  <chifflier@wzdftpd.net>
+/**
+ * \file
+ *
+ * \author FirstName LastName <name@domain>
+ */
 
-pub mod krb5;
-pub mod log;
+#ifndef __OUTPUT_JSON_KRB5_H__
+#define __OUTPUT_JSON_KRB5_H__
+
+void JsonKRB5LogRegister(void);
+
+#endif /* __OUTPUT_JSON_KRB5_H__ */
