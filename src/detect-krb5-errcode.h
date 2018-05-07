2644081b407bdbdac16dbfc0bc32b7af356e4e9c
Pierre Chifflier <chifflier@wzdftpd.net>
2018-05-07 08:16:53 +0200

    Add krb5_err_code detection keyword

---
+++ src/detect-krb5-errcode.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2015-2017 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Pierre Chifflier <chifflier@wzdftpd.net>
+ */
+
+#ifndef __DETECT_KRB5_ERRCODE_H__
+#define __DETECT_KRB5_ERRCODE_H__
+
+typedef struct DetectKrb5ErrCodeData_ {
+    int32_t err_code;
+} DetectKrb5ErrCodeData;
+
+void DetectKrb5ErrCodeRegister(void);
+
+#endif /* __DETECT_KRB5_ERRCODE_H__ */
