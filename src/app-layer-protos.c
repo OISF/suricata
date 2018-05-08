9210d8743bef65fee0d6ef0a07368e42d3c48f65
Jason Ish <ish@unx.ca>
2018-05-08 17:49:52 -0600

    rust/dhcp: Rust based DHCP decoder and logger.
    This is a DHCP decoder and logger written in Rust. Unlike most
    parsers, this one is stateless so responses are not matched
    up to requests by Suricata. However, the output does contain
    enough fields to match them up in post-processing.
    
    Rules are included to alert of malformed or truncated options.

--- src/app-layer-protos.c
+++ src/app-layer-protos.c
@@ -99,6 +99,9 @@ const char *AppProtoToString(AppProto alproto)
         case ALPROTO_KRB5:
             proto_name = "krb5";
             break;
+        case ALPROTO_DHCP:
+            proto_name = "dhcp";
+            break;
         case ALPROTO_TEMPLATE:
             proto_name = "template";
             break;
@@ -140,6 +143,7 @@ AppProto StringToAppProto(const char *proto_name)
     if (strcmp(proto_name,"ntp")==0) return ALPROTO_NTP;
     if (strcmp(proto_name,"ikev2")==0) return ALPROTO_IKEV2;
     if (strcmp(proto_name,"krb5")==0) return ALPROTO_KRB5;
+    if (strcmp(proto_name,"dhcp")==0) return ALPROTO_DHCP;
     if (strcmp(proto_name,"template")==0) return ALPROTO_TEMPLATE;
     if (strcmp(proto_name,"failed")==0) return ALPROTO_FAILED;
 
