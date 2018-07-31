8ec005afd6da5e88afdf7165647e1419abc08308
Maurizio Abba <mabba@lastline.com>
2018-07-31 07:08:10 +0100

    detect: fix fileext and filename negated match
    fix bug in fileext and filename preventing negated match to work
    correctly. Previously, negated fileext (such as !"php") would cause a
    match anyway on files that have extension php, as the last if would not
    be accessed.
    
    Using the same workflow as detect-filemagic we remove the final
    isolated if and set it as a branch of the previous if.

--- src/detect-fileext.c
+++ src/detect-fileext.c
@@ -116,9 +116,7 @@ static int DetectFileextMatch (ThreadVars *t, DetectEngineThreadCtx *det_ctx,
             ret = 1;
             SCLogDebug("File ext found");
         }
-    }
-
-    if (ret == 0 && (fileext->flags & DETECT_CONTENT_NEGATED)) {
+    } else if (fileext->flags & DETECT_CONTENT_NEGATED) {
         SCLogDebug("negated match");
         ret = 1;
     }
