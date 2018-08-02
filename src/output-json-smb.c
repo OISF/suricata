9cf62352e1f1791f9707cd6af8e5f40427f75014
Victor Julien <victor@inliniac.net>
2018-08-02 15:10:15 +0200

    eve/smb: use flow direction for tuple

--- src/output-json-smb.c
+++ src/output-json-smb.c
@@ -77,11 +77,10 @@ json_t *JsonSMBAddMetadata(const Flow *f, uint64_t tx_id)
 static int JsonSMBLogger(ThreadVars *tv, void *thread_data,
     const Packet *p, Flow *f, void *state, void *tx, uint64_t tx_id)
 {
-    //SMBTransaction *smbtx = tx;
     LogSMBLogThread *thread = thread_data;
     json_t *js, *smbjs;
 
-    js = CreateJSONHeader((Packet *)p, 0, "smb");
+    js = CreateJSONHeader(p, LOG_DIR_FLOW, "smb");
     if (unlikely(js == NULL)) {
         return TM_ECODE_FAILED;
     }
