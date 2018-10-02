4336f6e597ab798ef834009c42ade9a907d97307
Bendik Hagen <bendik@mnemonic.no>
2018-10-03 00:07:40 +0200

    Fix Bug #2057: Setting vlan from flow
    When Suricata generates pseudopackets vlan is not included and logged pseudopackets will not include vlan.
    This commit copies the vlan from the flow to the generated pseudopacket, solving the problem when pseudopackets get logged or alerted.

--- src/flow-timeout.c
+++ src/flow-timeout.c
@@ -89,6 +89,16 @@ static inline Packet *FlowForceReassemblyPseudoPacketSetup(Packet *p,
     p->flags |= PKT_HAS_FLOW;
     p->flags |= PKT_PSEUDO_STREAM_END;
 
+    if (f->vlan_id[0]) {
+        p->vlan_id[0] = f->vlan_id[0];
+        p->vlan_idx=1;
+
+        if (f->vlan_id[1]) {
+            p->vlan_id[1] = f->vlan_id[1];
+            p->vlan_idx=2;
+        }
+    }
+
     if (f->flags & FLOW_NOPACKET_INSPECTION) {
         DecodeSetNoPacketInspectionFlag(p);
     }
