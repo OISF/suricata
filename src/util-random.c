8702fb9bea128f648768d4c723c0d37145f5155e
Victor Julien <victor@inliniac.net>
2018-10-23 12:57:50 +0200

    coverity: don't warn on fall back random

--- src/util-random.c
+++ src/util-random.c
@@ -34,6 +34,7 @@ static long int RandomGetClock(void)
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
 
+    // coverity[dont_call : FALSE]
     srandom(ts.tv_nsec ^ ts.tv_sec);
     long int value = random();
     return value;
@@ -47,6 +48,7 @@ static long int RandomGetPosix(void)
     memset(&tv, 0, sizeof(tv));
     gettimeofday(&tv, NULL);
 
+    // coverity[dont_call : FALSE]
     srandom(tv.tv_usec ^ tv.tv_sec);
     long int value = random();
     return value;
