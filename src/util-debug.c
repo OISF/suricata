1bf658f9ee713464903cfc3f74733d910b0bd802
Victor Julien <victor@inliniac.net>
2018-10-23 14:54:17 +0200

    debug/log: suppress coverity checked_return warning

--- src/util-debug.c
+++ src/util-debug.c
@@ -1330,7 +1330,7 @@ void SCLogLoadConfig(int daemon, int verbose)
     if (ConfGet("logging.default-log-format", &sc_lid->global_log_format) != 1)
         sc_lid->global_log_format = SC_LOG_DEF_LOG_FORMAT;
 
-    ConfGet("logging.default-output-filter", &sc_lid->op_filter);
+    (void)ConfGet("logging.default-output-filter", &sc_lid->op_filter);
 
     ConfNode *seq_node, *output;
     TAILQ_FOREACH(seq_node, &outputs->head, next) {
