42e6dbb87c09753c88d28691efa24103fac8d61c
Victor Julien <victor@inliniac.net>
2018-11-26 11:38:35 +0100

    detect/http_stat_msg: switch to inspect v2

--- src/detect-engine-hsmd.h
+++ src/detect-engine-hsmd.h
@@ -23,16 +23,6 @@
 #ifndef __DETECT_ENGINE_HSMD_H__
 #define __DETECT_ENGINE_HSMD_H__
 
-#include "app-layer-htp.h"
-
-int PrefilterTxHttpStatMsgRegister(DetectEngineCtx *de_ctx,
-        SigGroupHead *sgh, MpmCtx *mpm_ctx);
-
-int DetectEngineInspectHttpStatMsg(ThreadVars *tv,
-        DetectEngineCtx *de_ctx, DetectEngineThreadCtx *det_ctx,
-        const Signature *s, const SigMatchData *smd,
-        Flow *f, uint8_t flags, void *alstate, void *tx, uint64_t tx_id);
-
 void DetectEngineHttpStatMsgRegisterTests(void);
 
 #endif /* __DETECT_ENGINE_HSMD_H__ */
