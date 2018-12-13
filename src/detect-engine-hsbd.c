ce677abdf080725301f5db4ba466e8268af95f23
Victor Julien <victor@inliniac.net>
2018-12-13 11:28:51 +0100

    detect/file-data: minor cleanups

--- src/detect-engine-hsbd.c
+++ src/detect-engine-hsbd.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2016 Open Information Security Foundation
+/* Copyright (C) 2007-2018 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -67,7 +67,7 @@
 
 #include "util-validate.h"
 
-static HtpBody *GetResponseBody(htp_tx_t *tx)
+static inline HtpBody *GetResponseBody(htp_tx_t *tx)
 {
     HtpTxUserData *htud = (HtpTxUserData *)htp_tx_get_user_data(tx);
     if (htud == NULL) {
