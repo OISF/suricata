c3a2aa9628ff9bd895d61ea0ce6b0c0d8e08e630
mb <murat@sunnyvalley.io>
2019-01-17 23:00:27 -0800

    Although, there are no visible API changes, moving from API version 11 to 12, for every rx/tx ring that is being opened, one needs to explicitly call NETMAP_IF to get the fresh address of the netmap_if struct.
    When the netmap-rework branch is done, it will also handle the
    situation, but anyhow, this is a quick fix for 4.1.3 so that Suricata
    can still happily run on FreeBSD 12 & 11 newcoming releases, since
    they all come with the new netmap code.
    
    Contributed by: Sunny Valley Networks
    
    Attn: @inliniac

--- src/source-netmap.c
+++ src/source-netmap.c
@@ -268,6 +268,7 @@ static int NetmapOpen(char *ifname, int promisc, NetmapDevice **pdevice, int ver
 {
     NetmapDevice *pdev = NULL;
     struct nmreq nm_req;
+    struct netmap_if *nifp = NULL;
 
     *pdevice = NULL;
 
@@ -367,7 +368,7 @@ static int NetmapOpen(char *ifname, int promisc, NetmapDevice **pdevice, int ver
             nm_req.nr_ringid = i | NETMAP_NO_TX_POLL;
         } else {
             nm_req.nr_flags = NR_REG_SW;
-            nm_req.nr_ringid = NETMAP_NO_TX_POLL;
+            nm_req.nr_ringid = i | NETMAP_NO_TX_POLL;
         }
         if (ioctl(pring->fd, NIOCREGIF, &nm_req) != 0) {
             SCLogError(SC_ERR_NETMAP_CREATE,
@@ -385,14 +386,15 @@ static int NetmapOpen(char *ifname, int promisc, NetmapDevice **pdevice, int ver
                            strerror(errno));
                 break;
             }
-            pdev->nif = NETMAP_IF(pdev->mem, nm_req.nr_offset);
-        }
+            pdev->nif = nifp = NETMAP_IF(pdev->mem, nm_req.nr_offset);
+        } else
+		nifp = NETMAP_IF(pdev->mem, nm_req.nr_offset);
 
         if ((i < pdev->rx_rings_cnt) || (i == pdev->rings_cnt)) {
-            pring->rx = NETMAP_RXRING(pdev->nif, i);
+            pring->rx = NETMAP_RXRING(nifp, i);
         }
         if ((i < pdev->tx_rings_cnt) || (i == pdev->rings_cnt)) {
-            pring->tx = NETMAP_TXRING(pdev->nif, i);
+            pring->tx = NETMAP_TXRING(nifp, i);
         }
         SCSpinInit(&pring->tx_lock, 0);
         success_cnt++;
