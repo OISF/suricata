e88acf2e92b5c9e7c11b2ac5456b833db63e1225
Giuseppe Longo <giuseppe@glongo.it>
2019-02-17 18:06:17 +0100

    rust/sip: add SIP logger

---
+++ rust/src/sip/log.rs
@@ -0,0 +1,58 @@
+/* Copyright (C) 2019 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+// written by Giuseppe Longo <giuseppe@glongo.it>
+
+use json::*;
+use sip::sip::{SIPState, SIPTransaction};
+
+#[no_mangle]
+pub extern "C" fn rs_sip_log_json(_state: &mut SIPState, tx: &mut SIPTransaction) -> *mut JsonT
+{
+    let js = Json::object();
+
+    match tx.request {
+        Some(ref req) => {
+            js.set_string("method", &format!("{}", req.method));
+            js.set_string("uri", &format!("{}", req.path));
+            js.set_string("version", &format!("{}", req.version));
+        },
+        None => {}
+    }
+    match tx.request_line {
+        Some (ref req_line) => {
+            js.set_string("request_line", &format!("{}", req_line));
+        },
+        None => {}
+    }
+    match tx.response {
+        Some(ref resp) => {
+            js.set_string("version", &format!("{}", resp.version));
+            js.set_string("code", &format!("{}", resp.code));
+            js.set_string("reason", &format!("{}", resp.reason));
+        },
+        None => {}
+    }
+    match tx.response_line {
+        Some (ref resp_line) => {
+            js.set_string("response_line", &format!("{}", resp_line));
+        },
+        None => {}
+    }
+
+    return js.unwrap();
+}
