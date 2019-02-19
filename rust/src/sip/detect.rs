6604f66f03faf2de0d938a06731bbe3a97a4e525
Giuseppe Longo <giuseppe@glongo.it>
2019-02-19 21:11:31 +0100

    detect/sip.method: add sticky buffer
    Matches on uri field in SIP.

--- rust/src/sip/detect.rs
+++ rust/src/sip/detect.rs
@@ -17,7 +17,31 @@
 
 // written by Giuseppe Longo <giuseppe@glongo.it>
 
-pub mod sip;
-pub mod parser;
-pub mod log;
-pub mod detect;
+use std::ptr;
+
+use sip::sip::SIPTransaction;
+
+#[no_mangle]
+pub unsafe extern "C" fn rs_sip_tx_get_method(tx:  &mut SIPTransaction,
+                                              buffer: *mut *const u8,
+                                              buffer_len: *mut u32)
+                                              -> u8
+{
+    match tx.request {
+        Some(ref r) => {
+            let m = &r.method;
+            if m.len() > 0 {
+                *buffer = m.as_ptr();
+                *buffer_len = m.len() as u32;
+                return 1;
+            }
+        }
+        None => {}
+    }
+
+    *buffer = ptr::null();
+    *buffer_len = 0;
+
+    return 0;
+}
+
