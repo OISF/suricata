8454122eb2b9c46affea29e6f4fff5c3512570f1
Giuseppe Longo <giuseppe@glongo.it>
2019-03-01 21:27:23 +0100

    detect/sip.protocol: add sticky buffer
    Matches on protocol field in SIP.

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -181,6 +181,7 @@
 #include "detect-krb5-sname.h"
 #include "detect-sip-method.h"
 #include "detect-sip-uri.h"
+#include "detect-sip-protocol.h"
 #include "detect-target.h"
 #include "detect-template-rust-buffer.h"
 #include "detect-snmp-version.h"
@@ -548,6 +549,7 @@ void SigTableSetup(void)
     DetectKrb5SNameRegister();
     DetectSipMethodRegister();
     DetectSipUriRegister();
+    DetectSipProtocolRegister();
     DetectTargetRegister();
     DetectTemplateRustBufferRegister();
     DetectSNMPVersionRegister();
