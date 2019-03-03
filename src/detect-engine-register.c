bf1b013c3717e00281a4f7d50700fa8c6b82e7f4
Giuseppe Longo <giuseppe@glongo.it>
2019-03-03 21:37:30 +0100

    detect/sip.request_line: add sticky buffer
    Matches on request line field in SIP.

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -183,6 +183,7 @@
 #include "detect-sip-protocol.h"
 #include "detect-sip-stat-code.h"
 #include "detect-sip-stat-msg.h"
+#include "detect-sip-request-line.h"
 #include "detect-target.h"
 #include "detect-template-rust-buffer.h"
 #include "detect-snmp-version.h"
@@ -551,6 +552,7 @@ void SigTableSetup(void)
     DetectSipProtocolRegister();
     DetectSipStatCodeRegister();
     DetectSipStatMsgRegister();
+    DetectSipRequestLineRegister();
     DetectTargetRegister();
     DetectTemplateRustBufferRegister();
     DetectSNMPVersionRegister();
