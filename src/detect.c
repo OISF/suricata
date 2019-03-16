bd31e4e3320f74e67bf79d878979bb927af3e8ba
Victor Julien <victor@inliniac.net>
2019-03-16 07:34:40 +0100

    detect: packet match micro optimization

--- src/detect.c
+++ src/detect.c
@@ -672,21 +672,19 @@ static inline int DetectRunInspectRulePacketMatches(
         SigMatchData *smd = s->sm_arrays[DETECT_SM_LIST_MATCH];
 
         SCLogDebug("running match functions, sm %p", smd);
-        if (smd != NULL) {
-            while (1) {
-                KEYWORD_PROFILING_START;
-                if (sigmatch_table[smd->type].Match(tv, det_ctx, p, s, smd->ctx) <= 0) {
-                    KEYWORD_PROFILING_END(det_ctx, smd->type, 0);
-                    SCLogDebug("no match");
-                    return 0;
-                }
-                KEYWORD_PROFILING_END(det_ctx, smd->type, 1);
-                if (smd->is_last) {
-                    SCLogDebug("match and is_last");
-                    break;
-                }
-                smd++;
+        while (1) {
+            KEYWORD_PROFILING_START;
+            if (sigmatch_table[smd->type].Match(tv, det_ctx, p, s, smd->ctx) <= 0) {
+                KEYWORD_PROFILING_END(det_ctx, smd->type, 0);
+                SCLogDebug("no match");
+                return 0;
             }
+            KEYWORD_PROFILING_END(det_ctx, smd->type, 1);
+            if (smd->is_last) {
+                SCLogDebug("match and is_last");
+                break;
+            }
+            smd++;
         }
     }
     return 1;
