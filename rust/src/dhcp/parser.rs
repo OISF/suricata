41aab88ddc7bb593cf1be58e137af85b8bf553f5
Jason Ish <ish@unx.ca>
2019-03-26 16:46:27 -0600

    dhcp: verify client id len before parsing data
    Verify that the client id length is at least 2 per the DHCP
    protocol rfc before parsing the data.
    
    Redmine issue:
    https://redmine.openinfosecfoundation.org/issues/2902

--- rust/src/dhcp/parser.rs
+++ rust/src/dhcp/parser.rs
@@ -121,7 +121,7 @@ named!(pub parse_header<DHCPHeader>,
 named!(pub parse_clientid_option<DHCPOption>,
        do_parse!(
            code: be_u8 >>
-           len: be_u8 >>
+           len: verify!(be_u8, |v| v > 1) >>
            htype: be_u8 >>
            data: take!(len - 1) >>    
                (
@@ -284,4 +284,38 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_parse_client_id_too_short() {
+        // Length field of 0.
+        let buf: &[u8] = &[
+            0x01,
+            0x00, // Length of 0.
+            0x01,
+            0x01, // Junk data start here.
+            0x02,
+            0x03,
+        ];
+        let r = parse_clientid_option(buf);
+        assert!(r.is_err());
+
+        // Length field of 1.
+        let buf: &[u8] = &[
+            0x01,
+            0x01, // Length of 1.
+            0x01,
+            0x41,
+        ];
+        let r = parse_clientid_option(buf);
+        assert!(r.is_err());
+
+        // Length field of 2 -- OK.
+        let buf: &[u8] = &[
+            0x01,
+            0x02, // Length of 2.
+            0x01,
+            0x41,
+        ];
+        let r = parse_clientid_option(buf);
+        assert!(r.is_done());
+    }
 }
