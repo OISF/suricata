ff52bb14b7f5a15b906ac92cc2b74d74419f360a
Philippe Antoine <contact@catenacyber.fr>
2019-04-01 14:57:15 +0200

    ssh : code style consistency
    Adds SSH_FLAG_VERSION_PARSED to flags before each return
    This way, we are sure SSHParseBanner does not get called again
    And proto_version does not get leaked

--- src/app-layer-ssh.c
+++ src/app-layer-ssh.c
@@ -125,11 +125,13 @@ static int SSHParseBanner(SshState *state, SshHeader *header, const uint8_t *inp
     /* sanity check on this arithmetic */
     if ((sw_ver_len <= 1) || (sw_ver_len >= input_len)) {
         SCLogDebug("Should not have sw version length '%" PRIu64 "'", sw_ver_len);
+        header->flags |= SSH_FLAG_VERSION_PARSED;
         SCReturnInt(-1);
     }
 
     header->software_version = SCMalloc(sw_ver_len + 1);
     if (header->software_version == NULL) {
+        header->flags |= SSH_FLAG_VERSION_PARSED;
         SCReturnInt(-1);
     }
     memcpy(header->software_version, line_ptr, sw_ver_len);
