e5240cbf3863f52e1008522c22c8e536d0dda7ab
Mats Klepsland <mats.klepsland@gmail.com>
2019-05-14 07:54:03 +0200

    detect-tls: remove confusing underscores from variables
    Remove confusing underscore prefix from variables in GetData() for
    all tls keywords.

--- src/detect-tls-ja3-string.c
+++ src/detect-tls-ja3-string.c
@@ -62,7 +62,7 @@ static int DetectTlsJa3StringSetup(DetectEngineCtx *, Signature *, const char *)
 static void DetectTlsJa3StringRegisterTests(void);
 static InspectionBuffer *GetData(DetectEngineThreadCtx *det_ctx,
        const DetectEngineTransforms *transforms,
-       Flow *_f, const uint8_t _flow_flags,
+       Flow *f, const uint8_t flow_flags,
        void *txv, const int list_id);
 static int g_tls_ja3_str_buffer_id = 0;
 
@@ -119,12 +119,12 @@ static int DetectTlsJa3StringSetup(DetectEngineCtx *de_ctx, Signature *s, const 
 }
 
 static InspectionBuffer *GetData(DetectEngineThreadCtx *det_ctx,
-        const DetectEngineTransforms *transforms, Flow *_f,
-        const uint8_t _flow_flags, void *txv, const int list_id)
+        const DetectEngineTransforms *transforms, Flow *f,
+        const uint8_t flow_flags, void *txv, const int list_id)
 {
     InspectionBuffer *buffer = InspectionBufferGet(det_ctx, list_id);
     if (buffer->inspect == NULL) {
-        SSLState *ssl_state = (SSLState *)_f->alstate;
+        SSLState *ssl_state = (SSLState *)f->alstate;
 
         if (ssl_state->ja3_str == NULL ||
                 ssl_state->ja3_str->data == NULL) {
--- src/detect-tls-sni.c
+++ src/detect-tls-sni.c
@@ -58,7 +58,7 @@ static int DetectTlsSniSetup(DetectEngineCtx *, Signature *, const char *);
 static void DetectTlsSniRegisterTests(void);
 static InspectionBuffer *GetData(DetectEngineThreadCtx *det_ctx,
        const DetectEngineTransforms *transforms,
-       Flow *_f, const uint8_t _flow_flags,
+       Flow *f, const uint8_t flow_flags,
        void *txv, const int list_id);
 static int g_tls_sni_buffer_id = 0;
 
@@ -109,12 +109,12 @@ static int DetectTlsSniSetup(DetectEngineCtx *de_ctx, Signature *s, const char *
 }
 
 static InspectionBuffer *GetData(DetectEngineThreadCtx *det_ctx,
-        const DetectEngineTransforms *transforms, Flow *_f,
-        const uint8_t _flow_flags, void *txv, const int list_id)
+        const DetectEngineTransforms *transforms, Flow *f,
+        const uint8_t flow_flags, void *txv, const int list_id)
 {
     InspectionBuffer *buffer = InspectionBufferGet(det_ctx, list_id);
     if (buffer->inspect == NULL) {
-        SSLState *ssl_state = (SSLState *)_f->alstate;
+        SSLState *ssl_state = (SSLState *)f->alstate;
 
         if (ssl_state->client_connp.sni == NULL) {
             return NULL;
