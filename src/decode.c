fc2f2fa7d3eb6406307a38123fe171805113915f
Eric Leblond <eric@regit.org>
2019-05-25 16:04:03 +0200

    bypass: allow bypass for packet without flow
    For capture method that have their own flow structure (not maintained
    by Suricata), it can make sense to bypass a packet even if there is
    no Flow in Suricata.
    
    For AF_PACKET it does not make sense as the eBPF map entry will
    be destroyed as soon as it will be checked by the flow bypass
    manager. Thus we shortcut the bypass function if ever no Flow is
    attached to the packet.
    
    This path also removes reference to Flow in the bypass functions
    for AF_PACKET. It was not necessary and we possibly could benefit
    of it if ever we change the bypass algorithm.

--- src/decode.c
+++ src/decode.c
@@ -402,22 +402,27 @@ void PacketBypassCallback(Packet *p)
 {
     /* Don't try to bypass if flow is already out or
      * if we have failed to do it once */
-    int state = SC_ATOMIC_GET(p->flow->flow_state);
-    if ((state == FLOW_STATE_LOCAL_BYPASSED) ||
-           (state == FLOW_STATE_CAPTURE_BYPASSED)) {
-        return;
-    }
-
-    FlowCounters *fc = SCCalloc(sizeof(FlowCounters), 1);
-    if (fc) {
-        FlowSetStorageById(p->flow, GetFlowBypassCounterID(), fc);
-    } else {
-        return;
+    if (p->flow) {
+        int state = SC_ATOMIC_GET(p->flow->flow_state);
+        if ((state == FLOW_STATE_LOCAL_BYPASSED) ||
+                (state == FLOW_STATE_CAPTURE_BYPASSED)) {
+            return;
+        }
+        FlowCounters *fc = SCCalloc(sizeof(FlowCounters), 1);
+        if (fc) {
+            FlowSetStorageById(p->flow, GetFlowBypassCounterID(), fc);
+        } else {
+            return;
+        }
     }
     if (p->BypassPacketsFlow && p->BypassPacketsFlow(p)) {
-        FlowUpdateState(p->flow, FLOW_STATE_CAPTURE_BYPASSED);
+        if (p->flow) {
+            FlowUpdateState(p->flow, FLOW_STATE_CAPTURE_BYPASSED);
+        }
     } else {
-        FlowUpdateState(p->flow, FLOW_STATE_LOCAL_BYPASSED);
+        if (p->flow) {
+            FlowUpdateState(p->flow, FLOW_STATE_LOCAL_BYPASSED);
+        }
     }
 }
 
