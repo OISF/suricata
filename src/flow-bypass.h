c20ef3cb187329ec4268ee8a97a511f4143d5447
Eric Leblond <eric@regit.org>
2019-05-28 12:26:23 +0200

    bypass: fix wait time at exit
    The loop on bypassed flow maps can take a few seconds on heavily
    loaded system causing Suricata to not honor a stop before a few
    seconds.
    
    This patch adds the code needed to detect the need to exit from
    the check loop.

--- src/flow-bypass.h
+++ src/flow-bypass.h
@@ -30,9 +30,11 @@ struct flows_stats {
     uint64_t bytes;
 };
 
-typedef int (*BypassedCheckFunc)(struct flows_stats *bypassstats,
+typedef int (*BypassedCheckFunc)(ThreadVars *th_v,
+                                 struct flows_stats *bypassstats,
                                  struct timespec *curtime, void *data);
-typedef int (*BypassedCheckFuncInit)(struct timespec *curtime, void *data);
+typedef int (*BypassedCheckFuncInit)(ThreadVars *th_v,
+                                     struct timespec *curtime, void *data);
 typedef int (*BypassedUpdateFunc)(Flow *f, Packet *p, void *data);
 
 void FlowAddToBypassed(Flow *f);
