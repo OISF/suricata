df0f13a555681063eddcd779dfb0bfb0dbb5c81a
Jeff Lucovsky <jeff@lucovsky.org>
2019-08-31 14:23:11 -0400

    logging/anomaly: Add warning code for anomaly log

--- src/util-error.c
+++ src/util-error.c
@@ -363,6 +363,7 @@ const char * SCErrorToString(SCError err)
         CASE_CODE (SC_WARN_EVE_MISSING_EVENTS);
         CASE_CODE (SC_ERR_THASH_INIT);
         CASE_CODE (SC_ERR_DATASET);
+        CASE_CODE (SC_WARN_ANOMALY_CONFIG);
 
         CASE_CODE (SC_ERR_MAX);
     }
