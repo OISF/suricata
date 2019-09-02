f5d8e953a876bdbdd882fdf0260fc23023361180
Philippe Antoine <contact@catenacyber.fr>
2019-09-02 16:27:46 +0200

    protodetect: debug validation when multiple patterns match

--- src/app-layer-detect-proto.c
+++ src/app-layer-detect-proto.c
@@ -41,6 +41,7 @@
 #include "util-pool.h"
 #include "util-unittest.h"
 #include "util-unittest-helper.h"
+#include "util-validate.h"
 
 #include "flow.h"
 #include "flow-util.h"
@@ -1551,6 +1552,7 @@ AppProto AppLayerProtoDetectGetProto(AppLayerProtoDetectThreadCtx *tctx,
         uint16_t pm_matches = AppLayerProtoDetectPMGetProto(tctx, f,
                 buf, buflen, direction, pm_results, reverse_flow);
         if (pm_matches > 0) {
+            DEBUG_VALIDATE_BUG_ON(pm_matches > 1);
             alproto = pm_results[0];
 
             // rerun probing parser for other direction if it is unknown
