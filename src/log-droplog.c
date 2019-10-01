9b0232d061d12fe8f7410c5c9f84df51c13b31c1
Jason Ish <jason.ish@oisf.net>
2019-10-01 09:13:22 -0600

    drop.log: log deprecation warning if used

--- src/log-droplog.c
+++ src/log-droplog.c
@@ -137,6 +137,10 @@ static void LogDropLogDeInitCtx(OutputCtx *output_ctx)
  */
 static OutputInitResult LogDropLogInitCtx(ConfNode *conf)
 {
+    SCLogWarning(SC_WARN_DEPRECATED,
+        "The drop log has been deprecated and will be removed in "
+        "Suricata v6.0. Please use eve-log.");
+
     OutputInitResult result = { NULL, false };
     if (OutputDropLoggerEnable() != 0) {
         SCLogError(SC_ERR_CONF_YAML_ERROR, "only one 'drop' logger "
