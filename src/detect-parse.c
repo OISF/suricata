c1e116be115485c06b36109829d4716593fc7183
Jeff Lucovsky <jeff@lucovsky.org>
2019-11-17 16:07:56 -0500

    detect/parse Refactor interfaces/definitions
    This commit refactors existing code patterns to reduce code duplication
    and to be a base for supporting additional PCRE jit-related actions.

--- src/detect-parse.c
+++ src/detect-parse.c
@@ -2344,13 +2344,38 @@ error:
     return NULL;
 }
 
-typedef struct DetectParseRegex_ {
-    pcre *regex;
-    pcre_extra *study;
-    struct DetectParseRegex_ *next;
-} DetectParseRegex;
-
 static DetectParseRegex *g_detect_parse_regex_list = NULL;
+int DetectParsePcreExecLen(DetectParseRegex *parse_regex, const char *str,
+                   int str_len,
+                   int start_offset, int options,
+                   int *ovector, int ovector_size)
+{
+    return pcre_exec(parse_regex->regex, parse_regex->study, str, str_len,
+                     start_offset, options, ovector, ovector_size);
+}
+
+int DetectParsePcreExec(DetectParseRegex *parse_regex, const char *str,
+                   int start_offset, int options,
+                   int *ovector, int ovector_size)
+{
+    return pcre_exec(parse_regex->regex, parse_regex->study, str, strlen(str),
+                     start_offset, options, ovector, ovector_size);
+}
+
+void DetectParseFreeRegex(DetectParseRegex *r)
+{
+    if (r->regex) {
+        pcre_free(r->regex);
+    }
+    if (r->study) {
+        pcre_free_study(r->study);
+    }
+#ifdef PCRE_HAVE_JIT_EXEC
+    if (r->jit_stack) {
+        pcre_jit_stack_free(r->jit_stack);
+    }
+#endif
+}
 
 void DetectParseFreeRegexes(void)
 {
@@ -2358,12 +2383,8 @@ void DetectParseFreeRegexes(void)
     while (r) {
         DetectParseRegex *next = r->next;
 
-        if (r->regex) {
-            pcre_free(r->regex);
-        }
-        if (r->study) {
-            pcre_free_study(r->study);
-        }
+        DetectParseFreeRegex(r);
+
         SCFree(r);
         r = next;
     }
@@ -2372,38 +2393,46 @@ void DetectParseFreeRegexes(void)
 
 /** \brief add regex and/or study to at exit free list
  */
-void DetectParseRegexAddToFreeList(pcre *regex, pcre_extra *study)
+void DetectParseRegexAddToFreeList(DetectParseRegex *detect_parse)
 {
     DetectParseRegex *r = SCCalloc(1, sizeof(*r));
     if (r == NULL) {
         FatalError(SC_ERR_MEM_ALLOC, "failed to alloc memory for pcre free list");
     }
-    r->regex = regex;
-    r->study = study;
+    r->regex = detect_parse->regex;
+    r->study = detect_parse->study;
     r->next = g_detect_parse_regex_list;
+#ifdef PCRE_HAVE_JIT_EXEC
+    r->jit_stack = detect_parse->jit_stack;
+#endif
     g_detect_parse_regex_list = r;
 }
 
-void DetectSetupParseRegexes(const char *parse_str,
-                             pcre **parse_regex,
-                             pcre_extra **parse_regex_study)
+void DetectSetupParseRegexesOpts(const char *parse_str, DetectParseRegex *detect_parse, int opts)
 {
     const char *eb;
     int eo;
-    int opts = 0;
 
-    *parse_regex = pcre_compile(parse_str, opts, &eb, &eo, NULL);
-    if (*parse_regex == NULL) {
+    detect_parse->regex = pcre_compile(parse_str, opts, &eb, &eo, NULL);
+    if (detect_parse->regex == NULL) {
         FatalError(SC_ERR_PCRE_COMPILE, "pcre compile of \"%s\" failed at "
                 "offset %" PRId32 ": %s", parse_str, eo, eb);
     }
 
-    *parse_regex_study = pcre_study(*parse_regex, 0, &eb);
+    detect_parse->study = pcre_study(detect_parse->regex, 0 , &eb);
     if (eb != NULL) {
         FatalError(SC_ERR_PCRE_STUDY, "pcre study failed: %s", eb);
     }
 
-    DetectParseRegexAddToFreeList(*parse_regex, *parse_regex_study);
+
+    DetectParseRegexAddToFreeList(detect_parse);
+
+    return;
+}
+
+void DetectSetupParseRegexes(const char *parse_str, DetectParseRegex *detect_parse)
+{
+    DetectSetupParseRegexesOpts(parse_str, detect_parse, 0);
     return;
 }
 
