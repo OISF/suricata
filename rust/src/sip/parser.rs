ca3fca045a9f476dbefae76ed22e64de22822008
Giuseppe Longo <giuseppe@glongo.it>
2019-11-17 17:44:33 +0100

    rust/sip: accept chars for uri
    When SIP is running over TCP, uri may contain
    a string like ';transport=TCP' which is not accepted
    by the uri parser because ';' and '=' chars are not
    allowed at the moment.

--- rust/src/sip/parser.rs
+++ rust/src/sip/parser.rs
@@ -53,7 +53,7 @@ pub enum Method {
 
 #[inline]
 fn is_token_char(b: u8) -> bool {
-    is_alphanumeric(b) || b"!%'*+-._`".contains(&b)
+    is_alphanumeric(b) || b"!%'*+-._;=`".contains(&b)
 }
 
 #[inline]
