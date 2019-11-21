c48c0cbbd5542c6ea0b38cdff25159070519edc7
Eric Leblond <eric@regit.org>
2019-11-21 18:26:49 +0100

    qa: generate struct-flags.cocci at each run

--- qa/coccinelle/run_check.sh
+++ qa/coccinelle/run_check.sh
@@ -23,6 +23,12 @@ elif [ "${TOP_SRCDIR}" != "" ]; then
 	PREFIX=${TOP_SRCDIR}/
 fi
 
+if [ "${TOP_BUILDDIR}" != "" ]; then
+	BUILT_COCCI_FILES=$(ls ${TOP_BUILDDIR}/qa/coccinelle/*.cocci)
+else
+	BUILT_COCCI_FILES=""
+fi
+
 if [ -z "$CONCURRENCY_LEVEL" ]; then
 	CONCURRENCY_LEVEL=1
 	echo "No concurrency"
@@ -30,7 +36,7 @@ else
 	echo "Using concurrency level $CONCURRENCY_LEVEL"
 fi
 
-for SMPL in ${PREFIX}qa/coccinelle/*.cocci; do
+for SMPL in ${PREFIX}qa/coccinelle/*.cocci ${BUILT_COCCI_FILES}; do
 	echo "Testing cocci file: $SMPL"
 	if command -v parallel >/dev/null; then
 		echo -n $LIST | parallel -d ' ' -j $CONCURRENCY_LEVEL spatch --very-quiet -sp_file $SMPL --undefined UNITTESTS $PREFIX{} || if [ -z "$NOT_TERMINAL" ]; then exit 1; fi
