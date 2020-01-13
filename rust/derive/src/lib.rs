ebb7238a44e6ef209f82e38a21e51b290fdbca05
Jason Ish <jason.ish@oisf.net>
2020-01-12 22:56:47 -0600

    rust: derive crate: for custom derives
    Currently has one derive, AppLayerEvent to be used like:
    
    pub enum DNSEvent {
        MalformedData,
        NotRequest,
        NotResponse,
        ZFlagSet,
    }
    
    Code will be generated to:
    - Convert enum to a c type string
    - Convert string to enum variant
    - Convert id to enum variant

---
+++ rust/derive/src/lib.rs
@@ -0,0 +1,27 @@
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+
+mod applayerevent;
+
+#[proc_macro_derive(AppLayerEvent)]
+pub fn derive_app_layer_event(input: TokenStream) -> TokenStream {
+    applayerevent::derive_app_layer_event(input)
+}
