99c0047b51f44acf140714c8c393a43f0fd21073
Shivani Bhardwaj <shivanib134@gmail.com>
2020-01-17 15:04:18 +0530

    logging: Add DCERPC logger
    With this patch, a DCERPC request logs in eve.json as
    ```
    "dcerpc":{
        "request":"REQUEST",
        "opnum":32576,
        "req":
            {
                "frag_cnt":257,
                "stub_data_size":0
            },
        "response":"UNREPLIED"
    }
    ```

--- src/output.c
+++ src/output.c
@@ -81,6 +81,7 @@
 #include "output-lua.h"
 #include "output-json-dnp3.h"
 #include "output-json-metadata.h"
+#include "output-json-dcerpc.h"
 #include "output-filestore.h"
 
 typedef struct RootLogger_ {
@@ -1163,4 +1164,6 @@ void OutputRegisterLoggers(void)
     JsonTemplateRustLogRegister();
     /* RDP JSON logger. */
     JsonRdpLogRegister();
+    /* DCERPC JSON logger. */
+    JsonDCERPCLogRegister();
 }
