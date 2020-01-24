9a014ba5005dac0a9bff0b151d7482e8244a28ea
Philippe Antoine <contact@catenacyber.fr>
2020-01-24 15:20:58 +0100

    dummy before removing tx_id

---
+++ rust/src/ssh/logger.rs
@@ -0,0 +1,40 @@
+/* Copyright (C) 2018 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use std;
+use crate::json::*;
+use super::ssh::SSHTransaction;
+
+fn log_ssh(tx: &SSHTransaction) -> Option<Json> {
+    let js = Json::object();
+    if let Some(ref request) = tx.request {
+        js.set_string("request", request);
+    }
+    if let Some(ref response) = tx.response {
+        js.set_string("response", response);
+    }
+    return Some(js);
+}
+
+#[no_mangle]
+pub extern "C" fn rs_ssh_logger_log(tx: *mut std::os::raw::c_void) -> *mut JsonT {
+    let tx = cast_pointer!(tx, SSHTransaction);
+    match log_ssh(tx) {
+        Some(js) => js.unwrap(),
+        None => std::ptr::null_mut(),
+    }
+}
