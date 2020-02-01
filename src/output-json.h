a0a874874b04ab128165b35ad35b28140dab5d00
Eric Leblond <eric@regit.org>
2020-02-01 20:17:57 +0100

    eve: differentiate logging for alerted flows
    This patch introduces a new eve-log option that add differentiate
    logging for flows with alert. The concept is to get the maximal
    verbosity for suspect flows so the maximum of context is provided
    to the user. Alert contains metadata but for some alerts we don't
    have the full transaction yet so we are missing information.
    
    TLS and HTTP output are modified by this patch. For TLS, extended
    logging is activated for flows with alerts. For HTTP, extended
    logging is activated, full headers are dumped and HTTP body is
    added.
    
    Configuration allow to select if binary fields should be output
    in base64 format or in printable format.

--- src/output-json.h
+++ src/output-json.h
@@ -120,4 +120,13 @@ void SCJsonDecref(json_t *js);
 void EveAddCommonOptions(const OutputJsonCommonSettings *cfg, const Packet *p, const Flow *f,
         JsonBuilder *js, MemBuffer *payload);
 
+enum OutputJsonvalueType {
+    LOG_TYPE_BASE64 = 0,
+    LOG_TYPE_PRINTABLE,
+    LOG_TYPE_STRING,
+};
+
+bool OutputJSONNeedFullLog(const OutputJsonCommonSettings *cfg, const Flow *f, uint64_t tx_id,
+        enum OutputJsonvalueType type);
+
 #endif /* __OUTPUT_JSON_H__ */
