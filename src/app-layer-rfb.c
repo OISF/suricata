1c8943dedd0042acdd86e825fe4f145bf93d1bec
Frank Honza <frank.honza@dcso.de>
2020-02-07 13:22:17 +0100

    add RFB parser
    This commit adds support for the Remote Framebuffer Protocol (RFB) as
    used, for example, by various VNC implementations. It targets the
    official versions 3.3, 3.7 and 3.8 of the protocol and provides logging
    for the RFB handshake communication for now. Logged events include
    endpoint versions, details of the security (i.e. authentication)
    exchange as well as metadata about the image transfer parameters.
    Detection is enabled using keywords for:
    
     - rfb.name: Session name as sticky buffer
     - rfb.sectype: Security type, e.g. VNC-style challenge-response
     - rfb.secresult: Result of the security exchange, e.g. OK, FAIL, ...
    
    The latter could be used, for example, to detect brute-force attempts
    on open VNC servers, while the name could be used to map unwanted VNC
    sessions to the desktop owners or machines.
    
    We also ship example EVE-JSON output and keyword docs as part of the
    Sphinx source for Suricata's RTD documentation.

---
+++ src/app-layer-rfb.c
@@ -0,0 +1,155 @@
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Sascha Steinbiss <sascha.steinbiss@dcso.de>
+ *
+ * RFB (VNC) application layer detector and parser.
+ *
+ */
+
+#include "suricata-common.h"
+
+#include "util-unittest.h"
+
+#include "app-layer-detect-proto.h"
+#include "app-layer-parser.h"
+#include "app-layer-rfb.h"
+
+#include "rust-bindings.h"
+
+static int RFBRegisterPatternsForProtocolDetection(void)
+{
+    if (AppLayerProtoDetectPMRegisterPatternCI(IPPROTO_TCP, ALPROTO_RFB,
+                                               "RFB ", 4, 0, STREAM_TOCLIENT) < 0)
+    {
+        return -1;
+    }
+    if (AppLayerProtoDetectPMRegisterPatternCI(IPPROTO_TCP, ALPROTO_RFB,
+                                               "RFB ", 4, 0, STREAM_TOSERVER) < 0)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+void RFBParserRegisterTests(void);
+
+void RegisterRFBParsers(void)
+{
+    rs_rfb_register_parser();
+    if (RFBRegisterPatternsForProtocolDetection() < 0 )
+            return;
+#ifdef UNITTESTS
+    AppLayerParserRegisterProtocolUnittests(IPPROTO_TCP, ALPROTO_RFB,
+        RFBParserRegisterTests);
+#endif
+}
+
+
+#ifdef UNITTESTS
+
+#include "stream-tcp.h"
+#include "util-unittest-helper.h"
+
+static int RFBParserTest(void)
+{
+    uint64_t ret[4];
+    AppLayerParserThreadCtx *alp_tctx = AppLayerParserThreadCtxAlloc();
+    FAIL_IF_NULL(alp_tctx);
+
+    StreamTcpInitConfig(TRUE);
+    TcpSession ssn;
+    memset(&ssn, 0, sizeof(ssn));
+
+    Flow *f = UTHBuildFlow(AF_INET, "1.2.3.4", "1.2.3.5", 59001, 5900);
+    FAIL_IF_NULL(f);
+    f->protoctx = &ssn;
+    f->proto = IPPROTO_TCP;
+    f->alproto = ALPROTO_RFB;
+
+    static const unsigned char rfb_version_str[12] = {
+            0x52, 0x46, 0x42, 0x20, 0x30, 0x30, 0x33, 0x2e, 0x30, 0x30, 0x37, 0x0a
+    };
+
+    // the RFB server sending the first handshake message
+    int r = AppLayerParserParse(NULL, alp_tctx, f, ALPROTO_RFB, STREAM_TOCLIENT | STREAM_START,
+            (uint8_t *)rfb_version_str, sizeof(rfb_version_str));
+    FAIL_IF_NOT(r == 0);
+
+    r = AppLayerParserParse(
+            NULL, alp_tctx, f, ALPROTO_RFB, STREAM_TOSERVER, (uint8_t *)rfb_version_str, sizeof(rfb_version_str));
+    FAIL_IF_NOT(r == 0);
+
+    static const unsigned char security_types[3] = {
+            0x02, 0x01, 0x02
+    };
+    r = AppLayerParserParse(
+            NULL, alp_tctx, f, ALPROTO_RFB, STREAM_TOCLIENT, (uint8_t *)security_types, sizeof(security_types));
+    FAIL_IF_NOT(r == 0);
+
+    static const unsigned char type_selection[1] = {
+            0x01
+    };
+    r = AppLayerParserParse(
+            NULL, alp_tctx, f, ALPROTO_RFB, STREAM_TOSERVER, (uint8_t *)type_selection, sizeof(type_selection));
+    FAIL_IF_NOT(r == 0);
+
+    static const unsigned char client_init[1] = {
+            0x01
+    };
+    r = AppLayerParserParse(
+            NULL, alp_tctx, f, ALPROTO_RFB, STREAM_TOSERVER, (uint8_t *)client_init, sizeof(client_init));
+    FAIL_IF_NOT(r == 0);
+
+    static const unsigned char server_init[] = {
+          0x05, 0x00, 0x03, 0x20, 0x20, 0x18, 0x00, 0x01,
+          0x00, 0xff, 0x00, 0xff, 0x00, 0xff, 0x10, 0x08,
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1e,
+          0x61, 0x6e, 0x65, 0x61, 0x67, 0x6c, 0x65, 0x73,
+          0x40, 0x6c, 0x6f, 0x63, 0x61, 0x6c, 0x68, 0x6f,
+          0x73, 0x74, 0x2e, 0x6c, 0x6f, 0x63, 0x61, 0x6c,
+          0x64, 0x6f, 0x6d, 0x61, 0x69, 0x6e
+    };
+
+    r = AppLayerParserParse(
+            NULL, alp_tctx, f, ALPROTO_RFB, STREAM_TOCLIENT, (uint8_t *)server_init, sizeof(server_init));
+    FAIL_IF_NOT(r == 0);
+
+    AppLayerParserTransactionsCleanup(f);
+    UTHAppLayerParserStateGetIds(f->alparser, &ret[0], &ret[1], &ret[2], &ret[3]);
+    FAIL_IF_NOT(ret[0] == 1); // inspect_id[0]
+    FAIL_IF_NOT(ret[1] == 1); // inspect_id[1]
+    FAIL_IF_NOT(ret[2] == 1); // log_id
+    FAIL_IF_NOT(ret[3] == 1); // min_id
+
+    AppLayerParserTransactionsCleanup(f);
+    AppLayerParserThreadCtxFree(alp_tctx);
+    StreamTcpFreeConfig(TRUE);
+    UTHFreeFlow(f);
+
+    PASS;
+}
+
+void RFBParserRegisterTests(void)
+{
+    UtRegisterTest("RFBParserTest", RFBParserTest);
+}
+
+#endif
---
+++ src/detect-rfb-secresult.c
@@ -0,0 +1,311 @@
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Sascha Steinbiss <sascha.steinbiss@dcso.de>
+ */
+
+#include "suricata-common.h"
+#include "conf.h"
+#include "detect.h"
+#include "detect-parse.h"
+#include "detect-engine.h"
+#include "detect-engine-content-inspection.h"
+#include "detect-rfb-secresult.h"
+#include "util-unittest.h"
+
+#include "rust-bindings.h"
+
+#define PARSE_REGEX "\\S[A-z]"
+static DetectParseRegex parse_regex;
+
+static int rfb_secresult_id = 0;
+
+static int DetectRfbSecresultMatch(DetectEngineThreadCtx *det_ctx,
+                                   Flow *f, uint8_t flags, void *state,
+                                   void *txv, const Signature *s,
+                                   const SigMatchCtx *ctx);
+static int DetectRfbSecresultSetup (DetectEngineCtx *, Signature *, const char *);
+void RfbSecresultRegisterTests(void);
+void DetectRfbSecresultFree(void *);
+
+static int DetectEngineInspectRfbSecresultGeneric(ThreadVars *tv,
+        DetectEngineCtx *de_ctx, DetectEngineThreadCtx *det_ctx,
+        const Signature *s, const SigMatchData *smd,
+        Flow *f, uint8_t flags, void *alstate,
+        void *txv, uint64_t tx_id);
+
+typedef struct DetectRfbSecresultData_ {
+    uint32_t result; /** result code */
+} DetectRfbSecresultData;
+
+/**
+ * \brief Registration function for rfb.secresult: keyword
+ */
+void DetectRfbSecresultRegister (void)
+{
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].name = "rfb.secresult";
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].desc = "match RFB security result";
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].url = DOC_URL DOC_VERSION "/rules/rfb-keywords.html#rfb-secresult";
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].AppLayerTxMatch = DetectRfbSecresultMatch;
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].Setup = DetectRfbSecresultSetup;
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].Free  = DetectRfbSecresultFree;
+    sigmatch_table[DETECT_AL_RFB_SECRESULT].RegisterTests = RfbSecresultRegisterTests;
+
+    DetectSetupParseRegexes(PARSE_REGEX, &parse_regex);
+
+    DetectAppLayerInspectEngineRegister("rfb.secresult",
+            ALPROTO_RFB, SIG_FLAG_TOCLIENT, 1,
+            DetectEngineInspectRfbSecresultGeneric);
+
+    rfb_secresult_id = DetectBufferTypeGetByName("rfb.secresult");
+}
+
+static int DetectEngineInspectRfbSecresultGeneric(ThreadVars *tv,
+        DetectEngineCtx *de_ctx, DetectEngineThreadCtx *det_ctx,
+        const Signature *s, const SigMatchData *smd,
+        Flow *f, uint8_t flags, void *alstate,
+        void *txv, uint64_t tx_id)
+{
+    return DetectEngineInspectGenericList(tv, de_ctx, det_ctx, s, smd,
+                                          f, flags, alstate, txv, tx_id);
+}
+
+enum {
+    RFB_SECRESULT_OK = 0,
+    RFB_SECRESULT_FAIL,
+    RFB_SECRESULT_TOOMANY,
+    RFB_SECRESULT_UNKNOWN
+};
+
+/**
+ * \struct DetectRfbSecresult_
+ * DetectRfbSecresult_ is used to store values
+ */
+
+struct DetectRfbSecresult_ {
+    const char *result;
+    uint16_t code;
+} results[] = {
+    { "ok", RFB_SECRESULT_OK, },
+    { "fail", RFB_SECRESULT_FAIL, },
+    { "toomany", RFB_SECRESULT_TOOMANY, },
+    { "unknown", RFB_SECRESULT_UNKNOWN, },
+    { NULL, 0 },
+};
+
+/**
+ * \internal
+ * \brief Function to match security result of a RFB TX
+ *
+ * \param det_ctx Pointer to the pattern matcher thread.
+ * \param f       Pointer to the current flow.
+ * \param flags   Flags.
+ * \param state   App layer state.
+ * \param txv     Pointer to the RFBTransaction.
+ * \param s       Pointer to the Signature.
+ * \param ctx     Pointer to the sigmatch that we will cast into DetectRfbSecresultData.
+ *
+ * \retval 0 no match.
+ * \retval 1 match.
+ */
+static int DetectRfbSecresultMatch(DetectEngineThreadCtx *det_ctx,
+                                   Flow *f, uint8_t flags, void *state,
+                                   void *txv, const Signature *s,
+                                   const SigMatchCtx *ctx)
+{
+    const DetectRfbSecresultData *de = (const DetectRfbSecresultData *)ctx;
+    uint32_t resultcode;
+    int ret = 0;
+
+    if (!de)
+        return 0;
+
+    ret = rs_rfb_tx_get_secresult(txv, &resultcode);
+    if (ret == 0) {
+        return 0;
+    }
+
+    if (de->result < 3) {
+        /* we are asking for a defined code... */
+        if (resultcode == de->result) {
+            /* ... which needs to match */
+            return 1;
+        }
+    } else {
+        /* we are asking for an unknown code */
+        if (resultcode > 2) {
+            /* match any unknown code */
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * \internal
+ * \brief This function is used to parse options passed via rfb.secresults: keyword
+ *
+ * \param rawstr Pointer to the user provided secresult options
+ *
+ * \retval de pointer to DetectRfbSecresultData on success
+ * \retval NULL on failure
+ */
+static DetectRfbSecresultData *DetectRfbSecresultParse (const char *rawstr)
+{
+    int i;
+    DetectRfbSecresultData *de = NULL;
+#define MAX_SUBSTRINGS 30
+    int ret = 0, found = 0;
+    int ov[MAX_SUBSTRINGS];
+
+    ret = DetectParsePcreExec(&parse_regex, rawstr, 0, 0, ov, MAX_SUBSTRINGS);
+    if (ret < 1) {
+        SCLogError(SC_ERR_PCRE_MATCH, "pcre_exec parse error, ret %" PRId32 ", string %s", ret, rawstr);
+        goto error;
+    }
+
+    for(i = 0; results[i].result != NULL; i++)  {
+        if((strcasecmp(results[i].result,rawstr)) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0) {
+        SCLogError(SC_ERR_UNKNOWN_VALUE, "unknown secresult value %s", rawstr);
+        goto error;
+    }
+
+    de = SCMalloc(sizeof(DetectRfbSecresultData));
+    if (unlikely(de == NULL))
+        goto error;
+
+    de->result = results[i].code;
+
+    return de;
+
+error:
+    if (de) SCFree(de);
+    return NULL;
+}
+
+/**
+ * \internal
+ * \brief this function is used to add the parsed secresult into the current signature
+ *
+ * \param de_ctx pointer to the Detection Engine Context
+ * \param s pointer to the Current Signature
+ * \param rawstr pointer to the user provided secresult options
+ *
+ * \retval 0 on Success
+ * \retval -1 on Failure
+ */
+static int DetectRfbSecresultSetup (DetectEngineCtx *de_ctx, Signature *s, const char *rawstr)
+{
+    DetectRfbSecresultData *de = NULL;
+    SigMatch *sm = NULL;
+
+    de = DetectRfbSecresultParse(rawstr);
+    if (de == NULL)
+        goto error;
+
+    sm = SigMatchAlloc();
+    if (sm == NULL)
+        goto error;
+
+    sm->type = DETECT_AL_RFB_SECRESULT;
+    sm->ctx = (SigMatchCtx *)de;
+
+    SigMatchAppendSMToList(s, sm, rfb_secresult_id);
+
+    return 0;
+
+error:
+    if (de) SCFree(de);
+    if (sm) SCFree(sm);
+    return -1;
+}
+
+/**
+ * \internal
+ * \brief this function will free memory associated with DetectRfbSecresultData
+ *
+ * \param de pointer to DetectRfbSecresultData
+ */
+void DetectRfbSecresultFree(void *de_ptr)
+{
+    DetectRfbSecresultData *de = (DetectRfbSecresultData *)de_ptr;
+    if(de) SCFree(de);
+}
+
+/*
+ * ONLY TESTS BELOW THIS COMMENT
+ */
+
+#ifdef UNITTESTS
+/**
+ * \test RfbSecresultTestParse01 is a test for a valid secresult value
+ *
+ *  \retval 1 on succces
+ *  \retval 0 on failure
+ */
+static int RfbSecresultTestParse01 (void)
+{
+    DetectRfbSecresultData *de = NULL;
+    de = DetectRfbSecresultParse("fail");
+    if (de) {
+        DetectRfbSecresultFree(de);
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * \test RfbSecresultTestParse02 is a test for an invalid secresult value
+ *
+ *  \retval 1 on succces
+ *  \retval 0 on failure
+ */
+static int RfbSecresultTestParse02 (void)
+{
+    DetectRfbSecresultData *de = NULL;
+    de = DetectRfbSecresultParse("invalidopt");
+    if (de) {
+        DetectRfbSecresultFree(de);
+        return 0;
+    }
+
+    return 1;
+}
+
+#endif /* UNITTESTS */
+
+/**
+ * \brief this function registers unit tests for RfbSecresult
+ */
+void RfbSecresultRegisterTests(void)
+{
+#ifdef UNITTESTS
+    UtRegisterTest("RfbSecresultTestParse01", RfbSecresultTestParse01);
+    UtRegisterTest("RfbSecresultTestParse02", RfbSecresultTestParse02);
+#endif /* UNITTESTS */
+}
