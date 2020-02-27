1b1855c3bd3a243dad590a5fa3e1139d0a90e5b5
Victor Julien <victor@inliniac.net>
2020-02-27 20:27:20 +0100

    time: remove unused time structure

--- src/util-time.h
+++ src/util-time.h
@@ -24,16 +24,6 @@
 #ifndef __UTIL_TIME_H__
 #define __UTIL_TIME_H__
 
-/**
- * A timeval with 32 bit fields.
- *
- * Used by the unified on disk file format.
- */
-typedef struct SCTimeval32_ {
-    uint32_t tv_sec;
-    uint32_t tv_usec;
-} SCTimeval32;
-
 void TimeInit(void);
 void TimeDeinit(void);
 
