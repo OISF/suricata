41778256ccc711cc0fe272f3ada436065a5f787b
Philippe Antoine <contact@catenacyber.fr>
2020-02-28 15:08:33 +0100

    decode: fix endianness for ICMPv6 MTU

--- src/decode-icmpv6.h
+++ src/decode-icmpv6.h
@@ -118,7 +118,7 @@
 /** macro for icmpv6 "error_ptr" access */
 #define ICMPV6_GET_ERROR_PTR(p)    (p)->icmpv6h->icmpv6b.icmpv6e.error_ptr
 /** macro for icmpv6 "mtu" access */
-#define ICMPV6_GET_MTU(p)          (p)->icmpv6h->icmpv6b.icmpv6e.mtu
+#define ICMPV6_GET_MTU(p)          SCNtohl((p)->icmpv6h->icmpv6b.icmpv6e.mtu)
 
 /** macro for icmpv6 embedded "protocol" access */
 #define ICMPV6_GET_EMB_PROTO(p)    (p)->icmpv6vars.emb_ip6_proto_next
