3b87f50d3d02d2d7d0b485fcf9fbffd7dd5c2c47
Victor Julien <victor@inliniac.net>
2020-03-06 12:34:34 +0100

    detect/config: initial version

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -71,6 +71,8 @@
 #include "detect-engine-event.h"
 #include "decode.h"
 
+#include "detect-keyword-config.h"
+
 #include "detect-smb-share.h"
 
 #include "detect-base64-decode.h"
@@ -567,6 +569,7 @@ void SigTableSetup(void)
     DetectSNMPPduTypeRegister();
     DetectTemplateBufferRegister();
     DetectBypassRegister();
+    DetectConfigRegister();
 
     DetectTransformCompressWhitespaceRegister();
     DetectTransformStripWhitespaceRegister();
