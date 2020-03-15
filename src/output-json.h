785189839f58dfb4b324e1b11ce77dbf6f573252
Jason Ish <jason.ish@oisf.net>
2020-03-15 11:05:16 -0600

    alert/eve: convert to jsonbuilder
    Convert alert Eve logging JsonBuilder. Currently
    makes heavy use of JsonBuilder being able to log Jansson's json_t
    which is a temporary measure until all protocols loggers can be
    converted to JsonBuilder.
    
    New functions that replace Jansson versions with JsonBuilder
    variations use "Eve" instead of "JSON".

--- src/output-json.h
+++ src/output-json.h
@@ -28,6 +28,7 @@
 #include "util-buffer.h"
 #include "util-logopenfile.h"
 #include "output.h"
+#include "rust.h"
 
 #include "app-layer-htp-xff.h"
 
@@ -69,15 +70,21 @@ typedef struct OutputJSONMemBufferWrapper_ {
 int OutputJSONMemBufferCallback(const char *str, size_t size, void *data);
 
 void CreateJSONFlowId(json_t *js, const Flow *f);
+void CreateEveFlowId(JsonBuilder *js, const Flow *f);
 void JsonTcpFlags(uint8_t flags, json_t *js);
 void JsonPacket(const Packet *p, json_t *js, unsigned long max_length);
+void EvePacket(const Packet *p, JsonBuilder *js, unsigned long max_length);
 void JsonFiveTuple(const Packet *, enum OutputJsonLogDirection, json_t *);
 json_t *CreateJSONHeader(const Packet *p,
+        enum OutputJsonLogDirection dir, const char *event_type,
+        JsonAddrInfo *addr);
+JsonBuilder *CreateEveHeader(const Packet *p,
         enum OutputJsonLogDirection dir, const char *event_type,
         JsonAddrInfo *addr);
 json_t *CreateJSONHeaderWithTxId(const Packet *p,
         enum OutputJsonLogDirection dir, const char *event_type, uint64_t tx_id);
 int OutputJSONBuffer(json_t *js, LogFileCtx *file_ctx, MemBuffer **buffer);
+int OutputJsonBuilderBuffer(JsonBuilder *js, LogFileCtx *file_ctx, MemBuffer **buffer);
 OutputInitResult OutputJsonInitCtx(ConfNode *);
 
 OutputInitResult OutputJsonLogInitSub(ConfNode *conf, OutputCtx *parent_ctx);
@@ -112,5 +119,7 @@ void SCJsonDecref(json_t *js);
 
 void JsonAddCommonOptions(const OutputJsonCommonSettings *cfg,
         const Packet *p, const Flow *f, json_t *js);
+void EveAddCommonOptions(const OutputJsonCommonSettings *cfg,
+        const Packet *p, const Flow *f, JsonBuilder *js);
 
 #endif /* __OUTPUT_JSON_H__ */
