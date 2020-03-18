4cad4c4bbd3836071a4943d497bbc67620eae9b6
Jeff Lucovsky <jeff@lucovsky.org>
2020-03-18 08:43:59 -0400

    detect/pcre: Add warning for failed registrations
    This commit adds a warning used by the PCRE detect logic when it fails
    to register initialization and free functions for per-thread JIT stack
    handling.
    
    This error code is only used when the platform has PCRE JIT exec
    functionality.

--- src/util-error.c
+++ src/util-error.c
@@ -367,6 +367,7 @@ const char * SCErrorToString(SCError err)
         CASE_CODE (SC_ERR_DATASET);
         CASE_CODE (SC_WARN_ANOMALY_CONFIG);
         CASE_CODE (SC_WARN_ALERT_CONFIG);
+        CASE_CODE (SC_WARN_REGISTRATION_FAILED);
 
         CASE_CODE (SC_ERR_MAX);
     }
