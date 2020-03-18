a2d34279043db7077199a74027e419778f4f5da0
Victor Julien <victor@inliniac.net>
2020-03-18 14:08:37 +0100

    WIP app-layer: define AppLayerTxData (Rust)

--- rust/src/rdp/rdp.rs
+++ rust/src/rdp/rdp.rs
@@ -535,6 +535,7 @@ pub unsafe extern "C" fn rs_rdp_register_parser() {
         get_tx_iterator: None,
         get_tx_detect_flags: None,
         set_tx_detect_flags: None,
+        get_tx_data: None,
     };
 
     /* For 5.0 we want this disabled by default, so check that it
