63863702a4576b709c6002e884696a09070a15c3
Victor Julien <victor@inliniac.net>
2020-03-31 14:04:07 +0200

    detect/parse: fix crash on 'internal' keyword use
    When keyword __flowvar__postmatch__, an internal keyword, is used
    in a rule the 'Setup' func ptr will be NULL. This caused a crash.
    
    (cherry picked from commit 095981cb2a7b204e66770ab5ab4efcf5034caa27)

--- src/detect-parse.c
+++ src/detect-parse.c
@@ -630,7 +630,7 @@ static int SigParseOptions(DetectEngineCtx *de_ctx, Signature *s, char *optstr, 
 
     /* Call option parsing */
     st = SigTableGet(optname);
-    if (st == NULL) {
+    if (st == NULL || st->Setup == NULL) {
         SCLogError(SC_ERR_RULE_KEYWORD_UNKNOWN, "unknown rule keyword '%s'.", optname);
         goto error;
     }
