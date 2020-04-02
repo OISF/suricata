4c97ac5267e9b1779e510691e5dbbe8c2cdcd19d
Jason Ish <jason.ish@oisf.net>
2020-04-02 11:41:06 -0600

    python: setup for building/install with pip
    Writes out a VERSION file that will be bundled in the Python
    sdist allowing the package to be installed with pip, directly
    from the file or from PyPI.
    
    Add a long description which is the description used in PyPI.

--- python/setup.py
+++ python/setup.py
@@ -10,6 +10,7 @@ from distutils.command.build_py import build_py
 
 # Get the Suricata version from configure.ac.
 version = None
+overwrite_version = False
 if os.path.exists("../configure.ac"):
     with open("../configure.ac", "r") as conf:
         for line in conf:
@@ -17,14 +18,25 @@ if os.path.exists("../configure.ac"):
                 m = re.search("AC_INIT\(\[suricata\],\[(\d.+)\]\)", line)
                 if m:
                     version = m.group(1)
+                    overwrite_version = True
                     break
                 else:
                     print("error: failed to parse Suricata version from: %s" % (
                         line.strip()), file=sys.stderr)
                     sys.exit(1)
+elif os.path.exists("VERSION"):
+    with open("./VERSION") as f:
+        version = f.read().strip()
 if version is None:
     print("error: failed to find Suricata version", file=sys.stderr)
     sys.exit(1)
+
+# Write the version file so it can be included in the Python sdist
+# allowing this Python code to be pip installable. We only do this for
+# sdist as it causes issues when doing an out of tree autoconf build.
+if "sdist" in sys.argv and overwrite_version:
+    with open("./VERSION", "w") as f:
+        f.write("{}\n".format(version))
 
 class do_build(build_py):
     def run(self):
@@ -41,9 +53,23 @@ class do_build(build_py):
                 print("error: failed to find defaults.py")
                 sys.exit(1)
 
+# Long description, this is what will show up on PyPI.
+long_description = """
+
+This package contains the Python support tools and libraries for
+Suricata.
+
+It is important to note that these tools are bundled and installed by
+Suricata, so you likely don't need to install this package, unless you
+need access to the libraries inside a virtualenv, or have a need to
+install them without installing Suricata.
+
+"""
+
 setup(
     name="suricata",
     description="Suricata control tools",
+    long_description=long_description,
     version=version,
     author='OISF Developers, Eric Leblond',
     author_email='oisf-devel@lists.openinfosecfoundation.org, eric@regit.org',
