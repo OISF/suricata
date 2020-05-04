b5f7436744126b9c49c3524bf9552777a22a271b
Sascha Steinbiss <satta@debian.org>
2020-05-04 15:29:23 +0200

    rust/mqtt: add MQTT parser

---
+++ rust/src/mqtt/mqtt_message.rs
@@ -0,0 +1,187 @@
+
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+// written by Sascha Steinbiss <sascha@steinbiss.name>
+
+use crate::mqtt::mqtt_property::*;
+use crate::mqtt::parser::*;
+use std::ffi::CStr;
+
+#[derive(Debug)]
+pub struct MQTTMessage {
+    pub header: FixedHeader,
+    pub op: MQTTOperation,
+}
+
+#[derive(Debug)]
+pub enum MQTTOperation {
+    UNASSIGNED,
+    CONNECT(MQTTConnectData),
+    CONNACK(MQTTConnackData),
+    PUBLISH(MQTTPublishData),
+    PUBACK(MQTTMessageIdOnly),
+    PUBREC(MQTTMessageIdOnly),
+    PUBREL(MQTTMessageIdOnly),
+    PUBCOMP(MQTTMessageIdOnly),
+    SUBSCRIBE(MQTTSubscribeData),
+    SUBACK(MQTTSubackData),
+    UNSUBSCRIBE(MQTTUnsubscribeData),
+    UNSUBACK(MQTTUnsubackData),
+    AUTH(MQTTAuthData),
+    PINGREQ,
+    PINGRESP,
+    DISCONNECT(MQTTDisconnectData),
+    // TRUNCATED is special, representing a message that was not parsed
+    // in its entirety due to size constraints. There is no equivalent in
+    // the MQTT specification.
+    TRUNCATED(MQTTTruncatedData),
+}
+
+#[derive(Debug)]
+pub struct MQTTConnectData {
+    pub protocol_string: String,
+    pub protocol_version: u8,
+    pub username_flag: bool,
+    pub password_flag: bool,
+    pub will_retain: bool,
+    pub will_qos: u8,
+    pub will_flag: bool,
+    pub clean_session: bool,
+    pub keepalive: u16,
+    pub client_id: String,
+    pub will_topic: Option<String>,
+    pub will_message: Option<Vec<u8>>,
+    pub username: Option<String>,
+    pub password: Option<Vec<u8>>,
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+    pub will_properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTConnackData {
+    pub return_code: u8,
+    pub session_present: bool,                 // MQTT 3.1.1
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTPublishData {
+    pub topic: String,
+    pub message_id: Option<u16>,
+    pub message: Vec<u8>,
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTMessageIdOnly {
+    pub message_id: u16,
+    pub reason_code: Option<u8>,               // MQTT 5.0
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTSubscribeTopicData {
+    pub topic_name: String,
+    pub qos: u8,
+}
+
+#[derive(Debug)]
+pub struct MQTTSubscribeData {
+    pub message_id: u16,
+    pub topics: Vec<MQTTSubscribeTopicData>,
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTSubackData {
+    pub message_id: u16,
+    pub qoss: Vec<u8>,
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTUnsubscribeData {
+    pub message_id: u16,
+    pub topics: Vec<String>,
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTUnsubackData {
+    pub message_id: u16,
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+    pub reason_codes: Option<Vec<u8>>,         // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTAuthData {
+    pub reason_code: u8,                       // MQTT 5.0
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTDisconnectData {
+    pub reason_code: Option<u8>,               // MQTT 5.0
+    pub properties: Option<Vec<MQTTProperty>>, // MQTT 5.0
+}
+
+#[derive(Debug)]
+pub struct MQTTTruncatedData {
+    pub original_message_type: u8,
+    pub skipped_length: usize,
+}
+
+pub fn message_code_string(op: u8) -> String {
+    match op {
+        1 => "connect",
+        2 => "connack",
+        3 => "publish",
+        4 => "puback",
+        5 => "pubrec",
+        6 => "pubrel",
+        7 => "pubcomp",
+        8 => "subscribe",
+        9 => "suback",
+        10 => "unsubscribe",
+        11 => "unsuback",
+        12 => "pingreq",
+        13 => "pingresp",
+        14 => "disconnect",
+        15 => "auth",
+        _ => "unassigned",
+    }
+    .to_string()
+}
+
+#[no_mangle]
+pub extern "C" fn rs_cstr_message_code(
+    op: *mut u8,
+    msgtype: *const std::os::raw::c_char,
+) -> u8 {
+    unsafe {
+        if let Ok(c_str) = CStr::from_ptr(msgtype).to_str() {
+            for i in 0..16 {
+                if c_str.to_lowercase() == message_code_string(i) {
+                    *op = i as u8;
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
---
+++ src/detect-mqtt-subscribe-topic.c
@@ -0,0 +1,239 @@
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Sascha Steinbiss <sascha@steinbiss.name>
+ */
+
+#include "suricata-common.h"
+
+#include "app-layer.h"
+#include "app-layer-parser.h"
+
+#include "conf.h"
+#include "debug.h"
+#include "decode.h"
+#include "detect.h"
+#include "detect-content.h"
+#include "detect-parse.h"
+#include "detect-pcre.h"
+#include "detect-engine.h"
+#include "detect-engine-content-inspection.h"
+#include "detect-engine-mpm.h"
+#include "detect-engine-prefilter.h"
+#include "detect-mqtt-subscribe-topic.h"
+#include "util-unittest.h"
+#include "util-unittest-helper.h"
+
+#include "rust-bindings.h"
+
+#include "threads.h"
+
+#include "flow.h"
+#include "flow-util.h"
+#include "flow-var.h"
+
+#include "util-debug.h"
+#include "util-unittest.h"
+#include "util-spm.h"
+#include "util-print.h"
+
+static int DetectMQTTSubscribeTopicSetup (DetectEngineCtx *, Signature *, const char *);
+
+static int g_mqtt_subscribe_topic_buffer_id = 0;
+
+struct MQTTSubscribeTopicGetDataArgs {
+    int local_id;
+    void *txv;
+};
+
+static InspectionBuffer *MQTTSubscribeTopicGetData(DetectEngineThreadCtx *det_ctx,
+        const DetectEngineTransforms *transforms,
+        Flow *f, struct MQTTSubscribeTopicGetDataArgs *cbdata, int list_id, bool first)
+{
+    SCEnter();
+
+    InspectionBufferMultipleForList *fb = InspectionBufferGetMulti(det_ctx, list_id);
+    InspectionBuffer *buffer = InspectionBufferMultipleForListGet(fb, cbdata->local_id);
+    if (buffer == NULL)
+        return NULL;
+    if (!first && buffer->inspect != NULL)
+        return buffer;
+
+    const uint8_t *data;
+    uint32_t data_len;
+    if (rs_mqtt_tx_get_topic(cbdata->txv, (uint16_t)cbdata->local_id,
+                &data, &data_len) == 0) {
+        return NULL;
+    }
+
+    InspectionBufferSetup(buffer, data, data_len);
+    InspectionBufferApplyTransforms(buffer, transforms);
+
+    SCReturnPtr(buffer, "InspectionBuffer");
+}
+
+static int DetectEngineInspectMQTTSubscribeTopic(
+        DetectEngineCtx *de_ctx, DetectEngineThreadCtx *det_ctx,
+        const DetectEngineAppInspectionEngine *engine,
+        const Signature *s,
+        Flow *f, uint8_t flags, void *alstate, void *txv, uint64_t tx_id)
+{
+    int local_id = 0;
+
+    const DetectEngineTransforms *transforms = NULL;
+    if (!engine->mpm) {
+        transforms = engine->v2.transforms;
+    }
+
+    while(1) {
+        struct MQTTSubscribeTopicGetDataArgs cbdata = { local_id, txv, };
+        InspectionBuffer *buffer = MQTTSubscribeTopicGetData(det_ctx,
+            transforms, f, &cbdata, engine->sm_list, false);
+        if (buffer == NULL || buffer->inspect == NULL)
+            break;
+
+        det_ctx->buffer_offset = 0;
+        det_ctx->discontinue_matching = 0;
+        det_ctx->inspection_recursion_counter = 0;
+
+        const int match = DetectEngineContentInspection(de_ctx, det_ctx, s, engine->smd,
+                                              NULL, f,
+                                              (uint8_t *)buffer->inspect,
+                                              buffer->inspect_len,
+                                              buffer->inspect_offset, DETECT_CI_FLAGS_SINGLE,
+                                              DETECT_ENGINE_CONTENT_INSPECTION_MODE_STATE);
+        if (match == 1) {
+            return DETECT_ENGINE_INSPECT_SIG_MATCH;
+        }
+        local_id++;
+    }
+    return DETECT_ENGINE_INSPECT_SIG_NO_MATCH;
+}
+
+typedef struct PrefilterMpmMQTTSubscribeTopic {
+    int list_id;
+    const MpmCtx *mpm_ctx;
+    const DetectEngineTransforms *transforms;
+} PrefilterMpmMQTTSubscribeTopic;
+
+/** \brief MQTTSubscribeTopic MQTTSubscribeTopic Mpm prefilter callback
+ *
+ *  \param det_ctx detection engine thread ctx
+ *  \param p packet to inspect
+ *  \param f flow to inspect
+ *  \param txv tx to inspect
+ *  \param pectx inspection context
+ */
+static void PrefilterTxMQTTSubscribeTopic(DetectEngineThreadCtx *det_ctx,
+        const void *pectx,
+        Packet *p, Flow *f, void *txv,
+        const uint64_t idx, const uint8_t flags)
+{
+    SCEnter();
+
+    const PrefilterMpmMQTTSubscribeTopic *ctx = (const PrefilterMpmMQTTSubscribeTopic *)pectx;
+    const MpmCtx *mpm_ctx = ctx->mpm_ctx;
+    const int list_id = ctx->list_id;
+
+    int local_id = 0;
+    while(1) {
+        struct MQTTSubscribeTopicGetDataArgs cbdata = { local_id, txv };
+        InspectionBuffer *buffer = MQTTSubscribeTopicGetData(det_ctx, ctx->transforms,
+                f, &cbdata, list_id, true);
+        if (buffer == NULL)
+            break;
+
+        if (buffer->inspect_len >= mpm_ctx->minlen) {
+            (void)mpm_table[mpm_ctx->mpm_type].Search(mpm_ctx,
+                    &det_ctx->mtcu, &det_ctx->pmq,
+                    buffer->inspect, buffer->inspect_len);
+        }
+        local_id++;
+    }
+}
+
+static void PrefilterMpmMQTTSubscribeTopicFree(void *ptr)
+{
+    SCFree(ptr);
+}
+
+static int PrefilterMpmMQTTSubscribeTopicRegister(DetectEngineCtx *de_ctx,
+        SigGroupHead *sgh, MpmCtx *mpm_ctx,
+        const DetectBufferMpmRegistery *mpm_reg, int list_id)
+{
+    PrefilterMpmMQTTSubscribeTopic *pectx = SCCalloc(1, sizeof(*pectx));
+    if (pectx == NULL)
+        return -1;
+    pectx->list_id = list_id;
+    pectx->mpm_ctx = mpm_ctx;
+    pectx->transforms = &mpm_reg->transforms;
+
+    return PrefilterAppendTxEngine(de_ctx, sgh, PrefilterTxMQTTSubscribeTopic,
+            mpm_reg->app_v2.alproto, mpm_reg->app_v2.tx_min_progress,
+            pectx, PrefilterMpmMQTTSubscribeTopicFree, mpm_reg->pname);
+}
+
+/**
+ * \brief Registration function for keyword: mqtt.subscribe.topic
+ */
+void DetectMQTTSubscribeTopicRegister (void)
+{
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].name = "mqtt.subscribe.topic";
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].alias = "mqtt.unsubscribe.topic";
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].desc = "sticky buffer to match MQTT (UN)SUBSCRIBE topic";
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].url = "/rules/mqtt-keywords.html#mqtt-subscribe-topic";
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].Setup = DetectMQTTSubscribeTopicSetup;
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].flags |= SIGMATCH_NOOPT;
+    sigmatch_table[DETECT_AL_MQTT_SUBSCRIBE_TOPIC].flags |= SIGMATCH_INFO_STICKY_BUFFER;
+
+
+    DetectAppLayerMpmRegister2("mqtt.subscribe.topic", SIG_FLAG_TOSERVER, 1,
+            PrefilterMpmMQTTSubscribeTopicRegister, NULL,
+            ALPROTO_MQTT, 1);
+
+    DetectAppLayerInspectEngineRegister2("mqtt.subscribe.topic",
+            ALPROTO_MQTT, SIG_FLAG_TOSERVER, 1,
+            DetectEngineInspectMQTTSubscribeTopic, NULL);
+
+    DetectBufferTypeSetDescriptionByName("mqtt.subscribe.topic",
+            "(un)subscribe topic query");
+
+    g_mqtt_subscribe_topic_buffer_id = DetectBufferTypeGetByName("mqtt.subscribe.topic");
+}
+
+/**
+ * \brief setup the sticky buffer keyword used in the rule
+ *
+ * \param de_ctx   Pointer to the Detection Engine Context
+ * \param s        Pointer to the Signature to which the current keyword belongs
+ * \param str      Should hold an empty string always
+ *
+ * \retval  0 On success
+ * \retval -1 On failure
+ */
+
+static int DetectMQTTSubscribeTopicSetup(DetectEngineCtx *de_ctx, Signature *s, const char *str)
+{
+    if (DetectBufferSetActiveList(s, g_mqtt_subscribe_topic_buffer_id) < 0)
+        return -1;
+    if (DetectSignatureSetAppProto(s, ALPROTO_MQTT) < 0)
+        return -1;
+    return 0;
+}
