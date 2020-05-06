b912ba792498910d74bfba135aff5d45a36db115
MalakhatkoVadym <v.malakhatko@sirinsoftware.com>
2020-05-06 20:32:37 +0300

    runmode: add testimony runmode

---
+++ src/runmode-testimony.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2007-2012 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/** \file
+ *
+ *  \author Vadym Malakhatko <v.malakhatko@sirinsoftware.com>
+ */
+
+#ifndef __RUNMODE_TESTIMONY_H__
+#define __RUNMODE_TESTIMONY_H__
+
+int RunModeIdsTestimonySingle(void);
+int RunModeIdsTestimonyWorkers(void);
+
+const char *RunModeTestimonyGetDefaultMode(void);
+
+void RunModeIdsTestimonyRegister(void);
+
+#endif /* __RUNMODE_TESTIMONY_H__ */
