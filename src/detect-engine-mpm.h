d7a3eafb893e3be38f89da1682a89db657fb9c6a
Philippe Antoine <contact@catenacyber.fr>
2020-05-16 14:00:06 +0200

    detect: generic structures for mpm with lists

--- src/detect-engine-mpm.h
+++ src/detect-engine-mpm.h
@@ -120,5 +120,16 @@ int PrefilterGenericMpmPktRegister(DetectEngineCtx *de_ctx,
          const DetectBufferMpmRegistery *mpm_reg, int list_id);
 
 
+typedef struct PrefilterMpmListId {
+    int list_id;
+    const MpmCtx *mpm_ctx;
+    const DetectEngineTransforms *transforms;
+} PrefilterMpmListId;
+
+struct MpmListIdDataArgs {
+    int local_id;  /**< used as index into thread inspect array */
+    void *txv;
+};
+
 #endif /* __DETECT_ENGINE_MPM_H__ */
 
