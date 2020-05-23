eb93afd55e58e51eff1b08b2bc4df1cebe4889f0
MalakhatkoVadym <v.malakhatko@sirinsoftware.com>
2020-05-23 16:12:57 +0300

    rust/ssh: add key exchange fields parsing and functions to get hassh fields

--- rust/src/ssh/detect.rs
+++ rust/src/ssh/detect.rs
@@ -90,3 +90,135 @@ pub extern "C" fn rs_ssh_tx_get_software(
 
     return 0;
 }
+
+#[no_mangle]
+pub extern "C" fn rs_ssh_tx_get_hassh(
+    tx: *mut std::os::raw::c_void,
+    buffer: *mut *const u8,
+    buffer_len: *mut u32,
+    direction: u8,
+) -> u8 {
+    let tx = cast_pointer!(tx, SSHTransaction);
+    match direction {
+        STREAM_TOSERVER => {
+            let m = &tx.cli_hdr.hassh;
+            if m.len() > 0 {
+                unsafe {
+                    *buffer = m.as_ptr();
+                }
+                unsafe {
+                    *buffer_len = m.len() as u32;
+                }
+                return 1;
+            }
+        }
+        _ => {}
+    }
+    unsafe {
+        *buffer = ptr::null();
+    }
+    unsafe {
+        *buffer_len = 0;
+    }
+
+    return 0;
+}
+
+#[no_mangle]
+pub extern "C" fn rs_ssh_tx_get_hassh_server(
+    tx: *mut std::os::raw::c_void,
+    buffer: *mut *const u8,
+    buffer_len: *mut u32,
+    direction: u8,
+) -> u8 {
+    let tx = cast_pointer!(tx, SSHTransaction);
+    match direction {
+        STREAM_TOCLIENT => {
+            let m = &tx.srv_hdr.hassh;
+            if m.len() > 0 {
+                unsafe {
+                    *buffer = m.as_ptr();
+                }
+                unsafe {
+                    *buffer_len = m.len() as u32;
+                }
+                return 1;
+            }
+        }
+        _ => {}
+    }
+    unsafe {
+        *buffer = ptr::null();
+    }
+    unsafe {
+        *buffer_len = 0;
+    }
+
+    return 0;
+}
+
+#[no_mangle]
+pub extern "C" fn rs_ssh_tx_get_hassh_string(
+    tx: *mut std::os::raw::c_void,
+    buffer: *mut *const u8,
+    buffer_len: *mut u32,
+    direction: u8,
+) -> u8 {
+    let tx = cast_pointer!(tx, SSHTransaction);
+    match direction {
+        STREAM_TOSERVER => {
+            let m = &tx.cli_hdr.hassh_string;
+            if m.len() > 0 {
+                unsafe {
+                    *buffer = m.as_ptr();
+                }
+                unsafe {
+                    *buffer_len = m.len() as u32;
+                }
+                return 1;
+            }
+        }
+        _ => {}
+    }
+    unsafe {
+        *buffer = ptr::null();
+    }
+    unsafe {
+        *buffer_len = 0;
+    }
+
+    return 0;
+}
+
+#[no_mangle]
+pub extern "C" fn rs_ssh_tx_get_hassh_server_string(
+    tx: *mut std::os::raw::c_void,
+    buffer: *mut *const u8,
+    buffer_len: *mut u32,
+    direction: u8,
+) -> u8 {
+    let tx = cast_pointer!(tx, SSHTransaction);
+    match direction {
+        STREAM_TOCLIENT => {
+            let m = &tx.srv_hdr.hassh_string;
+            if m.len() > 0 {
+                unsafe {
+                    *buffer = m.as_ptr();
+                }
+                unsafe {
+                    *buffer_len = m.len() as u32;
+                }
+                return 1;
+            }
+        }
+        _ => {}
+    }
+    unsafe {
+        *buffer = ptr::null();
+    }
+    unsafe {
+        *buffer_len = 0;
+    }
+
+    return 0;
+}
