a65c272fa1b8b9bc808abd24ea5986df91b02eab
Victor Julien <victor@inliniac.net>
2020-05-26 14:34:57 +0200

    detect: store detect flags in AppLayerTxData

--- src/detect-engine-state.h
+++ src/detect-engine-state.h
@@ -97,6 +97,7 @@ typedef struct DetectEngineState_ {
 typedef struct DetectTransaction_ {
     void *tx_ptr;
     const uint64_t tx_id;
+    struct AppLayerTxData *tx_data_ptr;
     DetectEngineStateDirection *de_state;
     const uint64_t detect_flags;            /* detect flags get/set from/to applayer */
     uint64_t prefilter_flags;               /* prefilter flags for direction, to be updated by prefilter code */
