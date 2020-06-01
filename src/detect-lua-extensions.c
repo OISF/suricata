e94dde9e1d93e428a6aa2f9544ff02f41db1d83e
vadym <vadym@localhost.localdomain>
2020-06-01 17:58:28 +0300

    lua: add functions to get hassh HasshGet, HasshGetString, HasshServerGet, HasshServerGetString

--- src/detect-lua-extensions.c
+++ src/detect-lua-extensions.c
@@ -69,6 +69,7 @@
 #include "util-lua-ja3.h"
 #include "util-lua-tls.h"
 #include "util-lua-ssh.h"
+#include "util-lua-hassh.h"
 #include "util-lua-smtp.h"
 #include "util-lua-dnp3.h"
 #include "detect-lua-extensions.h"
@@ -536,6 +537,7 @@ int LuaRegisterExtensions(lua_State *lua_state)
     LuaRegisterJa3Functions(lua_state);
     LuaRegisterTlsFunctions(lua_state);
     LuaRegisterSshFunctions(lua_state);
+    LuaRegisterHasshFunctions(lua_state);
     LuaRegisterSmtpFunctions(lua_state);
     LuaRegisterDNP3Functions(lua_state);
     return 0;
