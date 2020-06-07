fa7e4ad557abd0284212aed489bad819bc07403a
Victor Julien <victor@inliniac.net>
2020-06-07 16:20:35 +0200

    enip: support AppLayerTxData

--- src/app-layer-enip-common.h
+++ src/app-layer-enip-common.h
@@ -210,8 +210,7 @@ typedef struct ENIPTransaction_
 
     TAILQ_ENTRY(ENIPTransaction_) next;
     DetectEngineState *de_state;
-    uint64_t detect_flags_ts;
-    uint64_t detect_flags_tc;
+    AppLayerTxData tx_data;
 } ENIPTransaction;
 
 /** \brief Per flow ENIP state container */
