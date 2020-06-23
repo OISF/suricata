bb45b28e472b929aef3dc70fbdbe0243d7b37e37
Philippe Antoine <contact@catenacyber.fr>
2020-06-23 09:56:32 +0200

    fixup

--- rust/src/http2/http2.rs
+++ rust/src/http2/http2.rs
@@ -718,8 +718,6 @@ pub unsafe extern "C" fn rs_http2_register_parser() {
         get_eventinfo_byid: Some(rs_http2_state_get_event_info_by_id),
         localstorage_new: None,
         localstorage_free: None,
-        get_tx_mpm_id: None,
-        set_tx_mpm_id: None,
         get_files: None,
         get_tx_iterator: Some(rs_http2_state_get_tx_iterator),
         get_tx_detect_flags: Some(rs_http2_get_tx_detect_flags),
