537fb7a1c60469f1fddf7bfdd90369213e8a2910
Shivani Bhardwaj <shivanib134@gmail.com>
2020-07-01 17:29:01 +0530

    hyperscan: better error message if not compiled

--- src/detect-engine-mpm.c
+++ src/detect-engine-mpm.c
@@ -643,6 +643,12 @@ uint16_t PatternMatchDefaultMatcher(void)
             }
         }
 
+#ifndef BUILD_HYPERSCAN
+    if ((mpm_algo != NULL) && (strcmp(mpm_algo, "hs") == 0)) {
+        FatalError(SC_ERR_INVALID_VALUE, "Hyperscan (hs) support for mpm-algo is "
+                   "not compiled into Suricata.");
+    }
+#endif
         SCLogError(SC_ERR_INVALID_YAML_CONF_ENTRY, "Invalid mpm algo supplied "
                 "in the yaml conf file: \"%s\"", mpm_algo);
         exit(EXIT_FAILURE);
