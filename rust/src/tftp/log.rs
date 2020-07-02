9ca29e014820fe9bd761d204b743d6c8e331c673
Jeff Lucovsky <jeff@lucovsky.org>
2020-07-02 10:01:12 -0400

    output/tftp: Convert to JsonBuilder
    This commit converts the TFTP logging mechanisms to JsonBuilder.

--- rust/src/tftp/log.rs
+++ rust/src/tftp/log.rs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Open Information Security Foundation
+/* Copyright (C) 2017-2020 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -17,19 +17,34 @@
 
 // written by Clément Galland <clement.galland@epita.fr>
 
-use crate::json::*;
+use crate::jsonbuilder::{JsonBuilder, JsonError};
 use crate::tftp::tftp::*;
 
-#[no_mangle]
-pub extern "C" fn rs_tftp_log_json_request(tx: &mut TFTPTransaction) -> *mut JsonT
+fn tftp_log_request(tx: &mut TFTPTransaction,
+                 jb: &mut JsonBuilder)
+                 -> Result<bool, JsonError>
 {
-    let js = Json::object();
     match tx.opcode {
-        1 => js.set_string("packet", "read"),
-        2 => js.set_string("packet", "write"),
-        _ => js.set_string("packet", "error")
+        1 => jb.set_string("packet", "read")?,
+        2 => jb.set_string("packet", "write")?,
+        _ => jb.set_string("packet", "error")?
     };
-    js.set_string("file", tx.filename.as_str());
-    js.set_string("mode", tx.mode.as_str());
-    js.unwrap()
+    jb.set_string("file", tx.filename.as_str())?;
+    jb.set_string("mode", tx.mode.as_str())?;
+    return Ok(true);
+}
+
+#[no_mangle]
+pub extern "C" fn rs_tftp_log_json_request(tx: &mut TFTPTransaction,
+                                       jb: &mut JsonBuilder)
+                                       -> bool
+{
+    match tftp_log_request(tx,jb) {
+        Ok(false) | Err(_) => {
+            return false;
+        }
+        Ok(true) => {
+            return true;
+        }
+    }
 }
