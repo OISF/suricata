a4b8c5ae8a7b6f7a7ffbd8db8a8747663549ac79
Jason Ish <jason.ish@oisf.net>
2020-07-03 16:25:55 -0600

    util-error: define generic plugin error code

--- src/util-error.c
+++ src/util-error.c
@@ -370,6 +370,7 @@ const char * SCErrorToString(SCError err)
         CASE_CODE (SC_WARN_REGISTRATION_FAILED);
         CASE_CODE (SC_ERR_ERF_BAD_RLEN);
         CASE_CODE (SC_WARN_ERSPAN_CONFIG);
+        CASE_CODE (SC_ERR_PLUGIN);
 
         CASE_CODE (SC_ERR_MAX);
     }
