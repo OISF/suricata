c06eed796743e330d0453d81e46a0742f7e64f00
Jason Ish <jason.ish@oisf.net>
2020-07-05 22:46:39 -0600

    plugin: pfring example plugin

---
+++ examples/plugins/pf-ring/plugin.c
@@ -0,0 +1,33 @@
+#include "suricata-plugin.h"
+
+#include "source-pfring.h"
+#include "runmode-pfring.h"
+
+static char *source_name = "pfring-plugin";
+
+void InitSourcePlugin(int plugin_slot, int receive_slot, int decode_slot)
+{
+    RunModeIdsPfringPluginRegister(plugin_slot);
+    TmModuleReceivePfringPluginRegister(receive_slot);
+    TmModuleDecodePfringPluginRegister(decode_slot);
+}
+
+void SCPluginInit(void)
+{
+    SCLogNotice("SCPluginInit");
+    SourcePlugin *plugin = SCCalloc(1, sizeof(SourcePlugin));
+    if (plugin == NULL) {
+        FatalError(SC_ERR_MEM_ALLOC, "Failed to allocate memory for source plugin");
+    }
+    plugin->name = source_name;
+    plugin->Init = InitSourcePlugin;
+    plugin->GetDefaultMode = RunModeIdsPfringPluginGetDefaultMode;
+    SCPluginRegisterSource(plugin);
+}
+
+const SCPlugin PluginSpec = {
+    .name = "source-pfring",
+    .author = "Some Developer",
+    .license = "GPLv2",
+    .Init = SCPluginInit,
+};
---
+++ examples/plugins/pf-ring/source-pfring.h
@@ -0,0 +1,80 @@
+/* Copyright (C) 2007-2010 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author William Metcalf <william.metcalf@gmail.com>
+ */
+
+#ifndef __SOURCE_PFRING_PLUGIN_H__
+#define __SOURCE_PFRING_PLUGIN_H__
+
+#define PFRING_IFACE_NAME_LENGTH 48
+
+#include "decode.h"
+
+#include <config.h>
+
+typedef struct PfringPluginThreadVars_ PfringPluginThreadVars;
+
+/* PfringPluginIfaceConfig flags */
+#define PFRING_CONF_FLAGS_CLUSTER (1 << 0)
+#define PFRING_CONF_FLAGS_BYPASS  (1 << 1)
+
+typedef struct PfringPluginIfaceConfig_
+{
+    uint32_t flags;
+
+    /* cluster param */
+    uint16_t cluster_id;
+    unsigned int ctype;
+
+    char iface[PFRING_IFACE_NAME_LENGTH];
+    /* number of threads */
+    uint16_t threads;
+
+    char *bpf_filter;
+
+    ChecksumValidationMode checksum_mode;
+    SC_ATOMIC_DECLARE(unsigned int, ref);
+    void (*DerefFunc)(void *);
+} PfringPluginIfaceConfig;
+
+/**
+ * \brief per packet Pfring vars
+ *
+ * This structure is used to pass packet metadata in callbacks.
+ */
+typedef struct PfringPluginPacketVars_
+{
+    PfringPluginThreadVars *ptv;
+    uint32_t flow_id;
+} PfringPluginPacketVars;
+
+
+void TmModuleReceivePfringPluginRegister(int slot);
+void TmModuleDecodePfringPluginRegister(int slot);
+
+int PfringPluginConfGetThreads(void);
+void PfringPluginLoadConfig(void);
+
+/* We don't have to use an enum that sucks in our code */
+#define CLUSTER_FLOW 0
+#define CLUSTER_ROUND_ROBIN 1
+#define CLUSTER_FLOW_5_TUPLE 4
+#endif /* __SOURCE_PFRING_H__ */
