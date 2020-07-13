428b06426ceb99fb09d398523ce4d1a5687610fe
Jason Ish <jason.ish@oisf.net>
2020-07-13 10:24:24 -0600

    applayer: add flags to parser registration struct
    This will allow Rust parsers to register for gap handing from
    Rust (some Rust parsers do handle gaps, but they set the flag
    from C).
    
    (cherry picked from commit 53aa967e0b89417f2714fcbcbb6463be6ce8282e)

--- rust/src/ikev2/ikev2.rs
+++ rust/src/ikev2/ikev2.rs
@@ -732,6 +732,7 @@ pub unsafe extern "C" fn rs_register_ikev2_parser() {
         get_tx_iterator    : None,
         get_tx_detect_flags: None,
         set_tx_detect_flags: None,
+        flags              : 0,
     };
 
     let ip_proto_str = CString::new("udp").unwrap();
