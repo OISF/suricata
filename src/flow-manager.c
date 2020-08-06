e829967df2a4536bc40f4dae51987b7e25def788
Victor Julien <victor@inliniac.net>
2020-08-06 14:38:36 +0200

    flow/spare: implement pool shrinking
    Remove at most one block per run, so it shrinks slowly.

--- src/flow-manager.c
+++ src/flow-manager.c
@@ -872,7 +872,7 @@ static TmEcode FlowManager(ThreadVars *th_v, void *thread_data)
                 const uint32_t sq_len = FlowSpareGetPoolSize();
                 const uint32_t spare_perc = sq_len * 100 / flow_config.prealloc;
                 /* see if we still have enough spare flows */
-                if (spare_perc < 90) {
+                if (spare_perc < 90 || spare_perc > 110) {
                     FlowSparePoolUpdate(sq_len);
                 }
             }
