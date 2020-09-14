1d40d0c5f93f7233834eb1eeae569b7f794a8a6b
Jason Ish <jason.ish@oisf.net>
2020-09-14 13:31:56 -0600

    rdp: set unidirection transaction flag

--- rust/src/rdp/rdp.rs
+++ rust/src/rdp/rdp.rs
@@ -487,7 +487,7 @@ pub unsafe extern "C" fn rs_rdp_register_parser() {
         get_tx_iterator: None,
         get_tx_data: rs_rdp_get_tx_data,
         apply_tx_config: None,
-        flags: 0,
+        flags: APP_LAYER_PARSER_OPT_UNIDIR_TXS,
         truncate: None,
     };
 
