52434c8df4dda7720ab67ec0deb87d4ef50e851a
Sascha Steinbiss <satta@debian.org>
2021-01-03 23:42:24 +0100

    ikev1: add ikev1 parser

---
+++ rust/src/ike/ikev1.rs
@@ -0,0 +1,162 @@
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+// Author: Frank Honza <frank.honza@dcso.de>
+
+use crate::applayer::*;
+use crate::common::to_hex;
+use crate::core::STREAM_TOSERVER;
+use crate::ike::ike::{IKEState, IkeEvent};
+use crate::ike::parser::*;
+use nom;
+use std;
+use std::collections::HashSet;
+
+#[derive(Default)]
+pub struct IkeV1Header {
+    pub exchange_type: Option<u8>,
+    pub encrypted_payloads: bool,
+
+    pub key_exchange: Vec<u8>,
+    pub nonce: Vec<u8>,
+    pub vendor_ids: Vec<String>,
+}
+
+#[derive(Default)]
+pub struct Ikev1ParticipantData {
+    pub key_exchange: String,
+    pub nonce: String,
+    pub vendor_ids: HashSet<String>,
+    /// nested Vec, outer Vec per Proposal/Transform, inner Vec has the list of attributes.
+    pub transforms: Vec<Vec<SaAttribute>>,
+}
+
+impl Ikev1ParticipantData {
+    pub fn reset(&mut self) {
+        self.key_exchange.clear();
+        self.nonce.clear();
+        self.vendor_ids.clear();
+        self.transforms.clear();
+    }
+
+    pub fn update(
+        &mut self, key_exchange: &String, nonce: &String, vendor_ids: &Vec<String>,
+        transforms: &Vec<Vec<SaAttribute>>,
+    ) {
+        self.key_exchange = key_exchange.clone();
+        self.nonce = nonce.clone();
+        self.vendor_ids.extend(vendor_ids.iter().cloned());
+        self.transforms.extend(transforms.iter().cloned());
+    }
+}
+
+#[derive(Default)]
+pub struct Ikev1Container {
+    pub domain_of_interpretation: Option<u32>,
+    pub client: Ikev1ParticipantData,
+    pub server: Ikev1ParticipantData,
+}
+
+pub fn handle_ikev1(
+    state: &mut IKEState, current: &[u8], isakmp_header: IsakmpHeader, direction: u8,
+) -> AppLayerResult {
+    let mut tx = state.new_tx();
+
+    tx.ike_version = 1;
+    tx.hdr.spi_initiator = format!("{:016x}", isakmp_header.init_spi);
+    tx.hdr.spi_responder = format!("{:016x}", isakmp_header.resp_spi);
+    tx.hdr.maj_ver = isakmp_header.maj_ver;
+    tx.hdr.min_ver = isakmp_header.min_ver;
+    tx.hdr.ikev1_header.exchange_type = Some(isakmp_header.exch_type);
+    tx.hdr.msg_id = isakmp_header.msg_id;
+    tx.hdr.flags = isakmp_header.flags;
+
+    let mut cur_payload_type = isakmp_header.next_payload;
+    let mut payload_types: HashSet<u8> = HashSet::new();
+    payload_types.insert(cur_payload_type);
+
+    if isakmp_header.flags & 0x01 != 0x01 {
+        match parse_ikev1_payload_list(current) {
+            Ok((rem, payload_list)) => {
+                for isakmp_payload in payload_list {
+                    if let Err(_) = parse_payload(
+                        cur_payload_type,
+                        isakmp_payload.data,
+                        isakmp_payload.data.len() as u16,
+                        &mut state.ikev1_container.domain_of_interpretation,
+                        &mut tx.hdr.ikev1_header.key_exchange,
+                        &mut tx.hdr.ikev1_header.nonce,
+                        &mut tx.hdr.ikev1_transforms,
+                        &mut tx.hdr.ikev1_header.vendor_ids,
+                        &mut payload_types,
+                    ) {
+                        SCLogDebug!("Error while parsing IKEV1 payloads");
+                        return AppLayerResult::err();
+                    }
+
+                    cur_payload_type = isakmp_payload.payload_header.next_payload;
+                }
+
+                if payload_types.contains(&(IsakmpPayloadType::SecurityAssociation as u8)) {
+                    // clear transforms on a new SA in case there is happening a new key exchange
+                    // on the same flow, elsewise properties would be added to the old/other SA
+                    if direction == STREAM_TOSERVER {
+                        state.ikev1_container.client.reset();
+                    } else {
+                        state.ikev1_container.server.reset();
+                    }
+                }
+
+                // add transaction values to state values
+                if direction == STREAM_TOSERVER {
+                    state.ikev1_container.client.update(
+                        &to_hex(tx.hdr.ikev1_header.key_exchange.as_ref()),
+                        &to_hex(tx.hdr.ikev1_header.nonce.as_ref()),
+                        &tx.hdr.ikev1_header.vendor_ids,
+                        &tx.hdr.ikev1_transforms,
+                    );
+                } else {
+                    state.ikev1_container.server.update(
+                        &to_hex(tx.hdr.ikev1_header.key_exchange.as_ref()),
+                        &to_hex(tx.hdr.ikev1_header.nonce.as_ref()),
+                        &tx.hdr.ikev1_header.vendor_ids,
+                        &tx.hdr.ikev1_transforms,
+                    );
+                }
+
+                if rem.len() > 0 {
+                    // more data left unread than should be
+                    SCLogDebug!("Unread Payload Data");
+                    state.set_event(IkeEvent::PayloadExtraData);
+                }
+            }
+            Err(nom::Err::Incomplete(_)) => {
+                SCLogDebug!("Insufficient data while parsing IKEV1");
+                return AppLayerResult::err();
+            }
+            Err(_) => {
+                SCLogDebug!("Error while parsing payloads and adding to the state");
+                return AppLayerResult::err();
+            }
+        }
+    }
+
+    tx.payload_types.ikev1_payload_types = Some(payload_types);
+    tx.hdr.ikev1_header.encrypted_payloads = isakmp_header.flags & 0x01 == 0x01;
+    state.transactions.push(tx);
+    return AppLayerResult::ok();
+}
---
+++ src/detect-ike-key-exchange-payload.c
@@ -0,0 +1,120 @@
+/* Copyright (C) 2020 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ *
+ * \author Frank Honza <frank.honza@dcso.de>
+ */
+
+#include "suricata-common.h"
+#include "threads.h"
+#include "debug.h"
+#include "decode.h"
+#include "detect.h"
+
+#include "detect-parse.h"
+#include "detect-engine.h"
+#include "detect-engine-mpm.h"
+#include "detect-engine-prefilter.h"
+#include "detect-urilen.h"
+
+#include "flow.h"
+#include "flow-var.h"
+#include "flow-util.h"
+
+#include "util-debug.h"
+#include "util-unittest.h"
+#include "util-unittest-helper.h"
+#include "util-spm.h"
+
+#include "app-layer.h"
+#include "app-layer-parser.h"
+
+#include "detect-ike-key-exchange-payload.h"
+#include "stream-tcp.h"
+
+#include "rust.h"
+#include "app-layer-ike.h"
+#include "rust-bindings.h"
+
+#define KEYWORD_NAME_KEY_EXCHANGE "ike.key_exchange_payload"
+#define KEYWORD_DOC_KEY_EXCHANGE  "ike-keywords.html#ike-key_exchange_payload";
+#define BUFFER_NAME_KEY_EXCHANGE  "ike.key_exchange_payload"
+#define BUFFER_DESC_KEY_EXCHANGE  "ike key_exchange payload"
+
+static int g_buffer_key_exchange_id = 0;
+
+static int DetectKeyExchangeSetup(DetectEngineCtx *de_ctx, Signature *s, const char *str)
+{
+    if (DetectBufferSetActiveList(s, g_buffer_key_exchange_id) < 0)
+        return -1;
+
+    if (DetectSignatureSetAppProto(s, ALPROTO_IKE) < 0)
+        return -1;
+
+    return 0;
+}
+
+static InspectionBuffer *GetKeyExchangeData(DetectEngineThreadCtx *det_ctx,
+        const DetectEngineTransforms *transforms, Flow *_f, const uint8_t _flow_flags, void *txv,
+        const int list_id)
+{
+    InspectionBuffer *buffer = InspectionBufferGet(det_ctx, list_id);
+    if (buffer->inspect == NULL) {
+        const uint8_t *b = NULL;
+        uint32_t b_len = 0;
+
+        if (rs_ike_state_get_key_exchange(txv, &b, &b_len) != 1)
+            return NULL;
+        if (b == NULL || b_len == 0)
+            return NULL;
+
+        InspectionBufferSetup(det_ctx, list_id, buffer, b, b_len);
+        InspectionBufferApplyTransforms(buffer, transforms);
+    }
+
+    return buffer;
+}
+
+void DetectIkeKeyExchangeRegister(void)
+{
+    // register key_exchange
+    sigmatch_table[DETECT_AL_IKE_KEY_EXCHANGE].name = KEYWORD_NAME_KEY_EXCHANGE;
+    sigmatch_table[DETECT_AL_IKE_KEY_EXCHANGE].url =
+            "/rules/" KEYWORD_DOC_KEY_EXCHANGE sigmatch_table[DETECT_AL_IKE_KEY_EXCHANGE].desc =
+                    "sticky buffer to match on the IKE key_exchange_payload";
+    sigmatch_table[DETECT_AL_IKE_KEY_EXCHANGE].Setup = DetectKeyExchangeSetup;
+    sigmatch_table[DETECT_AL_IKE_KEY_EXCHANGE].flags |=
+            SIGMATCH_NOOPT | SIGMATCH_INFO_STICKY_BUFFER;
+
+    DetectAppLayerInspectEngineRegister2(BUFFER_NAME_KEY_EXCHANGE, ALPROTO_IKE, SIG_FLAG_TOSERVER,
+            1, DetectEngineInspectBufferGeneric, GetKeyExchangeData);
+
+    DetectAppLayerMpmRegister2(BUFFER_NAME_KEY_EXCHANGE, SIG_FLAG_TOSERVER, 1,
+            PrefilterGenericMpmRegister, GetKeyExchangeData, ALPROTO_IKE, 1);
+
+    DetectAppLayerInspectEngineRegister2(BUFFER_NAME_KEY_EXCHANGE, ALPROTO_IKE, SIG_FLAG_TOCLIENT,
+            1, DetectEngineInspectBufferGeneric, GetKeyExchangeData);
+
+    DetectAppLayerMpmRegister2(BUFFER_NAME_KEY_EXCHANGE, SIG_FLAG_TOCLIENT, 1,
+            PrefilterGenericMpmRegister, GetKeyExchangeData, ALPROTO_IKE, 1);
+
+    DetectBufferTypeSetDescriptionByName(BUFFER_NAME_KEY_EXCHANGE, BUFFER_DESC_KEY_EXCHANGE);
+
+    g_buffer_key_exchange_id = DetectBufferTypeGetByName(BUFFER_NAME_KEY_EXCHANGE);
+    SCLogDebug("registering " BUFFER_NAME_KEY_EXCHANGE " rule option");
+}
