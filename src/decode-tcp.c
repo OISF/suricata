5cfcee91054dc19780f39d2269c803620f5f519a
Jeff Lucovsky <jeff@lucovsky.org>
2021-01-05 08:17:09 -0500

    decode/tcp: Improved handling of TFO options
    This commit improves handling of TCP fast open options
    - Option length must be in [6, 18]
    - Option length must be an even value

--- src/decode-tcp.c
+++ src/decode-tcp.c
@@ -157,7 +157,7 @@ static void DecodeTCPOptions(Packet *p, const uint8_t *pkt, uint16_t pktlen)
                     if ((olen != 2) &&
                            (olen < TCP_OPT_TFO_MIN_LEN ||
                             olen > TCP_OPT_TFO_MAX_LEN ||
-                            !((olen - 2) % 8 == 0)))
+                            !(((olen - 2) & 0x1) == 0)))
                     {
                         ENGINE_SET_EVENT(p,TCP_OPT_INVALID_LEN);
                     } else {
