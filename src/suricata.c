7bb917b835287bbcabe069267d9743993a55ff2a
Jason Ish <jason.ish@oisf.net>
2021-01-08 09:05:32 -0600

    suricata.c: disable clang-format on long_opts init
    clang-format will reformat this to have multiple options on a
    single line which is hard to read and I don't see a specific
    clang-format option to format this better. So for now,
    disable clang-format on this section of code.

--- src/suricata.c
+++ src/suricata.c
@@ -1185,6 +1185,7 @@ static TmEcode ParseCommandLine(int argc, char** argv, SCInstance *suri)
     g_ut_covered = 0;
 #endif
 
+    // clang-format off
     struct option long_opts[] = {
         {"dump-config", 0, &dump_config, 1},
         {"dump-features", 0, &dump_features, 1},
@@ -1246,6 +1247,7 @@ static TmEcode ParseCommandLine(int argc, char** argv, SCInstance *suri)
 #endif
         {NULL, 0, NULL, 0}
     };
+    // clang-format on
 
     /* getopt_long stores the option index here. */
     int option_index = 0;
