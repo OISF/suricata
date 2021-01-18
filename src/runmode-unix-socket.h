84d34a3b324f7771f0498a6cdce9e9d64136d178
Eric Leblond <eric@regit.org>
2021-01-18 22:11:15 +0100

    dataset: add dataset-clear command

--- src/runmode-unix-socket.h
+++ src/runmode-unix-socket.h
@@ -34,6 +34,7 @@ TmEcode UnixSocketPcapFile(TmEcode tm, struct timespec *last_processed);
 TmEcode UnixSocketDatasetAdd(json_t *cmd, json_t* answer, void *data);
 TmEcode UnixSocketDatasetRemove(json_t *cmd, json_t* answer, void *data);
 TmEcode UnixSocketDatasetDump(json_t *cmd, json_t *answer, void *data);
+TmEcode UnixSocketDatasetClear(json_t *cmd, json_t *answer, void *data);
 TmEcode UnixSocketRegisterTenantHandler(json_t *cmd, json_t* answer, void *data);
 TmEcode UnixSocketUnregisterTenantHandler(json_t *cmd, json_t* answer, void *data);
 TmEcode UnixSocketRegisterTenant(json_t *cmd, json_t* answer, void *data);
