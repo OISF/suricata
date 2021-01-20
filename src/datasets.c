4edf75841a26e1016f5507fbe0df5683322e51a7
Eric Leblond <eric@regit.org>
2021-01-20 21:17:04 +0100

    dataset: fix dataset string lookup
    The data was unlocked but the use_cnt was not decreased resulting
    in the data entry not being removable.
    
    (cherry picked from commit 64f994f753b9109c8f788e3a6dbe4c72f6e69d94)

--- src/datasets.c
+++ src/datasets.c
@@ -705,7 +705,7 @@ static int DatasetLookupString(Dataset *set, const uint8_t *data, const uint32_t
     StringType lookup = { .ptr = (uint8_t *)data, .len = data_len, .rep.value = 0 };
     THashData *rdata = THashLookupFromHash(set->hash, &lookup);
     if (rdata) {
-        THashDataUnlock(rdata);
+        DatasetUnlockData(rdata);
         return 1;
     }
     return 0;
