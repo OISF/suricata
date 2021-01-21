0ef5d5c4e439a2420c7463b5bf74ff00b55cd7bf
Victor Julien <victor@inliniac.net>
2021-01-21 15:48:43 +0100

    SQUASH disable debug validation. Our goal is not to find crashes, but to max out coverage

--- qa/run-ossfuzz-corpus.sh
+++ qa/run-ossfuzz-corpus.sh
@@ -5,9 +5,9 @@ do
     echo "target $target"
     #download public corpus
     rm -f public.zip
-    wget "https://storage.googleapis.com/suricata-backup.clusterfuzz-external.appspot.com/corpus/libFuzzer/suricata_$target/public.zip"
+    wget --quiet "https://storage.googleapis.com/suricata-backup.clusterfuzz-external.appspot.com/corpus/libFuzzer/suricata_$target/public.zip"
     rm -rf corpus_$target
     unzip -q public.zip -d corpus_$target
-    #run target on corpus
+    #run target on corpus. Don't fail CI if the target fails.
     ./src/$target corpus_$target || true
 done
