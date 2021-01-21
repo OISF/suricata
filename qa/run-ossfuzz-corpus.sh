a62d821b125704758503440c317febfa21b5ec5f
Victor Julien <victor@inliniac.net>
2021-01-21 13:28:04 +0100

    fuzz: run OSS-Fuzz corpus and track coverage

---
+++ qa/run-ossfuzz-corpus.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+ls src/tests/fuzz/fuzz_*.c | sed 's/\.c//' | while read ftarget
+do
+    target=$(basename $ftarget)
+    echo "target $target"
+    #download public corpus
+    rm -f public.zip
+    wget --quiet "https://storage.googleapis.com/suricata-backup.clusterfuzz-external.appspot.com/corpus/libFuzzer/suricata_$target/public.zip"
+    rm -rf corpus_$target
+    unzip -q public.zip -d corpus_$target
+    #run target on corpus. Don't fail CI if the target fails.
+    find corpus_$target -type f | xargs -L1 ./src/$target &> /dev/null
+done
