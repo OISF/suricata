cafbea553da169db1f28d876a25bac6533f9e752
Eric Leblond <eric@regit.org>
2021-03-11 23:55:53 +0100

    suricata: introduce global linktype
    As Suricata is not supporting pcap-ng we have to stick with one single
    datalink type for the capture if ever we want to do pcap logging.
    Assuming this, this patch introduces a function to set the link
    type globally. This will be used with pcap conditional logging
    to get the logging of TCP segments with the correct link type.

---
+++ src/util-datalink.c
@@ -0,0 +1,44 @@
+/* Copyright (C) 2021 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "suricata-common.h"
+#include "conf.h"
+#include "util-datalink.h"
+
+int g_datalink_value = LINKTYPE_NULL;
+int g_datalink_is_multiple = 0;
+
+void DatalinkSetGlobalType(int datalink)
+{
+    if (g_datalink_value != LINKTYPE_NULL) {
+        if (datalink != g_datalink_value) {
+            g_datalink_is_multiple = 1;
+        }
+    } else {
+        g_datalink_value = datalink;
+    }
+}
+
+inline int DatalinkGetGlobalType(void)
+{
+    return g_datalink_value;
+}
+
+bool DatalinkHasMultipleValues(void)
+{
+    return g_datalink_is_multiple == 1;
+}
