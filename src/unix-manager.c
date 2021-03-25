cfd0981c5650fa459748d3175ccfc69737e13279
Eric Leblond <el@stamus-networks.com>
2021-03-25 09:16:48 +0100

    unix-socket: add command to get flow stats
    Add a command to extract the accounting data from a live
    flow using the unix socket. It takes the flow_id as param
    and return the volume of data seen on the flow as well as
    its age.

--- src/unix-manager.c
+++ src/unix-manager.c
@@ -1084,6 +1084,8 @@ int UnixManagerInit(void)
 
     UnixManagerRegisterCommand("dataset-add", UnixSocketDatasetAdd, &command, UNIX_CMD_TAKE_ARGS);
     UnixManagerRegisterCommand("dataset-remove", UnixSocketDatasetRemove, &command, UNIX_CMD_TAKE_ARGS);
+    UnixManagerRegisterCommand(
+            "get-flow-stats-by-id", UnixSocketGetFlowStatsById, &command, UNIX_CMD_TAKE_ARGS);
 
     return 0;
 }
