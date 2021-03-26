d9ada387bae918b9678de39b675557678e357604
Eric Leblond <el@stamus-networks.com>
2021-03-26 01:00:31 +0100

    flow: add function to get flow using flow_id

--- src/flow-hash.h
+++ src/flow-hash.h
@@ -81,6 +81,7 @@ Flow *FlowGetFlowFromHash(ThreadVars *tv, FlowLookupStruct *tctx, Packet *, Flow
 
 Flow *FlowGetFromFlowKey(FlowKey *key, struct timespec *ttime, const uint32_t hash);
 Flow *FlowGetExistingFlowFromHash(FlowKey * key, uint32_t hash);
+Flow *FlowGetExistingFlowFromFlowId(int64_t flow_id);
 uint32_t FlowKeyGetHash(FlowKey *flow_key);
 
 /** \note f->fb must be locked */
