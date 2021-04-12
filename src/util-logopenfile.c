5394126b10c2173d29ee83cd37e303f8f8da0a16
Jeff Lucovsky <jeff@lucovsky.org>
2021-04-12 09:12:23 -0400

    output/log: Removed pcie (Tilera) log vestiges
    This commit removes the last remnants of the Tilera log output mechanism
    (unsupported since 5.0.x).
    
    (cherry picked from commit aa9ad56a5b0faeb9528558f6197ff967c6714e94)

--- src/util-logopenfile.c
+++ src/util-logopenfile.c
@@ -575,7 +575,6 @@ SCConfLogOpenGeneric(ConfNode *conf,
     } else {
         SCLogError(SC_ERR_INVALID_YAML_CONF_ENTRY, "Invalid entry for "
                    "%s.filetype.  Expected \"regular\" (default), \"unix_stream\", "
-                   "\"pcie\" "
                    "or \"unix_dgram\"",
                    conf->name);
     }
