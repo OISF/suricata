38b044242b0f11d3c0ed9f3027060ad2edba1298
Juliana Fajardini <jufajardini@gmail.com>
2021-04-14 23:09:02 +0100

    ippair/storage: use dedicated 'id' type
    - Wrap the id in a new IPPairStorageId struct, to avoid id
    confusion with other storage API calls.
    - Formatting fixes by clang.
    
    (cherry picked from commit cf516de587d27300cb5af42ae548f54351f7de5d)

--- src/app-layer-expectation.c
+++ src/app-layer-expectation.c
@@ -63,7 +63,7 @@
 
 #include "util-print.h"
 
-static int g_ippair_expectation_id = -1;
+static IPPairStorageId g_ippair_expectation_id = { .id = -1 };
 static FlowStorageId g_flow_expectation_id = { .id = -1 };
 
 SC_ATOMIC_DECLARE(uint32_t, expectation_count);
