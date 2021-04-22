01ec2ed674de3bff207ea73b112b1013e852e61d
Jason Ish <jason.ish@oisf.net>
2021-04-22 08:52:08 -0600

    misc: include queue.h before other headers
    At least on FreeBSD, some other include is including "sys/queue.h"
    which results in FreeBSDs /usr/include/sys/queue.h being picked
    up and setting __SYS_QUEUE_H__ so our queue.h is not picked up.
    
    But the FreeBSD queue.h does not have the CIRCLEQ definitions. To
    fix just include our queue.h first, which also sets __SYS_QUEUE_H__
    preventing the system one from being picked up.
    
    (cherry picked from commit d4554ec6bb6e4e52fb2e36cf0412c981a4d1d2e9)

--- src/app-layer-expectation.c
+++ src/app-layer-expectation.c
@@ -52,6 +52,7 @@
  * \author Eric Leblond <eric@regit.org>
  */
 
+#include "queue.h"
 #include "suricata-common.h"
 #include "debug.h"
 
@@ -61,7 +62,6 @@
 #include "app-layer-expectation.h"
 
 #include "util-print.h"
-#include "queue.h"
 
 static int g_expectation_id = -1;
 static int g_expectation_data_id = -1;
