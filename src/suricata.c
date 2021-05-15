7f5f35862f558893b35062beeb98bc0ce289dcb6
Jeff Lucovsky <jeff@lucovsky.org>
2021-05-15 09:23:54 -0400

    output/plugin: Load internal plugins
    This commit adds an interface for loading plugins that are internal to
    Suricata. These are always loaded and are in a modified format from
    external pliguns.

--- src/suricata.c
+++ src/suricata.c
@@ -2555,6 +2555,7 @@ int PostConfLoadedSetup(SCInstance *suri)
     FeatureTrackingRegister(); /* must occur prior to output mod registration */
     RegisterAllModules();
 #ifdef HAVE_PLUGINS
+    SCInternalLoad();
     SCPluginsLoad(suri->capture_plugin_name, suri->capture_plugin_args);
 #endif
     AppLayerHtpNeedFileInspection();
