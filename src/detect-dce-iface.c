bac69af7e4082d4829cd9aad5bdfd0b0d35070b9
Shivani Bhardwaj <shivanib134@gmail.com>
2021-05-18 17:18:00 +0530

    dcerpc: Add rust registration function
    Get rid of the C glue code and move registration completely to Rust.

--- src/detect-dce-iface.c
+++ src/detect-dce-iface.c
@@ -38,7 +38,6 @@
 #include "flow-util.h"
 
 #include "app-layer.h"
-#include "app-layer-dcerpc.h"
 #include "queue.h"
 #include "stream-tcp-reassemble.h"
 
--- src/detect-dce-stub-data.c
+++ src/detect-dce-stub-data.c
@@ -40,7 +40,6 @@
 #include "flow-util.h"
 
 #include "app-layer.h"
-#include "app-layer-dcerpc.h"
 #include "queue.h"
 #include "stream-tcp-reassemble.h"
 
