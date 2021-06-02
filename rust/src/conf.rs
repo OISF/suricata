1a5aa30e4759059f52cc95973d536d9dbef2b316
Jason Ish <jason.ish@oisf.net>
2021-06-02 15:48:36 -0600

    rust/conf: export ConfNodeLookupChild to rust

--- rust/src/conf.rs
+++ rust/src/conf.rs
@@ -39,6 +39,7 @@ extern {
     fn ConfGetChildValueBool(conf: *const c_void, key: *const c_char,
                              vptr: *mut c_int) -> i8;
     fn ConfGetNode(key: *const c_char) -> *const c_void;
+    fn ConfNodeLookupChild(node: *const c_void, name: *const c_char) -> *const c_void;
 }
 
 pub fn conf_get_node(key: &str) -> Option<ConfNode> {
@@ -142,6 +143,22 @@ impl ConfNode {
         return false;
     }
 
+    // Get a child node of this node by name.
+    //
+    // Wrapper around ConfNodeLookupChild.
+    //
+    // Returns None if the child is not found.
+    pub fn get_child(&self, name: &str) -> Option<ConfNode> {
+        unsafe {
+            let name = CString::new(name).unwrap();
+            let child = ConfNodeLookupChild(self.conf, name.as_ptr());
+            if child.is_null() {
+                None
+            } else {
+                Some(ConfNode { conf: child })
+            }
+        }
+    }
 }
 
 const BYTE: u64       = 1;
