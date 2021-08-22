020cfbcd6145bf4ac4d2ac37c4730e5449eec04c
Eric Leblond <el@stamus-networks.com>
2021-08-22 23:55:42 +0200

    profiling: introduce rules profiling
    Performance measurement of rules is important on live Suricata
    as bad rules can cause severe performance regression. This patch
    introduces the --enable-profiling-rules that activate profiling
    for the rules. This reduces the performance impact of full
    profiling  and provide visiblity on the rules performance at
    the same time.

--- src/detect.h
+++ src/detect.h
@@ -596,7 +596,7 @@ typedef struct Signature_ {
     /** port settings for this signature */
     DetectPort *sp, *dp;
 
-#ifdef PROFILING
+#ifdef PROFILE_RULES
     uint16_t profiling_id;
 #endif
 
@@ -912,8 +912,10 @@ typedef struct DetectEngineCtx_ {
         uint32_t content_inspect_window;
     } filedata_config[ALPROTO_MAX];
 
-#ifdef PROFILING
+#ifdef PROFILE_RULES
     struct SCProfileDetectCtx_ *profile_ctx;
+#endif
+#ifdef PROFILING
     struct SCProfileKeywordDetectCtx_ *profile_keyword_ctx;
     struct SCProfilePrefilterDetectCtx_ *profile_prefilter_ctx;
     struct SCProfileKeywordDetectCtx_ **profile_keyword_ctx_per_list;
@@ -1183,9 +1185,11 @@ typedef struct DetectEngineThreadCtx_ {
     uint64_t stream_persig_cnt;
     uint64_t stream_persig_size;
 #endif
-#ifdef PROFILING
+#ifdef PROFILE_RULES
     struct SCProfileData_ *rule_perf_data;
     int rule_perf_data_size;
+#endif
+#ifdef PROFILING
     struct SCProfileKeywordData_ *keyword_perf_data;
     struct SCProfileKeywordData_ **keyword_perf_data_per_list;
     int keyword_perf_list; /**< list we're currently inspecting, DETECT_SM_LIST_* */
