377231a9e984ca6ba0ad8d37aa6300d946e47333
Modupe Falodun <falodunmodupeola@gmail.com>
2021-10-21 14:32:12 +0100

    rust: remove unnecessary nested match
    rust: remove [allow(clippy::collapsible_match)]

--- rust/src/dhcp/logger.rs
+++ rust/src/dhcp/logger.rs
@@ -60,15 +60,8 @@ impl DHCPLogger {
     pub fn do_log(&self, tx: &DHCPTransaction) -> bool {
         if !self.extended {
             match self.get_type(tx) {
-                Some(t) => {
-                    match t {
-                        DHCP_TYPE_ACK => {
-                            return true;
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
+                Some(DHCP_TYPE_ACK) => return true,
+                        _ => {},
             }
             return false;
         }
