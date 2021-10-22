cd14f70dfb56ef69a2760c0070998fa663304fcc
Philippe Antoine <contact@catenacyber.fr>
2021-10-22 19:47:49 +0200

    detect: xor transform
    Ticket: 3285
    
    The xor transform applies xor decoding to a buffer, with a key
    specified as an option in hexadecimal. Arbitrary key sizes are
    accepted.

---
+++ src/detect-transform-xor.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 2021 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Philippe Antoine <contact@catenacyber.fr>
+ */
+
+#ifndef __DETECT_TRANSFORM_XOR_H__
+#define __DETECT_TRANSFORM_XOR_H__
+
+/* prototypes */
+void DetectTransformXorRegister(void);
+
+#endif /* __DETECT_TRANSFORM_XOR_H__ */
