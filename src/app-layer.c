f50cc8958500adadb50a85577e85681c6c888794
Philippe Antoine <contact@catenacyber.fr>
2021-10-23 12:53:32 +0200

    app-layer: use bool instead of int or char

--- src/app-layer.c
+++ src/app-layer.c
@@ -303,7 +303,7 @@ static int TCPProtoDetect(ThreadVars *tv,
 {
     AppProto *alproto;
     AppProto *alproto_otherdir;
-    int direction = (flags & STREAM_TOSERVER) ? 0 : 1;
+    bool direction = (flags & STREAM_TOSERVER) ? false : true;
 
     if (flags & STREAM_TOSERVER) {
         alproto = &f->alproto_ts;
@@ -370,7 +370,7 @@ static int TCPProtoDetect(ThreadVars *tv,
             } else {
                 *stream = &ssn->client;
             }
-            direction = 1 - direction;
+            direction = !direction;
         }
 
         /* account flow if we have both sides */
@@ -593,7 +593,7 @@ int AppLayerHandleTCPData(ThreadVars *tv, TcpReassemblyThreadCtx *ra_ctx,
         goto end;
     }
 
-    const int direction = (flags & STREAM_TOSERVER) ? 0 : 1;
+    const bool direction = (flags & STREAM_TOSERVER) ? false : true;
 
     if (flags & STREAM_TOSERVER) {
         alproto = f->alproto_ts;
