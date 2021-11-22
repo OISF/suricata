3f7a50eeb721bcc2a9f14b4d62925d46e5f202c6
Lukas Sismis <lukas.sismis@gmail.com>
2021-11-22 16:43:29 +0100

    dpdk/ixgbe: setup RSS for Intel IXGBE PMD
    Set RSS hash function according to Intel IXGBE PMD available hash functions.
    
    During configuration, a warning appeared stating that RSS hash function
    has been changed from one value to the other. This has meant that
    the supported hash functions did not cover all required hash functions
    by the configuration. This commit solves the warning.

---
+++ src/util-dpdk-ixgbe.c
@@ -0,0 +1,45 @@
+/* Copyright (C) 2021 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ *  \defgroup dpdk DPDK Intel IXGBE driver helpers functions
+ *
+ *  @{
+ */
+
+/**
+ * \file
+ *
+ * \author Lukas Sismis <lukas.sismis@gmail.com>
+ *
+ * DPDK driver's helper functions
+ *
+ */
+
+#include "util-dpdk-ixgbe.h"
+
+#ifdef HAVE_DPDK
+
+void ixgbeDeviceSetRSSHashFunction(uint64_t *rss_hf)
+{
+    *rss_hf = ETH_RSS_IPV4 | ETH_RSS_IPV6 | ETH_RSS_IPV6_EX;
+}
+
+#endif /* HAVE_DPDK */
+/**
+ * @}
+ */
