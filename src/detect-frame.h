f0049d339670f741c659754b60018b8ff685a902
Victor Julien <vjulien@oisf.net>
2021-12-03 08:14:34 +0100

    detect/frames: implement 'frame' keyword
    Implement a special sticky buffer to select frames for inspection.
    
    This keyword takes an argument to specify the per protocol frame type:
    
        alert <app proto name> ... frame:<specific frame name>
    
    Or it can specify both in the keyword:
    
        alert tcp ... frame:<app proto name>.<specific frame name>
    
    The latter is useful in some cases like http, where "http" applies to
    both HTTP and HTTP/2.
    
        alert http ... frame:http1.request;
        alert http1 ... frame:request;
    
    Examples:
    
        tls.pdu
        smb.smb2.hdr
        smb.smb3.data
    
    Consider a rule like:
    
        alert tcp ... flow:to_server; content:"|ff|SMB"; content:"some smb 1 issue";
    
    this will scan all toserver TCP traffic, where it will only be limited by a port,
    depending on how rules are grouped.
    
    With this work we'll be able to do:
    
        alert smb ... flow:to_server; frame:smb1.data; content:"some smb 1 issue";
    
    This rule will only inspect the data portion of SMB1 frames. It will not affect
    any other protocol, and it won't need special patterns to "search" for the
    SMB1 frame in the raw stream.

---
+++ src/detect-frame.h
@@ -0,0 +1,28 @@
+/* Copyright (C) 2021 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ */
+
+#ifndef __DETECT_FRAME_H__
+#define __DETECT_FRAME_H__
+
+/* Prototypes */
+void DetectFrameRegister(void);
+
+#endif /* __DETECT_FRAME_H__ */
