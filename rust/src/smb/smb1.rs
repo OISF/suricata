88c1fea77f55284c74443eaaa230e0c21956433f
Victor Julien <vjulien@oisf.net>
2021-12-06 11:35:23 +0100

    WIP smb: implement frames

--- rust/src/smb/smb1.rs
+++ rust/src/smb/smb1.rs
@@ -20,6 +20,8 @@
  */
 
 use crate::core::*;
+use crate::applayer::*;
+use crate::frames::*;
 
 use crate::smb::smb::*;
 use crate::smb::dcerpc::*;
@@ -596,7 +598,7 @@ fn smb1_request_record_one<'b>(state: &mut SMBState, r: &SmbRecord<'b>, command:
     return 0;
 }
 
-pub fn smb1_request_record<'b>(state: &mut SMBState, r: &SmbRecord<'b>) -> u32 {
+pub fn smb1_request_record<'b>(state: &mut SMBState, r: &SmbRecord<'b>, flow: *const Flow, stream_slice: &StreamSlice, frame_start: &[u8]) -> u32 {
     SCLogDebug!("record: command {}: record {:?}", r.command, r);
 
     let mut andx_offset = SMB1_HEADER_SIZE;
@@ -605,6 +607,16 @@ pub fn smb1_request_record<'b>(state: &mut SMBState, r: &SmbRecord<'b>) -> u32 {
         if smb1_request_record_one(state, r, command, &mut andx_offset) != 0 {
              break;
         }
+        let _smb_hdr_frame = Frame::new_ts(flow, stream_slice, frame_start,
+                std::cmp::min(frame_start.len(),SMB1_HEADER_SIZE) as i64, SMBFrameType::SMB1Hdr as u8);
+        SCLogDebug!("SMB1 HDR frame {:?}", _smb_hdr_frame);
+
+        let o = if r.data.len() >= andx_offset { andx_offset } else { 0 };
+        SCLogDebug!("o {} o r.data.len() {} andx_offset {}", o, r.data.len(), andx_offset);
+        let _smb_data_frame = Frame::new_ts(flow, stream_slice, r.data,
+                (r.data.len() - o) as i64, SMBFrameType::SMB1Data as u8);
+        SCLogDebug!("SMB1 DATA frame {:?}", _smb_data_frame);
+
         // continue for next andx command if any
         if smb1_command_is_andx(command) {
             if let Ok((_, andx_hdr)) = smb1_parse_andx_header(&r.data[andx_offset-SMB1_HEADER_SIZE..]) {
