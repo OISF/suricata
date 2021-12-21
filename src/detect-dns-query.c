fcbdc304265aaba724b245a5c3f02e00c8059a1a
Jason Ish <jason.ish@oisf.net>
2021-12-21 16:34:05 -0600

    dns: create transaction even if z-bit was set
    It appears that DNS servers will still process a DNS request even if the
    z-bit is set, our parser will fail the transaction. So create the
    transaction, but still set the event.
    
    Ticket #4924

--- src/detect-dns-query.c
+++ src/detect-dns-query.c
@@ -829,8 +829,8 @@ static int DetectDnsQueryTest05(void)
     FLOWLOCK_WRLOCK(&f);
     r = AppLayerParserParse(NULL, alp_tctx, &f, ALPROTO_DNS, STREAM_TOCLIENT,
                             buf2, sizeof(buf2));
-    if (r != -1) {
-        printf("toserver client 1 returned %" PRId32 ", expected -1\n", r);
+    if (r != 0) {
+        printf("toserver client 1 returned %" PRId32 ", expected 0\n", r);
         FLOWLOCK_UNLOCK(&f);
         FAIL;
     }
