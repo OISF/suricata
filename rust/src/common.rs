84d382be38b59d3851f565a1441beaf567ad3383
Pierre Chifflier <chifflier@wzdftpd.net>
2022-01-19 15:56:50 +0100

    rust: remove nom 5 dependency

--- rust/src/common.rs
+++ rust/src/common.rs
@@ -43,19 +43,6 @@ pub mod nom7 {
     }
 }
 
-#[macro_export]
-macro_rules! take_until_and_consume (
- ( $i:expr, $needle:expr ) => (
-    {
-      let input: &[u8] = $i;
-
-      let (rem, res) = ::nom::take_until!(input, $needle)?;
-      let (rem, _) = ::nom::take!(rem, $needle.len())?;
-      Ok((rem, res))
-    }
-  );
-);
-
 #[cfg(not(feature = "debug-validate"))]
 #[macro_export]
 macro_rules! debug_validate_bug_on (
