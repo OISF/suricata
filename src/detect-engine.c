41f38bd5c0ab7837f122be847fae881c85a63295
Jeff Lucovsky <jeff@lucovsky.org>
2022-01-26 16:35:12 -0500

    config/ref: Raise errors for ref.config parsing
    This commit raises an error in configuration test mode if there was an
    error parsing reference.config.
    
    Issue: 4659
    (cherry picked from commit be2155b4edb712913166cb5e8c10b36fb4362f2a)

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2020 Open Information Security Foundation
+/* Copyright (C) 2007-2022 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -2058,7 +2058,10 @@ static DetectEngineCtx *DetectEngineCtxInitReal(enum DetectEngineType type, cons
     (void)SRepInit(de_ctx);
 
     SCClassConfLoadClassficationConfigFile(de_ctx, NULL);
-    SCRConfLoadReferenceConfigFile(de_ctx, NULL);
+    if (SCRConfLoadReferenceConfigFile(de_ctx, NULL) < 0) {
+        if (RunmodeGetCurrent() == RUNMODE_CONF_TEST)
+            goto error;
+    }
 
     if (ActionInitConfig() < 0) {
         goto error;
