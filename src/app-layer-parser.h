33cd43d599d6e56acb9c368c0ecaaf5f03176895
Victor Julien <vjulien@oisf.net>
2022-02-05 08:53:58 +0100

    app-layer: parser trunc flags per direction

--- src/app-layer-parser.h
+++ src/app-layer-parser.h
@@ -41,6 +41,8 @@
 #define APP_LAYER_PARSER_BYPASS_READY          BIT_U16(4)
 #define APP_LAYER_PARSER_EOF_TS                BIT_U16(5)
 #define APP_LAYER_PARSER_EOF_TC                BIT_U16(6)
+#define APP_LAYER_PARSER_TRUNC_TS              BIT_U16(7)
+#define APP_LAYER_PARSER_TRUNC_TC              BIT_U16(8)
 
 /* Flags for AppLayerParserProtoCtx. */
 #define APP_LAYER_PARSER_OPT_ACCEPT_GAPS        BIT_U32(0)
