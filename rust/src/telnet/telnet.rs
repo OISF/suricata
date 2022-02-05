12a75829d77e73d0298ee028b6c51e44e94ba09f
Victor Julien <vjulien@oisf.net>
2022-02-05 09:07:28 +0100

    TXFILE: telnet

--- rust/src/telnet/telnet.rs
+++ rust/src/telnet/telnet.rs
@@ -550,7 +550,7 @@ pub unsafe extern "C" fn rs_telnet_register_parser() {
         get_eventinfo_byid : Some(TelnetEvent::get_event_info_by_id),
         localstorage_new: None,
         localstorage_free: None,
-        get_files: None,
+        get_tx_files: None,
         get_tx_iterator: Some(applayer::state_get_tx_iterator::<TelnetState, TelnetTransaction>),
         get_tx_data: rs_telnet_get_tx_data,
         get_state_data: rs_telnet_get_state_data,
