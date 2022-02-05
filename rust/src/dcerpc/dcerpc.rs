519305aeebaa00ec41fa8a51e9f213831c82604d
Victor Julien <vjulien@oisf.net>
2022-02-05 09:07:59 +0100

    TXFILE: dcerpc

--- rust/src/dcerpc/dcerpc.rs
+++ rust/src/dcerpc/dcerpc.rs
@@ -1349,7 +1349,7 @@ pub unsafe extern "C" fn rs_dcerpc_register_parser() {
         get_eventinfo_byid : None,
         localstorage_new: None,
         localstorage_free: None,
-        get_files: None,
+        get_tx_files: None,
         get_tx_iterator: None,
         get_tx_data: rs_dcerpc_get_tx_data,
         get_state_data: rs_dcerpc_get_state_data,
