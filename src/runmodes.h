9d66541128a66356fa032aed398101ed6ba56fdb
Jeff Lucovsky <jeff@lucovsky.org>
2022-02-11 08:36:28 -0500

    config/thread: Use config'd per-thread stack size
    Issue: 4550
    
    This commit checks if there's a config setting for threading.stack-size
    and assigns the value to a global variable for use during thread
    creation.
    
    (cherry picked from commit e4d60f451b8a226e32a3df8e232efe437b11e2e3)

--- src/runmodes.h
+++ src/runmodes.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2013 Open Information Security Foundation
+/* Copyright (C) 2007-2022 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -113,6 +113,7 @@ int RunModeNeedsBypassManager(void);
 
 extern int threading_set_cpu_affinity;
 extern float threading_detect_ratio;
+extern uint64_t threading_set_stack_size;
 
 extern int debuglog_enabled;
 
