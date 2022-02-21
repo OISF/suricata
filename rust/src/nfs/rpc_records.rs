7259debf89675ee083d6a1fae4e15a2fea7aae52
Victor Julien <vjulien@oisf.net>
2022-02-21 10:08:41 +0100

    rpc: enforce minimal frag_len field

--- rust/src/nfs/rpc_records.rs
+++ rust/src/nfs/rpc_records.rs
@@ -132,7 +132,7 @@ fn parse_bits(i: &[u8]) -> IResult<&[u8], (u8, u32)> {
 }
 
 pub fn parse_rpc_packet_header(i: &[u8]) -> IResult<&[u8], RpcPacketHeader> {
-    let (i, fraghdr) = parse_bits(i)?;
+    let (i, fraghdr) = verify(parse_bits, |v: &(u8,u32)| v.1 >= 24)(i)?;
     let (i, xid) = be_u32(i)?;
     let (i, msgtype) = be_u32(i)?;
     let hdr = RpcPacketHeader {
