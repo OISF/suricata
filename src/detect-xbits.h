deb2e1d225562f463d3a61f3ab77622bbe2fab1b
Shivani Bhardwaj <shivanib134@gmail.com>
2022-03-04 00:30:46 +0530

    remove unecesary headers

--- src/detect-xbits.h
+++ src/detect-xbits.h
@@ -24,31 +24,33 @@
 #ifndef __DETECT_XBITS_H__
 #define __DETECT_XBITS_H__
 
-#define DETECT_XBITS_CMD_SET      0
-#define DETECT_XBITS_CMD_TOGGLE   1
-#define DETECT_XBITS_CMD_UNSET    2
-#define DETECT_XBITS_CMD_ISNOTSET 3
-#define DETECT_XBITS_CMD_ISSET    4
-#define DETECT_XBITS_CMD_NOALERT  5
-#define DETECT_XBITS_CMD_MAX      6
-
-#define DETECT_XBITS_TRACK_IPSRC  0
-#define DETECT_XBITS_TRACK_IPDST  1
-#define DETECT_XBITS_TRACK_IPPAIR 2
-#define DETECT_XBITS_TRACK_FLOW   3
-
-#define DETECT_XBITS_EXPIRE_DEFAULT 30
-
-typedef struct DetectXbitsData_ {
-    uint32_t idx;
-    uint8_t cmd;
-    uint8_t tracker;
-    uint32_t expire;
-    /** data type: host/ippair/flow used for sig sorting in sigorder */
-    enum VarTypes type;
-} DetectXbitsData;
-
-/* prototypes */
+#include "rust.h"
+//
+//#define DETECT_XBITS_CMD_SET      0
+//#define DETECT_XBITS_CMD_TOGGLE   1
+//#define DETECT_XBITS_CMD_UNSET    2
+//#define DETECT_XBITS_CMD_ISNOTSET 3
+//#define DETECT_XBITS_CMD_ISSET    4
+//#define DETECT_XBITS_CMD_NOALERT  5
+//#define DETECT_XBITS_CMD_MAX      6
+//
+//#define DETECT_XBITS_TRACK_IPSRC  0
+//#define DETECT_XBITS_TRACK_IPDST  1
+//#define DETECT_XBITS_TRACK_IPPAIR 2
+//#define DETECT_XBITS_TRACK_FLOW   3
+//
+//#define DETECT_XBITS_EXPIRE_DEFAULT 30
+//
+//typedef struct DetectXbitsData_ {
+//    uint32_t idx;
+//    uint8_t cmd;
+//    uint8_t tracker;
+//    uint32_t expire;
+//    /** data type: host/ippair/flow used for sig sorting in sigorder */
+//    enum VarTypes type;
+//} DetectXbitsData;
+//
+///* prototypes */
 void DetectXbitsRegister (void);
 
 #endif /* __DETECT_XBITS_H__ */
