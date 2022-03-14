a6c48114c8949225f1c1a46fbe3a54887547bef5
Victor Julien <vjulien@oisf.net>
2022-03-14 07:20:11 +0100

    exceptions: initial exception-policy implementation
    Adds a framework for setting exception policies. These would be called
    when the engine reaches some kind of exception condition, like hitting
    a memcap or some traffic processing error.
    
    The policy gives control over what should happen next: drop the packet,
    drop the packet and flow, bypass, etc.
    
    Implements the policy for:
    
        stream: If stream session or reassembly memcaps are hit call the
        memcap policy on the packet and flow.
    
        flow: Apply policy when memcap is reached and no flow could be
        freed up.
    
        defrag: Apply policy when no tracker could be picked up.
    
        app-layer: Apply ppolicy if a parser reaches an error state.
    
    All options default to 'ignore', which means the default behavior
    is unchanged.
    
    Adds commandline options: add simulation options for exceptions. These
    are only exposed if compiled with `--enable-debug`.

---
+++ src/util-exception-policy.h
@@ -0,0 +1,50 @@
+/* Copyright (C) 2022 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ */
+
+#ifndef __UTIL_EXCEPTION_POLICY_H__
+#define __UTIL_EXCEPTION_POLICY_H__
+
+enum ExceptionPolicy {
+    EXCEPTION_POLICY_IGNORE = 0,
+    EXCEPTION_POLICY_PASS_PACKET,
+    EXCEPTION_POLICY_PASS_FLOW,
+    EXCEPTION_POLICY_BYPASS_FLOW,
+    EXCEPTION_POLICY_DROP_PACKET,
+    EXCEPTION_POLICY_DROP_FLOW,
+};
+
+void ExceptionPolicyApply(
+        Packet *p, enum ExceptionPolicy policy, enum PacketDropReason drop_reason);
+enum ExceptionPolicy ExceptionPolicyParse(const char *option, const bool support_flow);
+
+#ifdef DEBUG
+extern uint64_t g_eps_applayer_error_offset_ts;
+extern uint64_t g_eps_applayer_error_offset_tc;
+extern uint64_t g_eps_pcap_packet_loss;
+extern uint64_t g_eps_stream_ssn_memcap;
+extern uint64_t g_eps_stream_reassembly_memcap;
+extern uint64_t g_eps_flow_memcap;
+extern uint64_t g_eps_defrag_memcap;
+#endif
+
+int ExceptionSimulationCommandlineParser(const char *name, const char *arg);
+
+#endif
