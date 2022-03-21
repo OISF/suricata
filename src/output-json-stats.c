a1494efb72d63dc9dd00bcaea8e56a31b2a1e414
Jeff Lucovsky <jeff@lucovsky.org>
2022-03-21 10:36:13 -0400

    output/stats: Use thread-aware log initializer
    Issue: 5198
    
    This commit corrects the log instance initializer within the stats
    sub-logger to use the thread-aware LogFileEnsureExists function.

--- src/output-json-stats.c
+++ src/output-json-stats.c
@@ -395,7 +395,7 @@ static OutputInitResult OutputStatsLogInitSub(ConfNode *conf, OutputCtx *parent_
         return result;
     }
 
-    OutputStatsCtx *stats_ctx = SCMalloc(sizeof(OutputStatsCtx));
+    OutputStatsCtx *stats_ctx = SCCalloc(1, sizeof(OutputStatsCtx));
     if (unlikely(stats_ctx == NULL))
         return result;
 
@@ -440,7 +440,12 @@ static OutputInitResult OutputStatsLogInitSub(ConfNode *conf, OutputCtx *parent_
         return result;
     }
 
-    stats_ctx->file_ctx = ajt->file_ctx;
+    stats_ctx->file_ctx = LogFileEnsureExists(ajt->file_ctx, 1);
+    if (!stats_ctx->file_ctx) {
+        SCFree(stats_ctx);
+        SCFree(output_ctx);
+        return result;
+    }
 
     output_ctx->data = stats_ctx;
     output_ctx->DeInit = OutputStatsLogDeinitSub;
