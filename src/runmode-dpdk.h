b953beacc0ee60ae5c465f8ca7fcc688ef0f41cd
Lukas Sismis <lukas.sismis@gmail.com>
2022-04-21 14:44:49 +0200

    dpdk: make functions available for Suricata library

--- src/runmode-dpdk.h
+++ src/runmode-dpdk.h
@@ -41,6 +41,7 @@ typedef struct DPDKIfaceConfigAttributes_ {
     const char *copy_iface;
 } DPDKIfaceConfigAttributes;
 
+int DeviceConfigure(DPDKIfaceConfig *iconf);
 int RunModeIdsDpdkWorkers(void);
 void RunModeDpdkRegister(void);
 const char *RunModeDpdkGetDefaultMode(void);
