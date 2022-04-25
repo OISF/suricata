9b7ad3043f649b37e023ea0e6293af9b1fec2d83
Philippe Antoine <contact@catenacyber.fr>
2022-04-25 17:59:00 +0200

    detect: use generic integer functions for streamsize
    By the way, adds the prefilter feature
    
    Ticket: #2697
    Ticket: #4112

--- src/detect-stream_size.h
+++ src/detect-stream_size.h
@@ -24,24 +24,6 @@
 #ifndef _DETECT_STREAM_SIZE_H
 #define	_DETECT_STREAM_SIZE_H
 
-#define DETECTSSIZE_LT 0
-#define DETECTSSIZE_LEQ 1
-#define DETECTSSIZE_EQ 2
-#define DETECTSSIZE_NEQ 3
-#define DETECTSSIZE_GT 4
-#define DETECTSSIZE_GEQ 5
-
-#define STREAM_SIZE_SERVER 0x01
-#define STREAM_SIZE_CLIENT 0x02
-#define STREAM_SIZE_BOTH   0x04
-#define STREAM_SIZE_EITHER 0x08
-
-typedef struct DetectStreamSizeData_ {
-    uint8_t flags;
-    uint8_t mode;
-    uint32_t ssize;
-}DetectStreamSizeData;
-
 void DetectStreamSizeRegister(void);
 
 #endif	/* _DETECT_STREAM_SIZE_H */
