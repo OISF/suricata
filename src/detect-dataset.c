34acfbcc31d146f504a29fe431f62028cb19ebb6
Eric Leblond <el@stamus-networks.com>
2022-05-31 18:53:17 +0200

    datasets: introduce new IPv6 type
    This patch also simplifies IPv6 parsing.
    
    Feature: #5383

--- src/detect-dataset.c
+++ src/detect-dataset.c
@@ -158,6 +158,10 @@ static int DetectDatasetParse(const char *str, char *cmd, int cmd_len, char *nam
                     *type = DATASET_TYPE_STRING;
                 } else if (strcmp(val, "ipv4") == 0) {
                     *type = DATASET_TYPE_IPV4;
+                } else if (strcmp(val, "ipv6") == 0) {
+                    *type = DATASET_TYPE_IPV6;
+                } else if (strcmp(val, "ip") == 0) {
+                    *type = DATASET_TYPE_IPV6;
                 } else {
                     SCLogError(SC_ERR_INVALID_SIGNATURE, "bad type %s", val);
                     return -1;
