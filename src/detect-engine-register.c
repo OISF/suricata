1ac9145e0ab4b9cfdbf1990d5389aa034521372e
Eric Leblond <el@stamus-networks.com>
2022-06-11 23:28:37 +0200

    detect/ipaddr: add ip.src keyword
    It is a sticky buffer matching on src_ip.
    
    Feature: #5383

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -77,6 +77,7 @@
 
 #include "detect-base64-decode.h"
 #include "detect-base64-data.h"
+#include "detect-ipaddr.h"
 #include "detect-ipopts.h"
 #include "detect-tcp-flags.h"
 #include "detect-fragbits.h"
@@ -615,6 +616,7 @@ void SigTableSetup(void)
     DetectTcpmssRegister();
     DetectICMPv6hdrRegister();
     DetectICMPv6mtuRegister();
+    DetectIPAddrBufferRegister();
     DetectIpv4hdrRegister();
     DetectIpv6hdrRegister();
     DetectKrb5CNameRegister();
