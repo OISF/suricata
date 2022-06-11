7e516aad94bd2dec45aa9da8f295629747b56404
Eric Leblond <el@stamus-networks.com>
2022-06-11 23:28:37 +0200

    detect: add ip.src keyword
    It is a sticky buffer matching on src_ip.
    
    Feature: #5383

---
+++ src/tests/detect-ipaddr.c
@@ -0,0 +1,47 @@
+/* Copyright (C) 2022 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "../suricata-common.h"
+
+#include "../detect.h"
+#include "../detect-parse.h"
+#include "../detect-engine-prefilter-common.h"
+
+#include "../detect-ipaddr.h"
+
+#include "../util-unittest.h"
+
+static int DetectIPAddrParseTest01(void)
+{
+    DetectEngineCtx *de_ctx = DetectEngineCtxInit();
+    FAIL_IF_NULL(de_ctx);
+
+    Signature *sig = DetectEngineAppendSig(
+            de_ctx, "alert ip any any -> any any (ip.src; content:\"A\"; sid:1; rev:1;)");
+    FAIL_IF_NULL(sig);
+
+    DetectEngineCtxFree(de_ctx);
+    PASS;
+}
+
+/**
+ * \brief this function registers unit tests for DetectIpv4hdr
+ */
+void DetectIPAddrRegisterTests(void)
+{
+    UtRegisterTest("DetectIPAddrParseTest01", DetectIPAddrParseTest01);
+}
