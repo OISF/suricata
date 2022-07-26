e8381fcf3eec3520da0e1f7c1ee8bcc73a48c2b0
Shivani Bhardwaj <shivani@oisf.net>
2022-07-26 16:12:31 +0530

    tls: add tls.random* keywords
    Add tls.random keyword that matches on the 32 bytes of the TLS
    random field for client as well as server.
    Add tls.random_time keyword that matches on the first 4 bytes of the TLS
    random field for client as well as server.
    Add tls.random_bytes keyword that matches on the last 28 bytes of the TLS
    random field for client as well as server.
    
    All these are sticky buffers.
    
    Feature 5190

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -53,6 +53,7 @@
 #include "detect-tls-cert-issuer.h"
 #include "detect-tls-cert-subject.h"
 #include "detect-tls-cert-serial.h"
+#include "detect-tls-random.h"
 #include "detect-tls-ja3-hash.h"
 #include "detect-tls-ja3-string.h"
 #include "detect-tls-ja3s-hash.h"
@@ -520,6 +521,7 @@ void SigTableSetup(void)
     DetectTlsSerialRegister();
     DetectTlsFingerprintRegister();
     DetectTlsCertsRegister();
+    DetectTlsRandomRegister();
 
     DetectTlsJa3HashRegister();
     DetectTlsJa3StringRegister();
