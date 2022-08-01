d7294ef19506dc36b22cc5f9e6efa55a8bf105bb
Lukas Sismis <lukas.sismis@gmail.com>
2022-08-01 13:42:09 +0200

    dpdk: add debug xstats

--- dpdk/prefilter/lcore-worker.c
+++ dpdk/prefilter/lcore-worker.c
@@ -34,6 +34,59 @@
 #include "logger.h"
 #include "util-prefilter.h"
 
+// debug function for printing out xstats
+static void print_stats(unsigned  int port_id)
+{
+    int len, ret, i;
+    struct rte_eth_xstat *xstats;
+    struct rte_eth_xstat_name *xstats_names;
+    static const char *stats_border = "_______";
+    const char clr[] = { 27, '[', '2', 'J', '\0' };
+    const char topLeft[] = { 27, '[', '1', ';', '1', 'H', '\0' };
+    /* Clear screen and move to top left */
+    printf("PORT STATISTICS:\n================\n");
+    len = rte_eth_xstats_get(port_id, NULL, 0);
+    if (len < 0)
+        rte_exit(EXIT_FAILURE,
+                "rte_eth_xstats_get(%u) failed: %d", port_id,
+                len);
+    xstats = calloc(len, sizeof(*xstats));
+    if (xstats == NULL)
+        rte_exit(EXIT_FAILURE,
+                "Failed to calloc memory for xstats");
+    ret = rte_eth_xstats_get(port_id, xstats, len);
+    if (ret < 0 || ret > len) {
+        free(xstats);
+        rte_exit(EXIT_FAILURE,
+                "rte_eth_xstats_get(%u) len%i failed: %d",
+                port_id, len, ret);
+    }
+    xstats_names = calloc(len, sizeof(*xstats_names));
+    if (xstats_names == NULL) {
+        free(xstats);
+        rte_exit(EXIT_FAILURE,
+                "Failed to calloc memory for xstats_names");
+    }
+    ret = rte_eth_xstats_get_names(port_id, xstats_names, len);
+    if (ret < 0 || ret > len) {
+        free(xstats);
+        free(xstats_names);
+        rte_exit(EXIT_FAILURE,
+                "rte_eth_xstats_get_names(%u) len%i failed: %d",
+                port_id, len, ret);
+    }
+    for (i = 0; i < len; i++) {
+        if (xstats[i].value > 0)
+            printf("Port %u: %s %s:\t\t%"PRIu64"\n",
+                    port_id, stats_border,
+                    xstats_names[i].name,
+                    xstats[i].value);
+    }
+    fflush(stdout);
+    free(xstats);
+    free(xstats_names);
+}
+
 int ThreadMain(void *init_values)
 {
     struct lcore_init *vals = (struct lcore_init *)init_values;
@@ -61,6 +114,12 @@ int ThreadMain(void *init_values)
             ThreadSuricataStatsDump(lv);
             ThreadSuricataStatsExit(lv, vals->stats);
             Log().info("DUMP finish");
+
+            if (lv->qid == 0) {
+                print_stats(lv->port1_id);
+                print_stats(lv->port2_id);
+            }
+
             // only work with tasks and result ring/s and bt
         } else if (LcoreStateCheck(vals->state, LCORE_DETACH)) {
             Log().debug("Lcore %d detach", rte_lcore_id());
