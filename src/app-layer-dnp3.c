cc23923de14aa703690de2bdb50090e12e54d5ed
Philippe Antoine <pantoine@oisf.net>
2022-08-02 11:42:38 +0200

    src: remove obsolete comment
    Should have been removed along by commit
    82dba07579fcd188bc537b57c52eea4974f2d9e1

--- src/app-layer-dnp3.c
+++ src/app-layer-dnp3.c
@@ -34,8 +34,6 @@
 #include "app-layer-dnp3.h"
 #include "app-layer-dnp3-objects.h"
 
-/* For hexdump(). */
-
 /* Default number of unreplied requests to be considered a flood. */
 #define DNP3_DEFAULT_REQ_FLOOD_COUNT 500
 
