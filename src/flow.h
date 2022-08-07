0d2268ddfcc65462a53fc7f27dcc68ae87c8ecf8
Jeff Lucovsky <jeff.lucovsky@corelight.com>
2022-08-07 15:53:21 -0400

    decode/vlan: Decode upto 3 layers of VLAN
    Issue: 2816
    
    This commit increase the number of VLAN layers supported by Suricata
    from 2 to 3. 3-layers are dubbed "Q-in-Q-in-Q".
    
    Note that 3 layers are not compliant with any existing standard but are
    often seen in larger deployments.

--- src/flow.h
+++ src/flow.h
@@ -301,7 +301,7 @@ typedef struct FlowKey_
     Port sp, dp;
     uint8_t proto;
     uint8_t recursion_level;
-    uint16_t vlan_id[2];
+    uint16_t vlan_id[VLAN_MAX_LAYERS];
 } FlowKey;
 
 typedef struct FlowAddress_ {
@@ -364,7 +364,7 @@ typedef struct Flow_
     };
     uint8_t proto;
     uint8_t recursion_level;
-    uint16_t vlan_id[2];
+    uint16_t vlan_id[VLAN_MAX_LAYERS];
 
     uint8_t vlan_idx;
 
