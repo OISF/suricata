5b73ed0d629bb2e08541e6588df4be0fc11d1600
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-08-09 02:24:36 -0400

    spelling: function
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/detect-flowint.c
+++ src/detect-flowint.c
@@ -429,7 +429,7 @@ void DetectFlowintFree(DetectEngineCtx *de_ctx, void *tmp)
 
 #ifdef UNITTESTS
 /**
- * \brief This is a helper funtion used for debugging purposes
+ * \brief This is a helper function used for debugging purposes
  */
 static void DetectFlowintPrintData(DetectFlowintData *sfd)
 {
