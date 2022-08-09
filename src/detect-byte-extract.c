c418720df79d626ef7cff3f7d985629c1ad55b54
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-08-09 02:24:22 -0400

    spelling: endianness
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/detect-byte-extract.c
+++ src/detect-byte-extract.c
@@ -50,7 +50,7 @@
 #include "util-unittest-helper.h"
 #include "util-spm.h"
 
-/* the default value of endianess to be used, if none's specified */
+/* the default value of endianness to be used, if none's specified */
 #define DETECT_BYTE_EXTRACT_ENDIAN_DEFAULT DETECT_BYTE_EXTRACT_ENDIAN_BIG
 
 /* the base to be used if string mode is specified.  These options would be
