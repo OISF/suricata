65b89b69eb4a23451f1f05766c6a503f92bc5060
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-08-09 07:09:33 -0400

    spelling: search
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/util-spm-bm.c
+++ src/util-spm-bm.c
@@ -24,7 +24,7 @@
  *
  * Boyer Moore algorithm has a really good performance. It need two arrays
  * of context for each pattern that hold applicable shifts on the text
- * to seach in, based on characters not available in the pattern
+ * to search in, based on characters not available in the pattern
  * and combinations of characters that start a sufix of the pattern.
  * If possible, we should store the context of patterns that we are going
  * to search for multiple times, so we don't spend time on rebuilding them.
