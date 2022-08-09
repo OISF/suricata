4b38d058e53a5a0837e338ba6d799f16f935583a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-08-09 02:52:11 -0400

    spelling: subtract
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- rust/src/smb/ntlmssp_records.rs
+++ rust/src/smb/ntlmssp_records.rs
@@ -101,7 +101,7 @@ pub fn parse_ntlm_auth_record(i: &[u8]) -> IResult<&[u8], NTLMSSPAuthRecord> {
     let (i, nego_flags) = parse_ntlm_auth_nego_flags(i)?;
     let (i, version) = cond(nego_flags.1==1, parse_ntlm_auth_version)(i)?;
 
-    // subtrack 12 as identifier (8) and type (4) are cut before we are called
+    // subtract 12 as identifier (8) and type (4) are cut before we are called
     // subtract 60 for the len/offset/maxlen fields above
     let (i, _) = cond(nego_flags.1==1 && domain_blob_offset > 72, |b| take(domain_blob_offset - (12 + 60))(b))(i)?;
     // or 52 if we have no version
