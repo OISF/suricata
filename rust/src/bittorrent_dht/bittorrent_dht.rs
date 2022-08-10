927f47b1e01e77ef102d16c5e7dcb3789108eeb6
Jason Ish <jason.ish@oisf.net>
2022-08-10 11:49:06 -0600

    bittorrent-dht: set parser to unidirectional
    This parser does not match up responses with requests so flag it as
    unidirectional.

--- rust/src/bittorrent_dht/bittorrent_dht.rs
+++ rust/src/bittorrent_dht/bittorrent_dht.rs
@@ -277,7 +277,7 @@ pub unsafe extern "C" fn rs_bittorrent_dht_udp_register_parser() {
         get_tx_data: rs_bittorrent_dht_get_tx_data,
         get_state_data: rs_bittorrent_dht_get_state_data,
         apply_tx_config: None,
-        flags: 0,
+        flags: APP_LAYER_PARSER_OPT_UNIDIR_TXS,
         truncate: None,
         get_frame_id_by_name: None,
         get_frame_name_by_id: None,
