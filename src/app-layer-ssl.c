64bc16f93f9b165f4cd94bf76a1198136055b094
Victor Julien <vjulien@oisf.net>
2022-08-11 12:18:28 +0200

    tls: don't set 2 events for a single exception
    Keep the more specific ones.

--- src/app-layer-ssl.c
+++ src/app-layer-ssl.c
@@ -1629,7 +1629,6 @@ static int SSLv3ParseHandshakeProtocol(SSLState *ssl_state, const uint8_t *input
 
             SSLParserHSReset(ssl_state->curr_connp);
             SSLSetEvent(ssl_state, TLS_DECODER_EVENT_INVALID_HANDSHAKE_MESSAGE);
-            SSLSetEvent(ssl_state, TLS_DECODER_EVENT_INVALID_SSL_RECORD);
             continue;
         }
 
@@ -2400,10 +2399,7 @@ static struct SSLDecoderResult SSLv3Decode(uint8_t direction, SSLState *ssl_stat
             SCLogDebug("retval %d", retval);
             if (retval < 0 || retval > (int)record_len) {
                 DEBUG_VALIDATE_BUG_ON(retval > (int)record_len);
-                SSLSetEvent(ssl_state,
-                        TLS_DECODER_EVENT_INVALID_HANDSHAKE_MESSAGE);
-                SSLSetEvent(ssl_state,
-                        TLS_DECODER_EVENT_INVALID_SSL_RECORD);
+                SSLSetEvent(ssl_state, TLS_DECODER_EVENT_INVALID_HANDSHAKE_MESSAGE);
                 SCLogDebug("SSLv3ParseHandshakeProtocol returned %d", retval);
                 return SSL_DECODER_ERROR(-1);
             }
@@ -2422,9 +2418,7 @@ static struct SSLDecoderResult SSLv3Decode(uint8_t direction, SSLState *ssl_stat
             break;
         }
         default:
-            /* \todo fix the event from invalid rule to unknown rule */
             SSLSetEvent(ssl_state, TLS_DECODER_EVENT_INVALID_RECORD_TYPE);
-            SSLSetEvent(ssl_state, TLS_DECODER_EVENT_INVALID_SSL_RECORD);
             SCLogDebug("unsupported record type");
             return SSL_DECODER_ERROR(-1);
     }
