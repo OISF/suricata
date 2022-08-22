067eebfc8d112cb2a222800d3bdea6c78548d6f5
Philippe Antoine <pantoine@oisf.net>
2022-08-22 15:11:34 +0200

    dhcp: fix license in detect-dhcp-leasetime.c
    from search and replace overkill

--- src/detect-dhcp-leasetime.c
+++ src/detect-dhcp-leasetime.c
@@ -1,7 +1,7 @@
 /* Copyright (C) 2022 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
- * the GNU General Public License leasetime 2 as published by the Free
+ * the GNU General Public License version 2 as published by the Free
  * Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
@@ -10,7 +10,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * leasetime 2 along with this program; if not, write to the Free Software
+ * version 2 along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  */
