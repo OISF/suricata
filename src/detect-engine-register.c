6faf6299e0c2daac3dbe6e1f93e4e9b9624c1a3d
Philippe Antoine <pantoine@oisf.net>
2022-08-22 15:10:59 +0200

    dhcp: adds rebinding-time keyword
    Ticket: #5506

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -202,6 +202,7 @@
 #include "detect-target.h"
 #include "detect-template-rust-buffer.h"
 #include "detect-dhcp-leasetime.h"
+#include "detect-dhcp-rebinding-time.h"
 #include "detect-snmp-usm.h"
 #include "detect-snmp-version.h"
 #include "detect-snmp-community.h"
@@ -641,6 +642,7 @@ void SigTableSetup(void)
     DetectTargetRegister();
     DetectTemplateRustBufferRegister();
     DetectDHCPLeaseTimeRegister();
+    DetectDHCPRebindingTimeRegister();
     DetectSNMPUsmRegister();
     DetectSNMPVersionRegister();
     DetectSNMPCommunityRegister();
