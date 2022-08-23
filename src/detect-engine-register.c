7b65cd9573c3a882e8fe8f61d68864dbb675e640
Eric Leblond <el@stamus-networks.com>
2022-08-23 16:44:23 +0200

    domain: add new transformation
    Ticket: #5639

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -221,6 +221,7 @@
 
 #include "detect-transform-pcrexform.h"
 #include "detect-transform-base64.h"
+#include "detect-transform-domain.h"
 
 #include "util-rule-vars.h"
 
@@ -732,6 +733,7 @@ void SigTableSetup(void)
     DetectTransformToUpperRegister();
     DetectTransformHeaderLowercaseRegister();
     DetectTransformFromBase64DecodeRegister();
+    DetectTransformDomainRegister();
 
     DetectFileHandlerRegister();
 
