98d3ee2715dae23a541fbafbc906744a3b670e54
Philippe Antoine <pantoine@oisf.net>
2022-08-25 17:05:41 +0200

    detect: transforms check for 0-sized buffer
    So as to avoid undefined behavior with a 0-sized variable length
    array
    
    Ticket: #5521
    (cherry picked from commit 61b73416e27a07cab50743d69c1cf5cd7f07b45d)

--- src/detect-transform-strip-whitespace.c
+++ src/detect-transform-strip-whitespace.c
@@ -102,6 +102,9 @@ static void TransformStripWhitespace(InspectionBuffer *buffer, void *options)
 {
     const uint8_t *input = buffer->inspect;
     const uint32_t input_len = buffer->inspect_len;
+    if (input_len == 0) {
+        return;
+    }
     uint8_t output[input_len]; // we can only shrink
     uint8_t *oi = output, *os = output;
 
