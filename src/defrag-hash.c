f0143064bfaf032bd385f1b9e41afe8c00e0068c
Jeff Lucovsky <jeff.lucovsky@corelight.com>
2022-08-27 10:09:28 -0400

    general/typo: Correct misc. typos

--- src/defrag-hash.c
+++ src/defrag-hash.c
@@ -465,7 +465,7 @@ static inline int DefragTrackerCompare(DefragTracker *t, Packet *p)
  *  Get a new defrag tracker. We're checking memcap first and will try to make room
  *  if the memcap is reached.
  *
- *  \retval dt *LOCKED* tracker on succes, NULL on error.
+ *  \retval dt *LOCKED* tracker on success, NULL on error.
  */
 static DefragTracker *DefragTrackerGetNew(Packet *p)
 {
