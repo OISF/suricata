f82ee5127bde991a35fab7d2a656e2b3417b3242
Philippe Antoine <pantoine@oisf.net>
2022-09-01 16:02:05 +0200

    smtp: use rust for mime parsing
    Ticket: #3487

--- src/util-spm-bs.h
+++ src/util-spm-bs.h
@@ -29,6 +29,7 @@
 
 uint8_t *BasicSearch(const uint8_t *, uint32_t, const uint8_t *, uint16_t);
 uint8_t *BasicSearchNocase(const uint8_t *, uint32_t, const uint8_t *, uint16_t);
+uint32_t BasicSearchNocaseIndex(const uint8_t *, uint32_t, const uint8_t *, uint16_t);
 void BasicSearchInit (void);
 
 #endif /* __UTIL_SPM_BS__ */
