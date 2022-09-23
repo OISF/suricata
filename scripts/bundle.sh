20c3aa8a744614d62863de0a237a6adeabfea86c
Jason Ish <jason.ish@oisf.net>
2022-09-22 22:29:28 -0600

    bundle.sh: allow a PR # to be specified
    Respect LIBHTP_PR and SU_PR variables to allow a pull request to be
    used. This allows CI to use this script as well, instead of multiple
    variations of the same thing.

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -4,7 +4,22 @@
 #
 # To use, run from the top Suricata source directory:
 #
-#    ./scripts/bundle.sh
+#    ./scripts/bundle.sh [suricata-update|libhtp]
+#
+# If no arguments are provided, both suricata-update and libhtp will
+# be bundled.
+#
+# Environment variables:
+#
+#   SU_REPO:   Overrides the Suricata-Update git repo
+#   SU_BRANCH: Override the Suricata-Update branch
+#   SU_PR:     Use a GitHub pull-request from SU_REPO
+#
+#   LIBHTP_REPO:   Overrides the libhtp git repo
+#   LIBHTP_BRANCH: Override the libhtp branch
+#   LIBHTP_PR:     Use a GitHub pull-request from LIBHTP_REPO
+
+set -e
 
 what="$1"
 
@@ -20,20 +35,35 @@ while IFS= read -r requirement; do
     fi
     case "$1" in
         suricata-update)
-            repo=${SU_REPO:-$2}
-            branch=${SU_BRANCH:-$3}
-            echo "===> Bundling ${repo} -b ${branch}"
+            SU_REPO=${SU_REPO:-$2}
+            SU_BRANCH=${SU_BRANCH:-$3}
+            echo "===> Bundling ${SU_REPO} -b ${SU_BRANCH}"
             rm -rf suricata-update.tmp
-            git clone "${repo}" -b "${branch}" suricata-update.tmp
+            git clone --depth 1 "${SU_REPO}" -b "${SU_BRANCH}" suricata-update.tmp
+            if [[ "${SU_PR}" != "" ]]; then
+                echo "---> Switching to pull requrest ${SU_PR}"
+                cd suricata-update.tmp
+                git fetch origin pull/${SU_PR}/head:pr
+                git checkout pr
+                cd ..
+            fi
             cp -a suricata-update.tmp/* suricata-update/
             rm -rf suricata-update.tmp
             ;;
         libhtp)
-            repo=${LIBHTP_REPO:-$2}
-            branch=${LIBHTP_BRANCH:-$3}
-            echo "===> Bundling ${repo} -b ${branch}"
+            LIBHTP_REPO=${LIBHTP_REPO:-$2}
+            LIBHTP_BRANCH=${LIBHTP_BRANCH:-$3}
+            echo "===> Bundling ${LIBHTP_REPO} -b ${LIBHTP_BRANCH}"
             rm -rf libhtp
-            git clone "${repo}" -b "${branch}" libhtp
+            git clone "${LIBHTP_REPO}" -b "${LIBHTP_BRANCH}" libhtp
+            if [[ "${LIBHTP_PR}" != "" ]]; then
+                echo "---> Switching to pull requrest ${LIBHTP_PR}"
+                cd libhtp
+                git fetch origin pull/${SU_PR}/head:pr
+                git checkout pr
+                cd ..
+            fi
+            rm -rf libhtp/.git
             ;;
         \#)
             # Ignore comment.
