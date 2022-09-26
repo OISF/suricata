d30f777b342c0b2589d0a47c2be1c7db082e82f1
Victor Julien <vjulien@oisf.net>
2022-09-26 17:14:39 +0200

    stream: fix reachable assertion
    Fix `Flow::thread_id` not always getting properly set up, leading to
    a reachable assertion.
    
    Bug #4582.

--- src/stream-tcp.c
+++ src/stream-tcp.c
@@ -4941,8 +4941,6 @@ int StreamTcpPacket (ThreadVars *tv, Packet *p, StreamTcpThread *stt,
 
     SCLogDebug("p->pcap_cnt %"PRIu64, p->pcap_cnt);
 
-    HandleThreadId(tv, p, stt);
-
     TcpSession *ssn = (TcpSession *)p->flow->protoctx;
 
     /* track TCP flags */
@@ -5364,6 +5362,8 @@ TmEcode StreamTcp (ThreadVars *tv, Packet *p, void *data, PacketQueueNoLock *pq)
         StatsIncr(tv, stt->counter_tcp_no_flow);
         return TM_ECODE_OK;
     }
+
+    HandleThreadId(tv, p, stt);
 
     /* only TCP packets with a flow from here */
 
