47060839297d3301ee80ae88518cfc0d52d331ed
Philippe Antoine <pantoine@oisf.net>
2022-11-03 16:13:21 +0100

    detect: avoids memory leak on ja3 signature parsing
    If a later keyword enforces a protocol incompatible with ja3

--- src/detect-tls-ja3s-hash.c
+++ src/detect-tls-ja3s-hash.c
@@ -133,6 +133,7 @@ static int DetectTlsJa3SHashSetup(DetectEngineCtx *de_ctx, Signature *s, const c
         }
         return -2;
     }
+    s->init_data->init_flags |= SIG_FLAG_INIT_JA3;
 
     return 0;
 }
