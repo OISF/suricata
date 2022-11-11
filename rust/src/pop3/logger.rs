666d5919d35915b2527bcd66d7c862be28045345
Alexander Savage <alex.bumstead@gmail.com>
2022-11-11 09:41:30 -0500

    POP3: output error_flags

--- rust/src/pop3/logger.rs
+++ rust/src/pop3/logger.rs
@@ -18,7 +18,7 @@
 // Author: Alex Savage <alexander.savage@cyber.gc.ca>
 use super::pop3::POP3Transaction;
 use crate::jsonbuilder::{JsonBuilder, JsonError};
-use sawp_pop3::Keyword;
+use sawp_pop3::ErrorFlag;
 use std;
 
 fn log_pop3(tx: &POP3Transaction, js: &mut JsonBuilder) -> Result<(), JsonError> {
@@ -43,6 +43,23 @@ fn log_pop3(tx: &POP3Transaction, js: &mut JsonBuilder) -> Result<(), JsonError>
         js_data.close()?;
         js.set_object("data", &js_data)?;
     }
+
+    let mut js_flags = JsonBuilder::new_array();
+    if tx.error_flags.intersects(ErrorFlag::CommandTooLong) {
+        js_flags.append_string("CommandTooLong")?;
+    }
+    if tx.error_flags.intersects(ErrorFlag::ResponseTooLong) {
+        js_flags.append_string("ResponseTooLong")?;
+    }
+    if tx.error_flags.intersects(ErrorFlag::IncorrectArgumentNum) {
+        js_flags.append_string("IncorrectArgumentNum")?;
+    }
+    if tx.error_flags.intersects(ErrorFlag::UnknownKeyword) {
+        js_flags.append_string("UnknownKeyword")?;
+    }
+    js_flags.close();
+    js.set_object("error_flags", &js_flags)?;
+
     Ok(())
 }
 
