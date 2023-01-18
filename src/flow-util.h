4fac32fd6aaffb604b3a127526bc97aa6599b3b8
Jeff Lucovsky <jlucovsky@oisf.net>
2023-01-18 08:09:25 -0500

    format/clang: Apply clang-format
    Issue: 5718

--- src/flow-util.h
+++ src/flow-util.h
@@ -27,52 +27,54 @@
 #include "flow.h"
 #include "stream-tcp-private.h"
 
-#define COPY_TIMESTAMP(src,dst) ((dst)->tv_sec = (src)->tv_sec, (dst)->tv_usec = (src)->tv_usec)
+#define COPY_TIMESTAMP(src, dst) ((dst)->tv_sec = (src)->tv_sec, (dst)->tv_usec = (src)->tv_usec)
 
-#define RESET_COUNTERS(f) do { \
-        (f)->todstpktcnt = 0; \
-        (f)->tosrcpktcnt = 0; \
-        (f)->todstbytecnt = 0; \
-        (f)->tosrcbytecnt = 0; \
+#define RESET_COUNTERS(f)                                                                          \
+    do {                                                                                           \
+        (f)->todstpktcnt = 0;                                                                      \
+        (f)->tosrcpktcnt = 0;                                                                      \
+        (f)->todstbytecnt = 0;                                                                     \
+        (f)->tosrcbytecnt = 0;                                                                     \
     } while (0)
 
-#define FLOW_INITIALIZE(f) do { \
-        (f)->sp = 0; \
-        (f)->dp = 0; \
-        (f)->proto = 0; \
-        (f)->livedev = NULL; \
-        (f)->timeout_at = 0; \
-        (f)->timeout_policy = 0; \
-        (f)->vlan_idx = 0; \
-        (f)->next = NULL; \
-        (f)->flow_state = 0; \
-        (f)->use_cnt = 0; \
-        (f)->tenant_id = 0; \
-        (f)->parent_id = 0; \
-        (f)->probing_parser_toserver_alproto_masks = 0; \
-        (f)->probing_parser_toclient_alproto_masks = 0; \
-        (f)->flags = 0; \
-        (f)->file_flags = 0; \
-        (f)->protodetect_dp = 0; \
-        (f)->lastts.tv_sec = 0; \
-        (f)->lastts.tv_usec = 0; \
-        FLOWLOCK_INIT((f)); \
-        (f)->protoctx = NULL; \
-        (f)->flow_end_flags = 0; \
-        (f)->alproto = 0; \
-        (f)->alproto_ts = 0; \
-        (f)->alproto_tc = 0; \
-        (f)->alproto_orig = 0; \
-        (f)->alproto_expect = 0; \
-        (f)->de_ctx_version = 0; \
-        (f)->thread_id[0] = 0; \
-        (f)->thread_id[1] = 0; \
-        (f)->alparser = NULL; \
-        (f)->alstate = NULL; \
-        (f)->sgh_toserver = NULL; \
-        (f)->sgh_toclient = NULL; \
-        (f)->flowvar = NULL; \
-        RESET_COUNTERS((f)); \
+#define FLOW_INITIALIZE(f)                                                                         \
+    do {                                                                                           \
+        (f)->sp = 0;                                                                               \
+        (f)->dp = 0;                                                                               \
+        (f)->proto = 0;                                                                            \
+        (f)->livedev = NULL;                                                                       \
+        (f)->timeout_at = 0;                                                                       \
+        (f)->timeout_policy = 0;                                                                   \
+        (f)->vlan_idx = 0;                                                                         \
+        (f)->next = NULL;                                                                          \
+        (f)->flow_state = 0;                                                                       \
+        (f)->use_cnt = 0;                                                                          \
+        (f)->tenant_id = 0;                                                                        \
+        (f)->parent_id = 0;                                                                        \
+        (f)->probing_parser_toserver_alproto_masks = 0;                                            \
+        (f)->probing_parser_toclient_alproto_masks = 0;                                            \
+        (f)->flags = 0;                                                                            \
+        (f)->file_flags = 0;                                                                       \
+        (f)->protodetect_dp = 0;                                                                   \
+        (f)->lastts.tv_sec = 0;                                                                    \
+        (f)->lastts.tv_usec = 0;                                                                   \
+        FLOWLOCK_INIT((f));                                                                        \
+        (f)->protoctx = NULL;                                                                      \
+        (f)->flow_end_flags = 0;                                                                   \
+        (f)->alproto = 0;                                                                          \
+        (f)->alproto_ts = 0;                                                                       \
+        (f)->alproto_tc = 0;                                                                       \
+        (f)->alproto_orig = 0;                                                                     \
+        (f)->alproto_expect = 0;                                                                   \
+        (f)->de_ctx_version = 0;                                                                   \
+        (f)->thread_id[0] = 0;                                                                     \
+        (f)->thread_id[1] = 0;                                                                     \
+        (f)->alparser = NULL;                                                                      \
+        (f)->alstate = NULL;                                                                       \
+        (f)->sgh_toserver = NULL;                                                                  \
+        (f)->sgh_toclient = NULL;                                                                  \
+        (f)->flowvar = NULL;                                                                       \
+        RESET_COUNTERS((f));                                                                       \
     } while (0)
 
 /** \brief macro to recycle a flow before it goes into the spare queue for reuse.
@@ -80,59 +82,61 @@
  *  Note that the lnext, lprev, hnext fields are untouched, those are
  *  managed by the queueing code. Same goes for fb (FlowBucket ptr) field.
  */
-#define FLOW_RECYCLE(f) do { \
-        FlowCleanupAppLayer((f)); \
-        (f)->sp = 0; \
-        (f)->dp = 0; \
-        (f)->proto = 0; \
-        (f)->livedev = NULL; \
-        (f)->vlan_idx = 0; \
-        (f)->ffr = 0; \
-        (f)->next = NULL; \
-        (f)->timeout_at = 0; \
-        (f)->timeout_policy = 0; \
-        (f)->flow_state = 0; \
-        (f)->use_cnt = 0; \
-        (f)->tenant_id = 0; \
-        (f)->parent_id = 0; \
-        (f)->probing_parser_toserver_alproto_masks = 0; \
-        (f)->probing_parser_toclient_alproto_masks = 0; \
-        (f)->flags = 0; \
-        (f)->file_flags = 0; \
-        (f)->protodetect_dp = 0; \
-        (f)->lastts.tv_sec = 0; \
-        (f)->lastts.tv_usec = 0; \
-        (f)->protoctx = NULL; \
-        (f)->flow_end_flags = 0; \
-        (f)->alparser = NULL; \
-        (f)->alstate = NULL; \
-        (f)->alproto = 0; \
-        (f)->alproto_ts = 0; \
-        (f)->alproto_tc = 0; \
-        (f)->alproto_orig = 0; \
-        (f)->alproto_expect = 0; \
-        (f)->de_ctx_version = 0; \
-        (f)->thread_id[0] = 0; \
-        (f)->thread_id[1] = 0; \
-        (f)->sgh_toserver = NULL; \
-        (f)->sgh_toclient = NULL; \
-        GenericVarFree((f)->flowvar); \
-        (f)->flowvar = NULL; \
-        if (MacSetFlowStorageEnabled()) { \
-            MacSet *ms = FlowGetStorageById((f), MacSetGetFlowStorageID()); \
-            if (ms != NULL) { \
-                MacSetReset(ms); \
-            } \
-        } \
-        RESET_COUNTERS((f)); \
-    } while(0)
+#define FLOW_RECYCLE(f)                                                                            \
+    do {                                                                                           \
+        FlowCleanupAppLayer((f));                                                                  \
+        (f)->sp = 0;                                                                               \
+        (f)->dp = 0;                                                                               \
+        (f)->proto = 0;                                                                            \
+        (f)->livedev = NULL;                                                                       \
+        (f)->vlan_idx = 0;                                                                         \
+        (f)->ffr = 0;                                                                              \
+        (f)->next = NULL;                                                                          \
+        (f)->timeout_at = 0;                                                                       \
+        (f)->timeout_policy = 0;                                                                   \
+        (f)->flow_state = 0;                                                                       \
+        (f)->use_cnt = 0;                                                                          \
+        (f)->tenant_id = 0;                                                                        \
+        (f)->parent_id = 0;                                                                        \
+        (f)->probing_parser_toserver_alproto_masks = 0;                                            \
+        (f)->probing_parser_toclient_alproto_masks = 0;                                            \
+        (f)->flags = 0;                                                                            \
+        (f)->file_flags = 0;                                                                       \
+        (f)->protodetect_dp = 0;                                                                   \
+        (f)->lastts.tv_sec = 0;                                                                    \
+        (f)->lastts.tv_usec = 0;                                                                   \
+        (f)->protoctx = NULL;                                                                      \
+        (f)->flow_end_flags = 0;                                                                   \
+        (f)->alparser = NULL;                                                                      \
+        (f)->alstate = NULL;                                                                       \
+        (f)->alproto = 0;                                                                          \
+        (f)->alproto_ts = 0;                                                                       \
+        (f)->alproto_tc = 0;                                                                       \
+        (f)->alproto_orig = 0;                                                                     \
+        (f)->alproto_expect = 0;                                                                   \
+        (f)->de_ctx_version = 0;                                                                   \
+        (f)->thread_id[0] = 0;                                                                     \
+        (f)->thread_id[1] = 0;                                                                     \
+        (f)->sgh_toserver = NULL;                                                                  \
+        (f)->sgh_toclient = NULL;                                                                  \
+        GenericVarFree((f)->flowvar);                                                              \
+        (f)->flowvar = NULL;                                                                       \
+        if (MacSetFlowStorageEnabled()) {                                                          \
+            MacSet *ms = FlowGetStorageById((f), MacSetGetFlowStorageID());                        \
+            if (ms != NULL) {                                                                      \
+                MacSetReset(ms);                                                                   \
+            }                                                                                      \
+        }                                                                                          \
+        RESET_COUNTERS((f));                                                                       \
+    } while (0)
 
-#define FLOW_DESTROY(f) do { \
-        FlowCleanupAppLayer((f)); \
-        \
-        FLOWLOCK_DESTROY((f)); \
-        GenericVarFree((f)->flowvar); \
-    } while(0)
+#define FLOW_DESTROY(f)                                                                            \
+    do {                                                                                           \
+        FlowCleanupAppLayer((f));                                                                  \
+                                                                                                   \
+        FLOWLOCK_DESTROY((f));                                                                     \
+        GenericVarFree((f)->flowvar);                                                              \
+    } while (0)
 
 /** \brief check if a memory alloc would fit in the memcap
  *
@@ -141,8 +145,9 @@
  *  \retval 1 it fits
  *  \retval 0 no fit
  */
-#define FLOW_CHECK_MEMCAP(size) \
-    ((((uint64_t)SC_ATOMIC_GET(flow_memuse) + (uint64_t)(size)) <= SC_ATOMIC_GET(flow_config.memcap)))
+#define FLOW_CHECK_MEMCAP(size)                                                                    \
+    ((((uint64_t)SC_ATOMIC_GET(flow_memuse) + (uint64_t)(size)) <=                                 \
+            SC_ATOMIC_GET(flow_config.memcap)))
 
 Flow *FlowAlloc(void);
 Flow *FlowAllocDirect(void);
@@ -174,4 +179,3 @@ static inline void FlowEndCountersUpdate(ThreadVars *tv, FlowEndCounters *fec, F
 void FlowEndCountersRegister(ThreadVars *t, FlowEndCounters *fec);
 
 #endif /* __FLOW_UTIL_H__ */
-
