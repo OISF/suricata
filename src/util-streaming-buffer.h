2ab3f49a04bcdaa0fd2a06f06d184ddf10f6f74e
Victor Julien <vjulien@oisf.net>
2023-01-21 12:03:56 +0100

    streaming: add sbcfg to StreamingBufferAppendRaw

--- src/util-streaming-buffer.h
+++ src/util-streaming-buffer.h
@@ -164,7 +164,7 @@ void StreamingBufferSlideToOffset(
         StreamingBuffer *sb, const StreamingBufferConfig *cfg, uint64_t offset);
 
 StreamingBufferSegment *StreamingBufferAppendRaw(StreamingBuffer *sb,
-        const uint8_t *data, uint32_t data_len) WARN_UNUSED;
+        const StreamingBufferConfig *cfg, const uint8_t *data, uint32_t data_len) WARN_UNUSED;
 int StreamingBufferAppend(StreamingBuffer *sb, const StreamingBufferConfig *cfg,
         StreamingBufferSegment *seg, const uint8_t *data, uint32_t data_len) WARN_UNUSED;
 int StreamingBufferAppendNoTrack(StreamingBuffer *sb, const StreamingBufferConfig *cfg,
