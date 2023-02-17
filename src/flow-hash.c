662a7042b91068a9aedbf3cafcee1a22dc467fb8
Juliana Fajardini <jufajardini@oisf.net>
2023-02-17 17:34:09 -0300

    misc: fix typos, doc, update copyright years
    Updated FlowGetNew documentation, where it said NULL was only returned
    in case of error.
    
    (cherry picked from commit f511a4ae3f954a3c55b57a0fdffc0ae22a965dd5)

--- src/flow-hash.c
+++ src/flow-hash.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2020 Open Information Security Foundation
+/* Copyright (C) 2007-2023 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -534,7 +534,8 @@ static inline void NoFlowHandleIPS(Packet *p)
  *  \param tv thread vars
  *  \param fls lookup support vars
  *
- *  \retval f *LOCKED* flow on succes, NULL on error.
+ *  \retval f *LOCKED* flow on success, NULL on error or if we should not create
+ *  a new flow.
  */
 static Flow *FlowGetNew(ThreadVars *tv, FlowLookupStruct *fls, Packet *p)
 {
