43a22c9602a7dd5ab9936607dbc0a3a3a9cd873f
Victor Julien <vjulien@oisf.net>
2023-02-21 10:47:13 +0100

    stream: D-SACK detection and logging
    RFC 2883 specifies a special use of SACKs to indicate a host has
    received a segment it considers a spurious retransmission.

--- src/output-eve-stream.c
+++ src/output-eve-stream.c
@@ -343,6 +343,8 @@ static int EveStreamLogger(ThreadVars *tv, void *thread_data, const Packet *p)
             jb_append_string(js, "state_update");
         if (p->tcpvars.stream_pkt_flags & STREAM_PKT_FLAG_DUP_ACK)
             jb_append_string(js, "dup_ack");
+        if (p->tcpvars.stream_pkt_flags & STREAM_PKT_FLAG_DSACK)
+            jb_append_string(js, "dsack");
         jb_close(js);
     }
     jb_close(js);
