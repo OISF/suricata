15a61f02a0de0b5815d72c99c0d313ed8453f4d0
Lukas Sismis <lsismis@oisf.net>
2023-03-22 18:54:35 +0100

    dpdk: fix multibit comparison
    Ticket: #5925

--- src/runmode-dpdk.c
+++ src/runmode-dpdk.c
@@ -1020,7 +1020,8 @@ static void DeviceInitPortConf(const DPDKIfaceConfig *iconf,
 
     if (iconf->checksum_mode == CHECKSUM_VALIDATION_DISABLE) {
         SCLogConfig("%s: checksum validation disabled", iconf->iface);
-    } else if (dev_info->rx_offload_capa & RTE_ETH_RX_OFFLOAD_CHECKSUM) {
+    } else if ((dev_info->rx_offload_capa & RTE_ETH_RX_OFFLOAD_CHECKSUM) ==
+               RTE_ETH_RX_OFFLOAD_CHECKSUM) { // multibit comparison to make sure all bits are set
         if (iconf->checksum_mode == CHECKSUM_VALIDATION_ENABLE &&
                 iconf->flags & DPDK_RX_CHECKSUM_OFFLOAD) {
             SCLogConfig("%s: IP, TCP and UDP checksum validation offloaded", iconf->iface);
