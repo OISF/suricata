f96bcc27d9babab3db3e2c983baabfa70c466430
Lukas Sismis <lsismis@oisf.net>
2023-04-02 10:41:08 +0200

    policy: postpone setting of policy after determining engine mode
    Ticket: #5960

--- src/suricata.c
+++ src/suricata.c
@@ -2666,12 +2666,12 @@ int PostConfLoadedSetup(SCInstance *suri)
 
     MacSetRegisterFlowStorage();
 
-    SetMasterExceptionPolicy();
-
     LiveDeviceFinalize(); // must be after EBPF extension registration
 
     RunModeEngineIsIPS(
             suricata.run_mode, suricata.runmode_custom_mode, suricata.capture_plugin_name);
+
+    SetMasterExceptionPolicy();
 
     AppLayerSetup();
 
