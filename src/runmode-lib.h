f79c261a5396e3702286b07be2a1638276d01c5a
Angelo Mirabella <mirabellaa@vmware.com>
2023-04-06 06:44:10 -0700

    libsuricata: run suricata as a library
    This changeset is the first attempt at running suricata as a
    library by allowing to create worker threads from an external client.
    More in particular:
     * decode.h / flow-hash.c: Add the packet tenant_id integer to the
       flow hash generation to assign packets of the same flow to the
       same tenant.
     * examples/suricata_lib/main.c: Create a simple client able to
       open PCAPs and feed the packets into the library.
     * runmode-lib.c/.h: Add new runmode module to support running as a
       library.
     * source-lib.c/.h: Add new source module to support running as a
       library.
     * suricata.c/.h: Reorganize the code to provide a single interface
       when running as executable and as a library.
     * suricata-interface.c/.h: Add interface to the library.
     * tm*: Support "fake" threads when running as a library.

---
+++ src/runmode-lib.h
@@ -0,0 +1,35 @@
+/** \file
+ *
+ *  \author Angelo Mirabella <mirabellaa@vmware.com>
+ */
+
+#ifndef __RUNMODE_LIB_H__
+#define __RUNMODE_LIB_H__
+
+/** \brief register runmodes for suricata as a library */
+void RunModeIdsLibRegister(void);
+
+/** \brief runmode for live packet processing */
+int RunModeIdsLibLive(void);
+
+/** \brief runmode for offline packet processing (pcap files) */
+int RunModeIdsLibOffline(void);
+
+/** \brief create a "fake" worker thread in charge of processing the packets.
+ *
+ *  This method just creates a context representing the worker, which is handled from the library
+ *  client. No actual thread (pthread_t) is created.
+ *
+ *  \return Pointer to ThreadVars structure representing the worker thread */
+void *RunModeCreateWorker(void);
+
+/** \brief start the "fake" worker.
+ *
+ *  This method performs all the initialization tasks.
+ */
+void RunModeSpawnWorker(void *);
+
+/** \brief destroy a worker thread */
+void RunModeDestroyWorker(void *);
+
+#endif /* __RUNMODE_LIB_H__ */
