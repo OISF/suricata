0da676c9db7e52515e86e42fb8cf6ffe6467aad1
Angelo Mirabella <mirabellaa@vmware.com>
2023-04-08 10:58:30 -0700

    libsuricata: add callback for events
    This changeset adds a new output module category based on callbacks.
    Each output module belonging to this group will invoke a callback
    whenever an event is generated instead of logging it to file/socket.
    Added callback modules:
     * Alert
     * Flow
     * Http
     * Fileinfo

--- src/suricata-interface.h
+++ src/suricata-interface.h
@@ -20,18 +20,63 @@ typedef struct SuricataCtx {
     /* Number of worker threads that are done processing. */
     int n_workers_done;
 
+    /* Callbacks to invoke for each event. */
+    Callbacks callbacks;
+
     /* Mutex to access the fields. */
     pthread_mutex_t lock;
 } SuricataCtx;
 
 /**
+ * \brief Create a Suricata context.
+ *
+ * \param n_workers    Number of worker threads that will be allocated.
+ * \return SuricataCtx Pointer to the initialized Suricata context.
+ */
+SuricataCtx *suricata_create_ctx(int n_workers);
+
+/**
+ * \brief Register a callback that is invoked for every alert.
+ *
+ * \param ctx            Pointer to SuricataCtx.
+ * \param user_ctx       Pointer to a user-defined context object.
+ * \param callback       Pointer to a callback function.
+ */
+void suricata_register_alert_cb(SuricataCtx *ctx, void *user_ctx, CallbackFuncAlert callback);
+
+/**
+ * \brief Register a callback that is invoked for every fileinfo event.
+ *
+ * \param ctx            Pointer to SuricataCtx.
+ * \param user_ctx       Pointer to a user-defined context object.
+ * \param callback       Pointer to a callback function.
+ */
+void suricata_register_fileinfo_cb(SuricataCtx *ctx, void *user_ctx, CallbackFuncFileinfo callback);
+
+/**
+ * \brief Register a callback that is invoked for every flow.
+ *
+ * \param ctx            Pointer to SuricataCtx.
+ * \param user_ctx       Pointer to a user-defined context object.
+ * \param callback       Pointer to a callback function.
+ */
+void suricata_register_flow_cb(SuricataCtx *ctx, void *user_ctx, CallbackFuncFlow callback);
+
+/**
+ * \brief Register a callback that is invoked for every HTTP event.
+ *
+ * \param ctx            Pointer to SuricataCtx.
+ * \param user_ctx       Pointer to a user-defined context object.
+ * \param callback       Pointer to a callback function.
+ */
+void suricata_register_http_cb(SuricataCtx *ctx, void *user_ctx, CallbackFuncHttp callback);
+
+/**
  * \brief Initialize a Suricata context.
  *
  * \param config      Configuration string.
- * \param n_workers   Number of worker threads will be allocated.
- * \return SCInstance Pointer to the initialized Suricata context.
  */
-SuricataCtx *suricata_init(const char *config, int n_workers);
+void suricata_init(const char *config);
 
 /**
  * \brief Create a worker thread.
