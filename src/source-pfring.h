eceb3c8edb9a0e85350ecc5ec41b06b2dea458d9
Jeff Lucovsky <jlucovsky@oisf.net>
2023-04-16 08:35:02 -0400

    pf-ring: Add add'l cluster types
    This commit adds preprocessor values for additional pf-ring
    cluster-types:
    - CLUSTER_INNER_FLOW
    - CLUSTER_INNER_FLOW_2_TUPLE
    - CLUSTER_INNER_FLOW_4_TUPLE
    - CLUSTER_INNER_FLOW_5_TUPLE
    
    Issue: 5975

--- src/source-pfring.h
+++ src/source-pfring.h
@@ -69,8 +69,16 @@ void TmModuleDecodePfringRegister (void);
 int PfringConfGetThreads(void);
 void PfringLoadConfig(void);
 
-/* We don't have to use an enum that sucks in our code */
+/*
+ * We don't have to use an enum that sucks in our code
+ * these values must match with cluster_type in the kernel
+ * include file pf_ring.h
+ */
 #define CLUSTER_FLOW 0
 #define CLUSTER_ROUND_ROBIN 1
 #define CLUSTER_FLOW_5_TUPLE 4
+#define CLUSTER_INNER_FLOW         6
+#define CLUSTER_INNER_FLOW_2_TUPLE 7
+#define CLUSTER_INNER_FLOW_4_TUPLE 8
+#define CLUSTER_INNER_FLOW_5_TUPLE 9
 #endif /* __SOURCE_PFRING_H__ */
