ce9a7def5b6b4bf6188aad93b1bc3a7407e31e6f
Victor Julien <vjulien@oisf.net>
2023-04-28 14:56:45 +0200

    stream: minor const cleanup

--- src/stream-tcp.h
+++ src/stream-tcp.h
@@ -148,7 +148,7 @@ int StreamReassembleLog(TcpSession *ssn, TcpStream *stream,
 int StreamReassembleRaw(TcpSession *ssn, const Packet *p,
         StreamReassembleRawFunc Callback, void *cb_data,
         uint64_t *progress_out, bool respect_inspect_depth);
-void StreamReassembleRawUpdateProgress(TcpSession *ssn, Packet *p, uint64_t progress);
+void StreamReassembleRawUpdateProgress(TcpSession *ssn, Packet *p, const uint64_t progress);
 
 void StreamTcpDetectLogFlush(ThreadVars *tv, StreamTcpThread *stt, Flow *f, Packet *p, PacketQueueNoLock *pq);
 
