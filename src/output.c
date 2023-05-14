0eadae5fd5b494ab233957e73fefebe668d67019
Philippe Antoine <contact@catenacyber.fr>
2023-05-14 19:42:55 +0200

    output: comments for non-generic app-layers

--- src/output.c
+++ src/output.c
@@ -1131,33 +1131,33 @@ void OutputRegisterLoggers(void)
 
 static AppLayerLogger alert_applayer_loggers[ALPROTO_MAX] = {
     { ALPROTO_UNKNOWN, NULL, NULL },
-    { ALPROTO_HTTP1, NULL, NULL }, // TODO empty http object and option_flags
+    { ALPROTO_HTTP1, NULL, NULL }, // special: uses some options flags
     { ALPROTO_FTP, "ftp", EveFTPLogCommand },
-    { ALPROTO_SMTP, NULL, NULL }, // TODO state, + log email
+    { ALPROTO_SMTP, NULL, NULL }, // special: logs both smtp and email fields
     { ALPROTO_TLS, "tls", JsonTlsLogJSONExtended },
     { ALPROTO_SSH, "ssh", rs_ssh_log_json },
     { ALPROTO_IMAP, NULL, NULL },   // protocol detection only
     { ALPROTO_JABBER, NULL, NULL }, // no parser, no logging
-    { ALPROTO_SMB, NULL, NULL },    // TODO state
-    { ALPROTO_DCERPC, NULL, NULL }, // TODO
+    { ALPROTO_SMB, NULL, NULL },    // special: uses state
+    { ALPROTO_DCERPC, NULL, NULL }, // TODO missing
     { ALPROTO_IRC, NULL, NULL },    // no parser, no logging
     { ALPROTO_DNS, "dns", AlertJsonDns },
     { ALPROTO_MODBUS, "modbus", (bool (*)(void *tx, struct JsonBuilder *jb))rs_modbus_to_json },
     { ALPROTO_ENIP, NULL, NULL }, // no logging
     { ALPROTO_DNP3, "dnp3", AlertJsonDnp3 },
-    { ALPROTO_NFS, NULL, NULL }, // TODO log rpc field
+    { ALPROTO_NFS, NULL, NULL }, // special: logs both nfs and rpc fields
     { ALPROTO_NTP, NULL, NULL }, // no logging
     { ALPROTO_FTPDATA, "ftp_data", EveFTPDataAddMetadata },
     { ALPROTO_TFTP, "tftp", (bool (*)(void *tx, struct JsonBuilder *jb))rs_tftp_log_json_request },
-    { ALPROTO_IKE, NULL, NULL }, // TODO state + option
+    { ALPROTO_IKE, NULL, NULL }, // special: uses state
     { ALPROTO_KRB5, "krb5", (bool (*)(void *tx, struct JsonBuilder *jb))rs_krb5_log_json_response },
     { ALPROTO_QUIC, "quic", rs_quic_to_json },
-    { ALPROTO_DHCP, NULL, NULL }, // TODO logger with option
+    { ALPROTO_DHCP, NULL, NULL }, // TODO missing
     { ALPROTO_SNMP, "snmp", (bool (*)(void *tx, struct JsonBuilder *jb))rs_snmp_log_json_response },
     { ALPROTO_SIP, "sip", (bool (*)(void *tx, struct JsonBuilder *jb))rs_sip_log_json },
     { ALPROTO_RFB, "rfb", rs_rfb_logger_log },
     { ALPROTO_MQTT, "mqtt", JsonMQTTAddMetadata },
-    { ALPROTO_PGSQL, NULL, NULL },  // TODO flags
+    { ALPROTO_PGSQL, NULL, NULL },  // TODO missing
     { ALPROTO_TELNET, NULL, NULL }, // no logging
     { ALPROTO_TEMPLATE, "template", rs_template_logger_log },
     { ALPROTO_RDP, "rdp", (bool (*)(void *tx, struct JsonBuilder *jb))rs_rdp_to_json },
