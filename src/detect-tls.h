da0eaa02a159deda085b5646f1d9084dc2c43741
Victor Julien <vjulien@oisf.net>
2023-06-16 14:59:40 +0200

    detect/tls: minor fingerprint cleanup
    (cherry picked from commit ee6894746032629edbcfdf9a010bb1da1f3a52ed)

--- src/detect-tls.h
+++ src/detect-tls.h
@@ -38,8 +38,7 @@ typedef struct DetectTlsData_ {
     uint16_t ver; /** tls version to match */
     uint32_t flags; /** flags containing match variant (Negation for example) */
     char * subject; /** tls certificate subject substring to match */
-    char * issuerdn; /** tls certificate issuerDN substring to match */
-    char * fingerprint; /** tls fingerprint substring to match */
+    char *issuerdn; /** tls certificate issuerDN substring to match */
 } DetectTlsData;
 
 /* prototypes */
