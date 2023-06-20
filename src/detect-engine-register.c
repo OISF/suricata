87f18938b2ab7db46d62058d914594103f97167e
Philippe Antoine <pantoine@oisf.net>
2023-06-20 16:20:34 +0200

    detect: adds flow integer keywords
    Ticket: #6164
    
    flow.pkts_toclient
    flow.pkts_toserver
    flow.bytes_toclient
    flow.bytes_toserver

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -114,6 +114,7 @@
 #include "detect-rev.h"
 #include "detect-flow.h"
 #include "detect-flow-age.h"
+#include "detect-flow-pkts.h"
 #include "detect-tcp-window.h"
 #include "detect-ftpbounce.h"
 #include "detect-isdataat.h"
@@ -561,6 +562,10 @@ void SigTableSetup(void)
     DetectReplaceRegister();
     DetectFlowRegister();
     DetectFlowAgeRegister();
+    DetectFlowPktsToClientRegister();
+    DetectFlowPktsToServerRegister();
+    DetectFlowBytesToClientRegister();
+    DetectFlowBytesToServerRegister();
     DetectWindowRegister();
     DetectRpcRegister();
     DetectFtpbounceRegister();
