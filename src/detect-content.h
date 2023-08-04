2cc6de05e5249a61a007c447c6fa1f0e24b9b6de
Victor Julien <vjulien@oisf.net>
2023-08-04 14:18:20 +0200

    detect/content: cleanup nocase conversion

--- src/detect-content.h
+++ src/detect-content.h
@@ -132,5 +132,6 @@ void DetectContentPropagateLimits(Signature *s);
 void DetectContentPatternPrettyPrint(const DetectContentData *cd, char *str, size_t str_len);
 void SigParseRequiredContentSize(
         const Signature *s, const int max, const SigMatch *sm, int *len, int *offset);
+int DetectContentConvertToNocase(DetectEngineCtx *de_ctx, DetectContentData *cd);
 
 #endif /* __DETECT_CONTENT_H__ */
