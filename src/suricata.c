aa87971987877379e90a24bc692122b3be3cf7ff
Victor Julien <vjulien@oisf.net>
2023-08-08 15:50:53 +0200

    detect: free all tenant detect engines
    Free all tenants registered in the master.

--- src/suricata.c
+++ src/suricata.c
@@ -373,7 +373,7 @@ static void GlobalsDestroy(SCInstance *suri)
         DetectEngineMoveToFreeList(de_ctx);
         DetectEngineDeReference(&de_ctx);
     }
-    DetectEnginePruneFreeList();
+    DetectEngineClearMaster();
 
     AppLayerDeSetup();
     DatasetsSave();
