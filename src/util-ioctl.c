f3499867d5c7018e9c8dc1906ff339fc4a5d2dff
Shivani Bhardwaj <shivani@oisf.net>
2023-08-10 21:05:45 +0530

    af-packet: fetch mtu info once
    With the current layout and fn calls, it was seen that once in the
    beginning after the MTU was found and displayed to the user, when the
    threads spawned, each thread displayed MTU info as a part of
    AFPPeersListAdd fn. This happened in AF_PACKET IPS mode and led to
    excessive MTU logs.
    Save this info in the LiveDevice struct and avoid calling the unneeded
    fns later on.
    
    Bug 5831

--- src/util-ioctl.c
+++ src/util-ioctl.c
@@ -132,6 +132,7 @@ int GetIfaceMaxPacketSize(LiveDevice *ld)
         case -1:
             return 0;
     }
+    ld->mtu = mtu;
     int ll_header = GetIfaceMaxHWHeaderLength(dev);
     if (ll_header == -1) {
         /* be conservative, choose a big one */
