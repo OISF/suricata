9f5ceb3d5d7debffbe96a9aee658de2d18e7cdf0
Yatin Kanetkar <yatin.kanetkar@gmail.com>
2023-08-19 11:58:57 -0400

    dhcp: Log Vendor Class Identifier if present
    * Moved the validation to an SV Test and removed the previous added
    unit test and associated pcap.
    * Updates to the json schema were required as a result of the new
    option being added. In addition to `vendor_class_identifier`, it
    appears that `requested_ip` was also missing and causing my newly
    added test to fail, so I added that in as well.
    
    Reference Ticket : https://redmine.openinfosecfoundation.org/issues/4587

--- rust/src/dhcp/parser.rs
+++ rust/src/dhcp/parser.rs
@@ -282,35 +282,6 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_dhcp_option_60() {
-        let pcap = include_bytes!("singular_packet_option_60.pcap");
-        let payload = &pcap[24 + 16 + 42..];
-
-        match dhcp_parse(payload) {
-            Ok((_rem, message)) => {
-                assert_eq!(message.options.len(), 8);
-                let opt = &message.options[5];
-                let c = opt.code;
-                match opt.option {
-                    DHCPOptionWrapper::Generic(ref opt) => {
-                        match c {
-                            DHCP_OPT_VENDOR_CLASS_ID => {
-                                assert_eq!("MSFT 5.0", String::from_utf8_lossy(&opt.data))
-                            }
-                            _ => assert!(false)
-                        }
-                    }
-                    _ => assert!(false)
-                }
-            }
-            _ => {
-                assert!(false);
-            }
-        }
-    }
-
-
-    #[test]
     fn test_parse_client_id_too_short() {
         // Length field of 0.
         let buf: &[u8] = &[
