7bdce054b3db6f37fbe065d9925fd2774510f3e5
Philippe Antoine <pantoine@oisf.net>
2023-08-30 14:48:56 +0200

    config/flow: fix division by zero
    Fixes: 805b07fa4236 ("src: checks to avoid divisions by zero")
    
    Coverity id: 1539152
    
    Ticket: #5920
    Ticket: #6255

--- src/flow.c
+++ src/flow.c
@@ -606,7 +606,7 @@ void FlowInitConfig(bool quiet)
             FatalError("Invalid value for flow.hash-size: NULL");
         }
 
-        if (StringParseUint32(&configval, 10, strlen(conf_val), conf_val) > 0 || configval == 0) {
+        if (StringParseUint32(&configval, 10, strlen(conf_val), conf_val) > 0 && configval != 0) {
             flow_config.hash_size = configval;
         } else {
             FatalError("Invalid value for flow.hash-size");
