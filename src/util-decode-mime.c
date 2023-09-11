62b3bb0d5ed9a4aedc9b8007ff9d5971ca5b0b56
Philippe Antoine <pantoine@oisf.net>
2023-09-11 16:49:48 +0200

    mime: avoid quadratic complexity in MimeDecAddEntity
    Ticket: #6306
    
    Keep a reference to last child, consume a bit more RAM to save CPU
    
    (cherry picked from commit 737bc4f219ea36b4da4ffbebef15b0619dffbca1)

--- src/util-decode-mime.c
+++ src/util-decode-mime.c
@@ -390,7 +390,7 @@ static MimeDecUrl * MimeDecAddUrl(MimeDecEntity *entity, uint8_t *url, uint32_t 
  */
 MimeDecEntity * MimeDecAddEntity(MimeDecEntity *parent)
 {
-    MimeDecEntity *curr, *node = SCMalloc(sizeof(MimeDecEntity));
+    MimeDecEntity *node = SCMalloc(sizeof(MimeDecEntity));
     if (unlikely(node == NULL)) {
         return NULL;
     }
@@ -400,12 +400,10 @@ MimeDecEntity * MimeDecAddEntity(MimeDecEntity *parent)
     if (parent != NULL) {
         if (parent->child == NULL) {
             parent->child = node;
+            parent->last_child = node;
         } else {
-            curr = parent->child;
-            while (curr->next != NULL) {
-                curr = curr->next;
-            }
-            curr->next = node;
+            parent->last_child->next = node;
+            parent->last_child = node;
         }
     }
 
