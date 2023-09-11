14416e61b1070e2f500b7cd112243b9dec209fc3
Victor Julien <vjulien@oisf.net>
2023-09-11 21:17:36 +0200

    detect/threshold: implement per thread cache
    Thresholding often has 2 stages:
    
    1. recording matches
    2. appling an action, like suppress
    
    E.g. with something like:
    threshold:type limit, count 10, seconds 3600, track by_src;
    the recording state is about counting 10 first hits for an IP,
    then followed by the "suppress" state that might last an hour.
    
    By_src/by_dst are expensive, as they do a host table lookup and lock
    the host. If many threads require this access, lock contention becomes
    a serious problem.
    
    This patch adds a thread local cache to avoid the synchronization
    overhead. When the threshold for a host enters the "apply" stage,
    a thread local hash entry is added. This entry knows the expiry
    time and the action to apply. This way the action can be applied
    w/o the synchronization overhead.
    
    A rbtree is used to handle expiration.
    
    Implemented for IPv4.

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -3526,6 +3526,8 @@ static void DetectEngineThreadCtxFree(DetectEngineThreadCtx *det_ctx)
     AppLayerDecoderEventsFreeEvents(&det_ctx->decoder_events);
 
     SCFree(det_ctx);
+
+    ThresholdCacheThreadFree();
 }
 
 TmEcode DetectEngineThreadCtxDeinit(ThreadVars *tv, void *data)
