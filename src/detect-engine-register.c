300f5b9d43490f87a7a1913644ef4705be58d099
Giuseppe Longo <giuseppe@glongo.it>
2023-09-15 09:27:37 +0200

    detect/sip: register headers sticky buffers
    Ticket #6374

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -206,6 +206,7 @@
 #include "detect-sip-stat-msg.h"
 #include "detect-sip-request-line.h"
 #include "detect-sip-response-line.h"
+#include "detect-sip-headers.h"
 #include "detect-rfb-secresult.h"
 #include "detect-rfb-sectype.h"
 #include "detect-rfb-name.h"
@@ -673,6 +674,7 @@ void SigTableSetup(void)
     DetectSipStatMsgRegister();
     DetectSipRequestLineRegister();
     DetectSipResponseLineRegister();
+    DetectSipHeadersRegister();
     DetectRfbSecresultRegister();
     DetectRfbSectypeRegister();
     DetectRfbNameRegister();
