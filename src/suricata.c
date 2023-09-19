35cdabe155f3668b431413008e2cd6224d4f95c8
Philippe Antoine <pantoine@oisf.net>
2023-09-19 14:08:33 +0200

    stream: fix -Wshorten-64-to-32 warnings
    Ticket: #6186
    
    also for suricata args for bpf file over 4G

--- src/suricata.c
+++ src/suricata.c
@@ -517,7 +517,12 @@ static void SetBpfStringFromFile(char *filename)
         SCLogError("Failed to stat file %s", filename);
         exit(EXIT_FAILURE);
     }
-    bpf_len = st.st_size + 1;
+    // st.st_size is signed on Windows
+    if ((uint64_t)st.st_size >= UINT32_MAX) {
+        SCLogError("Cannot handle file %s because of its size", filename);
+        exit(EXIT_FAILURE);
+    }
+    bpf_len = (uint32_t)st.st_size + 1;
 
     bpf_filter = SCMalloc(bpf_len);
     if (unlikely(bpf_filter == NULL)) {
