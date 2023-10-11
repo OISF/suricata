d07e7f6862a3dfa732c6ebbafbbd42873f48d45a
Sascha Steinbiss <satta@debian.org>
2023-10-11 22:25:55 +0200

    detect: fix typo

--- src/detect-mqtt-type.c
+++ src/detect-mqtt-type.c
@@ -43,7 +43,7 @@ void MQTTTypeRegisterTests(void);
 void DetectMQTTTypeFree(DetectEngineCtx *de_ctx, void *);
 
 /**
- * \brief Registration function for ipopts: keyword
+ * \brief Registration function for mqtt.type: keyword
  */
 void DetectMQTTTypeRegister (void)
 {
