8efaebe293e2a74c8e323fa85a6f5fadf82801bc
Philippe Antoine <pantoine@oisf.net>
2023-10-17 15:28:53 +0200

    http1: configurable max number of live tx per flow
    Unlike the original commit, this commit just enforces a maximum limit,
    but does not expose the configuration of it.
    
    Ticket: #5921
    
    (cherry picked from commit 4175680a8a1c0dfaa491ee63d6e36c011d498473)

--- src/app-layer-htp.c
+++ src/app-layer-htp.c
@@ -2408,6 +2408,10 @@ static void HTPConfigSetDefaultsPhase1(HTPCfgRec *cfg_prec)
 #ifdef HAVE_HTP_CONFIG_SET_COMPRESSION_TIME_LIMIT
     htp_config_set_compression_time_limit(cfg_prec->cfg, HTP_CONFIG_DEFAULT_COMPRESSION_TIME_LIMIT);
 #endif
+#ifdef HAVE_HTP_CONFIG_SET_MAX_TX
+#define HTP_CONFIG_DEFAULT_MAX_TX_LIMIT 512
+    htp_config_set_max_tx(cfg_prec->cfg, HTP_CONFIG_DEFAULT_MAX_TX_LIMIT);
+#endif
     /* libhtp <= 0.5.9 doesn't use soft limit, but it's impossible to set
      * only the hard limit. So we set both here to the (current) htp defaults.
      * The reason we do this is that if the user sets the hard limit in the
