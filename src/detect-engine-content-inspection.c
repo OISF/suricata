OISF/suricata

commit 21f9cc3a39a0706ba3f44489a5253fcd0eef793f

discontinue matching on buffer if urilen returns a match failure.

src/detect-engine-content-inspection.c
@@ -519,6 +519,8 @@ int DetectEngineContentInspection(DetectEngineCtx *de_ctx, DetectEngineThreadCtx
             goto match;
         }
 
+        det_ctx->discontinue_matching = 0;
+
         SCReturnInt(0);
 #ifdef HAVE_LUAJIT
     }

commit 673406b8dbaf37174cd4a7c954763c274315f4ad

detect/urilen: fix discontinue matching logic
Actually discontinue matching.

Fixes: 21f9cc3a39a0 ("discontinue matching on buffer if urilen returns a match failure.")

src/detect-engine-content-inspection.c
@@ -656,7 +656,7 @@ uint8_t DetectEngineContentInspection(DetectEngineCtx *de_ctx, DetectEngineThrea
             goto match;
         }
 
-        det_ctx->discontinue_matching = 0;
+        det_ctx->discontinue_matching = 1;
 
         goto no_match;
 #ifdef HAVE_LUA