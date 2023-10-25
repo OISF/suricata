1e0327463d012451b69d034a1391a26c149625f4
Jeff Lucovsky <jlucovsky@oisf.net>
2023-10-25 09:34:44 -0400

    configure: Move configure.ac setup to autogen
    This commit changes how the product name and product version values for
    autoconf's 'AC_INIT' are populated.
    
    With this change, a 3rd party can specify a unique product name and
    product version by setting environment variables before running
    autogen.sh
    
    $ PRODUCT_NAME="my_suricata" PRODUCT_VERSION="7.0.3-my_suricata" ./autogen.sh
    
    The default name and version are unchanged if the environment variables
    are not defined:
    - suricata
    - 7.0.3-dev
    
    Future version updates should take place in 'autogen.sh'

--- autogen.sh
+++ autogen.sh
@@ -10,6 +10,9 @@ else
   echo "Failed to find libtoolize or glibtoolize, please ensure it is installed and accessible via your PATH env variable"
   exit 1
 fi;
+PRODUCT_NAME=${PRODUCT_NAME:-suricata}
+PRODUCT_VERSION=${PRODUCT_VERSION:-7.0.3-dev}
+sed -e "s/@PRODUCT_NAME@/$PRODUCT_NAME/" -e "s/@PRODUCT_VERSION@/$PRODUCT_VERSION/"  configure.ac.in > configure.ac
 autoreconf -fv --install || exit 1
 if which cargo > /dev/null; then
     if [ -f rust/Cargo.lock ] ; then
