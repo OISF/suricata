1ff2fb281f7686514d41bcde22ad9685af598e49
Juliana Fajardini <jufajardini@oisf.net>
2023-12-05 10:56:28 -0300

    pgsql: don't log password msg if password disabled
    If the logging of the password is disabled, there isn't much point in
    logging the password message itself.

--- rust/src/pgsql/logger.rs
+++ rust/src/pgsql/logger.rs
@@ -78,8 +78,6 @@ fn log_request(req: &PgsqlFEMessage, flags: u32) -> Result<JsonBuilder, JsonErro
         }) => {
             if flags & PGSQL_LOG_PASSWORDS != 0 {
                 js.set_string_from_bytes("password", payload)?;
-            } else {
-                js.set_string(req.to_str(), "password log disabled")?;
             }
         }
         PgsqlFEMessage::SASLResponse(RegularPacket {
