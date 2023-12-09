4c39a9cd6cf7af4bbf8aa60a97bed0f85fe27022
Shivani Bhardwaj <shivani@oisf.net>
2023-12-09 12:49:31 +0530

    detect/flowbits: remove DETECT_FLOWBITS_CMD_NOALERT
    DETECT_FLOWBITS_CMD_NOALERT is misleading as it gives an impression that
    noalert is a flowbit specific command that'll be used and dealt with at
    some point but as soon as noalert is found in the rule lang, signature
    flag for noalert is set and control is returned. It never gets added to
    cmd of the flowbits object.
    
    (cherry picked from commit 75471dd69b78d0915819b978e937483dca8b4a04)

--- src/detect-flowbits.c
+++ src/detect-flowbits.c
@@ -286,7 +286,10 @@ int DetectFlowbitSetup (DetectEngineCtx *de_ctx, Signature *s, const char *rawst
     }
 
     if (strcmp(fb_cmd_str,"noalert") == 0) {
-        fb_cmd = DETECT_FLOWBITS_CMD_NOALERT;
+        if (strlen(fb_name) != 0)
+            goto error;
+        s->flags |= SIG_FLAG_NOALERT;
+        return 0;
     } else if (strcmp(fb_cmd_str,"isset") == 0) {
         fb_cmd = DETECT_FLOWBITS_CMD_ISSET;
     } else if (strcmp(fb_cmd_str,"isnotset") == 0) {
@@ -303,11 +306,6 @@ int DetectFlowbitSetup (DetectEngineCtx *de_ctx, Signature *s, const char *rawst
     }
 
     switch (fb_cmd) {
-        case DETECT_FLOWBITS_CMD_NOALERT:
-            if (strlen(fb_name) != 0)
-                goto error;
-            s->flags |= SIG_FLAG_NOALERT;
-            return 0;
         case DETECT_FLOWBITS_CMD_ISNOTSET:
         case DETECT_FLOWBITS_CMD_ISSET:
         case DETECT_FLOWBITS_CMD_SET:
@@ -347,8 +345,7 @@ int DetectFlowbitSetup (DetectEngineCtx *de_ctx, Signature *s, const char *rawst
     sm->ctx = (SigMatchCtx *)cd;
 
     switch (fb_cmd) {
-        /* case DETECT_FLOWBITS_CMD_NOALERT can't happen here */
-
+        /* noalert can't happen here */
         case DETECT_FLOWBITS_CMD_ISNOTSET:
         case DETECT_FLOWBITS_CMD_ISSET:
             /* checks, so packet list */
