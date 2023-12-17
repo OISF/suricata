OISF/suricata

commit b591813b8690753ee4a678f8b10ff1928de1fe15

profiling/rules: reduce sync logic scope
Use a simple once a second scheme.

src/detect.c
@@ -1792,6 +1792,15 @@ TmEcode Detect(ThreadVars *tv, Packet *p, void *data)
     } else {
         DetectNoFlow(tv, de_ctx, det_ctx, p);
     }
+
+#ifdef PROFILE_RULES
+    /* aggregate statistics */
+    if (SCTIME_SECS(p->ts) != det_ctx->rule_perf_last_sync) {
+        SCProfilingRuleThreatAggregate(det_ctx);
+        det_ctx->rule_perf_last_sync = SCTIME_SECS(p->ts);
+    }
+#endif
+
     return TM_ECODE_OK;
 error:
     return TM_ECODE_FAILED;

src/counters.c
@@ -512,7 +512,6 @@ static void *StatsWakeupThread(void *arg)
             /* assuming the assignment of an int to be atomic, and even if it's
              * not, it should be okay */
             tv->perf_public_ctx.perf_flag = 1;
-            tv->profile_flag = 1;
 
             if (tv->inq != NULL) {
                 PacketQueue *q = tv->inq->pq;
@@ -533,7 +532,6 @@ static void *StatsWakeupThread(void *arg)
             /* assuming the assignment of an int to be atomic, and even if it's
              * not, it should be okay */
             tv->perf_public_ctx.perf_flag = 1;
-            tv->profile_flag = 1;
 
             tv = tv->next;
         }

src/detect.h
@@ -1188,6 +1188,7 @@ typedef struct DetectEngineThreadCtx_ {
 #ifdef PROFILE_RULES
     struct SCProfileData_ *rule_perf_data;
     int rule_perf_data_size;
+    uint32_t rule_perf_last_sync;
 #endif
 #ifdef PROFILING
     struct SCProfileKeywordData_ *keyword_perf_data;

src/flow-worker.c
@@ -614,14 +614,6 @@ housekeeping:
     /* process local work queue */
     FlowWorkerProcessLocalFlows(tv, fw, p);
 
-#ifdef PROFILE_RULES
-    /* aggregate statistics */
-    if (tv->profile_flag == 1) {
-        SCProfilingRuleThreatAggregate((DetectEngineThreadCtx *)detect_thread);
-        tv->profile_flag = 0;
-    }
-#endif
-
     return TM_ECODE_OK;
 }
 

src/threadvars.h
@@ -127,9 +127,6 @@ typedef struct ThreadVars_ {
     /** public counter store: counter syncs update this */
     StatsPublicThreadContext perf_public_ctx;
 
-    /** profile sync needed */
-    uint32_t profile_flag;
-
     /* mutex and condition used by management threads */
 
     SCCtrlMutex *ctrl_mutex;

commit 7206dde9b2774d592972ccfd56076dfc79732f47

detect/profiling: improve pcap reading performance
When reading a pcap, packet time can move much faster than wall
clock time. This would trigger many more profile syncs than before.

As the sync is using a lock to synchronize with other threads, this
is an expensive operation.

Bug: #6619.

Fixes: b591813b8690 ("profiling/rules: reduce sync logic scope")

src/detect.c
@@ -1797,9 +1797,11 @@ TmEcode Detect(ThreadVars *tv, Packet *p, void *data)
 
 #ifdef PROFILE_RULES
     /* aggregate statistics */
-    if (SCTIME_SECS(p->ts) != det_ctx->rule_perf_last_sync) {
+    struct timeval ts;
+    gettimeofday(&ts, NULL);
+    if (ts.tv_sec != det_ctx->rule_perf_last_sync) {
         SCProfilingRuleThreatAggregate(det_ctx);
-        det_ctx->rule_perf_last_sync = SCTIME_SECS(p->ts);
+        det_ctx->rule_perf_last_sync = ts.tv_sec;
     }
 #endif
 