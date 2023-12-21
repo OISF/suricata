ef52a65050687dc5e243cc432d9c3fdb060e5079
Jeff Lucovsky <jlucovsky@oisf.net>
2023-12-21 09:41:29 -0500

    mqtt: Move conf code to rust
    Issue: 6387
    
    This commit moves the configuration logic to Rust.

--- src/app-layer-mqtt.c
+++ src/app-layer-mqtt.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 Open Information Security Foundation
+/* Copyright (C) 2020-2023 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -22,13 +22,7 @@
  */
 
 #include "suricata-common.h"
-#include "stream.h"
-#include "conf.h"
 
-#include "util-misc.h"
-#include "util-unittest.h"
-
-#include "app-layer-detect-proto.h"
 #include "app-layer-parser.h"
 
 #include "app-layer-mqtt.h"
@@ -37,20 +31,9 @@
 void RegisterMQTTParsers(void)
 {
     SCLogDebug("Registering Rust mqtt parser.");
-    uint32_t max_msg_len = 1048576; /* default: 1MB */
 
-    if (AppLayerParserConfParserEnabled("tcp", "mqtt")) {
-        ConfNode *p = ConfGetNode("app-layer.protocols.mqtt.max-msg-length");
-        if (p != NULL) {
-            uint32_t value;
-            if (ParseSizeStringU32(p->val, &value) < 0) {
-                SCLogError("invalid value for max-msg-length: %s", p->val);
-            } else {
-                max_msg_len = value;
-            }
-        }
-        rs_mqtt_register_parser(max_msg_len);
-    }
+    rs_mqtt_register_parser();
+
 #ifdef UNITTESTS
     AppLayerParserRegisterProtocolUnittests(IPPROTO_TCP, ALPROTO_MQTT,
         MQTTParserRegisterTests);
