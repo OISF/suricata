f178db3fa555b1aa85a31a4127d189250068ca5a
Hadiqa Alamdar Bukhari <hadiqaalamdar@gmail.com>
2024-01-24 17:10:49 +0500

    dns: add dns.rcode keyword
    dns.rcode matches the rcode header field in DNS messages
    It's an unsigned integer
    valid ranges = [0-15]
    Does not support prefilter
    Supports matches in both flow directions
    
    Task #6621

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -48,6 +48,7 @@
 #include "detect-engine-payload.h"
 #include "detect-engine-dcepayload.h"
 #include "detect-dns-opcode.h"
+#include "detect-dns-rcode.h"
 #include "detect-dns-rrtype.h"
 #include "detect-dns-query.h"
 #include "detect-dns-answer-name.h"
@@ -523,6 +524,7 @@ void SigTableSetup(void)
 
     DetectDnsQueryRegister();
     DetectDnsOpcodeRegister();
+    DetectDnsRcodeRegister();
     DetectDnsRrtypeRegister();
     DetectDnsAnswerNameRegister();
     DetectDnsQueryNameRegister();
