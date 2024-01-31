84788a93c9c5f65eea1462b4e8c31a2f6bca2056
Hadiqa Alamdar Bukhari <hadiqaalamdar@gmail.com>
2024-01-31 17:56:33 +0500

    dns: add dns.rrtype keyword
    It matches the rrtype field in DNS
    It's an unsigned integer match
    valid ranges = [0-65535]
    Does not support prefilter
    Supports flow in both directions
    Feature #6666

--- rust/src/dns/detect.rs
+++ rust/src/dns/detect.rs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 Open Information Security Foundation
+/* Copyright (C) 2019-2024 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -47,6 +47,39 @@ pub extern "C" fn rs_dns_opcode_match(
     if detect_match_uint(detect, opcode) {
         return 1;
     }
+    return 0;
+}
+
+/// Perform the DNS rrtype match.
+/// 1 will be returned on match, otherwise 0 will be returned.
+#[no_mangle]
+pub extern "C" fn rs_dns_rrtype_match(
+    tx: &mut DNSTransaction, detect: &mut DetectUintData<u16>, flags: u8,
+) -> u16 {
+    if flags & Direction::ToServer as u8 != 0 {
+        if let Some(request) = &tx.request {
+            for i in 0..request.queries.len() {
+                if detect_match_uint(detect, request.queries[i].rrtype) {
+                    return 1;
+                }
+            }
+        } else {
+            return 0;
+        }
+    } else if flags & Direction::ToClient as u8 != 0 {
+        if let Some(response) = &tx.response {
+            for i in 0..response.answers.len() {
+                if detect_match_uint(detect, response.answers[i].rrtype) {
+                    return 1;
+                }
+            }
+        } else {
+            return 0;
+        }
+    } else {
+        // Not to server or to client??
+        return 0;
+    };
     return 0;
 }
 
@@ -123,6 +156,85 @@ mod test {
                 arg2: 0,
             },
             ((0b0010_0000_0000_0000 >> 11) & 0xf) as u8,
+        ));
+    }
+
+    #[test]
+    fn parse_rrtype_good() {
+        assert_eq!(
+            detect_parse_uint::<u16>("1").unwrap().1,
+            DetectUintData {
+                mode: DetectUintMode::DetectUintModeEqual,
+                arg1: 1,
+                arg2: 0,
+            }
+        );
+        assert_eq!(
+            detect_parse_uint::<u16>("123").unwrap().1,
+            DetectUintData {
+                mode: DetectUintMode::DetectUintModeEqual,
+                arg1: 123,
+                arg2: 0,
+            }
+        );
+        assert_eq!(
+            detect_parse_uint::<u16>("!123").unwrap().1,
+            DetectUintData {
+                mode: DetectUintMode::DetectUintModeNe,
+                arg1: 123,
+                arg2: 0,
+            }
+        );
+        assert_eq!(
+            detect_parse_uint::<u16>("7-15").unwrap().1,
+            DetectUintData {
+                mode: DetectUintMode::DetectUintModeRange,
+                arg1: 7,
+                arg2: 15,
+            }
+        );
+        assert!(detect_parse_uint::<u16>("").is_err());
+        assert!(detect_parse_uint::<u16>("!").is_err());
+        assert!(detect_parse_uint::<u16>("!   ").is_err());
+        assert!(detect_parse_uint::<u16>("!asdf").is_err());
+    }
+
+    #[test]
+    fn test_match_rrtype() {
+        assert!(detect_match_uint(
+            &DetectUintData {
+                mode: DetectUintMode::DetectUintModeEqual,
+                arg1: 0,
+                arg2: 0,
+            },
+            0b0000_0000_0000_0000,
+        ));
+
+        assert!(!detect_match_uint(
+            &DetectUintData {
+                mode: DetectUintMode::DetectUintModeNe,
+                arg1: 0,
+                arg2: 0,
+            },
+            0b0000_0000_0000_0000,
+        ));
+
+        assert!(detect_match_uint(
+            &DetectUintData {
+                mode: DetectUintMode::DetectUintModeEqual,
+                arg1: 4,
+                arg2: 0,
+            },
+            4u16,
+        ));
+
+        assert!(!detect_match_uint(
+            &DetectUintData {
+                mode: DetectUintMode::DetectUintModeNe,
+                arg1: 4,
+                arg2: 0,
+            },
+            4u16,
         ));
     }
 }
