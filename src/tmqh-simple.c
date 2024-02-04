b0fbfaf9ac33152ea2edc49e995a7fdf55b747ab
Jeff Lucovsky <jlucovsky@oisf.net>
2024-02-04 09:44:44 -0500

    threads/mutex: Ensure mutex held before signaling
    Ensure that the mutex protecting the condition variable is held before
    signaling it. This ensures that the thread(s) awaiting the signal are
    notified.
    
    Issue: 6569

--- src/tmqh-simple.c
+++ src/tmqh-simple.c
@@ -76,8 +76,11 @@ void TmqhInputSimpleShutdownHandler(ThreadVars *tv)
         return;
     }
 
-    for (i = 0; i < (tv->inq->reader_cnt + tv->inq->writer_cnt); i++)
+    for (i = 0; i < (tv->inq->reader_cnt + tv->inq->writer_cnt); i++) {
+        SCMutexLock(&tv->inq->pq->mutex_q);
         SCCondSignal(&tv->inq->pq->cond_q);
+        SCMutexUnlock(&tv->inq->pq->mutex_q);
+    }
 }
 
 void TmqhOutputSimple(ThreadVars *t, Packet *p)
