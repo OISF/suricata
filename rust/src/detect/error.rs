fca86bb7bbc5a9b4e367d2ef5c10ade14f69b1c9
Jeff Lucovsky <jlucovsky@oisf.net>
2024-02-22 09:47:18 -0500

    detect/transform: from_base64 option parsing
    Issue: 6487
    
    Implement from_base64 option parsing in Rust. The Rust module also
    contains unit tests.

--- rust/src/detect/error.rs
+++ rust/src/detect/error.rs
@@ -20,9 +20,12 @@ use nom7::error::{ErrorKind, ParseError};
 /// Custom rule parse errors.
 ///
 /// Implemented based on the Nom example for implementing custom errors.
+/// The string is an error message provided by the parsing logic, e.g.,
+///      Incorrect usage because of "x", "y" and "z"
 #[derive(Debug, PartialEq, Eq)]
 pub enum RuleParseError<I> {
     InvalidByteMath(String),
+    InvalidTransformBase64(String),
 
     Nom(I, ErrorKind),
 }
