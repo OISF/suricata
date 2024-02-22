52c30e49fb28742a1d673ae90bc6f8abac099233
Jason Ish <jason.ish@oisf.net>
2024-02-22 14:27:50 -0600

    examples/lib: replicate Suricata using the library
    With more functions exposed via the library, a library user can now
    replicate the Suricata "main" function.

--- examples/lib/simple/main.c
+++ examples/lib/simple/main.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char **argv)
 {
-    SuricataMain(argc, argv);
-    return 0;
+    SuricataPreInit(argv[0]);
+    SuricataInit(argc, argv);
+    SuricataPostInit();
+
+    /* Suricata is now running, but we enter a loop to keep it running
+     * until it shouldn't be running anymore. */
+    SuricataMainLoop();
+
+    /* Shutdown engine. */
+    SuricataShutdown();
+    GlobalsDestroy();
+
+    return EXIT_SUCCESS;
 }
