9943940269802b8fef3cdfe56be8be13c90aa147
Sascha Steinbiss <satta@debian.org>
2024-02-24 10:34:50 +0100

    suricata: initialize feature tracking earlier
    This gives app layer code a chance to access feature
    information.
    
    (cherry picked from commit 32f23b6d2f71a289e78e67b9121dfa661c78aef3)

--- src/suricata.c
+++ src/suricata.c
@@ -2691,6 +2691,9 @@ int PostConfLoadedSetup(SCInstance *suri)
 
     SetMasterExceptionPolicy();
 
+    /* Must occur prior to output mod registration
+       and app layer setup. */
+    FeatureTrackingRegister();
     AppLayerSetup();
 
     /* Suricata will use this umask if provided. By default it will use the
@@ -2748,7 +2751,6 @@ int PostConfLoadedSetup(SCInstance *suri)
         SCReturnInt(TM_ECODE_FAILED);
     }
 
-    FeatureTrackingRegister(); /* must occur prior to output mod registration */
     RegisterAllModules();
     AppLayerHtpNeedFileInspection();
 
