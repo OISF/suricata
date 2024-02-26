32c7ab967924a733a3f2c8e51245c58c4a0beea7
Philippe Antoine <pantoine@oisf.net>
2024-02-26 22:32:56 +0100

    detect: bidirectional rules can use dir-ambiguous keywords
    By using keywords bidir.toclient and bidir.toserver, the following
    keywords are forced to the direction even if they can match
    on both directions.
    
    Ticket: 5665

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -590,6 +590,7 @@ void SigTableSetup(void)
     DetectOffsetRegister();
     DetectReplaceRegister();
     DetectFlowRegister();
+    DetectBidirRegister();
     DetectFlowAgeRegister();
     DetectFlowPktsToClientRegister();
     DetectFlowPktsToServerRegister();
