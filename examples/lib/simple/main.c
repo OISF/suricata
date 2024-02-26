ce7509a7ac223472d300957383da792f139965c4
Jason Ish <jason.ish@oisf.net>
2024-02-26 15:39:56 -0600

    suricata: move WindowsInitService to SuricataMain
    Move WindowsInitService from SuricataInit() to SuricataMain(), as
    initializing Suricata as a service is very specific to the application
    and not something you'd want to happen in a library, and SuricataInit
    is more common initialization for application and library usage.

--- examples/lib/simple/main.c
+++ examples/lib/simple/main.c
@@ -3,6 +3,17 @@
 int main(int argc, char **argv)
 {
     SuricataPreInit(argv[0]);
+
+#ifdef OS_WIN32
+    /* If on Windows, and you wanted initialize as a service, you
+     * might register that here. Its at this point in the
+     * initialization that the Suricata application initializes as a
+     * Windows service. */
+    if (WindowsInitService(argc, argv) != 0) {
+        exit(EXIT_FAILURE);
+    }
+#endif /* OS_WIN32 */
+
     SuricataInit(argc, argv);
     SuricataPostInit();
 
