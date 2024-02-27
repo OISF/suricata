12ffd23170911b3fb9bb3b1ec313e409abc71732
Jason Ish <jason.ish@oisf.net>
2024-02-27 16:16:08 -0600

    plugins: initialize plugins earlier
    Capture plugins need to be registered before LiveDeviceFinalize,
    otherwise Suricata errors before the plugin gets a chance to load.
    
    Bug: #6811
    (cherry picked from commit 25d0e0b68c333029fb8263cb9a2a287f9064b5c8)

--- src/suricata.c
+++ src/suricata.c
@@ -2669,6 +2669,10 @@ int PostConfLoadedSetup(SCInstance *suri)
 
     MacSetRegisterFlowStorage();
 
+#ifdef HAVE_PLUGINS
+    SCPluginsLoad(suri->capture_plugin_name, suri->capture_plugin_args);
+#endif
+
     LiveDeviceFinalize(); // must be after EBPF extension registration
 
     RunModeEngineIsIPS(
@@ -2740,9 +2744,6 @@ int PostConfLoadedSetup(SCInstance *suri)
 
     FeatureTrackingRegister(); /* must occur prior to output mod registration */
     RegisterAllModules();
-#ifdef HAVE_PLUGINS
-    SCPluginsLoad(suri->capture_plugin_name, suri->capture_plugin_args);
-#endif
     AppLayerHtpNeedFileInspection();
 
     StorageFinalize();
