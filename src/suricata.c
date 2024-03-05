f5440822c6a5a295b1bf9ae94f50f3617a9304fe
Victor Julien <vjulien@oisf.net>
2024-03-05 13:06:08 +0530

    detect: optimize sig_cnt setting
    Utilize _popcnt64 where available.
    
    (cherry picked from commit c4ac6cd)

--- src/suricata.c
+++ src/suricata.c
@@ -768,6 +768,9 @@ static void PrintBuildInfo(void)
 #if defined(SC_ADDRESS_SANITIZER)
     strlcat(features, "ASAN ", sizeof(features));
 #endif
+#if defined(HAVE_POPCNT64)
+    strlcat(features, "POPCNT64 ", sizeof(features));
+#endif
     if (strlen(features) == 0) {
         strlcat(features, "none", sizeof(features));
     }
