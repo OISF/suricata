a3979c9b6f26558f8842ff97e414654bff98527a
Lukas Sismis <lsismis@oisf.net>
2024-04-07 22:18:58 +0200

    dpdk: add unit tests for mempool cache calculation and threading assignment
    Ticket: 6927

--- src/runmode-unittests.c
+++ src/runmode-unittests.c
@@ -123,6 +123,8 @@
 #include "source-windivert.h"
 #endif
 
+#include "runmode-dpdk.h"
+
 #endif /* UNITTESTS */
 
 void TmqhSetup (void);
@@ -214,6 +216,9 @@ static void RegisterUnittests(void)
     SCProtoNameRegisterTests();
     UtilCIDRTests();
     OutputJsonStatsRegisterTests();
+#ifdef HAVE_DPDK
+    DPDKRunmodeRegisterTests();
+#endif
 }
 #endif
 
