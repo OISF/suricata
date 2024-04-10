88edf640533cffc764c13b455745c93e6469800d
Giuseppe Longo <giuseppe@glongo.it>
2024-04-10 13:59:22 +0200

    rust/ldap: implement types and filters
    This implementation adds types and filters specified in the LDAP RFC to
    work with the ldap_parser.
    Although using the parser directly would be
    best, strange behavior has been observed during transaction logging.
    It appears that C pointers are being overwritten, leading to incorrect
    output when LDAP fields are logged.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -62,6 +62,7 @@ extern crate der_parser;
 extern crate kerberos_parser;
 extern crate tls_parser;
 extern crate x509_parser;
+extern crate ldap_parser;
 
 #[macro_use]
 extern crate suricata_derive;
@@ -124,3 +125,4 @@ pub mod util;
 pub mod ffi;
 pub mod feature;
 pub mod sdp;
+pub mod ldap;
