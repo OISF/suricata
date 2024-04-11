1f05a17fb99fb1cdb8e545d7b53e5749bd01d0b6
Jason Ish <jason.ish@oisf.net>
2024-04-11 11:06:29 -0600

    lua: misc cleanups in sandbox implementation
    Including:
    - rename guards
    - SCMalloc to SCCalloc
    - remove unused enum
    - rename public functions to our naming standard

--- src/util-lua-sandbox.h
+++ src/util-lua-sandbox.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2023 Open Information Security Foundation
+/* Copyright (C) 2023-2024 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -21,45 +21,33 @@
  * \author Jo Johnson <pyrojoe314@gmail.com>
  */
 
-#ifndef __UTIL_LUA_SANDBOX_H__
-#define __UTIL_LUA_SANDBOX_H__
+#ifndef SURICATA_UTIL_LUA_SANDBOX_H
+#define SURICATA_UTIL_LUA_SANDBOX_H
 
-#ifndef HAVE_LUA
-/* If we don't have Lua, create a typedef for sb_lua_State so the
- * exported Lua functions don't fail the build. */
-typedef void sb_lua_state;
-#else
-#include <stdlib.h>
 #include "lua.h"
-
-#if !defined(SANDBOX_ALLOC_CTX)
-#define SANDBOX_CTX "SANDBOX_CTX"
-#endif
 
 /*
  *  Lua sandbox usage:  The only needed changes to use the sandboxed lua state are
- *      to replace calls to lua_newstate and lua_close with sb_newstate and sb_close
- *      Additionally, sb_loadrestricted can be used to load a restricted set of packages
+ *      to replace calls to lua_newstate and lua_close with SCLuaSbStateNew and SCLuaSbStateClose
+ *      Additionally, SCLuaSbLoadRestricted can be used to load a restricted set of packages
  *      that prevent side effecting outside of the lua runtime
  */
 
 /*
  *  Struct to store a lua_state and the additional metadata required to sandbox it
  */
-typedef struct sb_lua_state {
+typedef struct SCLuaSbState {
     lua_State *L;
 
-    // Allocation limits
+    /* Allocation limits */
     uint64_t alloc_bytes;
     uint64_t alloc_limit;
 
-    // Execution Limits
+    /* Execution Limits */
     uint64_t instruction_count;
     uint64_t instruction_limit;
     uint64_t hook_instruction_count;
-} sb_lua_state;
-
-enum sb_level { NONE, EXTERNAL_RESTRICTED, PERFORMANCE_RESTRICTED };
+} SCLuaSbState;
 
 /*
  *  Replaces luaL_newstate.  Sets an upper bound for allocations and bytecode
@@ -70,29 +58,22 @@ enum sb_level { NONE, EXTERNAL_RESTRICTED, PERFORMANCE_RESTRICTED };
  *  instructionlimit - maximum number of lua bytecode instructions before an error is thrown
  *      A value of zero will not limit the number of instructions
  */
-lua_State *sb_newstate(uint64_t alloclimit, uint64_t instructionlimit);
+lua_State *SCLuaSbStateNew(uint64_t alloclimit, uint64_t instructionlimit);
 
 /*
- *  Replaces lua_close.  Handles freeing the sb_lua_state
+ *  Replaces lua_close.  Handles freeing the SCLuaSbState
  */
-void sb_close(lua_State *sb);
+void SCLuaSbStateClose(lua_State *sb);
 
 /*
  *  Resets the instruction counter for the sandbox to 0
  */
-void sb_resetinstructioncounter(lua_State *sb);
+void SCLuaSbResetInstructionCounter(lua_State *sb);
 
 /*
- *  Sets the maximum number of lua instructions before erroring out
- */
-void sb_setinstructionlimit(lua_State *L, uint64_t instruction_limit);
-
-/*
- *  Replaces luaL_openlibs.  Only opens allowed paackages for the sandbox and
+ *  Replaces luaL_openlibs.  Only opens allowed packages for the sandbox and
  *  masks out dangerous functions from the base.
  */
-LUALIB_API void sb_loadrestricted(lua_State *L);
+void SCLuaSbLoadRestricted(lua_State *L);
 
-#endif /* HAVE_LUA */
-
-#endif /*  __UTIL_LUA_SANDBOX_H__ */
+#endif /*  SURICATA_UTIL_LUA_SANDBOX_H */
