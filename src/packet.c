c06453722b5c14b894a0a83bc01c4a56f4d6b4f9
Victor Julien <vjulien@oisf.net>
2024-04-12 15:26:46 +0200

    decode/ppp: clean up ppph pointer use
    No users of the pointer anymore, so remove it.
    
    (cherry picked from commit 7e3f071e4967303c3424d0c0ea812afb2889bbec)

--- src/packet.c
+++ src/packet.c
@@ -137,7 +137,6 @@ void PacketReinit(Packet *p)
     if (p->icmpv6h != NULL) {
         CLEAR_ICMPV6_PACKET(p);
     }
-    p->ppph = NULL;
     p->pppoesh = NULL;
     p->pppoedh = NULL;
     p->greh = NULL;
