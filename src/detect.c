81e6a3e3b02ef7a837d58f0002b070175fcad06e
Philippe Antoine <pantoine@oisf.net>
2024-06-04 08:56:53 +0200

    detect: minor optimization for tx
    do not bother to clean the buffers, if we did not run detection
    for this transaction.

--- src/detect.c
+++ src/detect.c
@@ -1612,9 +1612,9 @@ static void DetectRunTx(ThreadVars *tv,
 
             StoreDetectFlags(&tx, flow_flags, ipproto, alproto, new_detect_flags);
         }
-next:
         InspectionBufferClean(det_ctx);
 
+    next:
         if (!ires.has_next)
             break;
     }
