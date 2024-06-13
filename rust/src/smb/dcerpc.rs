b042f40d75bd16e22dc18dbc476f4dfa73b11f66
Jason Ish <jason.ish@oisf.net>
2024-06-13 14:23:28 -0600

    rust: simply matches with unwrap_or_default
    New default clippy warning:
    https://rust-lang.github.io/rust-clippy/master/index.html#manual_unwrap_or_default
    
    (cherry picked from commit 29d7ff026a3aca5a718f09112c3c0304feb4b453)

--- rust/src/smb/dcerpc.rs
+++ rust/src/smb/dcerpc.rs
@@ -455,10 +455,7 @@ pub fn smb_read_dcerpc_record(state: &mut SMBState,
     // msg_id 0 as this data crosses cmd/reply pairs
     let ehdr = SMBHashKeyHdrGuid::new(SMBCommonHdr::new(SMBHDR_TYPE_TRANS_FRAG,
             hdr.ssn_id, hdr.tree_id, 0_u64), guid.to_vec());
-    let mut prevdata = match state.ssnguid2vec_map.remove(&ehdr) {
-        Some(s) => s,
-        None => Vec::new(),
-    };
+    let mut prevdata = state.ssnguid2vec_map.remove(&ehdr).unwrap_or_default();
     SCLogDebug!("indata {} prevdata {}", indata.len(), prevdata.len());
     prevdata.extend_from_slice(indata);
     let data = prevdata;
