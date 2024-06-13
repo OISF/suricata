87fa7f10ef2b7194eb3e854ac7671e52033041da
Shivani Bhardwaj <shivani@oisf.net>
2024-06-13 18:33:30 +0530

    flow: use bool wherever possible

--- src/flow-timeout.h
+++ src/flow-timeout.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2012 Open Information Security Foundation
+/* Copyright (C) 2007-2024 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -27,7 +27,7 @@
 #include "stream-tcp-private.h"
 
 void FlowForceReassemblyForFlow(Flow *f);
-int FlowForceReassemblyNeedReassembly(Flow *f);
+bool FlowForceReassemblyNeedReassembly(Flow *f);
 void FlowForceReassembly(void);
 Packet *FlowForceReassemblyPseudoPacketGet(int direction, Flow *f, const TcpSession *ssn);
 
