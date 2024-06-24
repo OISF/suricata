300599266f704185e58f15bd79ae1af3faaaefcf
Victor Julien <vjulien@oisf.net>
2024-06-24 10:55:45 +0200

    detect/threshold: implement backoff type
    Implement new `type backoff` for thresholding. This allows alerts to be
    limited.
    
    A count of 1 with a multiplier of 10 would generate alerts for matching packets:
    1, 10, 100, 1000, 10000, 100000, etc.
    
    A count of 1 with a multiplier of 2 would generate alerts for matching packets:
    1, 2, 4, 8, 16, 32, etc.
    
    Like with other thresholds, rule actions like drop and setting of
    flowbits will still be performed for each matching packet.
    
    Current implementation is only for the by_flow tracker and for per rule
    threshold statements.
    
    Tracking is done using uint32_t. When it reaches this value, the rest of
    the packets in the tracker will use the silent match.
    
    Ticket: #7120.

--- src/detect-engine-threshold.c
+++ src/detect-engine-threshold.c
@@ -87,10 +87,17 @@ typedef struct ThresholdEntry_ {
     uint32_t seconds;       /**< Event seconds */
     uint32_t current_count; /**< Var for count control */
 
-    SCTime_t tv1; /**< Var for time control */
+    union {
+        struct {
+            uint32_t next_value;
+        } backoff;
+        struct {
+            SCTime_t tv1;  /**< Var for time control */
+            Address addr;  /* used for src/dst/either tracking */
+            Address addr2; /* used for both tracking */
+        };
+    };
 
-    Address addr;  /* used for src/dst/either tracking */
-    Address addr2; /* used for both tracking */
 } ThresholdEntry;
 
 static int ThresholdEntrySet(void *dst, void *src)
@@ -639,6 +646,24 @@ static inline void RateFilterSetAction(PacketAlert *pa, uint8_t new_action)
     }
 }
 
+/** \internal
+ *  \brief Apply the multiplier and return the new value.
+ *  If it would overflow the uint32_t we return UINT32_MAX.
+ */
+static uint32_t BackoffCalcNextValue(const uint32_t cur, const uint32_t m)
+{
+    /* goal is to see if cur * m would overflow uint32_t */
+    if (unlikely(UINT32_MAX / m < cur)) {
+        return UINT32_MAX;
+    }
+    return cur * m;
+}
+
+/**
+ *  \retval 2 silent match (no alert but apply actions)
+ *  \retval 1 normal match
+ *  \retval 0 no match
+ */
 static int ThresholdSetup(const DetectThresholdData *td, ThresholdEntry *te,
         const SCTime_t packet_time, const uint32_t sid, const uint32_t gid, const uint32_t rev,
         const uint32_t tenant_id)
@@ -648,11 +673,19 @@ static int ThresholdSetup(const DetectThresholdData *td, ThresholdEntry *te,
     te->key[REV] = rev;
     te->key[TRACK] = td->track;
     te->key[TENANT] = tenant_id;
-    te->seconds = td->seconds;
 
+    te->seconds = td->seconds;
     te->current_count = 1;
-    te->tv1 = packet_time;
-    te->tv_timeout = 0;
+
+    switch (td->type) {
+        case TYPE_BACKOFF:
+            te->backoff.next_value = td->count;
+            break;
+        default:
+            te->tv1 = packet_time;
+            te->tv_timeout = 0;
+            break;
+    }
 
     switch (td->type) {
         case TYPE_LIMIT:
@@ -662,6 +695,13 @@ static int ThresholdSetup(const DetectThresholdData *td, ThresholdEntry *te,
         case TYPE_BOTH:
             if (td->count == 1)
                 return 1;
+            return 0;
+        case TYPE_BACKOFF:
+            if (td->count == 1) {
+                te->backoff.next_value =
+                        BackoffCalcNextValue(te->backoff.next_value, td->multiplier);
+                return 1;
+            }
             return 0;
         case TYPE_DETECTION:
             return 0;
@@ -780,6 +820,24 @@ static int ThresholdCheckUpdate(const DetectThresholdData *td, ThresholdEntry *t
                     te->tv1 = packet_time;
                     te->current_count = 1;
                 }
+            }
+            break;
+        case TYPE_BACKOFF:
+            SCLogDebug("backoff");
+
+            if (te->current_count < UINT32_MAX) {
+                te->current_count++;
+                if (te->backoff.next_value == te->current_count) {
+                    te->backoff.next_value =
+                            BackoffCalcNextValue(te->backoff.next_value, td->multiplier);
+                    SCLogDebug("te->backoff.next_value %u", te->backoff.next_value);
+                    ret = 1;
+                } else {
+                    ret = 2;
+                }
+            } else {
+                /* if count reaches UINT32_MAX, we just silent match on the rest of the flow */
+                ret = 2;
             }
             break;
     }
