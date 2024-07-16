c0db39294dd92470fdcd702111e8c4a5597d7deb
Jeff Lucovsky <jlucovsky@oisf.net>
2024-07-16 10:12:11 -0400

    ftp: Convert enumerations to Rust
    Issue: 4082
    
    As part of the effort to convert the FTP/FTPDATA parser to Rust, move
    the enums from C to Rust.

--- rust/src/ftp/mod.rs
+++ rust/src/ftp/mod.rs
@@ -27,6 +27,7 @@ use std::str;
 use std::str::FromStr;
 
 pub mod event;
+pub mod ftp;
 
 // We transform an integer string into a i64, ignoring surrounding whitespaces
 // We look for a digit suite, and try to convert it.
