93374e1fbc464f2c7880d3a22e303d79d7dd10e8
Juliana Fajardini <jufajardini@oisf.net>
2024-07-17 17:22:04 -0300

    pgsql: trigger raw stream reassembly at tx completion
    Once we are tracking tx progress per-direction for PGSQL, we can trigger
    the raw stream reassembly for detection purposes once the transactions
    are completed.
    
    Task #7000

--- rust/src/pgsql/pgsql.rs
+++ rust/src/pgsql/pgsql.rs
@@ -358,7 +358,6 @@ impl PgsqlState {
             );
             match PgsqlState::state_based_req_parsing(self.state_progress, start) {
                 Ok((rem, request)) => {
-                    sc_app_layer_parser_trigger_raw_stream_reassembly(flow, Direction::ToServer as i32);
                     start = rem;
                     let (temp_state, tx_completed) = if let Some(state) = PgsqlState::request_next_state(&request) {
                         self.state_progress = state;
@@ -382,6 +381,7 @@ impl PgsqlState {
                             } else {
                                 tx.tx_req_state = PgsqlTxReqProgress::TxReqDone;
                             }
+                            sc_app_layer_parser_trigger_raw_stream_reassembly(flow, Direction::ToServer as i32);
                         }
                     } else {
                         // If there isn't a new transaction, we'll consider Suri should move on
@@ -521,7 +521,6 @@ impl PgsqlState {
         while !start.is_empty() {
             match PgsqlState::state_based_resp_parsing(self.state_progress, start) {
                 Ok((rem, response)) => {
-                    sc_app_layer_parser_trigger_raw_stream_reassembly(flow, Direction::ToClient as i32);
                     start = rem;
                     SCLogDebug!("Response is {:?}", &response);
                     let (curr_state, tx_completed) = if let Some(state) = self.response_process_next_state(&response, flow) {
