20a6627511fa4d0860e49150f824c310368e863e
Shivani Bhardwaj <shivani@oisf.net>
2024-08-23 12:27:35 +0530

    flow/pkts: allow matching on either direction
    For flow.bytes and flow.pkts keywords, allow matching in either
    direction.
    
    Feature 5646

--- src/detect-flow-pkts.c
+++ src/detect-flow-pkts.c
@@ -26,6 +26,7 @@
 enum FlowDirection {
     DETECT_FLOW_TOSERVER = 1,
     DETECT_FLOW_TOCLIENT,
+    DETECT_FLOW_TOEITHER,
 };
 
 typedef struct DetectFlow_ {
@@ -46,6 +47,11 @@ static int DetectFlowPktsMatch(
         return DetectU32Match(p->flow->todstpktcnt, df->pkt_data);
     } else if (df->dir == DETECT_FLOW_TOCLIENT) {
         return DetectU32Match(p->flow->tosrcpktcnt, df->pkt_data);
+    } else if (df->dir == DETECT_FLOW_TOEITHER) {
+        if (DetectU32Match(p->flow->tosrcpktcnt, df->pkt_data)) {
+            return 1;
+        }
+        return DetectU32Match(p->flow->todstpktcnt, df->pkt_data);
     }
     return 0;
 }
@@ -133,6 +139,8 @@ static int DetectFlowPktsSetup(DetectEngineCtx *de_ctx, Signature *s, const char
             dir = DETECT_FLOW_TOSERVER;
         } else if (strcmp(token, "toclient") == 0) {
             dir = DETECT_FLOW_TOCLIENT;
+        } else if (strcmp(token, "either") == 0) {
+            dir = DETECT_FLOW_TOEITHER;
         }
 
         if (dir) {
@@ -243,6 +251,11 @@ static int DetectFlowBytesMatch(
         return DetectU64Match(p->flow->todstbytecnt, df->byte_data);
     } else if (df->dir == DETECT_FLOW_TOCLIENT) {
         return DetectU64Match(p->flow->tosrcbytecnt, df->byte_data);
+    } else if (df->dir == DETECT_FLOW_TOEITHER) {
+        if (DetectU64Match(p->flow->tosrcbytecnt, df->byte_data)) {
+            return 1;
+        }
+        return DetectU64Match(p->flow->todstbytecnt, df->byte_data);
     }
     return 0;
 }
@@ -330,6 +343,8 @@ static int DetectFlowBytesSetup(DetectEngineCtx *de_ctx, Signature *s, const cha
             dir = DETECT_FLOW_TOSERVER;
         } else if (strcmp(token, "toclient") == 0) {
             dir = DETECT_FLOW_TOCLIENT;
+        } else if (strcmp(token, "either") == 0) {
+            dir = DETECT_FLOW_TOEITHER;
         }
 
         if (dir) {
