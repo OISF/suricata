f8063b1d3805d1ce977466373341d71a17c57722
Jeff Lucovsky <jlucovsky@oisf.net>
2024-09-09 11:06:32 -0400

    detect/luaxform: initial lua transform support
    Adds a new lua script capability to use a script as a buffer transform
    keyword.
    
    It uses a `transform` lua function that returns the input buffer after
    modifying it.
    
    Issue: 2290

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -221,6 +221,7 @@
 
 #include "detect-transform-pcrexform.h"
 #include "detect-transform-base64.h"
+#include "detect-transform-luaxform.h"
 
 #include "util-rule-vars.h"
 
@@ -747,6 +748,7 @@ void SigTableSetup(void)
     DetectTransformHeaderLowercaseRegister();
     DetectTransformFromBase64DecodeRegister();
     SCDetectTransformDomainRegister();
+    DetectTransformLuaxformRegister();
 
     DetectFileHandlerRegister();
 
