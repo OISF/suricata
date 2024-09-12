a843a76baa47c28c7c83c6c3499406ae6b08ab46
Jeff Lucovsky <jlucovsky@oisf.net>
2024-09-12 10:51:03 -0400

    detect/engine: Prevent double-free of keyword hash
    Issue: 2290
    
    Defer freeing the keyword hash table until the engine context has
    been freed. This eliminates a double-free from occurring.
    
    For the unittests ONLY, clear the keyword_hash to prevent a double
    free attempt.

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -2587,6 +2587,9 @@ DetectEngineCtx *DetectEngineCtxInitWithPrefix(const char *prefix, uint32_t tena
 static void DetectEngineCtxFreeThreadKeywordData(DetectEngineCtx *de_ctx)
 {
     HashListTableFree(de_ctx->keyword_hash);
+#if UNITTESTS
+    de_ctx->keyword_hash = NULL;
+#endif
 }
 
 static void DetectEngineCtxFreeFailedSigs(DetectEngineCtx *de_ctx)
@@ -2659,7 +2662,6 @@ void DetectEngineCtxFree(DetectEngineCtx *de_ctx)
 
     MpmFactoryDeRegisterAllMpmCtxProfiles(de_ctx);
 
-    DetectEngineCtxFreeThreadKeywordData(de_ctx);
     SRepDestroy(de_ctx);
     DetectEngineCtxFreeFailedSigs(de_ctx);
 
@@ -2682,6 +2684,7 @@ void DetectEngineCtxFree(DetectEngineCtx *de_ctx)
     DetectPortCleanupList(de_ctx, de_ctx->udp_priorityports);
 
     DetectBufferTypeFreeDetectEngine(de_ctx);
+    DetectEngineCtxFreeThreadKeywordData(de_ctx);
     SCClassConfDeinit(de_ctx);
     SCReferenceConfDeinit(de_ctx);
 
