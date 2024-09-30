40c5533d95d52ced041958cff39b6f42bc05813e
Giuseppe Longo <giuseppe@glongo.it>
2024-09-30 10:44:42 +0200

    sdp: add sdp.origin sticky buffer
    This adds a sticky buffer to match the "Origin" field in both requests
    and responses.
    
    Ticket #7291

--- rust/src/sdp/detect.rs
+++ rust/src/sdp/detect.rs
@@ -26,6 +26,7 @@ use crate::sip::sip::{SIPTransaction, ALPROTO_SIP};
 use std::os::raw::{c_int, c_void};
 use std::ptr;
 
+static mut G_SDP_ORIGIN_BUFFER_ID: c_int = 0;
 static mut G_SDP_SESSION_NAME_BUFFER_ID: c_int = 0;
 static mut G_SDP_SESSION_INFO_BUFFER_ID: c_int = 0;
 
@@ -124,6 +125,54 @@ unsafe extern "C" fn sdp_session_info_get_data(
     false
 }
 
+unsafe extern "C" fn sdp_origin_setup(
+    de: *mut c_void, s: *mut c_void, _raw: *const std::os::raw::c_char,
+) -> c_int {
+    if DetectSignatureSetAppProto(s, ALPROTO_SIP) != 0 {
+        return -1;
+    }
+    if DetectBufferSetActiveList(de, s, G_SDP_ORIGIN_BUFFER_ID) < 0 {
+        return -1;
+    }
+    return 0;
+}
+
+unsafe extern "C" fn sdp_origin_get(
+    de: *mut c_void, transforms: *const c_void, flow: *const c_void, flow_flags: u8,
+    tx: *const c_void, list_id: c_int,
+) -> *mut c_void {
+    return DetectHelperGetData(
+        de,
+        transforms,
+        flow,
+        flow_flags,
+        tx,
+        list_id,
+        sdp_origin_get_data,
+    );
+}
+
+unsafe extern "C" fn sdp_origin_get_data(
+    tx: *const c_void, direction: u8, buffer: *mut *const u8, buffer_len: *mut u32,
+) -> bool {
+    let tx = cast_pointer!(tx, SIPTransaction);
+    let sdp_option = match direction.into() {
+        Direction::ToServer => tx.request.as_ref().and_then(|req| req.body.as_ref()),
+        Direction::ToClient => tx.response.as_ref().and_then(|resp| resp.body.as_ref()),
+    };
+    if let Some(sdp) = sdp_option {
+        let origin = &sdp.origin;
+        if !origin.is_empty() {
+            *buffer = origin.as_ptr();
+            *buffer_len = origin.len() as u32;
+            return true;
+        }
+    }
+    *buffer = ptr::null();
+    *buffer_len = 0;
+    false
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn ScDetectSdpRegister() {
     let kw = SCSigTableElmt {
@@ -163,5 +212,23 @@ pub unsafe extern "C" fn ScDetectSdpRegister() {
         true,
         true,
         sdp_session_info_get,
+    );
+    let kw = SCSigTableElmt {
+        name: b"sdp.origin\0".as_ptr() as *const libc::c_char,
+        desc: b"sticky buffer to match on the SDP origin field\0".as_ptr() as *const libc::c_char,
+        url: b"/rules/sdp-keywords.html#sdp-origin\0".as_ptr() as *const libc::c_char,
+        Setup: sdp_origin_setup,
+        flags: SIGMATCH_NOOPT,
+        AppLayerTxMatch: None,
+        Free: None,
+    };
+    let _ = DetectHelperKeywordRegister(&kw);
+    G_SDP_ORIGIN_BUFFER_ID = DetectHelperBufferMpmRegister(
+        b"sdp.origin\0".as_ptr() as *const libc::c_char,
+        b"sdp.origin\0".as_ptr() as *const libc::c_char,
+        ALPROTO_SIP,
+        true,
+        true,
+        sdp_origin_get,
     );
 }
