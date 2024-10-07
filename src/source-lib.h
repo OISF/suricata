81289f51f6312d3163f72e38fd398050f5386560
Jason Ish <jason.ish@oisf.net>
2024-10-07 16:08:59 -0600

    source-lib: remove TmModuleLibHandlePacket
    This is better handled directly in the library user program so the
    user has more control of the Packet structure, before and after
    handling.

--- src/source-lib.h
+++ src/source-lib.h
@@ -34,19 +34,4 @@
  *  The "Decode" module is the first module invoked when processing a packet */
 void TmModuleDecodeLibRegister(void);
 
-/** \brief process a single packet.
- *
- * \param tv                    Pointer to the per-thread structure.
- * \param data                  Pointer to the raw packet.
- * \param datalink              Datalink type.
- * \param ts                    Timeval structure.
- * \param len                   Packet length.
- * \param tenant_id             Tenant id of the detection engine to use.
- * \param flags                 Packet flags (packet checksum, rule profiling...).
- * \param iface                 Sniffing interface this packet comes from (can be NULL).
- * \return                      Error code.
- */
-int TmModuleLibHandlePacket(ThreadVars *tv, LiveDevice *device, const uint8_t *data, int datalink,
-        struct timeval ts, uint32_t len, uint32_t tenant_id, uint32_t flags);
-
 #endif /* SURICATA_SOURCE_LIB_H */
