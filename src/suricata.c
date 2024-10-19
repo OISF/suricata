a2bacb0fba6fe867734d3cb9e1f2cb0493081ac3
Jeff Lucovsky <jlucovsky@oisf.net>
2024-10-19 08:07:01 -0400

    output/datalink: Use Rust-based linktype hashmap
    Use the hasmap to gather linktype display names.
    
    Issue: 6954

--- src/suricata.c
+++ src/suricata.c
@@ -397,6 +397,7 @@ void GlobalsDestroy(void)
     TmqhCleanup();
     TmModuleRunDeInit();
     ParseSizeDeinit();
+    DatalinkTableDeinit();
 
 #ifdef HAVE_DPDK
     DPDKCleanupEAL();
@@ -2881,6 +2882,7 @@ int InitGlobal(void)
 
     /* Initialize the configuration module. */
     ConfInit();
+    DatalinkTableInit();
 
     VarNameStoreInit();
 
