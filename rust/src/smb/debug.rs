2bcb393f637394bd6c811eb4b923c3d04a6df117
Victor Julien <vjulien@oisf.net>
2024-10-24 15:13:40 +0200

    smb: use lru for ssnguid2vec_map; rename
    Reimplement the ssnguid2vec_map HashMap as a LruCache.
    
    Since this is a DCERPC record cache, name it as such.
    
    Default size is 128. Can be controlled by
    `app-layer.protocols.smb.max-dcerpc-frag-cache-size`.
    
    Ticket: #5672.

--- rust/src/smb/debug.rs
+++ rust/src/smb/debug.rs
@@ -69,12 +69,12 @@ impl SMBState {
 
     #[cfg(feature = "debug")]
     pub fn _debug_state_stats(&self) {
-        SCLogDebug!("ssn2vec_map {} guid2name_cache {} read_offset_cache {} ssn2tree_cache {} ssnguid2vec_map {} file_ts_guid {} file_tc_guid {} transactions {}",
+        SCLogDebug!("ssn2vec_map {} guid2name_cache {} read_offset_cache {} ssn2tree_cache {} dcerpc_rec_frag_cache {} file_ts_guid {} file_tc_guid {} transactions {}",
             self.ssn2vec_map.len(),
             self.guid2name_cache.len(),
             self.read_offset_cache.len(),
             self.ssn2tree_cache.len(),
-            self.ssnguid2vec_map.len(),
+            self.dcerpc_rec_frag_cache.len(),
             self.file_ts_guid.len(),
             self.file_tc_guid.len(),
             self.transactions.len());
