14fab940b0a9470ddf807aba77f86055b4ada2e6
Jason Ish <jason.ish@oisf.net>
2024-10-29 12:26:32 -0600

    bindgen: export AppProto and AppProtoEnum to Rust
    This exposes the C define ALPROTO values to Rust without having to
    perform some runtime initialization with init_ffi.
    
    As app-layer-protos.h was clean of a circular reference to rust.h we
    could use it directly, it just needed the addition of
    suricata-common.h.

--- rust/src/build.rs
+++ rust/src/build.rs
@@ -11,18 +11,28 @@ fn main() {
 
     // Pull in a simple header that presents no issues with bindgen at
     // this time. Multiple headers can be specified.
-    let bindings = bindgen::Builder::default()
+    let mut builder = bindgen::Builder::default()
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .header(format!("{}/src/app-layer-events.h", &src_dir))
-        .allowlist_item("SCAppLayerEventType")
-        .rustified_enum("SCAppLayerEventType")
-        .header(format!("{}/src/app-layer-types.h", &src_dir))
-        .allowlist_item("SCAppLayer.*")
         .clang_arg("-DHAVE_CONFIG_H")
         .clang_arg("-D__SCFILENAME__=\"\"")
-        .clang_arg(format!("-I{}/src", &build_dir))
-        .generate()
-        .unwrap();
+        .clang_arg(format!("-I{}/src", &build_dir));
+
+    let headers = &["app-layer-types.h", "app-layer-protos.h"];
+    for header in headers {
+        builder = builder.header(format!("{}/src/{}", &src_dir, header));
+    }
+
+    // Patterns.
+    builder = builder
+        .allowlist_item("SCAppLayer.*")
+        .allowlist_item("AppProto.*");
+
+    // Rustified enums.
+    builder = builder
+        .rustified_enum("SCAppLayerEventType")
+        .rustified_enum("AppProtoEnum");
+
+    let bindings = builder.generate().unwrap();
 
     // Write out the bindings. *Rules* say we should only write into
     // the target directory (idiomatically the OUT_DIR env var), so
