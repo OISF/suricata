a8c63992fb987640828996ef9759687dd820aba8
Victor Julien <vjulien@oisf.net>
2024-10-30 09:06:19 +0100

    detect/sigorder: remove data structs from global namespace
    Rename types enum to reflect it is not using a radix tree anymore.

--- src/detect-engine-sigorder.h
+++ src/detect-engine-sigorder.h
@@ -24,45 +24,6 @@
 #ifndef SURICATA_DETECT_ENGINE_SIGORDER_H
 #define SURICATA_DETECT_ENGINE_SIGORDER_H
 
-/**
- * \brief Different kinds of helper data that can be used by the signature
- *        ordering module.  Used by the "user" field in SCSigSignatureWrapper
- */
-typedef enum{
-    SC_RADIX_USER_DATA_FLOWBITS,
-    SC_RADIX_USER_DATA_FLOWVAR,
-    SC_RADIX_USER_DATA_PKTVAR,
-    SC_RADIX_USER_DATA_FLOWINT,
-    SC_RADIX_USER_DATA_HOSTBITS,
-    SC_RADIX_USER_DATA_IPPAIRBITS,
-    SC_RADIX_USER_DATA_MAX
-} SCRadixUserDataType;
-
-/**
- * \brief Signature wrapper used by signature ordering module while ordering
- *        signatures
- */
-typedef struct SCSigSignatureWrapper_ {
-    /* the wrapped signature */
-    Signature *sig;
-
-    /* user data that is to be associated with this sigwrapper */
-    int user[SC_RADIX_USER_DATA_MAX];
-
-    struct SCSigSignatureWrapper_ *next;
-} SCSigSignatureWrapper;
-
-/**
- * \brief Structure holding the signature ordering function used by the
- *        signature ordering module
- */
-typedef struct SCSigOrderFunc_ {
-    /* Pointer to the Signature Ordering function */
-    int (*SWCompare)(SCSigSignatureWrapper *sw1, SCSigSignatureWrapper *sw2);
-
-    struct SCSigOrderFunc_ *next;
-} SCSigOrderFunc;
-
 void SCSigOrderSignatures(DetectEngineCtx *);
 void SCSigRegisterSignatureOrderingFuncs(DetectEngineCtx *);
 void SCSigRegisterSignatureOrderingTests(void);
