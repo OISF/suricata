c3aa3ae102e1a20ea2e36e0639030d934725888b
Jason Ish <jason.ish@oisf.net>
2024-11-01 09:58:33 -0600

    http2: rename event variant to match rule
    Rename InvalidHTTP1Settings to InvalidHttp1Settings so it gets the
    expected name transformation of "invalid_http1_settings".
    
    Ticket: #7361
    (cherry picked from commit b1c26dccf3d361109ddf5b163a8a4268423f0a9c)

--- rust/src/http2/http2.rs
+++ rust/src/http2/http2.rs
@@ -401,7 +401,7 @@ pub enum HTTP2Event {
     ExtraHeaderData,
     LongFrameData,
     StreamIdReuse,
-    InvalidHTTP1Settings,
+    InvalidHttp1Settings,
     FailedDecompression,
     InvalidRange,
     HeaderIntegerOverflow,
