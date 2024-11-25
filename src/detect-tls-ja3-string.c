8cfbbea80b7540bf240f99807cd86a2f4a852940
Philippe Antoine <contact@catenacyber.fr>
2024-11-25 09:30:51 +0100

    detect/ja: use multi-protocol support
    instead of hardcoding list : removes usage of ALPROTO_QUIC and
    ALPROTO_TLS in generic SigValidate
    
    Ticket: 7304

--- src/detect-tls-ja3-string.c
+++ src/detect-tls-ja3-string.c
@@ -125,7 +125,8 @@ static int DetectTlsJa3StringSetup(DetectEngineCtx *de_ctx, Signature *s, const 
     if (DetectBufferSetActiveList(de_ctx, s, g_tls_ja3_str_buffer_id) < 0)
         return -1;
 
-    if (s->alproto != ALPROTO_UNKNOWN && s->alproto != ALPROTO_TLS && s->alproto != ALPROTO_QUIC) {
+    AppProto alprotos[] = { ALPROTO_TLS, ALPROTO_QUIC, ALPROTO_UNKNOWN };
+    if (DetectSignatureSetMultiAppProto(s, alprotos) < 0) {
         SCLogError("rule contains conflicting protocols.");
         return -1;
     }
@@ -140,7 +141,6 @@ static int DetectTlsJa3StringSetup(DetectEngineCtx *de_ctx, Signature *s, const 
         }
         return -2;
     }
-    s->init_data->init_flags |= SIG_FLAG_INIT_JA;
 
     return 0;
 }
