b815e4e4d6018f28772f050fc8e49626705b5998
Shivani Bhardwaj <shivani@oisf.net>
2024-12-03 18:25:18 +0530

    datasets: move initial file reading to rust
    In a recent warning reported by scan-build, datasets were found to be
    using a blocking call in a critical section.
    
    datasets.c:187:12: warning: Call to blocking function 'fgets' inside of critical section [unix.BlockInCriticalSection]
      187 |     while (fgets(line, (int)sizeof(line), fp) != NULL) {
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    datasets.c:292:12: warning: Call to blocking function 'fgets' inside of critical section [unix.BlockInCriticalSection]
      292 |     while (fgets(line, (int)sizeof(line), fp) != NULL) {
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    datasets.c:368:12: warning: Call to blocking function 'fgets' inside of critical section [unix.BlockInCriticalSection]
      368 |     while (fgets(line, (int)sizeof(line), fp) != NULL) {
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    datasets.c:442:12: warning: Call to blocking function 'fgets' inside of critical section [unix.BlockInCriticalSection]
      442 |     while (fgets(line, (int)sizeof(line), fp) != NULL) {
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    datasets.c:512:12: warning: Call to blocking function 'fgets' inside of critical section [unix.BlockInCriticalSection]
      512 |     while (fgets(line, (int)sizeof(line), fp) != NULL) {
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    5 warnings generated.
    
    These calls are blocking in the multi tenant mode where several tenants
    may be trying to load the same dataset in parallel.
    In a single tenant mode, this operation is performed as a part of a
    single thread before the engine startup.
    
    In order to evade the warning and simplify the code, the initial file
    reading is moved to Rust with this commit with a much simpler handling
    of dataset and datarep.
    
    Bug 7398

---
+++ rust/src/detect/datasets.rs
@@ -0,0 +1,105 @@
+/* Copyright (C) 2024 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+// Author: Shivani Bhardwaj <shivani@oisf.net>
+
+//! This module exposes items from the datasets C code to Rust.
+
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::ffi::{c_char, CStr};
+use base64::{Engine, engine::general_purpose::STANDARD};
+
+/// Opaque Dataset type defined in C
+#[derive(Copy, Clone)]
+pub enum Dataset {}
+
+// Simple C type converted to Rust
+#[derive(Debug, PartialEq)]
+#[repr(C)]
+pub struct DataRepType {
+    pub value: u16,
+}
+
+// Extern fns operating on the opaque Dataset type above
+/// cbindgen:ignore
+extern {
+    pub fn DatasetAdd(set: &Dataset, data: *const u8, len: u32) -> i32;
+    pub fn DatasetAddwRep(set: &Dataset, data: *const u8, len: u32, rep: *const DataRepType) -> i32;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ProcessDatasets(set: &Dataset, name: *const c_char, fname: *const c_char, fmode: *const c_char) -> i32 {
+    let file_string = CStr::from_ptr(fname).to_str().unwrap();
+    let mode = CStr::from_ptr(fmode).to_str().unwrap();
+    let set_name = CStr::from_ptr(name).to_str().unwrap();
+    let filename = Path::new(file_string);
+ //   SCLogNotice!("Path: {:?}", filename);
+    if let Ok(lines) = read_lines(filename, mode) {
+        for line in lines.flatten() {
+//            SCLogNotice!("{}", line);
+            let v: Vec<&str> = line.split(',').collect();
+            // Ignore empty and invalid lines in dataset/rep file
+            if v.is_empty() || v.len() > 2 {
+                continue;
+            }
+            if v.len() == 1 {
+                // Dataset
+                let mut decoded: Vec<u8> = vec![];
+                if STANDARD.decode_vec(v[0], &mut decoded).is_err() {
+                    // FatalErrorOnInit STODO
+                    SCLogError!("bad base64 encoding {}", set_name);
+                    return -2;
+                }
+                DatasetAdd(&set, decoded.as_ptr(), decoded.len() as u32);
+            } else {
+                // Datarep
+                let mut decoded: Vec<u8> = vec![];
+                if STANDARD.decode_vec(v[0], &mut decoded).is_err() {
+                    // FatalErrorOnInit STODO
+                    SCLogError!("bad base64 encoding {}", set_name);
+                    return -2;
+                }
+                if let Ok(val) = v[1].to_string().parse::<u16>() {
+                    let rep: DataRepType = DataRepType { value: val };
+                    DatasetAddwRep(&set, decoded.as_ptr(), decoded.len() as u32, &rep);
+                } else {
+                    // FatalErrorOnInit STODO
+                    SCLogError!("Invalid datarep value {}", set_name);
+                    return -2;
+                }
+            }
+        }
+    } else {
+        SCLogNotice!("Couldn't open file");
+        return -1;
+    }
+    SCLogNotice!("All OK from rust parser");
+    0
+}
+
+fn read_lines<P>(filename: P, fmode: &str) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file: File;
+    if fmode == "r" {
+        file = File::open(filename)?;
+    } else {
+        file = OpenOptions::new().append(true).create(true).open(filename)?;
+    }
+    Ok(io::BufReader::new(file).lines())
+}
