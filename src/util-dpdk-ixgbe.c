f2e9cd6523659cf930bff54f225eb829518e63ec
Adam Kiripolsky <Adam.Kiripolsky@cesnet.cz>
2024-12-10 16:26:49 +0100

    dpdk/rss: add rte_flow rss support for ixgbe
    ixgbe driver requires different configuration of RSS rte_flow
    rule than i40e, with just one generic rule matching all traffic.
    
    The generic rule configured by DeviceCreateRSSFlowGeneric() has pattern
    equivalent to "pattern eth / end" in dpdk-testpmd syntax. The rule must
    have rx queues configured. The rule hashes traffic to different queues
    based on ipv4 and ipv6 hash types (ipv4 src dst / ipv6 src dst).
    The hash key is 40 bytes long  symmetric hash key. ixgbe does not
    support any other hash function than RTE_ETH_HASH_FUNCTION_DEFAULT.
    
    The syntax in dpdk-testpmd for this rule with attributes:
    	port index == 0
    	used rx queue indices == 0 1 2 3
    	<hash_key> == 6d5a symmetric hash key
    is as follows:
    "flow create 0 ingress pattern eth / end actions rss types ipv4 ipv6
    end queues 0 1 2 3 end key <hash_key> key_len 40 func default / end"
    
    Ticket: 7337

--- src/util-dpdk-ixgbe.c
+++ src/util-dpdk-ixgbe.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Open Information Security Foundation
+/* Copyright (C) 2021-2024 Open Information Security Foundation
  *
  * You can copy, redistribute or modify this Program under the terms of
  * the GNU General Public License version 2 as published by the Free
@@ -32,12 +32,47 @@
 
 #include "util-dpdk-ixgbe.h"
 #include "util-dpdk.h"
+#include "util-debug.h"
+#include "util-dpdk-bonding.h"
+#include "util-dpdk-rss.h"
 
 #ifdef HAVE_DPDK
+
+#define IXGBE_RSS_HKEY_LEN 40
 
 void ixgbeDeviceSetRSSHashFunction(uint64_t *rss_hf)
 {
     *rss_hf = RTE_ETH_RSS_IPV4 | RTE_ETH_RSS_IPV6 | RTE_ETH_RSS_IPV6_EX;
+}
+
+int ixgbeDeviceSetRSS(int port_id, int nb_rx_queues, char *port_name)
+{
+    uint16_t queues[RTE_MAX_QUEUES_PER_PORT];
+    struct rte_flow_error flush_error = { 0 };
+    struct rte_eth_rss_conf rss_conf = {
+        .rss_key = RSS_HKEY,
+        .rss_key_len = IXGBE_RSS_HKEY_LEN,
+    };
+
+    if (nb_rx_queues < 1) {
+        FatalError("The number of queues for RSS configuration must be "
+                   "configured with a positive number");
+    }
+
+    struct rte_flow_action_rss rss_action_conf = DeviceInitRSSAction(
+            rss_conf, nb_rx_queues, queues, RTE_ETH_HASH_FUNCTION_DEFAULT, true);
+
+    int retval = DeviceCreateRSSFlowGeneric(port_id, port_name, rss_action_conf);
+    if (retval != 0) {
+        retval = rte_flow_flush(port_id, &flush_error);
+        if (retval != 0) {
+            SCLogError("Unable to flush rte_flow rules of %s: %s Flush error msg: %s", port_name,
+                    rte_strerror(-retval), flush_error.message);
+        }
+        return retval;
+    }
+
+    return 0;
 }
 
 #endif /* HAVE_DPDK */
