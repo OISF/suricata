ddc808755e0fbc57b568413934071c44e7bda373
Victor Julien <vjulien@oisf.net>
2025-01-14 09:41:11 +0100

    WIP detect: introduce explicit hooks
    Generic:
            <app_proto>:request_done and <app_proto>:response_done
    
    Per protocol, it uses the registered progress (state) values. E.g.
    
            tls:client_hello_done
    
    A rule ruleset could be:
    
            pass tls:client_hello_done any any -> any any (tls.sni; content:"www.google.com"; sid:21; alert;)
            drop tls:client_hello_done any any -> any any (sid:22;)
    
    The pass rule is evaluated when the client hello is parsed, and if it
    doesn't match the drop rule will be evaluated.
    
    Registers each generic lists as "<alproto>:<progress state>:generic"
    (e.g. "tls:client_hello_done:generic").
    
    Ticket: #7485.

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -504,6 +504,8 @@ void SigTableInit(void)
 
 void SigTableSetup(void)
 {
+    DetectRegisterAppLayerHookLists();
+
     DetectSidRegister();
     DetectPriorityRegister();
     DetectPrefilterRegister();
