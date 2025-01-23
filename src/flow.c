7ff6bd1734c4e81fad40714e9516eb145399e418
Victor Julien <vjulien@oisf.net>
2025-01-23 16:11:30 +0100

    detect: start of pkt hooks
    New facility to allow a user to specify where to hook a rule
    into the engine. This patch adds this for packets, adding two
    hooks:
    
    - `all`: to let a rule be evaluated by all rules
    - `flow_start`: to have a rule be evaluated only for the first
      packet in both directions
    
    Implemented by adding a hook flags field in the packet.

--- src/flow.c
+++ src/flow.c
@@ -438,6 +438,7 @@ void FlowHandlePacketUpdate(Flow *f, Packet *p, ThreadVars *tv, DecodeThreadVars
             if (FlowUpdateSeenFlag(p)) {
                 f->flags |= FLOW_TO_DST_SEEN;
                 p->flowflags |= FLOW_PKT_TOSERVER_FIRST;
+                p->pkt_hooks |= BIT_U16(SIGNATURE_HOOK_PKT_FLOW_START);
             }
         }
         /* xfer proto detect ts flag to first packet in ts dir */
@@ -463,6 +464,7 @@ void FlowHandlePacketUpdate(Flow *f, Packet *p, ThreadVars *tv, DecodeThreadVars
             if (FlowUpdateSeenFlag(p)) {
                 f->flags |= FLOW_TO_SRC_SEEN;
                 p->flowflags |= FLOW_PKT_TOCLIENT_FIRST;
+                p->pkt_hooks |= BIT_U16(SIGNATURE_HOOK_PKT_FLOW_START);
             }
         }
         /* xfer proto detect tc flag to first packet in tc dir */
