53b93519ec4914cb8ed9390ee8d7adae032a43d1
Adam Kiripolsky <adam.kiripolsky@cesnet.cz>
2025-02-25 21:32:30 +0100

    dpdk/rte_flow: drop filter packet counter
    Added counter for packets filtered by the drop filter.
    These statistics are only available on mlx5 and ice drivers.
    Additionally, ice driver supports gathering statistic only when any
    of the rules does not contain range of any kind (mask on IP address,
    last on ports etc.). These statistics are available in eve.json under
    "dpdk.rte_flow_filtered".

--- src/util-dpdk-rte-flow.c
+++ src/util-dpdk-rte-flow.c
@@ -49,6 +49,9 @@ static int RteFlowRuleStorageInit(RteFlowRuleStorage *);
 static int RteFlowRuleStorageAddRule(RteFlowRuleStorage *, const char *);
 static int RteFlowRuleStorageExtendCapacity(RteFlowRuleStorage *);
 static char *DriverSpecificErrorMessage(const char *, struct rte_flow_item *);
+static bool RteFlowRulesContainPatternWildcard(char **, uint32_t);
+static bool RteFlowDropFilterInit(uint32_t, char **, struct rte_flow_attr *,
+        struct rte_flow_action *, uint32_t *, const char *, const char *);
 
 /**
  * \brief Specify ambiguous error messages as some drivers have specific
@@ -69,6 +72,81 @@ static char *DriverSpecificErrorMessage(const char *driver_name, struct rte_flow
     }
 
     return NULL;
+}
+
+/**
+ * \brief Checks whether at least one pattern contains wildcard matching
+ *
+ * \param patterns array of loaded rte_flow rule patterns from suricata.yaml
+ * \param rule_count number of loaded rte_flow rule patterns
+ * \return true pattern contains wildcard matching
+ * \return false pattern does not contain wildcard matching
+ */
+static bool RteFlowRulesContainPatternWildcard(char **patterns, uint32_t rule_count)
+{
+    for (size_t i = 0; i < rule_count; i++) {
+        char *pattern = patterns[i];
+        if (strstr(pattern, " mask ") != NULL || (strstr(pattern, " last ") != NULL))
+            return true;
+    }
+    return false;
+}
+
+/**
+ * \brief Initializes rte_flow rules and decides whether statistics about the rule (count of
+ *        filtered packets) can be gathered or not
+ *
+ * \param rule_count number of rte_flow rules present
+ * \param patterns array of patterns for rte_flow rules
+ * \param attr out variable for initialized rte_flow attributes
+ * \param action out variable for initialized rte_flow action
+ * \param counter_id id of a rte_flow counter action
+ * \param driver_name name of the driver
+ * \param port_name name of the port
+ * \return true if statistics about rte_flow rules can be gathered
+ * \return false if statistics about rte_flow rules can not be gathered
+ */
+static bool RteFlowDropFilterInit(uint32_t rule_count, char **patterns, struct rte_flow_attr *attr,
+        struct rte_flow_action *action, uint32_t *counter_id, const char *driver_name,
+        const char *port_name)
+{
+    attr->ingress = 1;
+    attr->priority = 0;
+
+    /* ICE PMD does not support count action with wildcard pattern (mask and last pattern item
+     * types). The count action is omitted when wildcard pattern is detected */
+    if (strcmp(driver_name, "net_ice") == 0) {
+        if (RteFlowRulesContainPatternWildcard(patterns, rule_count) == true) {
+            action[0].type = RTE_FLOW_ACTION_TYPE_DROP;
+            action[1].type = RTE_FLOW_ACTION_TYPE_END;
+            SCLogWarning(
+                    "%s: gathering statistic for the rte_flow rule is disabled because of wildcard "
+                    "pattern (ice PMD specific)",
+                    port_name);
+            return false;
+        }
+/* ICE PMD has to have attribute group set to 2 on DPDK 23.11 and higher for the count action to
+ * work properly */
+#if RTE_VERSION >= RTE_VERSION_NUM(23, 11, 0, 0)
+        attr->group = 2;
+#else
+        attr->group = 0;
+#endif /* RTE_VERSION >= RTE_VERSION_NUM(23, 11, 0, 0) */
+    }
+
+    if (strcmp(driver_name, "net_ice") == 0 || strcmp(driver_name, "mlx5_pci") == 0) {
+
+        action[0].type = RTE_FLOW_ACTION_TYPE_COUNT;
+        action[0].conf = counter_id;
+        action[1].type = RTE_FLOW_ACTION_TYPE_DROP;
+        action[2].type = RTE_FLOW_ACTION_TYPE_END;
+
+        return true;
+    }
+
+    action[0].type = RTE_FLOW_ACTION_TYPE_DROP;
+    action[1].type = RTE_FLOW_ACTION_TYPE_END;
+    return false;
 }
 
 static int RteFlowRuleStorageInit(RteFlowRuleStorage *rule_storage)
@@ -190,6 +268,44 @@ int ConfigLoadRteFlowRules(
 }
 
 /**
+ * \brief Query the number of packets filtered by rte_flow rules defined by user in suricata.yaml
+ *
+ * \param rules array of rte_flow rule handlers
+ * \param rule_count number of existing rules
+ * \param port_id id of a port
+ * \param filtered_packets out variable for the number of packets filtered by the rte_flow rules
+ * \return int 0 on success, a negative errno value otherwise and rte_errno is set
+ */
+uint64_t RteFlowFilteredPacketsQuery(struct rte_flow **rules, uint16_t rule_count,
+        char *device_name, int port_id, uint64_t *filtered_packets)
+{
+#if RTE_VERSION >= RTE_VERSION_NUM(21, 0, 0, 0)
+    struct rte_flow_query_count query_count = { 0 };
+    struct rte_flow_action action[] = { { 0 }, { 0 }, { 0 } };
+    struct rte_flow_error flow_error = { 0 };
+    uint32_t counter_id = COUNT_ACTION_ID;
+    int retval = 0;
+
+    query_count.reset = 0;
+    action[0].type = RTE_FLOW_ACTION_TYPE_COUNT;
+    action[0].conf = &counter_id;
+    action[1].type = RTE_FLOW_ACTION_TYPE_END;
+
+    for (uint16_t i = 0; i < rule_count; i++) {
+        retval +=
+                rte_flow_query(port_id, rules[i], &(action[0]), (void *)&query_count, &flow_error);
+        if (retval != 0) {
+            SCLogError("%s: rte_flow count query error %s errmsg: %s", device_name,
+                    rte_strerror(-retval), flow_error.message);
+            SCReturnInt(retval);
+        };
+        *filtered_packets += query_count.hits;
+    }
+#endif /* RTE_VERSION >= RTE_VERSION_NUM(21, 0, 0, 0) */
+    SCReturnInt(0);
+}
+
+/**
  * \brief Create rte_flow drop rules with patterns stored in rule_storage on a port with id
  *        port_id
  *
@@ -205,13 +321,13 @@ int RteFlowRulesCreate(
 #if RTE_VERSION >= RTE_VERSION_NUM(21, 0, 0, 0)
     SCEnter();
     int failed_rule_count = 0;
+    uint32_t counter_id = COUNT_ACTION_ID;
     struct rte_flow_error flush_error = { 0 };
     struct rte_flow_attr attr = { 0 };
     struct rte_flow_action action[] = { { 0 }, { 0 }, { 0 } };
 
-    attr.ingress = 1;
-    action[0].type = RTE_FLOW_ACTION_TYPE_DROP;
-    action[1].type = RTE_FLOW_ACTION_TYPE_END;
+    bool should_gather_stats = RteFlowDropFilterInit(rule_storage->rule_cnt, rule_storage->rules,
+            &attr, action, &counter_id, driver_name, port_name);
 
     rule_storage->rule_handlers = SCCalloc(rule_storage->rule_size, sizeof(struct rte_flow *));
     if (rule_storage->rule_handlers == NULL) {
@@ -269,6 +385,12 @@ int RteFlowRulesCreate(
         }
         SCReturnInt(-1);
     }
+
+    if (!should_gather_stats) {
+        SCFree(rule_storage->rule_handlers);
+        rule_storage->rule_cnt = 0;
+    }
+
 #endif /* RTE_VERSION >= RTE_VERSION_NUM(21, 0, 0, 0)*/
     SCReturnInt(0);
 }
