ce26159a03b6ace0f4f7fe837d4b62b102d2b5ee
Victor Julien <vjulien@oisf.net>
2025-02-26 11:37:54 +0100

    detect: constify rule file and lines in parsing and analyzer

--- src/detect.h
+++ src/detect.h
@@ -947,7 +947,7 @@ typedef struct DetectEngineCtx_ {
 
     /** Store rule file and line so that parsers can use them in errors. */
     int rule_line;
-    char *rule_file;
+    const char *rule_file;
     const char *sigerror;
     bool sigerror_silent;
     bool sigerror_ok;
