d88ef201b4efc84958947749e8fbee59ac60e0ac
Jeff Lucovsky <jlucovsky@oisf.net>
2025-03-29 11:01:17 -0400

    detect/ftp: Add ftp.completion_code keyword
    Issue: 7507
    
    Implement the ftp.completion_code sticky buffer. Multi-buffer as an FTP
    command can produce multiple responses.
    
    E.g., with the FTP command RETR
        RETR temp.txt
        150 Opening BINARY mode data connection for temp.txt (1164 bytes).
        226 Transfer complete.

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -212,6 +212,7 @@
 #include "detect-ftp-command.h"
 #include "detect-entropy.h"
 #include "detect-ftp-command-data.h"
+#include "detect-ftp-completion-code.h"
 #include "detect-ftp-reply.h"
 
 #include "detect-bypass.h"
@@ -725,6 +726,7 @@ void SigTableSetup(void)
     DetectJa4HashRegister();
     DetectFtpCommandRegister();
     DetectFtpCommandDataRegister();
+    DetectFtpCompletionCodeRegister();
     DetectFtpReplyRegister();
 
     DetectBypassRegister();
