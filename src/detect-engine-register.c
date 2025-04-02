122c3e53a1284d433ad9f8a4b09c511be9e13082
Victor Julien <vjulien@oisf.net>
2025-04-02 10:30:01 +0200

    detect/sdp: minor style fixup

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -748,7 +748,7 @@ void SigTableSetup(void)
     SCDetectSipRegister();
     SCDetectTemplateRegister();
     SCDetectLdapRegister();
-    ScDetectSdpRegister();
+    SCDetectSdpRegister();
 
     for (size_t i = 0; i < preregistered_callbacks_nb; i++) {
         PreregisteredCallbacks[i]();
