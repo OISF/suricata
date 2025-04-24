abaff956586991b764d404475c5985eb978128de
Philippe Antoine <contact@catenacyber.fr>
2025-04-24 10:25:01 +0200

    xdp: handle erspan2 tunnels
    Ticket: 7674

--- src/decode.h
+++ src/decode.h
@@ -1173,6 +1173,8 @@ void DecodeUpdatePacketCounters(ThreadVars *tv,
                                 const DecodeThreadVars *dtv, const Packet *p);
 const char *PacketDropReasonToString(enum PacketDropReason r);
 
+void *DecodeTunnelsGetMapIter(void);
+
 /* decoder functions */
 int DecodeEthernet(ThreadVars *, DecodeThreadVars *, Packet *, const uint8_t *, uint32_t);
 int DecodeSll(ThreadVars *, DecodeThreadVars *, Packet *, const uint8_t *, uint32_t);
