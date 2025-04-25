dcdc06d7bbe5d0d67c80ae36819558cd9840d5f8
Jeff Lucovsky <jlucovsky@oisf.net>
2025-04-25 10:47:47 -0400

    detect/ftp: Add ftp.mode command
    Issue: 7505
    
    This commit adds support for the ftp.mode keyword.
    
    ftp.mode: active|passive

---
+++ src/detect-ftp-mode.c
@@ -0,0 +1,186 @@
+/* Copyright (C) 2025 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ *
+ * \author Jeff Lucovsky <jlucovsky@oisf.net>
+ *
+ * Implements the ftp.mode sticky buffer
+ *
+ */
+
+#include "suricata-common.h"
+
+#include "detect.h"
+#include "detect-parse.h"
+#include "detect-engine.h"
+
+#include "flow.h"
+
+#include "util-debug.h"
+
+#include "app-layer.h"
+#include "app-layer-ftp.h"
+
+#include "detect-ftp-mode.h"
+
+#define KEYWORD_NAME "ftp.mode"
+#define KEYWORD_DOC  "ftp-keywords.html#ftp-mode"
+#define BUFFER_NAME  "ftp.mode"
+#define BUFFER_DESC  "ftp mode"
+
+typedef struct DetectFtpModeData_ {
+    bool active;
+} DetectFtpModeData;
+
+/**
+ * \brief Regex for parsing our keyword options
+ */
+#define PARSE_REGEX "^\\s*(active|passive)\\s*$"
+static DetectParseRegex parse_regex;
+
+static int g_ftp_mode_buffer_id = 0;
+
+/**
+ * \brief This function is used to check matches from the FTP App Layer Parser
+ *
+ * \param t pointer to thread vars
+ * \param det_ctx pointer to the pattern matcher thread
+ * \param p pointer to the current packet
+ * \param m pointer to the sigmatch
+ * \retval 0 no match
+ * \retval 1 match
+ */
+static int DetectFtpModeMatch(DetectEngineThreadCtx *det_ctx, Flow *f, uint8_t flags, void *state,
+        void *txv, const Signature *s, const SigMatchCtx *m)
+{
+    FTPTransaction *tx = (FTPTransaction *)txv;
+    if (tx->command_descriptor.command_code == FTP_COMMAND_UNKNOWN) {
+        return 0;
+    }
+    if (!tx->dyn_port) {
+        return 0;
+    }
+
+    const DetectFtpModeData *ftpmoded = (const DetectFtpModeData *)m;
+    return 1 ? ftpmoded->active == tx->active : 0;
+}
+
+/**
+ * \brief this function will free memory associated with DetectFtpModeData
+ *
+ * \param ptr pointer to DetectFtpModeData
+ */
+static void DetectFtpModeFree(DetectEngineCtx *de_ctx, void *ptr)
+{
+    SCFree(ptr);
+}
+
+/**
+ * \brief This function is used to parse ftp.mode options passed via ftp.mode keyword
+ *
+ * \param str Pointer to the user provided ftp.mode options
+ *
+ * \retval  pointer to DetectFtpModeData on success
+ * \retval NULL on failure
+ */
+static DetectFtpModeData *DetectFtpModeParse(const char *optstr)
+{
+    DetectFtpModeData *ftpmoded = NULL;
+    char arg1[8] = "";
+    size_t pcre2len;
+    pcre2_match_data *match = NULL;
+
+    int ret = DetectParsePcreExec(&parse_regex, &match, optstr, 0, 0);
+    if (ret != 2) {
+        SCLogError("parse error, ret %" PRId32 "", ret);
+        goto error;
+    }
+
+    pcre2len = sizeof(arg1);
+    int res = pcre2_substring_copy_bynumber(match, 1, (PCRE2_UCHAR8 *)arg1, &pcre2len);
+    if (res < 0) {
+        SCLogError("pcre2_substring_copy_bynumber failed");
+        goto error;
+    }
+
+    ftpmoded = SCCalloc(1, sizeof(DetectFtpModeData));
+    if (unlikely(ftpmoded == NULL))
+        goto error;
+    if (!strcmp(arg1, "active")) {
+        ftpmoded->active = true;
+    } else if (!strcmp(arg1, "passive")) {
+        ftpmoded->active = false;
+    } else {
+        SCLogError("Invalid command value");
+        goto error;
+    }
+
+    pcre2_match_data_free(match);
+    return ftpmoded;
+
+error:
+    if (match) {
+        pcre2_match_data_free(match);
+    }
+    if (ftpmoded)
+        SCFree(ftpmoded);
+    return NULL;
+}
+
+static int DetectFtpModeSetup(DetectEngineCtx *de_ctx, Signature *s, const char *str)
+{
+    if (DetectSignatureSetAppProto(s, ALPROTO_FTP) != 0)
+        return -1;
+
+    DetectFtpModeData *ftpmoded = DetectFtpModeParse(str);
+    if (ftpmoded == NULL)
+        return -1;
+
+    if (SigMatchAppendSMToList(de_ctx, s, DETECT_FTP_MODE, (SigMatchCtx *)ftpmoded,
+                g_ftp_mode_buffer_id) == NULL) {
+        DetectFtpModeFree(de_ctx, ftpmoded);
+        return -1;
+    }
+
+    return 0;
+}
+
+void DetectFtpModeRegister(void)
+{
+    /* ftp.mode sticky buffer */
+    sigmatch_table[DETECT_FTP_MODE].name = KEYWORD_NAME;
+    sigmatch_table[DETECT_FTP_MODE].desc = "sticky buffer to match on the FTP mode buffer";
+    sigmatch_table[DETECT_FTP_MODE].url = "/rules/" KEYWORD_DOC;
+    sigmatch_table[DETECT_FTP_MODE].Setup = DetectFtpModeSetup;
+    sigmatch_table[DETECT_FTP_MODE].AppLayerTxMatch = DetectFtpModeMatch;
+    sigmatch_table[DETECT_FTP_MODE].Free = DetectFtpModeFree;
+
+    DetectAppLayerInspectEngineRegister(
+            BUFFER_NAME, ALPROTO_FTP, SIG_FLAG_TOCLIENT, 0, DetectEngineInspectGenericList, NULL);
+
+    DetectAppLayerInspectEngineRegister(
+            BUFFER_NAME, ALPROTO_FTP, SIG_FLAG_TOSERVER, 0, DetectEngineInspectGenericList, NULL);
+
+    DetectBufferTypeSetDescriptionByName(BUFFER_NAME, BUFFER_DESC);
+
+    g_ftp_mode_buffer_id = DetectBufferTypeGetByName(BUFFER_NAME);
+
+    /* set up the PCRE for keyword parsing */
+    DetectSetupParseRegexes(PARSE_REGEX, &parse_regex);
+    SCLogDebug("registering " BUFFER_NAME " rule option");
+}
