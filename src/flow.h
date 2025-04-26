00d395e0ba1732d3cb68937c914b66f28e958897
Lukas Sismis <lsismis@oisf.net>
2025-04-26 14:56:26 +0200

    flow: change conversion type to match the assignment type

--- src/flow.h
+++ src/flow.h
@@ -640,8 +640,8 @@ static inline void FlowDeReference(Flow **d)
  */
 static inline int64_t FlowGetId(const Flow *f)
 {
-    int64_t id = (uint64_t)(SCTIME_SECS(f->startts) & 0x0000FFFF) << 48 |
-                 (uint64_t)(SCTIME_USECS(f->startts) & 0x0000FFFF) << 32 | (int64_t)f->flow_hash;
+    int64_t id = (int64_t)(SCTIME_SECS(f->startts) & 0x0000FFFF) << 48 |
+                 (int64_t)(SCTIME_USECS(f->startts) & 0x0000FFFF) << 32 | (int64_t)f->flow_hash;
     /* reduce to 51 bits as JavaScript and even JSON often seem to
      * max out there. */
     id &= 0x7ffffffffffffLL;
