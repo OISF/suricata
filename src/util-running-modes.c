7170db4d664d2960fdc6fd2e3d55eae5e62da217
Victor Julien <vjulien@oisf.net>
2025-05-01 22:43:27 +0200

    app-layer: add --list-app-layer-hooks options
    This provides a list of available hooks.
    
    Ticket: #7702.

--- src/util-running-modes.c
+++ src/util-running-modes.c
@@ -24,6 +24,7 @@
 #include "app-layer-detect-proto.h"
 #include "app-layer.h"
 #include "app-layer-parser.h"
+#include "detect-engine.h"
 #include "util-unittest.h"
 #include "util-debug.h"
 #include "conf-yaml-loader.h"
@@ -54,3 +55,63 @@ int ListAppLayerProtocols(const char *conf_filename)
     return TM_ECODE_DONE;
 }
 
+static bool IsBuiltIn(const char *n)
+{
+    if (strcmp(n, "request_started") == 0 || strcmp(n, "response_started") == 0) {
+        return true;
+    }
+    if (strcmp(n, "request_complete") == 0 || strcmp(n, "response_complete") == 0) {
+        return true;
+    }
+    return false;
+}
+
+int ListAppLayerHooks(const char *conf_filename)
+{
+    EngineModeSetIDS();
+    if (SCConfYamlLoadFile(conf_filename) != -1)
+        SCLogLoadConfig(0, 0, 0, 0);
+    MpmTableSetup();
+    SpmTableSetup();
+    AppLayerSetup();
+
+    AppProto alprotos[g_alproto_max];
+    AppLayerProtoDetectSupportedAppProtocols(alprotos);
+
+    printf("=========Supported App Layer Hooks=========\n");
+    for (AppProto a = 0; a < g_alproto_max; a++) {
+        if (alprotos[a] != 1)
+            continue;
+
+        const char *alproto_name = AppProtoToString(a);
+        if (strcmp(alproto_name, "http") == 0)
+            alproto_name = "http1";
+        SCLogDebug("alproto %u/%s", a, alproto_name);
+
+        const int max_progress_ts =
+                AppLayerParserGetStateProgressCompletionStatus(a, STREAM_TOSERVER);
+        const int max_progress_tc =
+                AppLayerParserGetStateProgressCompletionStatus(a, STREAM_TOCLIENT);
+
+        printf("%s:%s\n", alproto_name, "request_started");
+        for (int p = 0; p <= max_progress_ts; p++) {
+            const char *name = AppLayerParserGetStateNameById(
+                    IPPROTO_TCP /* TODO no ipproto */, a, p, STREAM_TOSERVER);
+            if (name != NULL && !IsBuiltIn(name)) {
+                printf("%s:%s\n", alproto_name, name);
+            }
+        }
+        printf("%s:%s\n", alproto_name, "request_complete");
+
+        printf("%s:%s\n", alproto_name, "response_started");
+        for (int p = 0; p <= max_progress_tc; p++) {
+            const char *name = AppLayerParserGetStateNameById(
+                    IPPROTO_TCP /* TODO no ipproto */, a, p, STREAM_TOCLIENT);
+            if (name != NULL && !IsBuiltIn(name)) {
+                printf("%s:%s\n", alproto_name, name);
+            }
+        }
+        printf("%s:%s\n", alproto_name, "response_complete");
+    }
+    return TM_ECODE_DONE;
+}
