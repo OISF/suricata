995e2566677d888dccc7001fa9f877ad2c8d09cc
Philippe Antoine <pantoine@oisf.net>
2025-05-15 14:15:47 +0200

    flow: add config option to skip non-tunneled packets
    Ticket: 7674
    
    On interfaces meant to receive only tunneled traffic

--- src/flow-worker.c
+++ src/flow-worker.c
@@ -565,6 +565,9 @@ static TmEcode FlowWorker(ThreadVars *tv, Packet *p, void *data)
 
     SCLogDebug("packet %" PRIu64, PcapPacketCntGet(p));
 
+    if (p->flags & PKT_SKIP_WORK) {
+        return TM_ECODE_OK;
+    }
     /* handle Flow */
     if (det_ctx != NULL && det_ctx->de_ctx->PreFlowHook != NULL) {
         const uint8_t action = det_ctx->de_ctx->PreFlowHook(tv, det_ctx, p);
