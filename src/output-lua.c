1a9303ae03449615778b69a5267d468799d95a5a
Jason Ish <jason.ish@oisf.net>
2025-06-02 16:54:16 -0600

    lua: simplify streaming output setup
    Setup the init function to simply return:
    
       {streaming = "tcp"}
    
    or
    
       {streaming = "http"}
    
    The returned table can have a lot of parameters that don't make sense
    together, this should simplify this one case.

--- src/output-lua.c
+++ src/output-lua.c
@@ -505,7 +505,16 @@ static int LuaScriptInit(const char *filename, LogLuaScriptOptions *options, Log
 
         SCLogDebug("k='%s', v='%s'", k, v);
 
-        if (strcmp(k,"protocol") == 0 && strcmp(v, "http") == 0)
+        if (strcmp(k, "streaming") == 0) {
+            options->streaming = 1;
+            if (strcmp(v, "http") == 0) {
+                options->alproto = ALPROTO_HTTP1;
+            } else if (strcmp(v, "tcp") == 0) {
+                options->tcp_data = 1;
+            } else {
+                SCLogError("unsupported streaming argument: %s", v);
+            }
+        } else if (strcmp(k, "protocol") == 0 && strcmp(v, "http") == 0)
             options->alproto = ALPROTO_HTTP1;
         else if (strcmp(k,"protocol") == 0 && strcmp(v, "dns") == 0)
             options->alproto = ALPROTO_DNS;
