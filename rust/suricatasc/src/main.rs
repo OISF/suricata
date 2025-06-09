39a9a570e3ffbb76a879717ce1da3061e77d615d
Jason Ish <jason.ish@oisf.net>
2025-06-09 09:28:53 -0600

    rust: allow some lints in suricatactl and suricatasc
    These are lints we allow in the Suricata Rust source code for style
    reasons.

--- rust/suricatasc/src/main.rs
+++ rust/suricatasc/src/main.rs
@@ -1,6 +1,11 @@
 // SPDX-FileCopyrightText: Copyright 2023 Open Information Security Foundation
 // SPDX-License-Identifier: GPL-2.0-only
 
+// Allow these patterns as its a style we like.
+#![allow(clippy::needless_return)]
+#![allow(clippy::let_and_return)]
+#![allow(clippy::uninlined_format_args)]
+
 #[cfg(not(target_os = "windows"))]
 mod unix;
 
