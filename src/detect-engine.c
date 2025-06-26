c7a642c7c28377f3af5c9706803c5ec21cf9ded0
Jeff Lucovsky <jlucovsky@oisf.net>
2025-06-26 10:17:45 -0400

    misc/debug: Use SCConfDump
    The API entrypoint for ConfDump has changed so update the #ifdef'd
    code to use it.

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -2707,7 +2707,7 @@ void DetectEngineCtxFree(DetectEngineCtx *de_ctx)
             SCConfNodeRemove(node); /* frees node */
         }
 #if 0
-        ConfDump();
+        SCConfDump();
 #endif
     }
 
@@ -4820,7 +4820,7 @@ int DetectEngineReload(const SCInstance *suri)
         }
 
 #if 0
-        ConfDump();
+        SCConfDump();
 #endif
     }
 
