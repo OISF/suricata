498ae6d1fa67ec58c236669e8a09f38e79e18391
Thomas Winter <Thomas.Winter@alliedtelesis.co.nz>
2025-07-22 11:38:39 +1200

    decode/pppoe: Run clang-format
    These files have existing problems which prevent patches from
    passing the format check on them.

--- src/decode-pppoe.h
+++ src/decode-pppoe.h
@@ -26,14 +26,13 @@
 
 // Session header length minus the protocol field
 #define PPPOE_SESSION_HEADER_MIN_LEN     7
-#define PPPOE_DISCOVERY_HEADER_MIN_LEN 6
-#define PPPOE_SESSION_GET_VERSION(hdr) ((hdr)->pppoe_version_type & 0xF0) >> 4
-#define PPPOE_SESSION_GET_TYPE(hdr) ((hdr)->pppoe_version_type & 0x0F)
+#define PPPOE_DISCOVERY_HEADER_MIN_LEN   6
+#define PPPOE_SESSION_GET_VERSION(hdr)   ((hdr)->pppoe_version_type & 0xF0) >> 4
+#define PPPOE_SESSION_GET_TYPE(hdr)      ((hdr)->pppoe_version_type & 0x0F)
 #define PPPOE_DISCOVERY_GET_VERSION(hdr) ((hdr)->pppoe_version_type & 0xF0) >> 4
-#define PPPOE_DISCOVERY_GET_TYPE(hdr) ((hdr)->pppoe_version_type & 0x0F)
+#define PPPOE_DISCOVERY_GET_TYPE(hdr)    ((hdr)->pppoe_version_type & 0x0F)
 
-typedef struct PPPOESessionHdr_
-{
+typedef struct PPPOESessionHdr_ {
     uint8_t pppoe_version_type;
     uint8_t pppoe_code;
     uint16_t session_id;
@@ -41,14 +40,12 @@ typedef struct PPPOESessionHdr_
     uint16_t protocol;
 } PPPOESessionHdr;
 
-typedef struct PPPOEDiscoveryTag_
-{
+typedef struct PPPOEDiscoveryTag_ {
     uint16_t pppoe_tag_type;
     uint16_t pppoe_tag_length;
 } __attribute__((__packed__)) PPPOEDiscoveryTag;
 
-typedef struct PPPOEDiscoveryHdr_
-{
+typedef struct PPPOEDiscoveryHdr_ {
     uint8_t pppoe_version_type;
     uint8_t pppoe_code;
     uint16_t discovery_id;
@@ -63,16 +60,16 @@ typedef struct PPPOEDiscoveryHdr_
 #define PPPOE_CODE_PADT 0xa7
 
 /* see RFC 2516 Appendix A */
-#define PPPOE_TAG_END_OF_LIST         0x0000 /* End-Of-List */
-#define PPPOE_TAG_SERVICE_NAME        0x0101 /* Service-Name */
-#define PPPOE_TAG_AC_NAME             0x0102 /* AC-Name */
-#define PPPOE_TAG_HOST_UNIQ           0x0103 /* Host-Uniq */
-#define PPPOE_TAG_AC_COOKIE           0x0104 /* AC-Cookie */
-#define PPPOE_TAG_VENDOR_SPECIFIC     0x0105 /* Vendor-Specific */
-#define PPPOE_TAG_RELAY_SESSION_ID    0x0110 /* Relay-Session-Id */
-#define PPPOE_TAG_SERVICE_NAME_ERROR  0x0201 /* Service-Name-Error */
-#define PPPOE_TAG_AC_SYS_ERROR        0x0202 /* AC-System Error */
-#define PPPOE_TAG_GEN_ERROR           0x0203 /* Generic-Error */
+#define PPPOE_TAG_END_OF_LIST        0x0000 /* End-Of-List */
+#define PPPOE_TAG_SERVICE_NAME       0x0101 /* Service-Name */
+#define PPPOE_TAG_AC_NAME            0x0102 /* AC-Name */
+#define PPPOE_TAG_HOST_UNIQ          0x0103 /* Host-Uniq */
+#define PPPOE_TAG_AC_COOKIE          0x0104 /* AC-Cookie */
+#define PPPOE_TAG_VENDOR_SPECIFIC    0x0105 /* Vendor-Specific */
+#define PPPOE_TAG_RELAY_SESSION_ID   0x0110 /* Relay-Session-Id */
+#define PPPOE_TAG_SERVICE_NAME_ERROR 0x0201 /* Service-Name-Error */
+#define PPPOE_TAG_AC_SYS_ERROR       0x0202 /* AC-System Error */
+#define PPPOE_TAG_GEN_ERROR          0x0203 /* Generic-Error */
 
 void DecodePPPOERegisterTests(void);
 
