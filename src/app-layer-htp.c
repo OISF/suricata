02c04d3c21c69a69dade84ec84747b194e6e84c5
Juliana Fajardini <jufajardini@oisf.net>
2025-08-05 12:45:32 -0300

    applayer/htp: initialize flag in unit test
    To prevent the compiler warning
    warning: 'MEM[(struct HtpTxUserData *)ptrmem_54].tsflags' may be used
     uninitialized [-Wmaybe-uninitialized]
    with LTO.

--- src/app-layer-htp.c
+++ src/app-layer-htp.c
@@ -7073,6 +7073,7 @@ static int HTPParserTest27(void)
     HtpTxUserData *tx_ud = SCMalloc(sizeof(HtpTxUserData));
     FAIL_IF_NULL(tx_ud);
 
+    tx_ud->tsflags = BIT_U8(0);
     tx_ud->tsflags |= HTP_STREAM_DEPTH_SET;
     tx_ud->request_body.content_len_so_far = 2500;
 
