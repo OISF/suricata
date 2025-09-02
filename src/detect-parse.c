229591fc7c279cd2a02a9cc6b53e257f847f0d01
Philippe Antoine <pantoine@oisf.net>
2025-09-02 09:28:08 +0200

    detect: add arguments for multi-buffer
    As for multi-integers, multi-buffers can now have the following
    arguments
    - nb
    - precise index
    - all
    
    Ticket: 5044

--- src/detect-parse.c
+++ src/detect-parse.c
@@ -36,6 +36,7 @@
 #include "detect-content.h"
 #include "detect-bsize.h"
 #include "detect-isdataat.h"
+#include "detect-multi.h"
 #include "detect-pcre.h"
 #include "detect-uricontent.h"
 #include "detect-reference.h"
@@ -2642,6 +2643,9 @@ static int SigValidateCheckBuffers(
             SCReturnInt(0);
         }
         if (!DetectAbsentValidateContentCallback(s, b)) {
+            SCReturnInt(0);
+        }
+        if (!DetectMultiValidateContentCallback(s, b)) {
             SCReturnInt(0);
         }
     }
