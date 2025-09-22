19fc253d6b606baa339d5414979f954c0187bdaa
Philippe Antoine <pantoine@oisf.net>
2025-09-22 16:20:12 +0200

    ci: update address for oss-fuzz public corpus

--- qa/run-ossfuzz-corpus.sh
+++ qa/run-ossfuzz-corpus.sh
@@ -5,7 +5,7 @@ do
     echo "target $target"
     #download public corpus
     rm -f public.zip
-    wget --quiet "https://storage.googleapis.com/suricata-backup.clusterfuzz-external.appspot.com/corpus/libFuzzer/suricata_$target/public.zip"
+    wget --quiet "https://console.cloud.google.com/storage/browser/_details/suricata-backup.clusterfuzz-external.appspot.com/corpus/libFuzzer/suricata_$target/public.zip"
     rm -rf corpus_$target
     unzip -q public.zip -d corpus_$target
     #run target on corpus.
