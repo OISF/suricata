c516d36f70627418ea02297aa03b5322650cfde2
Victor Julien <vjulien@oisf.net>
2025-09-28 08:07:12 +0200

    flow: decrypted flow support
    Allow tagging of a flow as decrypted. This also allows the caller to set
    a new flow tuple that will be used to translate new packets to the new
    tuple.
    
    This is developed for the SSLproxy usecase, where the packets on the
    wire do not reflect the real tuple, but instead a connection between the
    proxy and a helper tool. The SSLproxy header at the start of the
    connection holds the original tuple, so that can then be applied to the
    packets.

--- rust/sys/src/sys.rs
+++ rust/sys/src/sys.rs
@@ -1032,5 +1032,10 @@ extern "C" {
     pub fn SCFlowGetDestinationPort(flow: *const Flow) -> u16;
 }
 extern "C" {
+    pub fn SCFlowSetDecrypted(
+        f: *mut Flow, proto: u8, src_ip: u32, sp: u16, dest_ip: u32, dp: u16,
+    ) -> ::std::os::raw::c_int;
+}
+extern "C" {
     pub fn SCSRepCatGetByShortname(shortname: *const ::std::os::raw::c_char) -> u8;
 }
