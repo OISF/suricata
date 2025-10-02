c7fd49a809c7767ab69a9f07d251e9076d75ed3f
Victor Julien <vjulien@oisf.net>
2025-10-02 16:02:12 +0200

    sslproxy: initial parser
    Implement support for SSLproxy as a minimalistic app-layer parser. It
    will:
    
    - be detected as a SSLproxy connection
    - parse the special header containing the original tuple
    - mark the flow as decrypted and set the original tuple as the
      translation tuple
    - trigger a protocol detection on the data post SSLproxy header
    
    Flow records will then have `app_proto_orig` set to `sslproxy`.
    
    Ticket: #7979.

--- src/app-layer-parser.c
+++ src/app-layer-parser.c
@@ -1810,6 +1810,7 @@ void AppLayerParserRegisterProtocolParsers(void)
     SCRegisterLdapTcpParser();
     SCRegisterLdapUdpParser();
     SCRegisterMdnsParser();
+    SCRegisterSSLProxyParser();
     SCRegisterTemplateParser();
     SCRfbRegisterParser();
     SCMqttRegisterParser();
