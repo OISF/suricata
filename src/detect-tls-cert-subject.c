abc4106cfb6fc46c81558128b09c5d24ea8300b2
Shivani Bhardwaj <shivani@oisf.net>
2025-10-10 19:41:39 +0530

    tls/subject: use byte array instead of string
    TLS parsers use x509-parser crate which parses X.509 certificates that
    use ASN.1 DER encoding that can allow arbitrary byte sequences. An
    attacker could inject null byte in a certificate anywhere to stump the
    common language parsers terminating the string at a null byte leading to
    a bypass of a possibly malicious certificate.
    
    So far, the rust TLS parser for "Subject" used a pattern that involved:
    -> Get ASN.1 DER encoded raw data from the x509-parser crate
    -> Convert this raw data to a decoded string (Rust)
    -> Convert the Rust string to CString
    -- The problem lies here. CString only accepts proper strings/byte
    buffers and converts it into an owned C-compatible, null-terminated
    string. However, if any null byte occurs in the string passed to the
    CString then it panics.
    In the rust TLS parser, this panic is handled by returning NULL.
    
    This means that the parser will error out during the decoding of the
    certificate. However, Suricata must be able to detect the null byte
    injection attack being an IDS/IPS.
    
    Hence, replace all such string patterns w.r.t. TLS Subject with a byte
    array.
    
    Bug 7887

--- src/detect-tls-cert-subject.c
+++ src/detect-tls-cert-subject.c
@@ -138,8 +138,8 @@ static InspectionBuffer *GetData(DetectEngineThreadCtx *det_ctx,
             return NULL;
         }
 
-        const uint32_t data_len = (uint32_t)strlen(connp->cert0_subject);
-        const uint8_t *data = (uint8_t *)connp->cert0_subject;
+        const uint32_t data_len = connp->cert0_subject_len;
+        const uint8_t *data = connp->cert0_subject;
 
         InspectionBufferSetupAndApplyTransforms(
                 det_ctx, list_id, buffer, data, data_len, transforms);
