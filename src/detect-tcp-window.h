f7b472b7c454cfaefb988837b194f64af1e4a882
Philippe Antoine <pantoine@oisf.net>
2025-10-16 11:15:35 +0200

    detect: tcp.window is now a generic integer
    Ticket: 7889

--- src/detect-tcp-window.h
+++ src/detect-tcp-window.h
@@ -18,11 +18,6 @@
 #ifndef SURICATA_DETECT_WINDOW_H
 #define SURICATA_DETECT_WINDOW_H
 
-typedef struct DetectWindowData_ {
-    uint8_t negated;	/** negated? 1=True : 0=False */
-    uint16_t size;     /** window size to match */
-} DetectWindowData;
-
 /* prototypes */
 void DetectWindowRegister (void);
 
