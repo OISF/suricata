85fa8944254d1b340e6a0d1a963855ff055c895c
Philippe Antoine <pantoine@oisf.net>
2025-10-16 09:42:26 +0200

    detect: dnp3.func is now a generic integer
    Ticket: 7889

--- rust/src/dnp3/detect.rs
+++ rust/src/dnp3/detect.rs
@@ -15,7 +15,9 @@
  * 02110-1301, USA.
  */
 
-use crate::detect::uint::{detect_parse_uint_bitflags, DetectBitflagModifier, DetectUintData};
+use crate::detect::uint::{
+    detect_parse_uint_bitflags, detect_parse_uint_enum, DetectBitflagModifier, DetectUintData,
+};
 
 use std::ffi::CStr;
 
@@ -45,6 +47,48 @@ fn dnp3_detect_ind_parse(s: &str) -> Option<DetectUintData<u16>> {
     detect_parse_uint_bitflags::<u16, Dnp3IndFlag>(s, DetectBitflagModifier::Any, false)
 }
 
+#[repr(u8)]
+#[derive(EnumStringU8)]
+pub enum Dnp3Func {
+    Confirm = 0,
+    Read = 1,
+    Write = 2,
+    Select = 3,
+    Operate = 4,
+    DirectOperate = 5,
+    DirectOperateNr = 6,
+    ImmedFreeze = 7,
+    ImmedFreezeNr = 8,
+    FreezeClear = 9,
+    FreezeClearNr = 10,
+    FreezeAtTime = 11,
+    FreezeAtTimeNr = 12,
+    ColdRestart = 13,
+    WarmRestart = 14,
+    InitializeData = 15,
+    InitializeAppl = 16,
+    StartAppl = 17,
+    StopAppl = 18,
+    SaveConfig = 19,
+    EnableUnsolicited = 20,
+    DisableUnsolicited = 21,
+    AssignClass = 22,
+    DelayMeasure = 23,
+    RecordCurrentTime = 24,
+    OpenFile = 25,
+    CloseFile = 26,
+    DeleteFile = 27,
+    GetFileInfo = 28,
+    AuthenticateFile = 29,
+    AbortFile = 30,
+    ActivateConfig = 31,
+    AutenthicateReq = 32,
+    AutenthicateErr = 33,
+    Response = 129,
+    UnsolicitedResponse = 130,
+    AuthenticateResp = 131,
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn SCDnp3DetectIndParse(
     ustr: *const std::os::raw::c_char,
@@ -52,6 +96,20 @@ pub unsafe extern "C" fn SCDnp3DetectIndParse(
     let ft_name: &CStr = CStr::from_ptr(ustr); //unsafe
     if let Ok(s) = ft_name.to_str() {
         if let Some(ctx) = dnp3_detect_ind_parse(s) {
+            let boxed = Box::new(ctx);
+            return Box::into_raw(boxed) as *mut _;
+        }
+    }
+    return std::ptr::null_mut();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn SCDnp3DetectFuncParse(
+    ustr: *const std::os::raw::c_char,
+) -> *mut DetectUintData<u8> {
+    let ft_name: &CStr = CStr::from_ptr(ustr); //unsafe
+    if let Ok(s) = ft_name.to_str() {
+        if let Some(ctx) = detect_parse_uint_enum::<u8, Dnp3Func>(s) {
             let boxed = Box::new(ctx);
             return Box::into_raw(boxed) as *mut _;
         }
@@ -83,5 +141,26 @@ mod test {
         let ctx = dnp3_detect_ind_parse("class_1_events , class_2_events").unwrap();
         assert_eq!(ctx.arg1, 0x600);
         assert!(dnp3_detect_ind_parse("something",).is_none());
+    }
+
+    #[test]
+    fn dnp3_func_parse() {
+        let ctx = detect_parse_uint_enum::<u8, Dnp3Func>("0").unwrap();
+        assert_eq!(ctx.arg1, 0);
+        let ctx = detect_parse_uint_enum::<u8, Dnp3Func>("1").unwrap();
+        assert_eq!(ctx.arg1, 1);
+        let ctx = detect_parse_uint_enum::<u8, Dnp3Func>("254").unwrap();
+        assert_eq!(ctx.arg1, 254);
+        let ctx = detect_parse_uint_enum::<u8, Dnp3Func>("255").unwrap();
+        assert_eq!(ctx.arg1, 255);
+        let ctx = detect_parse_uint_enum::<u8, Dnp3Func>("confirm").unwrap();
+        assert_eq!(ctx.arg1, 0);
+        let ctx = detect_parse_uint_enum::<u8, Dnp3Func>("CONFIRM").unwrap();
+        assert_eq!(ctx.arg1, 0);
+        assert!(detect_parse_uint_enum::<u8, Dnp3Func>("").is_none());
+        assert!(detect_parse_uint_enum::<u8, Dnp3Func>("-1").is_none());
+        assert!(detect_parse_uint_enum::<u8, Dnp3Func>("-2").is_none());
+        assert!(detect_parse_uint_enum::<u8, Dnp3Func>("256").is_none());
+        assert!(detect_parse_uint_enum::<u8, Dnp3Func>("unknown_function_code").is_none());
     }
 }
