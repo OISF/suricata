3f3541516ac70495b7f641d3fc176a2eceb8c5d1
Philippe Antoine <pantoine@oisf.net>
2025-10-30 13:34:59 +0100

    datasets: use fixed constant for stack allocation
    instead of smaller run-time value.
    Allows easier reading of the code for unbounded stack allocations
    
    Ticket: 8001

--- src/datasets-context-json.c
+++ src/datasets-context-json.c
@@ -116,9 +116,8 @@ static json_t *GetSubObjectByKey(json_t *json, const char *key)
     while (current_key) {
         const char *dot = strchr(current_key, '.');
 
-        size_t key_len = dot ? (size_t)(dot - current_key) : strlen(current_key);
-        char key_buffer[key_len + 1];
-        strlcpy(key_buffer, current_key, key_len + 1);
+        char key_buffer[SIG_JSON_CONTENT_KEY_LEN + 1];
+        strlcpy(key_buffer, current_key, SIG_JSON_CONTENT_KEY_LEN + 1);
 
         if (json_is_object(current) == false) {
             return NULL;
