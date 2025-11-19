OISF/suricata

commit f68e2f5537c95388858129aa1b076e976354584a

files: append data on closing even with FILE_NOSTORE
Ticket: 7577

When HTTP1 post multipart handles a small file, it will call
HTPFileClose with some data
This data needs to be appended to the streaming buffer for usage
by file.data keyword even if we do not end up storing the file

src/util-file.c
@@ -1019,11 +1019,10 @@ int FileCloseFilePtr(File *ff, const StreamingBufferConfig *sbcfg, const uint8_t
                 SCLogDebug("file %p data %p data_len %u", ff, data, data_len);
                 SCSha256Update(ff->sha256_ctx, data, data_len);
             }
-        } else {
-            if (AppendData(sbcfg, ff, data, data_len) != 0) {
-                ff->state = FILE_STATE_ERROR;
-                SCReturnInt(-1);
-            }
+        }
+        if (AppendData(sbcfg, ff, data, data_len) != 0) {
+            ff->state = FILE_STATE_ERROR;
+            SCReturnInt(-1);
         }
     }
 

commit 790ca3669ddd98d86c88331abf52ddf2e013b65e

file: fix hash computation for small multipart files
Ticket: 8119

Fixes: f68e2f5537 ("files: append data on closing even with
FILE_NOSTORE")

src/util-file.c
@@ -991,17 +991,6 @@ int FileCloseFilePtr(File *ff, const StreamingBufferConfig *sbcfg, const uint8_t
 
     ff->size += data_len;
     if (data != NULL) {
-        if (ff->flags & FILE_NOSTORE) {
-            /* no storage but hashing */
-            if (ff->md5_ctx)
-                SCMd5Update(ff->md5_ctx, data, data_len);
-            if (ff->sha1_ctx)
-                SCSha1Update(ff->sha1_ctx, data, data_len);
-            if (ff->sha256_ctx) {
-                SCLogDebug("file %p data %p data_len %u", ff, data, data_len);
-                SCSha256Update(ff->sha256_ctx, data, data_len);
-            }
-        }
         if (AppendData(sbcfg, ff, data, data_len) != 0) {
             ff->state = FILE_STATE_ERROR;
             SCReturnInt(-1);