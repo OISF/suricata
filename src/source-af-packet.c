49c3b353e020ce03b44088a0adbe342aa18a9350
Victor Julien <vjulien@oisf.net>
2025-12-01 21:25:25 +0100

    af-packet: disable hwtimestamp by default
    Add enable-hwtimestamp option to replace disable-hwtimestamp.
    
    Ticket: #7585.

--- src/source-af-packet.c
+++ src/source-af-packet.c
@@ -1736,11 +1736,13 @@ static int AFPSetupRing(AFPThreadVars *ptv, char *devname)
     }
 
 #ifdef HAVE_HW_TIMESTAMPING
-    if ((ptv->flags & AFP_DISABLE_HWTIMESTAMP) == 0) {
+    if (ptv->flags & AFP_ENABLE_HWTIMESTAMP) {
         int req = SOF_TIMESTAMPING_RAW_HARDWARE;
         if (setsockopt(ptv->socket, SOL_PACKET, PACKET_TIMESTAMP, (void *)&req, sizeof(req)) < 0) {
             SCLogWarning("%s: failed to activate hardware timestamping on packet socket: %s",
                     devname, strerror(errno));
+        } else {
+            SCLogConfig("%s: hardware timestamping enabled", devname);
         }
     } else {
         SCLogConfig("%s: hardware timestamping disabled", devname);
