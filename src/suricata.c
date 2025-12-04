5d841e45445751811926afaab89a87b564a9a2b5
Promise Charles <descencybobby@gmail.com>
2025-12-04 04:35:26 -0500

    runmodes: add hints about missing runmode
    Bug: #5711
    
    Signed-off-by: Promise Charles <descencybobby@gmail.com>

--- src/suricata.c
+++ src/suricata.c
@@ -2448,13 +2448,21 @@ int SCStartInternalRunMode(int argc, char **argv)
     return TM_ECODE_OK;
 }
 
-int SCFinalizeRunMode(void)
+int SCFinalizeRunMode(int argc)
 {
     SCInstance *suri = &suricata;
+
     switch (suri->run_mode) {
         case RUNMODE_UNKNOWN:
+            /* Only warn if user passed arguments */
+            if (argc > 1) {
+                SCLogWarning("Please specify a runmode or capture option. "
+                             "Use --list-runmodes to see available runmodes.");
+            }
+
             PrintUsage(suri->progname);
             return TM_ECODE_FAILED;
+
         default:
             break;
     }
