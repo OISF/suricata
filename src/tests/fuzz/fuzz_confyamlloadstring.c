6882159692a03be4146ca4fc1ca1a94b1a53d477
Philippe Antoine <pantoine@oisf.net>
2025-12-11 14:00:45 +0100

    fuzz: set engine stage for conf yaml target
    Ticket: 6188
    
    Allows allocations failures not to be fatal as in init

--- src/tests/fuzz/fuzz_confyamlloadstring.c
+++ src/tests/fuzz/fuzz_confyamlloadstring.c
@@ -9,6 +9,8 @@
 #include "conf-yaml-loader.h"
 #include "nallocinc.c"
 
+SC_ATOMIC_EXTERN(unsigned int, engine_stage);
+
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 
 static int initialized = 0;
@@ -22,6 +24,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         //global init
         InitGlobal();
         SCRunmodeSet(RUNMODE_UNITTEST);
+        SC_ATOMIC_SET(engine_stage, SURICATA_RUNTIME);
         nalloc_init(NULL);
         // do not restrict nalloc
         initialized = 1;
