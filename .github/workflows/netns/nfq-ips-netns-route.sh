b59ebfa145ae3ff375c4fd0a92c9a86d5a24286d
Victor Julien <vjulien@oisf.net>
2026-01-26 15:22:54 +0100

    WIP

--- .github/workflows/netns/nfq-ips-netns-route.sh
+++ .github/workflows/netns/nfq-ips-netns-route.sh
@@ -201,7 +201,7 @@ if [ $ACCEPTED -eq 0 ]; then
     echo "ERROR no packets captured"
     RES=1
 fi
-if [ $ACCEPTED -ne 10 ]; then
+if [ $BLOCKED -ne 10 ]; then
     echo "ERROR should have seen 10 blocked"
     RES=1
 fi
