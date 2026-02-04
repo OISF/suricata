rtkwlf/suricata-awn

[AWN-39270] Add http hostname to flow json log output
This entry is being added to remove the need for YAF and the subsequent
parsing of its ipfix-format output, which has become a performance
bottleneck.

Testing:
Basic manual testing was done with some simple HTTP flows, non-HTTP TCP
flows and non-TCP flows. AddressSanitizer was used to test for memory
leaks.

src/output-json-flow.c
@@ -180,6 +180,12 @@ void EveAddAppProto(Flow *f, JsonBuilder *js)
     if (f->alproto) {
         jb_set_string(js, "app_proto", AppProtoToString(f->alproto));
     }
+
+    /* Log the hostname for HTTP flows */
+    if (f->alproto == ALPROTO_HTTP && f->http_hostname != NULL) {
+        jb_set_string(js, "hostname", f->http_hostname);
+    }
+
     if (f->alproto_ts && f->alproto_ts != f->alproto) {
         jb_set_string(js, "app_proto_ts", AppProtoToString(f->alproto_ts));
     }

OISF/suricata

src/output-json-flow.c
@@ -180,6 +180,12 @@ void EveAddAppProto(Flow *f, SCJsonBuilder *js)
     if (f->alproto) {
         SCJbSetString(js, "app_proto", AppProtoToString(f->alproto));
     }
+
+    /* Log the hostname for HTTP flows */
+    if (f->alproto == ALPROTO_HTTP && f->http_hostname != NULL) {
+        SCJbSetString(js, "hostname", f->http_hostname);
+    }
+
     if (f->alproto_ts && f->alproto_ts != f->alproto) {
         SCJbSetString(js, "app_proto_ts", AppProtoToString(f->alproto_ts));
     }