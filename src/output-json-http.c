rtkwlf/suricata-awn

[AWN-48015] AWN allows the http logger to force hash/magic on files.
As part of another commit (to send file summaries which contain
magic and hash content for files from http transactions) we parse
the force-hash and force-magic config inside the http logger
config.

src/output-json-http.c
@@ -671,6 +671,15 @@ static OutputInitResult OutputHttpLogInitSub(ConfNode *conf, OutputCtx *parent_c
                 }
             }
         }
+
+        FileForceHashParseCfg(conf);
+
+        const char *force_magic = ConfNodeLookupChildValue(conf, "force-magic");
+        if (force_magic != NULL && ConfValIsTrue(force_magic)) {
+            FileForceMagicEnable();
+            SCLogInfo("forcing magic lookup for logged files");
+        }
+        
         const char *all_headers = ConfNodeLookupChildValue(
                 conf, "dump-all-headers");
         if (all_headers != NULL) {

OISF/suricata

src/output-json-http.c
@@ -602,7 +602,13 @@ static OutputInitResult OutputHttpLogInitSub(SCConfNode *conf, OutputCtx *parent
                 http_ctx->flags = LOG_HTTP_EXTENDED;
             }
         }
+        FileForceHashParseCfg(conf);
 
+        const char *force_magic = SCConfNodeLookupChildValue(conf, "force-magic");
+        if (force_magic != NULL && SCConfValIsTrue(force_magic)) {
+            FileForceMagicEnable();
+            SCLogInfo("forcing magic lookup for logged files");
+        }
         const char *all_headers = SCConfNodeLookupChildValue(conf, "dump-all-headers");
         if (all_headers != NULL) {
             if (strncmp(all_headers, "both", 4) == 0) {