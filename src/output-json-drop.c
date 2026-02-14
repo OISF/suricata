14214ed2a919f076a5c17e31ad49553ff87b9b66
Jeff Lucovsky <jlucovsky@oisf.net>
2026-02-14 09:37:32 -0500

    output/flush: Remove flush functions/registration
    Remove log flush functions and update registration logic as
    context-based flushing doesn't require it.
    
    Issue: 8286

--- src/output-json-drop.c
+++ src/output-json-drop.c
@@ -392,7 +392,6 @@ void JsonDropLogRegister (void)
 {
     OutputPacketLoggerFunctions output_logger_functions = {
         .LogFunc = JsonDropLogger,
-        .FlushFunc = OutputJsonLogFlush,
         .ConditionFunc = JsonDropLogCondition,
         .ThreadInitFunc = JsonDropLogThreadInit,
         .ThreadDeinitFunc = JsonDropLogThreadDeinit,
