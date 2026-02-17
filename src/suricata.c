e1f4f3b267c9c608a0c03c344e63ed52d4862949
Victor Julien <vjulien@oisf.net>
2026-02-17 14:55:59 +0100

    openbsd: pledge before allowing threads to continue
    Avoids a race condition with pcap file directory mode.
    
    Ticket: #8300.

--- src/suricata.c
+++ src/suricata.c
@@ -3191,6 +3191,10 @@ void SuricataPostInit(void)
     SC_ATOMIC_SET(engine_stage, SURICATA_RUNTIME);
     PacketPoolPostRunmodes();
 
+    /* pledge before allowing threads to continue to avoid an issue with pcap file direction mode,
+     * see ticket #8300. */
+    SCPledge();
+
     /* Un-pause all the paused threads */
     TmThreadContinueThreads();
 
@@ -3210,5 +3214,4 @@ void SuricataPostInit(void)
         SystemHugepageSnapshotDestroy(prerun_snap);
         SystemHugepageSnapshotDestroy(postrun_snap);
     }
-    SCPledge();
 }
