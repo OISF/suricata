b1d067c446fda61e0965482639fd91a478e0a097
Philippe Antoine <pantoine@oisf.net>
2026-02-19 16:37:02 +0100

    detect: replace stack alloc by member of DetectEngineThreadCtx
    To avoid running a big (when many signatures) stack allocation
    on each detection loop with postmatches
    
    Ticket: 8001
    (cherry picked from commit 7e97dfd521383522dcdb8021068e229f14daf32a)

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -3428,6 +3428,10 @@ static TmEcode ThreadCtxDoInit (DetectEngineCtx *de_ctx, DetectEngineThreadCtx *
         if (det_ctx->match_array == NULL) {
             return TM_ECODE_FAILED;
         }
+        det_ctx->replace = SCCalloc(de_ctx->sig_array_len, sizeof(Signature *));
+        if (det_ctx->replace == NULL) {
+            return TM_ECODE_FAILED;
+        }
 
         RuleMatchCandidateTxArrayInit(det_ctx, de_ctx->sig_array_len);
     }
@@ -3685,6 +3689,8 @@ static void DetectEngineThreadCtxFree(DetectEngineThreadCtx *det_ctx)
     }
     if (det_ctx->match_array != NULL)
         SCFree(det_ctx->match_array);
+    if (det_ctx->replace != NULL)
+        SCFree(det_ctx->replace);
 
     RuleMatchCandidateTxArrayFree(det_ctx);
 
