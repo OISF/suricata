88206c3e3f991f8bd917ac4b9a2deeee68d6baec
Jason Ish <jason.ish@oisf.net>
2026-03-16 15:53:50 -0600

    github-ci: update almalinux minimal build to use dist archive
    Update the AlmaLinux recommended minimal build to use the pre built
    distribution archive as this is the type of build the documentation is
    targetting.
    
    (cherry picked from commit 551f6ded69537b6bd6ff796ee2dbc9bc8fce2114)

--- scripts/docs-almalinux9-minimal-build.sh
+++ scripts/docs-almalinux9-minimal-build.sh
@@ -3,7 +3,6 @@
 # Serves for RPM-based docs and is verified by Github Actions
 
 # install-guide-documentation tag start: Minimal RPM-based dependencies
-sudo dnf install -y rustc cargo cbindgen
-sudo dnf install -y gcc gcc-c++ jansson-devel libpcap-devel \
+sudo dnf install -y cargo gcc jansson-devel libpcap-devel \
     libyaml-devel make pcre2-devel zlib-devel
 # install-guide-documentation tag end: Minimal RPM-based dependencies
