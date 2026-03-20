dc5185e3555f9c78179df41f0e89c9032f5b302f
Philippe Antoine <pantoine@oisf.net>
2026-03-20 16:08:59 +0100

    util/mpm-ac-ks: preallocate on the heap rather than stack
    Ticket: 8001
    
    As the the size of the allocation can get large

--- src/util-mpm-ac-ks-small.c
+++ src/util-mpm-ac-ks-small.c
@@ -48,8 +48,8 @@ uint32_t FUNC_NAME(const SCACTileSearchCtx *ctx, MpmThreadCtx *mpm_thread_ctx,
     uint32_t i = 0;
     int matches = 0;
 
-    uint8_t mpm_bitarray[ctx->mpm_bitarray_size];
-    memset(mpm_bitarray, 0, ctx->mpm_bitarray_size);
+    uint8_t *mpm_bitarray = (uint8_t *)mpm_thread_ctx->ctx;
+    memset(mpm_bitarray, 0, mpm_thread_ctx->memory_size);
 
     const uint8_t* restrict xlate = ctx->translate_table;
     STYPE *state_table = (STYPE*)ctx->state_table;
