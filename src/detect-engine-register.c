46cdd362126b2bce18525da897d3c69c111f7646
Giuseppe Longo <glongo@oisf.net>
2026-03-24 10:17:44 +0100

    detect/sctp: add sctp.chunk_type keyword
    Add a U8 numeric keyword to match the first SCTP chunk type in a packet
    with prefilter support.
    
    Ticket #4251

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -156,6 +156,7 @@
 #include "detect-igmphdr.h"
 #include "detect-igmp-type.h"
 #include "detect-sctphdr.h"
+#include "detect-sctp-chunk-type.h"
 #include "detect-urilen.h"
 #include "detect-bsize.h"
 #include "detect-detection-filter.h"
@@ -678,6 +679,7 @@ void SigTableSetup(void)
     DetectIGMPHdrRegister();
     DetectIGMPTypeRegister();
     DetectSCTPHdrRegister();
+    DetectSCTPChunkTypeRegister();
     DetectTlsRegister();
     DetectTlsValidityRegister();
     DetectTlsVersionRegister();
