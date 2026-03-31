34152cd60eda63e7d4789150d29e2fdf48cf2c10
Victor Julien <vjulien@oisf.net>
2026-03-31 12:12:46 +0200

    respond/reject: use livedev in bridge mode
    Clean up host mode tracking, which is used by reject to control how
    rejects are sent. Before this patch there were 2 modes: sniffer only
    and router. This patch introduces a bridge mode that is automatically
    set by the bridge modes. In bridge mode the `Packet::livedev` is used.
    
    Ticket: #8390.

--- src/runmode-dpdk.c
+++ src/runmode-dpdk.c
@@ -1961,7 +1961,7 @@ static int DPDKRunModeEnableIPS(void)
     int r = DPDKRunModeIsIPS();
     if (r == 1) {
         SCLogInfo("Setting IPS mode");
-        EngineModeSetIPS();
+        EngineModeSetIPS(ENGINE_HOST_IS_BRIDGE);
     }
     return r;
 }
