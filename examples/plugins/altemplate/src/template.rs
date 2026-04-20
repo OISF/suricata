967785163708c58e0f309724457be9aed5a50078
Philippe Antoine <pantoine@oisf.net>
2026-04-20 17:58:31 +0200

    rust/ffi: move app-layer traits to ffi crate
    Ticket: 7666
    
    Traits around structures defined in C and bindgened to rust

--- examples/plugins/altemplate/src/template.rs
+++ examples/plugins/altemplate/src/template.rs
@@ -30,7 +30,7 @@ use suricata::applayer::{
     state_get_tx_iterator, AppLayerEvent, AppLayerTxData, State, Transaction,
     APP_LAYER_PARSER_EOF_TC, APP_LAYER_PARSER_EOF_TS, APP_LAYER_PARSER_OPT_ACCEPT_GAPS,
 };
-use suricata::applayer::{AppLayerResultRust, StreamSliceRust};
+use suricata_ffi::applayer::{AppLayerResultRust, StreamSliceRust};
 use suricata_ffi::conf::conf_get;
 use suricata_ffi::{
     build_slice, cast_pointer, export_state_data_get, export_tx_data_get, SCLogError, SCLogNotice,
