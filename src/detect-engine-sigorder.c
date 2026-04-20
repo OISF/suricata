b8037c6f6e958e697756df94e20ad86af5fc12d2
Shivani Bhardwaj <shivani@oisf.net>
2026-04-20 18:32:03 +0530

    detect/sigorder: do not disturb the order of SET_READ flowbit sigs
    The final list of signatures must be sorted with all the sorting fns
    available. However, flowbits SET_READ have their order derived from
    special handling between the dependent flowbits. Using the usual sorting
    criteria will mess up the order that was created.

--- src/detect-engine-sigorder.c
+++ src/detect-engine-sigorder.c
@@ -547,6 +547,14 @@ static int SCSigLessThan(SCSigSignatureWrapper *sw1,
 
         funcs = funcs->next;
     }
+
+    // Special handling for flowbits, do not touch the order if the comparison fn
+    // calculates two signatures to be equal
+    if ((sw1->user[DETECT_SIGORDER_FLOWBITS] > DETECT_FLOWBITS_NOT_USED) &&
+            (sw1->user[DETECT_SIGORDER_FLOWBITS] == DETECT_FLOWBITS_TYPE_SET_READ) &&
+            (sw1->user[DETECT_SIGORDER_FLOWBITS] == sw2->user[DETECT_SIGORDER_FLOWBITS]))
+        return 1;
+
     // They are equal, so use sid as the final decider.
     return sw1->sig->id < sw2->sig->id;
 }
