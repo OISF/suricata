eafbdf9fba6779cc4e3948170947bc4c44292618
Adam Kiripolsky <adam.kiripolsky@cesnet.cz>
2026-04-21 16:01:42 +0200

    dpdk: rename pkt_mempools to dpdk_dev_resources

--- src/source-dpdk.h
+++ src/source-dpdk.h
@@ -75,7 +75,7 @@ typedef struct DPDKIfaceConfig_ {
     uint32_t queue_mempool_size;
     uint32_t mempool_cache_size;
     bool mempool_cache_size_auto; // auto cache size based on mempool size
-    DPDKDeviceResources *pkt_mempools;
+    DPDKDeviceResources *dpdk_dev_resources;
     uint16_t linkup_timeout; // in seconds how long to wait for link to come up
     RteFlowRuleStorage drop_filter;
     SC_ATOMIC_DECLARE(uint16_t, ref);
