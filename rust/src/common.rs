a90f070e229e76df7912602f1b5c9f0b7657e86e
Philippe Antoine <pantoine@oisf.net>
2026-04-22 16:36:08 +0200

    rust: remove unused function
    rust_string_to_c was used when a buffer should be used as it could
    contain nul-byte

--- rust/src/common.rs
+++ rust/src/common.rs
@@ -18,8 +18,6 @@
 //! Utility library module for commonly used strings, hexadecimals and other elements.
 
 use crate::jsonbuilder::HEX;
-use std::ffi::CString;
-use std::os::raw::c_char;
 
 pub mod nom7 {
     use nom7::bytes::streaming::{tag, take_until};
@@ -99,32 +97,6 @@ pub mod nom8 {
         // use full path to disambiguate nom `bits` from this current function name
         nom8::bits::bits(parser)
     }
-}
-
-/// Convert a String to C-compatible string
-///
-/// This function will consume the provided data and use the underlying bytes to construct a new
-/// string, ensuring that there is a trailing 0 byte. This trailing 0 byte will be appended by this
-/// function; the provided data should *not* contain any 0 bytes in it.
-///
-/// Returns a valid pointer, or NULL
-pub fn rust_string_to_c(s: String) -> *mut c_char {
-    CString::new(s)
-        .map(|c_str| c_str.into_raw())
-        .unwrap_or(std::ptr::null_mut())
-}
-
-/// Free a CString allocated by Rust (for ex. using `rust_string_to_c`)
-///
-/// # Safety
-///
-/// s must be allocated by rust, using `CString::new`
-#[no_mangle]
-pub unsafe extern "C" fn SCRustCStringFree(s: *mut c_char) {
-    if s.is_null() {
-        return;
-    }
-    drop(CString::from_raw(s));
 }
 
 /// Convert an u8-array of data into a hexadecimal representation
