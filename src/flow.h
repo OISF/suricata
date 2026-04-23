1fce52965fdd6f5887630bc0b1c7f7f2b44a2359
Adam Kiripolsky <adam.kiripolsky@cesnet.cz>
2026-04-23 12:39:22 +0200

    flow: add LiveDev reference to FlowKey

--- src/flow.h
+++ src/flow.h
@@ -303,6 +303,7 @@ typedef struct FlowKey_
     uint8_t proto;
     uint8_t recursion_level;
     uint16_t livedev_id;
+    struct LiveDevice_ *livedev;
     uint16_t vlan_id[VLAN_MAX_LAYERS];
 } FlowKey;
 
