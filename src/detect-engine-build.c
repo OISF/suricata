OISF/suricata

commit 232276ac19e66ba9ea126c8f79f65ad29fab9360

detect: ethernet/arp matching
Support `alert ether` for matching all ethernet packets.

Add `alert arp` for matching ARP packets.

Ticket: #8313.

src/detect-engine-build.c
@@ -1468,6 +1468,31 @@ static inline int CreatePortList(DetectEngineCtx *de_ctx, const uint8_t *unique_
     return 0;
 }
 
+static bool SigIsEthernet(const Signature *s)
+{
+    return ((s->init_data->proto.flags & DETECT_PROTO_ETHERNET));
+}
+
+static bool SigIsEthernetAddToIP(const Signature *s)
+{
+    /* ARP and IP are mutually exclusive, so don't add an ARP rule
+     * to IP groups. */
+    if (s->init_data->proto.flags & DETECT_PROTO_ARP) {
+        SCLogDebug("rule %u: ARP is not for IP", s->id);
+        return false;
+    }
+
+    /* all other Ethernet may be IP as well, so add to these groups. */
+    return true;
+}
+
+static bool SigIsEthernetAddToNonIP(const Signature *s)
+{
+    /* add all Ethernet sigs to the Non IP group as there isn't enough
+     * info to know for sure if they are looking for IP traffic or not. */
+    return (SigIsEthernet(s));
+}
+
 static DetectPort *RulesGroupByPorts(DetectEngineCtx *de_ctx, uint8_t ipproto, uint32_t direction)
 {
     /* step 1: create a hash of 'DetectPort' objects based on all the
@@ -1487,28 +1512,39 @@ static DetectPort *RulesGroupByPorts(DetectEngineCtx *de_ctx, uint8_t ipproto, u
         /* IP Only rules are handled separately */
         if (s->type == SIG_TYPE_IPONLY)
             goto next;
-        /* Protocol does not match the Signature protocol and is neither IP or pkthdr */
-        if (!DetectProtoContainsProto(&s->init_data->proto, ipproto))
-            goto next;
-        /* Direction does not match Signature direction */
-        if (direction == SIG_FLAG_TOSERVER) {
-            if (!(s->flags & SIG_FLAG_TOSERVER))
-                goto next;
-        } else if (direction == SIG_FLAG_TOCLIENT) {
-            if (!(s->flags & SIG_FLAG_TOCLIENT))
-                goto next;
-        }
 
-        /* see if we want to exclude directionless sigs that really care only for
-         * to_server syn scans/floods */
-        if ((direction == SIG_FLAG_TOCLIENT) && DetectFlagsSignatureNeedsSynOnlyPackets(s) &&
-                ((s->flags & (SIG_FLAG_TOSERVER | SIG_FLAG_TOCLIENT)) ==
-                        (SIG_FLAG_TOSERVER | SIG_FLAG_TOCLIENT)) &&
-                (!(s->dp->port == 0 && s->dp->port2 == 65535))) {
-            SCLogWarning("rule %u: SYN-only to port(s) %u:%u "
-                         "w/o direction specified, disabling for toclient direction",
-                    s->id, s->dp->port, s->dp->port2);
-            goto next;
+        if (SigIsEthernet(s)) {
+            if (!SigIsEthernetAddToIP(s)) {
+                SCLogDebug("rule %u: not for IP", s->id);
+                goto next;
+            }
+            SCLogDebug("rule %u: add ethernet rule to IP group", s->id);
+        } else {
+            /* Protocol does not match the Signature protocol and is non of IP, pkthdr */
+            if (!DetectProtoContainsProto(&s->init_data->proto, ipproto)) {
+                SCLogDebug("skip");
+                goto next;
+            }
+            /* Direction does not match Signature direction */
+            if (direction == SIG_FLAG_TOSERVER) {
+                if (!(s->flags & SIG_FLAG_TOSERVER))
+                    goto next;
+            } else if (direction == SIG_FLAG_TOCLIENT) {
+                if (!(s->flags & SIG_FLAG_TOCLIENT))
+                    goto next;
+            }
+
+            /* see if we want to exclude directionless sigs that really care only for
+             * to_server syn scans/floods */
+            if ((direction == SIG_FLAG_TOCLIENT) && DetectFlagsSignatureNeedsSynOnlyPackets(s) &&
+                    ((s->flags & (SIG_FLAG_TOSERVER | SIG_FLAG_TOCLIENT)) ==
+                            (SIG_FLAG_TOSERVER | SIG_FLAG_TOCLIENT)) &&
+                    (!(s->dp->port == 0 && s->dp->port2 == 65535))) {
+                SCLogWarning("rule %u: SYN-only to port(s) %u:%u "
+                             "w/o direction specified, disabling for toclient direction",
+                        s->id, s->dp->port, s->dp->port2);
+                goto next;
+            }
         }
 
         DetectPort *p = NULL;
@@ -1652,8 +1688,11 @@ void SignatureSetType(DetectEngineCtx *de_ctx, Signature *s)
         SCReturn;
     }
 
-    /* see if the sig is dp only */
-    if (SignatureIsPDOnly(de_ctx, s) == 1) {
+    if (s->init_data->proto.flags & DETECT_PROTO_ETHERNET) {
+        s->type = SIG_TYPE_PKT;
+
+        /* see if the sig is dp only */
+    } else if (SignatureIsPDOnly(de_ctx, s) == 1) {
         s->type = SIG_TYPE_PDONLY;
 
         /* see if the sig is ip only */
@@ -1856,6 +1895,12 @@ static void DetectEngineAddDecoderEventSig(DetectEngineCtx *de_ctx, Signature *s
     SigGroupHeadAppendSig(de_ctx, &de_ctx->decoder_event_sgh, s);
 }
 
+static void DetectEngineAddEthernetSig(DetectEngineCtx *de_ctx, Signature *s)
+{
+    SCLogNotice("adding signature %" PRIu32 " to the eth non ip sgh", s->id);
+    SigGroupHeadAppendSig(de_ctx, &de_ctx->eth_non_ip_sgh, s);
+}
+
 static void DetectEngineAddSigToPreStreamHook(DetectEngineCtx *de_ctx, Signature *s)
 {
     SCLogDebug("adding signature %" PRIu32 " to the pre_stream hook sgh", s->id);
@@ -1915,6 +1960,13 @@ int SigPrepareStage2(DetectEngineCtx *de_ctx)
                    s->init_data->hook.t.pkt.ph == SIGNATURE_HOOK_PKT_PRE_FLOW) {
             DetectEngineAddSigToPreFlowHook(de_ctx, s);
         }
+
+        /* add ethernet sigs and decoder events to the ethernet sgh */
+        if ((s->type == SIG_TYPE_PKT && SigIsEthernetAddToNonIP(s)) || s->type == SIG_TYPE_DEONLY) {
+            // ethernet
+            SCLogNotice("rule: %u: add to non-IP", s->id);
+            DetectEngineAddEthernetSig(de_ctx, s);
+        }
     }
 
     IPOnlyPrepare(de_ctx);
@@ -1962,6 +2014,16 @@ static void DetectEngineBuildPreFlowHookSghs(DetectEngineCtx *de_ctx)
     }
 }
 
+static void DetectEngineBuildEthernetNonIPSgh(DetectEngineCtx *de_ctx)
+{
+    if (de_ctx->eth_non_ip_sgh != NULL) {
+        const uint32_t max_idx = DetectEngineGetMaxSigId(de_ctx);
+        SigGroupHeadSetSigCnt(de_ctx->eth_non_ip_sgh, max_idx);
+        SigGroupHeadBuildMatchArray(de_ctx, de_ctx->eth_non_ip_sgh, max_idx);
+        PrefilterSetupRuleGroup(de_ctx, de_ctx->eth_non_ip_sgh);
+    }
+}
+
 int SigPrepareStage3(DetectEngineCtx *de_ctx)
 {
     /* prepare the decoder event sgh */
@@ -1973,6 +2035,9 @@ int SigPrepareStage3(DetectEngineCtx *de_ctx)
     /* pre_stream hook sghs */
     DetectEngineBuildPreStreamHookSghs(de_ctx);
 
+    /* Ethernet Non IP */
+    DetectEngineBuildEthernetNonIPSgh(de_ctx);
+
     return 0;
 }
 
@@ -1982,6 +2047,8 @@ int SigAddressCleanupStage1(DetectEngineCtx *de_ctx)
 
     SCLogDebug("cleaning up signature grouping structure...");
 
+    if (de_ctx->eth_non_ip_sgh)
+        SigGroupHeadFree(de_ctx, de_ctx->eth_non_ip_sgh);
     if (de_ctx->decoder_event_sgh)
         SigGroupHeadFree(de_ctx, de_ctx->decoder_event_sgh);
     de_ctx->decoder_event_sgh = NULL;

src/detect-engine-proto.c
@@ -50,21 +50,25 @@ struct {
     uint8_t proto2;
     uint8_t flags;
 } proto_table[] = {
-    { "tcp", IPPROTO_TCP, 0, 0 },
-    { "tcp-pkt", IPPROTO_TCP, 0, DETECT_PROTO_ONLY_PKT },
-    { "tcp-stream", IPPROTO_TCP, 0, DETECT_PROTO_ONLY_STREAM },
-    { "udp", IPPROTO_UDP, 0, 0 },
-    { "icmpv4", IPPROTO_ICMP, 0, 0 },
-    { "icmpv6", IPPROTO_ICMPV6, 0, 0 },
-    { "icmp", IPPROTO_ICMP, IPPROTO_ICMPV6, 0 },
-    { "igmp", IPPROTO_IGMP, 0, 0 },
-    { "sctp", IPPROTO_SCTP, 0, 0 },
-    { "ipv4", 0, 0, DETECT_PROTO_IPV4 | DETECT_PROTO_ANY },
-    { "ip4", 0, 0, DETECT_PROTO_IPV4 | DETECT_PROTO_ANY },
-    { "ipv6", 0, 0, DETECT_PROTO_IPV6 | DETECT_PROTO_ANY },
-    { "ip6", 0, 0, DETECT_PROTO_IPV6 | DETECT_PROTO_ANY },
-    { "ip", 0, 0, DETECT_PROTO_ANY },
-    { "pkthdr", 0, 0, DETECT_PROTO_ANY },
+    // clang-format off
+    { "tcp", IPPROTO_TCP, 0, 0, },
+    { "tcp-pkt", IPPROTO_TCP, 0, DETECT_PROTO_ONLY_PKT, },
+    { "tcp-stream", IPPROTO_TCP, 0, DETECT_PROTO_ONLY_STREAM, },
+    { "udp", IPPROTO_UDP, 0, 0, },
+    { "icmpv4", IPPROTO_ICMP, 0, 0, },
+    { "icmpv6", IPPROTO_ICMPV6, 0, 0, },
+    { "icmp", IPPROTO_ICMP, IPPROTO_ICMPV6, 0, },
+    { "igmp", IPPROTO_IGMP, 0, 0, },
+    { "sctp", IPPROTO_SCTP, 0, 0, },
+    { "ipv4", 0, 0, DETECT_PROTO_IPV4 | DETECT_PROTO_ANY, },
+    { "ip4", 0, 0, DETECT_PROTO_IPV4 | DETECT_PROTO_ANY, },
+    { "ipv6", 0, 0, DETECT_PROTO_IPV6 | DETECT_PROTO_ANY, },
+    { "ip6", 0, 0, DETECT_PROTO_IPV6 | DETECT_PROTO_ANY, },
+    { "ip", 0, 0, DETECT_PROTO_ANY, },
+    { "pkthdr", 0, 0, DETECT_PROTO_ANY, },
+    { "ether", 0, 0, DETECT_PROTO_ETHERNET, },
+    { "arp", 0, 0, DETECT_PROTO_ARP | DETECT_PROTO_ETHERNET, },
+    // clang-format on
 };
 
 void DetectEngineProtoList(void)
@@ -96,6 +100,7 @@ int DetectProtoParse(DetectProto *dp, const char *str)
             if (proto_table[i].flags & DETECT_PROTO_ANY)
                 memset(dp->proto, 0xff, sizeof(dp->proto));
             found = 0;
+            SCLogDebug("protocol %s: flags %02x", str, dp->flags);
             break;
         }
     }

src/detect-engine-proto.h
@@ -30,6 +30,8 @@
 #define DETECT_PROTO_ONLY_STREAM       BIT_U8(2) /**< Indicate that we only care about stream payloads. */
 #define DETECT_PROTO_IPV4              BIT_U8(3) /**< IPv4 only */
 #define DETECT_PROTO_IPV6              BIT_U8(4) /**< IPv6 only */
+#define DETECT_PROTO_ETHERNET          BIT_U8(5) /**< Like ANY, but for Ethernet */
+#define DETECT_PROTO_ARP               BIT_U8(6) /**< ARP packets over for Ethernet, can have VLAN(s) in between */
 // clang-format on
 
 typedef struct DetectProto_ {

src/detect-parse.c
@@ -2801,6 +2801,18 @@ static int SigValidateFileHandling(const Signature *s)
     SCReturnInt(1);
 }
 
+static bool SigValidateEthernet(const Signature *s)
+{
+    if (s->init_data->proto.flags & (DETECT_PROTO_ETHERNET | DETECT_PROTO_ARP)) {
+        if ((s->flags & (SIG_FLAG_SP_ANY | SIG_FLAG_DP_ANY)) !=
+                (SIG_FLAG_SP_ANY | SIG_FLAG_DP_ANY)) {
+            SCLogError("can't use ports with ether or arp rule");
+            return false;
+        }
+    }
+    return true;
+}
+
 /**
  *  \internal
  *  \brief validate and consolidate parsed signature
@@ -2820,6 +2832,10 @@ static int SigValidateConsolidate(
         SCReturnInt(0);
 
     if (SigValidatePacketStream(s) == 0) {
+        SCReturnInt(0);
+    }
+
+    if (!SigValidateEthernet(s)) {
         SCReturnInt(0);
     }
 

src/detect.c
@@ -285,6 +285,12 @@ const SigGroupHead *SigMatchSignaturesGetSgh(const DetectEngineCtx *de_ctx,
     SCEnter();
     SigGroupHead *sgh = NULL;
 
+    /* use ethernet non-IP sgh if we're ethernet but have no (valid) IP layer on top of it. */
+    if (PacketIsEthernet(p) && p->proto == 0 && de_ctx->eth_non_ip_sgh != NULL) {
+        SCLogDebug("using eth_non_ip_sgh %p", de_ctx->eth_non_ip_sgh);
+        SCReturnPtr(de_ctx->eth_non_ip_sgh, "SigGroupHead");
+    }
+
     /* if the packet proto is 0 (not set), we're inspecting it against
      * the decoder events sgh we have. */
     if (p->proto == 0 && p->events.cnt > 0) {
@@ -536,7 +542,16 @@ static inline bool DetectRunInspectRuleHeader(
         }
         if (DetectProtoContainsProto(s->proto, PacketGetIPProto(p)) == 0) {
             SCLogDebug("proto didn't match");
-            return false;
+            if (PacketIsEthernet(p) &&
+                    (s_proto_flags & (DETECT_PROTO_ETHERNET | DETECT_PROTO_ARP))) {
+                SCLogDebug("checking ether/arp protocol");
+                if ((s_proto_flags & DETECT_PROTO_ARP) && !PacketIsARP(p)) {
+                    return false;
+                }
+                SCLogDebug("checking eth protocol: match!");
+            } else {
+                return false;
+            }
         }
     }
 

src/detect.h
@@ -1021,6 +1021,9 @@ typedef struct DetectEngineCtx_ {
      *  we can't lookup by proto, address, port as we don't have these */
     struct SigGroupHead_ *decoder_event_sgh;
 
+    /** sgh for `alert ether` / `alert arp` etc. */
+    struct SigGroupHead_ *eth_non_ip_sgh;
+
     /* Maximum size of the buffer for decoded base64 data. */
     uint16_t base64_decode_max_len;
 

commit e96485d640bd58debe9c39bfbe8a29ee62be990c

detect: suppress noisy debug messages
Fixes: 232276ac19e6 ("detect: ethernet/arp matching")

src/detect-engine-build.c
@@ -1897,7 +1897,7 @@ static void DetectEngineAddDecoderEventSig(DetectEngineCtx *de_ctx, Signature *s
 
 static void DetectEngineAddEthernetSig(DetectEngineCtx *de_ctx, Signature *s)
 {
-    SCLogNotice("adding signature %" PRIu32 " to the eth non ip sgh", s->id);
+    SCLogDebug("adding signature %" PRIu32 " to the eth non ip sgh", s->id);
     SigGroupHeadAppendSig(de_ctx, &de_ctx->eth_non_ip_sgh, s);
 }
 
@@ -1965,7 +1965,7 @@ int SigPrepareStage2(DetectEngineCtx *de_ctx)
         if ((s->type == SIG_TYPE_PKT && SigIsEthernetAddToNonIP(s)) || s->type == SIG_TYPE_DEONLY ||
                 (s->init_data->proto.flags & DETECT_PROTO_L2_ANY)) {
             // ethernet
-            SCLogNotice("rule: %u: add to non-IP", s->id);
+            SCLogDebug("rule: %u: add to non-IP", s->id);
             DetectEngineAddEthernetSig(de_ctx, s);
         }
     }