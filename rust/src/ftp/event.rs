880b5a2daa9d16a6c099c7ab83c0af8a9f76c608
Philippe Antoine <pantoine@oisf.net>
2026-04-27 17:26:18 +0200

    rust: format ftp files
    Ticket: 3836

--- rust/src/ftp/event.rs
+++ rust/src/ftp/event.rs
@@ -48,5 +48,6 @@ pub unsafe extern "C" fn ftp_get_event_info(
 pub unsafe extern "C" fn ftp_get_event_info_by_id(
     event_id: u8, event_name: *mut *const c_char, event_type: *mut AppLayerEventType,
 ) -> c_int {
-    suricata_ffi::applayer::get_event_info_by_id::<FtpEvent>(event_id, event_name, event_type) as c_int
+    suricata_ffi::applayer::get_event_info_by_id::<FtpEvent>(event_id, event_name, event_type)
+        as c_int
 }
