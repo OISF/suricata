63ef404073a62acc11411f86fd89f45ddd2230f9
Victor Julien <vjulien@oisf.net>
2026-05-02 12:19:37 +0200

    util/logopenfile: fix compile warnings
    util-logopenfile.c:837:11: error: initializing 'char *' with an expression of type 'const char *' discards qualifiers [-Werror,-Wincompatible-pointer-types-discards-qualifiers]
      837 |     char *dot = strrchr(base, '.');
          |           ^     ~~~~~~~~~~~~~~~~~~
    util-logopenfile.c:848:13: error: assigning to 'char *' from 'const char *' discards qualifiers [-Werror,-Wincompatible-pointer-types-discards-qualifiers]
      848 |         dot = strrchr(original_name, '.');
          |             ^ ~~~~~~~~~~~~~~~~~~~~~~~~~~~
    2 errors generated.
    
    (cherry picked from commit cd1c5967a5014d5a9060467acddabd7bfcfd0c13)

--- src/util-logopenfile.c
+++ src/util-logopenfile.c
@@ -818,7 +818,7 @@ static bool LogFileThreadedName(
     }
 
     /* Check if basename has an extension */
-    char *dot = strrchr(base, '.');
+    const char *dot = strrchr(base, '.');
     if (dot) {
         char *tname = SCStrdup(original_name);
         if (!tname) {
