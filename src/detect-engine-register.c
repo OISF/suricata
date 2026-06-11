56d5242d853e246bde88d1470e232fe8c3e321ce
Philippe Antoine <pantoine@oisf.net>
2026-06-11 10:14:16 +0200

    detect/krb5: move krb5_err_code ro rust
    Ticket: 8648
    
    Make it a generic integer on the way

--- src/detect-engine-register.c
+++ src/detect-engine-register.c
@@ -192,7 +192,6 @@
 #include "detect-ipv4hdr.h"
 #include "detect-ipv6hdr.h"
 #include "detect-krb5-cname.h"
-#include "detect-krb5-errcode.h"
 #include "detect-krb5-sname.h"
 #include "detect-krb5-ticket-encryption.h"
 #include "detect-sip-method.h"
@@ -707,8 +706,6 @@ void SigTableSetup(void)
     DetectIpv4hdrRegister();
     DetectIpv6hdrRegister();
     DetectKrb5CNameRegister();
-    DetectKrb5ErrCodeRegister();
-    SCDetectKrb5MsgTypeRegister();
     DetectKrb5SNameRegister();
     DetectKrb5TicketEncryptionRegister();
     DetectSipMethodRegister();
@@ -769,6 +766,7 @@ void SigTableSetup(void)
     SCDetectSmbRegister();
     SCDetectIkeRegister();
     SCDetectDcerpcRegister();
+    SCDetectKrb5Register();
 
     for (size_t i = 0; i < preregistered_callbacks_nb; i++) {
         PreregisteredCallbacks[i]();
