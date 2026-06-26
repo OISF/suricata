c42e56863c7b9b9d266ad5e098f5f1a587f3964f
Philippe Antoine <pantoine@oisf.net>
2026-06-26 16:19:00 +0200

    fixup! fixup! websocket: implement over HTTP2

--- rust/src/http2/http2.rs
+++ rust/src/http2/http2.rs
@@ -1346,12 +1346,11 @@ impl HTTP2State {
                             )
                         };
                         let (il, ol) = (il + comp_len, ol + decomp_len);
-                        if ol > decompression::DEFAULT_BOMB_RATIO * il {
-                            if ol > unsafe { HTTP2_COMPRESSION_BOMB_LIMIT } {
+                        if ol > decompression::DEFAULT_BOMB_RATIO * il
+                            && ol > unsafe { HTTP2_COMPRESSION_BOMB_LIMIT } {
                                 tx.set_event(HTTP2Event::CompressionBomb);
                                 return AppLayerResult::err();
                             }
-                        }
                         if let Some(wss) = &mut tx.is_websocket {
                             let websocket_txs = std::mem::take(&mut wss.transactions);
                             for t in websocket_txs {
@@ -1361,12 +1360,11 @@ impl HTTP2State {
                                 self.transactions.push_back(ht);
                             }
                         }
-                        if ol > decompression::DEFAULT_BOMB_RATIO * il {
-                            if over {
+                        if ol > decompression::DEFAULT_BOMB_RATIO * il
+                            && over {
                                 self.comp_len += il;
                                 self.decomp_len += ol;
                             }
-                        }
                     }
                     sc_app_layer_parser_trigger_raw_stream_inspection(flow, dir as i32);
                     input = &rem[hlsafe..];
